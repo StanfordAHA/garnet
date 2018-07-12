db7735bb4b929d8dfd60c9a798171ae7096aa768
Raj Setaluri <raj.setaluri@gmail.com>
2018-07-12 11:48:33 -0700

    Add config register module and test

---
+++ config_register_test.py
@@ -0,0 +1,33 @@
+import os
+import magma as m
+from magma.bit_vector import BitVector
+from magma.simulator.python_simulator import PythonSimulator
+from config_register import DefineConfigRegister
+
+
+if __name__ == "__main__":
+    WIDTH = 32
+    ADDR_WIDTH = 8
+    ADDR_VALUE = 4
+
+    # Check that compilation to CoreIR works. Delete JSON file afterwards.
+    cr = DefineConfigRegister(WIDTH, m.bits(ADDR_VALUE, ADDR_WIDTH), True)
+    m.compile("config_register", cr, output='coreir')
+    os.system("\\rm config_register.json")
+
+    # Check the module against a simple simulation.
+    simulator = PythonSimulator(cr, clock=cr.CLK)
+    reg_value = lambda : simulator.get_value(cr.O)
+    def step(I, addr):
+        simulator.set_value(cr.I, I)
+        simulator.set_value(cr.addr, addr)
+        simulator.advance(2)
+        return reg_value()
+    assert reg_value() == BitVector(0, WIDTH)
+    sequence = [(0, 0, 0),
+                (12, 4, 0),
+                (0, 0, 12),
+                (0, 0, 12)]
+    for (I, addr, out_expected) in sequence:
+        out = step(BitVector(I, WIDTH), BitVector(addr, ADDR_WIDTH))
+        assert out == BitVector(out_expected, WIDTH)
