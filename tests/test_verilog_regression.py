c220341cbda93fede7dea82878d5852e2bdcea1d
Lenny Truong <lenny@stanford.edu>
2018-07-13 09:17:54 -0700

    Debug travis env

--- tests/test_verilog_regression.py
+++ tests/test_verilog_regression.py
@@ -2,9 +2,10 @@ import os
 import pytest
 
 
-@pytest.mark.skipif(os.environ.get("TRAVIS") is "true",
+@pytest.mark.skipif(os.environ.get("TRAVIS") == "true",
                     reason="ncsim not available on travis")
 def test_cb_generation():
+    print(os.environ.get("TRAVIS"))
     res = os.system('python ./tests/gen_cb_16_10_1111101111_1_7.py')
     assert(res == 0)
 
