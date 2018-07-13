738ed2f0f696059ce401b8a96e567bf86e6c7f6e
Dillon Bailey Huff <dhuff@kiwi.Stanford.EDU>
2018-07-12 20:39:13 -0700

    Adding simulator tests to tests dir

--- tests/test_verilog_regression.py
+++ tests/test_verilog_regression.py
@@ -1,10 +1,10 @@
 import os
 
 def test_cb_generation():
-    res = os.system('python ./connect_box/gen_cb_16_10_1111101111_1_7.py')
+    res = os.system('python ./tests/gen_cb_16_10_1111101111_1_7.py')
     assert(res == 0)
 
     print( 'DONE with 16, 10 cb' )
 
-    res = os.system('python ./connect_box/gen_cb_7_8_11111111_0_0.py')
+    res = os.system('python ./tests/gen_cb_7_8_11111111_0_0.py')
     assert(res == 0)
