4f1649bdd419bdfc8fa2a916eb62a3ae7955fb62
Lenny Truong <lenny@stanford.edu>
2018-07-13 15:39:33 -0700

    Add peek/poke/expect interface for testing

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -2,6 +2,7 @@ import random
 import shutil
 import os
 import glob
+import functools
 from bit_vector import BitVector
 
 from connect_box.cb import define_cb
@@ -24,6 +25,57 @@ def random_bv(width):
     return BitVector(random.randint(0, (1 << width) - 1), width)
 
 
+def convert_value(fn):
+    @functools.wraps(fn)
+    def wrapped(self, port, value):
+        if isinstance(port, m.ArrayType) and isinstance(value, int):
+            value = BitVector(value, len(port))
+        elif isinstance(port, m._BitType) and isinstance(value, int):
+            value = BitVector(value, 1)
+        return fn(self, port, value)
+    return wrapped
+
+
+class Tester:
+    def __init__(self, circuit, clock=None):
+        self.circuit = circuit
+        self.test_vectors = []
+        self.port_index_mapping = {}
+        self.ports = self.circuit.interface.ports
+        self.clock_index = None
+        for i, (key, value) in enumerate(self.ports.items()):
+            self.port_index_mapping[value] = i
+            if value is clock:
+                self.clock_index = i
+        # Initialize first test vector to all Nones
+        self.test_vectors.append(
+            [BitVector(None, 1 if isinstance(port, m.BitType) else len(port))
+             for port in self.ports.values()])
+
+    def get_index(self, port):
+        return self.port_index_mapping[port]
+
+    @convert_value
+    def poke(self, port, value):
+        if port.isinput():
+            raise ValueError(f"Can only poke an input: {port} {type(port)}")
+        self.test_vectors[-1][self.get_index(port)] = value
+
+    @convert_value
+    def expect(self, port, value):
+        if port.isoutput():
+            raise ValueError(f"Can only expect an output: {port} {type(port)}")
+        self.test_vectors[-1][self.get_index(port)] = value
+
+    def eval(self):
+        self.test_vectors.append(self.test_vectors[-1][:])
+
+    def step(self):
+        self.eval()
+        self.test_vectors[-1][self.clock_index] ^= BitVector(1, 1)
+        self.eval()
+
+
 @pytest.mark.parametrize('default_value,has_constant',
                          # Test 10 random default values with has_constant
                          [(random_bv(16), 1) for _ in range(2)] +
@@ -33,10 +85,11 @@ def random_bv(width):
 # @pytest.mark.parametrize('num_tracks', range(2,10))
 @pytest.mark.parametrize('num_tracks', [10])
 def test_regression(default_value, num_tracks, has_constant):
+    feedthrough_outputs = "1111101111"
     params = {
         "width": 16,
         "num_tracks": num_tracks,
-        "feedthrough_outputs": "1111101111",
+        "feedthrough_outputs": feedthrough_outputs,
         "has_constant": has_constant,
         "default_value": default_value.as_int()
     }
@@ -73,45 +126,51 @@ def test_regression(default_value, num_tracks, has_constant):
 
     testvectors = []
 
-    GND = BitVector(0, 1)
-    VCC = BitVector(1, 1)
-
     # TODO: Do we need this extra instantiation, could the function do it for
     # us?
     cb_functional_model = gen_cb(**params)()
 
-    # Generate the configuration sequence
-    # Config logic
-    ins = [GND for _ in range(len(inputs))]
-    reset = GND
-    config_addr = BitVector(0, 32)
-    for config_data in [BitVector(x, 32) for x in range(0, len(inputs))]:
-        config_en = VCC
-        out = cb_functional_model(*ins)
-        read_data = cb_functional_model.config[0]
-        clk = GND
-        vector = [clk, reset] + ins + [out, config_addr, config_data,
-                                       config_en, read_data]
-        testvectors.append(vector)
-        clk = VCC
-        # Step the clock
-        cb_functional_model.configure(config_addr, config_data)
-        read_data = cb_functional_model.config[0]
-        out = cb_functional_model(*ins)
-        vector = [clk, reset] + ins + [out, config_addr, config_data,
-                                       config_en, read_data]
-        testvectors.append(vector)
+    tester = Tester(genesis_cb, clock=genesis_cb.clk)
 
-        ins = [random_bv(data_width) for _ in range(len(inputs))]
-        reset = GND
-        clk = GND
-        config_en = GND
-        out = cb_functional_model(*ins)
-        print(out, ins, config_data)
-        assert len(cb_functional_model.config) == 1
-        vector = [clk, reset] + ins + [out, config_addr, config_data,
-                                       config_en, read_data]
-        testvectors.append(vector)
+    config_addr = BitVector(0, 32)
+
+    # init inputs to 0
+    for i in range(0, num_tracks):
+        if feedthrough_outputs[i] == "1":
+            tester.poke(getattr(genesis_cb, f"in_{i}"), 0)
+
+    for config_data in [BitVector(x, 32) for x in range(0, 1)]:
+        tester.poke(genesis_cb.clk, 0)
+        tester.poke(genesis_cb.reset, 0)
+        tester.poke(genesis_cb.config_addr, 0)
+        tester.poke(genesis_cb.config_data, 0)
+        tester.poke(genesis_cb.config_en, 1)
+        # TODO: Technically we don't care about the output at this point,
+        # ideally we could just not expect anything
+        tester.expect(genesis_cb.out, 0)
+        tester.expect(genesis_cb.read_data, cb_functional_model.config[0])
+
+        tester.step()
+
+        # posedge of clock, so cb should now be configured
+        cb_functional_model.configure(config_addr, config_data)
+
+        tester.expect(genesis_cb.read_data, cb_functional_model.config[0])
+        tester.expect(genesis_cb.read_data, cb_functional_model.config[0])
+        tester.expect(genesis_cb.out, 0)  # 0 because inputs are 0
+
+        tester.step()
+
+        inputs = [random_bv(data_width) for _ in range(num_tracks)]
+        _inputs = []
+        for i in range(0, num_tracks):
+            if feedthrough_outputs[i] == "1":
+                _inputs.append(inputs[i])
+                tester.poke(getattr(genesis_cb, f"in_{i}"), inputs[i])
+        tester.expect(genesis_cb.out, cb_functional_model(*_inputs))
+        tester.eval()
+    testvectors = tester.test_vectors
+    print(testvectors)
 
     for cb in [genesis_cb, magma_cb]:
         compile(f"build/test_{cb.name}.cpp", cb, testvectors)
