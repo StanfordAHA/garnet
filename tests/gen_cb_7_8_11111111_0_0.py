c75e324c0937cd023ca329288030b6c9d2e45da6
Dillon Bailey Huff <dhuff@kiwi.Stanford.EDU>
2018-07-12 20:39:13 -0700

    Adding simulator tests to tests dir

--- tests/gen_cb_7_8_11111111_0_0.py
+++ tests/gen_cb_7_8_11111111_0_0.py
@@ -1,14 +1,14 @@
-from build_cb_top import define_connect_box, generate_genesis_cb
+from connect_box.build_cb_top import define_connect_box, generate_genesis_cb
 
 import os
 import mantle as mantle
 import magma as m
 
-param_width = 16
-param_num_tracks = 10
-param_feedthrough_outputs = "1111101111"
-param_has_constant = 1
-param_default_value = 7
+param_width = 7
+param_num_tracks = 8
+param_feedthrough_outputs = "11111111"
+param_has_constant = 0
+param_default_value = 0
 
 generate_genesis_cb(param_width, param_num_tracks, param_feedthrough_outputs, param_has_constant, param_default_value)
 
@@ -16,6 +16,4 @@ cb = define_connect_box(param_width, param_num_tracks, param_has_constant, param
 m.compile(cb.name, cb, output='coreir')
 os.system('coreir -i ' + cb.name + '.json -o ' + cb.name + '.v')
 
-os.system('./run_sim.sh')
-
-
+os.system('./run_sim_7_8_11111111_0_0.sh')
