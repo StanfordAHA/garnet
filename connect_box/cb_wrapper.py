7c6f62fdc16abcf170b5865554558641ddfe3991
Raj Setaluri <raj.setaluri@gmail.com>
2018-07-12 23:56:12 -0700

    Renaming some things
    -- Rename build_cb_top.py to cb.py
    -- Rename cb_wrapper.define_cb to cb_wrapper.define_cb_wrapper
    -- Rename cb.define_connect_box to cb.define_cb

--- connect_box/cb_wrapper.py
+++ connect_box/cb_wrapper.py
@@ -3,12 +3,12 @@ import magma as m
 from connect_box.genesis_wrapper import run_genesis
 
 
-def define_cb(width,
-              num_tracks,
-              feedthrough_outputs,
-              has_constant,
-              default_value,
-              filename="cb.vp"):
+def define_cb_wrapper(width,
+                      num_tracks,
+                      feedthrough_outputs,
+                      has_constant,
+                      default_value,
+                      filename="cb.vp"):
     parameters = {
         "width": width,
         "num_tracks": num_tracks,
