4c6bc44dac9d2198defd9aefa9ee4d7caf53b75a
Lenny Truong <lenny@stanford.edu>
2018-07-12 23:32:30 -0700

    Add test for main wrapper

--- connect_box/cb_wrapper_main.py
+++ connect_box/cb_wrapper_main.py
@@ -1,12 +1,12 @@
-import argparse
-from cb_wrapper import define_cb
-
-
 """
 This program generates the verilog for the connect box and parses it into a
 Magma circuit. The circuit declaration is printed at the end of the program.
 """
-if __name__ == "__main__":
+import argparse
+from connect_box.cb_wrapper import define_cb
+
+
+def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("infile", type=str)
     parser.add_argument("--width", type=int, default=32)
@@ -14,7 +14,10 @@ if __name__ == "__main__":
     parser.add_argument("--feedthrough_outputs", type=str, default=("1"*10))
     parser.add_argument("--has_constant", type=int, default=0)
     parser.add_argument("--default_value", type=int, default=0)
-    args = parser.parse_args()
+    return parser
+
+
+def main(args):
 
     # Check some of the inputs.
     for c in args.feedthrough_outputs:
@@ -27,3 +30,7 @@ if __name__ == "__main__":
                    args.default_value,
                    args.infile)
     print(cb)
+
+if __name__ == "__main__":
+    parser = create_parser()
+    main(parser.parse_args())
---
+++ tests/test_cb_wrapper_main.py
@@ -0,0 +1,21 @@
+from connect_box import cb_wrapper_main
+from util import make_relative
+
+
+def test_cb_wrapper(capsys):
+    parser = cb_wrapper_main.create_parser()
+    args = parser.parse_args([
+        "--width", "16",
+        "--num_tracks", "10",
+        "--feedthrough_outputs", "1111101111",
+        "--has_constant", "1",
+        "--default_value", "7",
+        make_relative("cb.vp")
+    ])
+
+    cb_wrapper_main.main(args)
+    out, _ = capsys.readouterr()
+    assert out == """\
+Running cmd 'Genesis2.pl -parse -generate -top cb -input /Users/lenny/google_drive/repos/magma_connect_box/tests/cb.vp -parameter cb.width='16' -parameter cb.num_tracks='10' -parameter cb.feedthrough_outputs='1111101111' -parameter cb.has_constant='1' -parameter cb.default_value='7''
+cb(clk: In(Bit), reset: In(Bit), in_0: Array(16,In(Bit)), in_1: Array(16,In(Bit)), in_2: Array(16,In(Bit)), in_3: Array(16,In(Bit)), in_4: Array(16,In(Bit)), in_6: Array(16,In(Bit)), in_7: Array(16,In(Bit)), in_8: Array(16,In(Bit)), in_9: Array(16,In(Bit)), out: Array(16,Out(Bit)), config_addr: Array(32,In(Bit)), config_data: Array(32,In(Bit)), config_en: In(Bit), read_data: Array(32,Out(Bit)))
+"""  # nopep8
