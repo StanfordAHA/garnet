3172ce5f06d56c0c7fbdf0cb76d24d891dab3343
Lenny Truong <lenny@stanford.edu>
2018-07-14 15:00:42 -0700

    Add some comments

--- mem/mem_genesis2.py
+++ mem/mem_genesis2.py
@@ -11,6 +11,15 @@ def define_mem_genesis2(data_width=16, data_depth=1024,
                                "mem/genesis/fifo_control.vp",
                                "mem/genesis/mem.vp",
                                "mem/genesis/memory_core.vp"]):
+    """
+    Defines the memory using genesis2.
+
+    `data_width`: width of an entry in the memory
+    `data_depth`: number of entries in the memory
+    `files`: a list of genesis files used to generate the memory
+    """
+
+    # Rename parameters to match the genesis files
     parameters = {
         "dwidth": data_width,
         "ddepth": data_depth
@@ -22,6 +31,9 @@ def define_mem_genesis2(data_width=16, data_depth=1024,
 
 
 def create_parser():
+    """
+    A parser for using this wrapper from the command line
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("in_file_or_files",
                         help="file or list of files for memory",
@@ -32,17 +44,19 @@ def create_parser():
 
 
 def main(args):
+    """
+    Main function when run from the command line.
+
+    This program generates the verilog for the memory core and parses it into a
+    Magma circuit. The circuit declaration is printed at the end of the
+    program.
+    """
     memory_core = define_mem_genesis2(args.data_width, args.data_depth,
                                       args.in_file_or_files)
     print(memory_core)
 
 
 if __name__ == "__main__":
-    """
-    This program generates the verilog for the memory core and parses it into a
-    Magma circuit. The circuit declaration is printed at the end of the
-    program.
-    """
     # These functions are unit tested directly, so no need to cover them
     parser = create_parser()  # pragma: no cover
     main(parser.parse_args())  # pragma: no cover
