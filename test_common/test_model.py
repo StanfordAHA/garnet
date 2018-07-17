c07b5244269faa8cc9268f49a6ae266808278937
Raj Setaluri <raj.setaluri@gmail.com>
2018-07-16 17:00:38 -0700

    Fix formatting issues

--- test_common/test_model.py
+++ test_common/test_model.py
@@ -2,12 +2,12 @@ from common.model import Model
 
 
 def test_model():
-    EXPECTED_MSG = "TypeError(\"Can't instantiate abstract class Model with abstract methods __call__, __init__\",)" # nopep8
+    EXPECTED_MSG = "TypeError(\"Can't instantiate abstract class Model with abstract methods __call__, __init__\",)"  # nopep8
     has_type_error = False
     try:
         my_model = Model()
     except TypeError as e:
-        msg = e.__repr__()        
+        msg = e.__repr__()
         has_type_error = True
     assert has_type_error
     assert msg == EXPECTED_MSG
