0b09061a507928f810e110ee27ee317cc5193305
Lenny Truong <lenny@stanford.edu>
2018-07-23 17:20:41 -0700

    Use magma's coreir-verilog target

--- common/util.py
+++ common/util.py
@@ -1,27 +1,5 @@
-import os
-import magma as m
-import tempfile
 import pytest
 import shutil
-
-
-def compile_to_verilog(module, name, outpath, use_coreir=True):
-    """
-    Note(rsetaluri): Be wary calling this function! Multiple invocations of
-    this function in the same runtime may not work due to a caching bug in
-    magma.
-    """
-    verilog_file = f"{outpath}/{name}.v"
-    if use_coreir:
-        with tempfile.TemporaryDirectory() as temp_dir:
-            m.compile(os.path.join(temp_dir, name), module, output="coreir")
-            json_file = os.path.join(temp_dir, f"{name}.json")
-            res = os.system(f"coreir -i {json_file} -o {verilog_file}")
-            return res == 0
-    print("Warning: compiling magma straight to verilog will not import "
-          "CoreIR modules")
-    m.compile(f"{verilog_file}", module, output="verilog")
-    return True
 
 
 def skip_unless_irun_available(fn):
