c138dcb0c352383d05b7dbf7fd475259289a3d2c
Lenny Truong <lenny@stanford.edu>
2018-07-25 08:39:30 -0700

    Address review comments

--- common/util.py
+++ common/util.py
@@ -1,5 +1,13 @@
 import shutil
 
 
-irun_available = shutil.which("irun") is not None
-iverilog_available = shutil.which("iverilog") is not None
+def irun_available():
+    return shutil.which("irun") is not None
+
+
+def iverilog_available():
+    return shutil.which("iverilog") is not None
+
+
+def verilog_sim_available():
+    return irun_available() or iverilog_available()
