4c23b9743571f43c0e1f3bf9f63b0c4c9e97af0a
Raj Setaluri <raj.setaluri@gmail.com>
2018-07-26 14:40:15 -0700

    [Issue #53] Update mem for refactored wrappers

--- mem/mem_genesis2.py
+++ mem/mem_genesis2.py
@@ -1,6 +1,5 @@
-from common.genesis_wrapper import define_genesis_generator
-
-import argparse
+from common.genesis_wrapper import GenesisWrapper
+from common.generator_interface import GeneratorInterface
 
 
 """
@@ -8,50 +7,30 @@ Defines the memory using genesis2.
 
 `data_width`: width of an entry in the memory
 `data_depth`: number of entries in the memory
-`input_files`: a list of genesis files used to generate the memory
 
 Example usage:
-    memory_core = define_mem_genesis2(data_width=16, data_depth=1024)
+    memory_core = mem_wrapper.generator()(data_width=16, data_depth=1024)
 """
-define_mem_genesis2 = define_genesis_generator(
-    top_name="memory_core",
-    input_files=["mem/genesis/input_sr.vp", "mem/genesis/output_sr.vp",
-                 "mem/genesis/linebuffer_control.vp",
-                 "mem/genesis/fifo_control.vp", "mem/genesis/mem.vp",
-                 "mem/genesis/memory_core.vp"],
-    param_mapping={"data_width": "dwidth", "data_depth": "ddepth"},
-    data_width=16,
-    data_depth=1024)
+interface = GeneratorInterface()\
+            .register("data_width", int, 16)\
+            .register("data_depth", int, 1024)
 
+mem_wrapper = GenesisWrapper(interface,
+                             "memory_core",
+                             ["mem/genesis/input_sr.vp",
+                              "mem/genesis/output_sr.vp",
+                              "mem/genesis/linebuffer_control.vp",
+                              "mem/genesis/fifo_control.vp",
+                              "mem/genesis/mem.vp",
+                              "mem/genesis/memory_core.vp"])
 
-def create_parser():
+param_mapping={"data_width": "dwidth", "data_depth": "ddepth"}
+
+if __name__ == "__main__":
     """
-    A parser for using this wrapper from the command line
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input_files",
-                        help="file or list of files for memory",
-                        nargs="+")
-    parser.add_argument("--data-width", type=int, default=16)
-    parser.add_argument("--data-depth", type=int, default=1024)
-    return parser
-
-
-def main(args):
-    """
-    Main function when run from the command line.
-
     This program generates the verilog for the memory core and parses it into a
     Magma circuit. The circuit declaration is printed at the end of the
     program.
     """
-    memory_core = define_mem_genesis2(data_width=args.data_width,
-                                      data_depth=args.data_depth,
-                                      input_files=args.input_files)
-    print(memory_core)
-
-
-if __name__ == "__main__":
     # These functions are unit tested directly, so no need to cover them
-    parser = create_parser()  # pragma: no cover
-    main(parser.parse_args())  # pragma: no cover
+    mem_wrapper.main(param_mapping=param_mapping)  # pragma: no cover
