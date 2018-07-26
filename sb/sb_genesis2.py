3be5c0f5a0fc968a8c67cf8ec67bf1d138937071
Raj Setaluri <raj.setaluri@gmail.com>
2018-07-26 14:31:31 -0700

    [Issue #53] Update sb for refactored wrappers

--- sb/sb_genesis2.py
+++ sb/sb_genesis2.py
@@ -1,54 +1,19 @@
-import argparse
-from common.genesis_wrapper import define_genesis_generator
+from common.genesis_wrapper import GenesisWrapper
+from common.generator_interface import GeneratorInterface
 
 
-define_sb_wrapper = define_genesis_generator(
-    top_name="sb",
-    input_files=["sb/genesis/sb.vp"],
-    width=16,
-    num_tracks=2,
-    sides=4,
-    feedthrough_outputs="00",
-    registered_outputs="11",
-    pe_output_count=1,
-    is_bidi=0,
-    sb_fs="10#10#10"
-)
+interface = GeneratorInterface()\
+            .register("width", int, 16)\
+            .register("num_tracks", int, 2)\
+            .register("sides", int, 4)\
+            .register("feedthrough_outputs", str, "00")\
+            .register("registered_outputs", str, "11")\
+            .register("pe_output_count", int, 1)\
+            .register("is_bidi", int, 0)\
+            .register("sb_fs", str, "10#10#10")
 
-
-def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("infile", type=str)
-    parser.add_argument("--width", type=int, default=16)
-    parser.add_argument("--num_tracks", type=int, default=2)
-    parser.add_argument("--sides", type=int, default=4)
-    parser.add_argument("--feedthrough_outputs", type=str, default=("00"))
-    parser.add_argument("--registered_outputs", type=str, default=("11"))
-    parser.add_argument("--pe_output_count", type=int, default=1)
-    parser.add_argument("--is_bidi", type=int, default=0)
-    parser.add_argument("--sb_fs", type=str, default="10#10#10")
-    return parser
-
-
-def main(args):
-    sb = define_sb_wrapper(width=args.width,
-                           num_tracks=args.num_tracks,
-                           sides=args.sides,
-                           feedthrough_outputs=args.feedthrough_outputs,
-                           registered_outputs=args.registered_outputs,
-                           pe_output_count=args.pe_output_count,
-                           is_bidi=args.is_bidi,
-                           sb_fs=args.sb_fs,
-                           input_files=[args.infile])
-    print(sb)
-
+sb_wrapper = GenesisWrapper(interface, "sb", ["sb/genesis/sb.vp"])
 
 if __name__ == "__main__":
-    """
-    This program generates the verilog for the memory core and parses it into a
-    Magma circuit. The circuit declaration is printed at the end of the
-    program.
-    """
     # These functions are unit tested directly, so no need to cover them
-    parser = create_parser()  # pragma: no cover
-    main(parser.parse_args())  # pragma: no cover
+    sb_wrapper.main()  # pragma: no cover
