7e3bbd662228ad7075a6980b1ad833841693c3cf
Alex Carsello <ajcars@stanford.edu>
2018-07-27 16:59:08 -0700

    Fix syntax error in test.svp and correctly get all of the filenames for irun. Simulation runs now

--- test_global_controller/test_global_controller_verilog_sim.py
+++ test_global_controller/test_global_controller_verilog_sim.py
@@ -3,26 +3,29 @@ from common.genesis_wrapper import run_genesis
 from common.util import verilog_sim_available
 from common.run_verilog_sim import irun, iverilog, run_verilog_sim
 import pytest
+import glob
 
 
 def run_verilog_regression(params):
     # Genesis version of global_controller
     genesis_outfile = run_genesis("top",
-                                  ["test_global_controller/genesis/top.svp",
-                                  "test_global_controller/genesis/test.svp",
-                                  "test_global_controller/genesis/JTAGDriver.svp",
-                                  "test_global_controller/genesis/clocker.svp",
-                                  "test_global_controller/genesis/template_ifc.svp",
-                                  "test_global_controller/genesis/cfg_ifc.svp",
-                                  "global_controller/genesis/global_controller.vp",
-                                  "global_controller/genesis/jtag.vp",
-                                  "global_controller/genesis/tap.vp",
-                                  "global_controller/genesis/cfg_and_dbg.vp",
-                                  "global_controller/genesis/analog_regfile.vp",
-                                  "global_controller/genesis/flop.vp"],
+                                  "test_global_controller/genesis/top.svp "
+                                  "test_global_controller/genesis/test.svp "
+                                  "test_global_controller/genesis/JTAGDriver.svp "
+                                  "test_global_controller/genesis/clocker.svp "
+                                  "test_global_controller/genesis/template_ifc.svp "
+                                  "test_global_controller/genesis/cfg_ifc.svp "
+                                  "global_controller/genesis/global_controller.vp "
+                                  "global_controller/genesis/jtag.vp "
+                                  "global_controller/genesis/tap.vp "
+                                  "global_controller/genesis/cfg_and_dbg.vp "
+                                  "global_controller/genesis/analog_regfile.vp "
+                                  "global_controller/genesis/flop.vp",
                                   params)
-
-    return run_verilog_sim(genesis_outfile)
+    files = glob.glob('genesis_verif/*')
+    #append path to TAP IP on kiwi
+    files += ["/cad/synopsys/syn/M-2017.06-SP3/dw/sim_ver/DW_tap.v"]
+    return run_verilog_sim(files)
 
 
 @pytest.mark.skipif(not verilog_sim_available(),
