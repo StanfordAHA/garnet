d18fe4f41db71b994b9ce530dde4c2fd896be86d
Lenny Truong <lenny@stanford.edu>
2018-07-27 16:30:40 -0700

    Add reuseable tester pattern

--- test_cb/test_cb.py
+++ test_cb/test_cb.py
@@ -7,9 +7,9 @@ def test_cb_functional_model():
     Small test to show the usage of the cb functional model. For now we can
     leave @num_tracks and @default_value to None.
     """
-    cb = gen_cb(16, None, "1111", False, None)
+    cb = gen_cb(16, 4, "1111", False, None)
     cb_inst = cb()
     cb_inst.configure(BitVector(0, 32), BitVector(2, 32))
     data = [8, 19, 2, 9]
-    res = cb_inst(0, 0, *data, 0, 0, 0)
+    res = cb_inst(*data)
     assert res == data[2]
