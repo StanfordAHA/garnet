d0f8697dd236f8c4e910737f60cb061a119f960a
rsetaluri <rsetaluri@users.noreply.github.com>
2018-08-01 12:58:23 -0700

    [Issue #64] Update run_verilog_sim
    Pass through kwargs from run_verilog_sim() to irun().
    
    Also change the default value for cleanup to False.

--- common/run_verilog_sim.py
+++ common/run_verilog_sim.py
@@ -7,16 +7,17 @@ TCL_FILE = "common/irun/cmd.tcl"
 
 
 # We don't cover this function because irun is not available on travis
-def irun(input_files,
+def irun(files,
          top_name="top",
          tcl_file=TCL_FILE,
-         cleanup=True):  # pragma: nocover
-    if len(input_files) == 0:
+         cleanup=False):  # pragma: nocover
+    if len(files) == 0:
         print("Warning: irun requires at least 1 input file. Skipping irun.")
         return True
-    files = " ".join(input_files)
+    files_string = " ".join(files)
     irun_cmd = f"irun -sv -top {top_name} -timescale 1ns/1ps -l irun.log " \
-               f"-access +rwc -notimingchecks -input {tcl_file} {files}"
+               f"-access +rwc -notimingchecks -input {tcl_file} "
+               f"{files_string}"
     print(f"Running irun cmd: {irun_cmd}")
     res = os.system(irun_cmd)
     if not res == 0:
@@ -34,10 +35,10 @@ def iverilog(files):
         return os.system(f"./{temp_file}") == 0
 
 
-def run_verilog_sim(files):
+def run_verilog_sim(files, **kwargs):
     # Don't cover irun branch because it's not available on travis
     if irun_available():  # pragma: nocover
-        return irun(files)
+        return irun(files, **kwargs)
     elif iverilog_available():
         return iverilog(files)
     else:
