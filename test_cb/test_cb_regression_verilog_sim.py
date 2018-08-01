1114e8ce469cda8948a4030f04992200cc39f91f
rsetaluri <rsetaluri@users.noreply.github.com>
2018-08-01 13:00:11 -0700

    [Issue #64] Add cleanup=False to verilog sim tests

--- test_cb/test_cb_regression_verilog_sim.py
+++ test_cb/test_cb_regression_verilog_sim.py
@@ -17,7 +17,7 @@ def run_verilog_regression(params):
     files = [f"test_cb/{magma_cb.name}_tb.v",  # test bench file
              genesis_outfile,
              f"{magma_cb.name}.v"]
-    return run_verilog_sim(files)
+    return run_verilog_sim(files, cleanup=False)
 
 
 @pytest.mark.skipif(not verilog_sim_available(),
