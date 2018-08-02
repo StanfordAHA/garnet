6aa41c0a2a475482fd8234e4b18c6cd74c0e9604
Raj Setaluri <raj.setaluri@gmail.com>
2018-08-01 17:34:57 -0700

    [Issue #64] Add tests for verilog sim utils

---
+++ test_common/test_run_verilog_sim.py
@@ -0,0 +1,21 @@
+from common.util import \
+    irun_available, iverilog_available, verilog_sim_available
+from common.run_verilog_sim import irun, iverilog, run_verilog_sim
+
+
+def run_sim(run_func, available_func):
+    files = ["test_common/hello_world.sv"]
+    res = run_func(files, top_name="hello_world", cleanup=True)
+    assert res == available_func()
+
+
+def test_irun():
+    run_sim(irun, irun_available)
+
+
+def test_iverilog():
+    run_sim(iverilog, iverilog_available)
+
+
+def test_run_verilog_sim():
+    run_sim(run_verilog_sim, verilog_sim_available)
