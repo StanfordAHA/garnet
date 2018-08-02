a16cf9cc6dd86fa1b13509e1fa2bbf9dda2480e8
Alex Carsello <ajcars@stanford.edu>
2018-08-01 19:32:45 -0700

    remove unnecessary imports

--- test_global_controller/test_global_controller_verilog_sim.py
+++ test_global_controller/test_global_controller_verilog_sim.py
@@ -1,7 +1,7 @@
 import magma as m
 from common.run_genesis import run_genesis
 from common.util import verilog_sim_available, ip_available
-from common.run_verilog_sim import irun, iverilog, run_verilog_sim
+from common.run_verilog_sim import run_verilog_sim
 import pytest
 import glob
 
