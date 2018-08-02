188e15383bec825c4f757c37edcabf8e77e0cb7a
Raj Setaluri <raj.setaluri@gmail.com>
2018-08-02 13:47:39 -0700

    Add deprecation decorator
    A simple way to mark functions as deprecated w/ a message. For now all
    deprecated functions raise an exception. In the future we want to
    introduce a warning mode.

--- common/util.py
+++ common/util.py
@@ -20,3 +20,14 @@ def ip_available(filename, paths):
         if fullpath.is_file():
             return True
     return False
+
+# TODO(rsetaluri): Add a version which just emits a warning. For now, our only
+# use case is to error for such functions.
+def deprecated(message):
+    def deprecated_decorator(func):
+        def deprecated_func(*args, **kwargs):
+            msg = f"Function {func.__name__} is deprecated: {message}"
+            raise RuntimeError(msg)
+        return deprecated_func
+    return deprecated_decorator
+    
