abd6894b01e28f78b8f2c6658f9e502043c6b758
Alex Carsello <ajcars@stanford.edu>
2018-08-06 16:38:25 -0700

    Fix typos and remove unnecessary state enum

--- global_controller/global_controller.py
+++ global_controller/global_controller.py
@@ -4,21 +4,32 @@ from enum import Enum
 import magma as m
 
 
-class State(Enum):
-    READY = 0
-    READING = 1
-    RESETTING = 2
-    ADVANCING_CLK = 3
-    SWITCHING_CLK = 4
+class opcode(Enum):
+    NOP = 0
+    write_config = 1
+    read_config = 2
+    write_A050 = 7
+    write_TST = 8
+    read_TST = 9
+    global_reset = 10
+    write_stall = 11
+    read_stall = 12
+    advance_clk = 13
+    read_clk_domain = 14
+    switch_clk = 15
+    wr_rd_delay_reg = 16
+    rd_rd_delay_reg = 17
+    wr_delay_sel_reg = 18
+    rd_delay_sel_reg = 19
+    wr_analog_reg = 20
+    rd_analog_reg = 21
 
 
-def gen_global_controller(config_data_with: int,
+
+def gen_global_controller(config_data_width: int,
                           config_addr_width: int,
-                          config_op_with: int,
+                          config_op_width: int,
                           num_analog_regs: int):
-    CONFIG_ADDR_WIDTH = 32
-    CONFIG_DATA_WIDTH = 32
-    CONFIG_OP_WIDTH = 5
 
     class GlobalController(Model()):
         def __init__(self):
@@ -32,20 +43,17 @@ def gen_global_controller(config_data_with: int,
             self.write = None
             self.cgra_stalled = None
             self.reset_out = 1
-            self.state = State.READY
 
         def config_read(self, addr):
-            if (self.state == State.READY):
-                self.read = 1
-                self.write = 0
-                self.config_addr_out = addr
+            self.read = 1
+            self.write = 0
+            self.config_addr_out = addr
 
         def config_write(self, addr, data):
-            if (self.state == State.READY):
-                self.read = 0
-                self.write = 1
-                self.config_addr_out = addr
-                self.config_data_out = data
+            self.read = 0
+            self.write = 1
+            self.config_addr_out = addr
+            self.config_data_out = data
 
         def __call__(self, *args, **kwargs):
             raise NotImplementedError()
