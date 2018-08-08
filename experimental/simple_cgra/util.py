606d156208fee9cf541f5d878617c9d5edfb80a4
Raj Setaluri <raj.setaluri@gmail.com>
2018-08-07 22:27:22 -0700

    WIP: More updates to simple cgra

--- experimental/simple_cgra/util.py
+++ experimental/simple_cgra/util.py
@@ -1,6 +1,13 @@
+import inspect
 import magma as m
 
 
 def wrap(inner, outer):
     for name in inner.interface.ports:
         m.wire(getattr(inner, name), getattr(outer, name))
+
+
+def subgenerator(fn):
+    def wrapped(*args, **kwargs):
+        return fn(*args, **kwargs)
+    return wrapped
