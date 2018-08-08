aa12d924fc057acdd24a05d26b9a7e88412ee4e4
Priyanka Raina <praina@kiwi.Stanford.EDU>
2018-08-08 00:03:39 -0700

    Fix formatting

--- test_io1bit/test_io1bit_genesis2.py
+++ test_io1bit/test_io1bit_genesis2.py
@@ -17,7 +17,6 @@ def test_io1bit_genesis2(capsys):
     ])
     io1bit_genesis2.main(args)
     out, _ = capsys.readouterr()
-   	
     assert out == f"""\
 Running genesis cmd 'Genesis2.pl -parse -generate -top io1bit -input io1bit/genesis/io1bit.vp -parameter io1bit.io_group='0' -parameter io1bit.side='0''
 io1bit(clk: In(Bit), reset: In(Bit), pad: Bit, p2f: Out(Bit), f2p_16: In(Bit), f2p_1: In(Bit), config_addr: Array(32,In(Bit)), config_data: Array(32,In(Bit)), config_write: In(Bit), config_read: In(Bit), tile_id: Array(16,In(Bit)), read_data: Array(32,Out(Bit)))
