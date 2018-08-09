9156f95c2da47815a5a190fe2162320ec8b980d0
Raj Setaluri <raj.setaluri@gmail.com>
2018-08-09 14:51:01 -0700

    Show to modify generators

--- experimental/simple_cgra/test.py
+++ experimental/simple_cgra/test.py
@@ -33,13 +33,25 @@ class BarGenerator(generator.Generator):
         )
 
         self.wire(self.I, self.O)
-        
+
     def name(self):
         return "Bar"
 
 
+def introduce_clock(module):
+    module.add_ports(clk=magma.In(magma.Clock))
+    for child in module.children():
+        introduce_clock(child)
+    for child in module.children():
+        module.wire(module.clk, child.clk)
+
+
 if __name__ == "__main__":
     foo_generator = FooGenerator()
+
+    # Now we can dynamically add and wire clocks.
+    introduce_clock(foo_generator)
+
     FooCircuit = foo_generator.circuit()
     magma.compile("foo", FooCircuit, output="coreir")
     print(open("foo.json").read())
