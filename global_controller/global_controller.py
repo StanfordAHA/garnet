1ec6fa6bc2bdc260fda503f918cdb5e7e0ee0aa0
Alex Carsello <ajcars@stanford.edu>
2018-08-08 17:00:32 -0700

    Add enum for global controller regs

--- global_controller/global_controller.py
+++ global_controller/global_controller.py
@@ -4,31 +4,15 @@ from enum import Enum
 import magma as m
 
 
-class opcode(Enum):
-    NOP = 0
-    write_config = 1
-    read_config = 2
-    write_A050 = 7
-    write_TST = 8
-    read_TST = 9
-    global_reset = 10
-    write_stall = 11
-    read_stall = 12
-    advance_clk = 13
-    read_clk_domain = 14
-    switch_clk = 15
-    wr_rd_delay_reg = 16
-    rd_rd_delay_reg = 17
-    wr_delay_sel_reg = 18
-    rd_delay_sel_reg = 19
-    wr_analog_reg = 20
-    rd_analog_reg = 21
+class GC_reg_addr(Enum):
+    TST_addr = 0
+    stall_addr = 1
+    
 
 
 def gen_global_controller(config_data_width: int,
                           config_addr_width: int,
-                          config_op_width: int,
-                          num_analog_regs: int):
+                          config_op_width: int):
 
     class GlobalController(Model()):
         def __init__(self):
@@ -36,12 +20,19 @@ def gen_global_controller(config_data_width: int,
             self.reset()
 
         def reset(self):
-            self.config_addr_out = None
-            self.config_data_out = None
-            self.read = None
-            self.write = None
-            self.cgra_stalled = None
-            self.reset_out = 1
+            self.TST = BitVector(0, config_data_width)
+            # There are 4 stall domains.
+            # Should parametrize this in the RTL
+            self.stall = BitVector(0, 4)
+            # 0: slow JTAG clk, 1: fast sys_clk
+            self.clock_sel = BitVector(0, 1)
+            self.reset_out = BitVector(0, 1)
+            self.config_addr_out = BitVector(0, config_addr_width)
+            self.config_data_out = BitVector(0, config_data_width)
+            self.read = BitVector(0, 1)
+            self.write = BitVector(0, 1)
+            self.rw_delay_sel = BitVector(config_data_width, 2)
+            self.clk_switch_delay_sel = BitVector(0, 2)
 
         def config_read(self, addr):
             self.read = 1
@@ -55,18 +46,13 @@ def gen_global_controller(config_data_width: int,
             self.config_data_out = data
 
         def clock_switch(self, data):
-            raise notimplementederror()
+            raise NotImplementedError()
 
         def read_GC_reg(self, addr):
-            raise notimplementederror()
+            raise NotImplementedError()
   
         def write_GC_reg(self, addr, data):
-            raise notimplementederror()
-
-
-
-
-
+            raise NotImplementedError()
 
         def __call__(self, *args, **kwargs):
-            raise notimplementederror()
+            raise NotImplementedError()
