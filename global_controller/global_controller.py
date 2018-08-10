c4b90e45fb88ec983acba5d831fc4f4160583a51
Alex Carsello <ajcars@stanford.edu>
2018-08-09 17:44:27 -0700

    Make all functions set the first value that they're supposed to change. Still need to add concept of output duration and return to another value

--- global_controller/global_controller.py
+++ global_controller/global_controller.py
@@ -31,22 +31,24 @@ def gen_global_controller(config_data_width: int,
                          BitVector(0, config_data_width),
                          GC_reg_addr.clk_switch_delay_sel_addr:
                          BitVector(0, 1)}
-            self.reset_out = BitVector(0, 1)
+            self.reset_out = 0
             self.config_addr_out = BitVector(0, config_addr_width)
             self.config_data_out = BitVector(0, config_data_width)
-            self.read = BitVector(0, 1)
-            self.write = BitVector(0, 1)
+            self.config_data_in = None
+            self.read = 0
+            self.write = 0
 
         def config_read(self, addr):
             self.read = 1
             self.write = 0
-            self.config_addr_out = addr
+            self.config_addr_out = BitVector(addr, config_addr_width)
+            self.config_data_to_jtag = self.config_data_in
 
         def config_write(self, addr, data):
             self.read = 0
             self.write = 1
-            self.config_addr_out = addr
-            self.config_data_out = data
+            self.config_addr_out = BitVector(addr, config_addr_width)
+            self.config_data_out = BitVector(data, config_data_width)
 
         def read_GC_reg(self, addr):
             self.config_data_to_jtag = BitVector(self.regs[addr]._value,
@@ -56,11 +58,16 @@ def gen_global_controller(config_data_width: int,
             reg_width = self.regs[addr].num_bits
             self.regs[addr] = BitVector(data._value, reg_width)
 
-        def advance_clk(self, addr: BitVector):
-            raise NotImplementedError()
-
         def global_reset(self, data: BitVector):
-            raise NotImplementedError()
+            self.reset_out = 0
+
+        def advance_clk(self, addr: BitVector, data: BitVector):
+            for i in range(self.NUM_STALL_DOMAINS):
+                if (data[i] == 1):
+                    self.regs[GC_reg_addr.stall_addr][i] = 0
+
+        def set_config_data_in(self, data):
+            self.config_data_in = BitVector(data, config_data_width)
 
         def __call__(self, *args, **kwargs):
             raise NotImplementedError()
