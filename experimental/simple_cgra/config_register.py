c470cafd78a03cccac4f414e20ff252d2bf88ca3
Raj Setaluri <raj.setaluri@gmail.com>
2018-08-12 19:29:05 -0700

    Move Configurable and ConfigRegister into separate files

---
+++ experimental/simple_cgra/config_register.py
@@ -0,0 +1,39 @@
+import generator
+import magma
+import mantle
+
+
+class ConfigRegister(generator.Generator):
+    def __init__(self, width):
+        super().__init__()
+
+        self.width = width
+        self.addr = None
+        self.addr_width = 32
+        self.data_width = 32
+
+        T = magma.Bits(self.width)
+
+        self.add_ports(
+            clk=magma.In(magma.Clock),
+            addr_in=magma.In(magma.Bits(self.addr_width)),
+            data_in=magma.In(magma.Bits(self.data_width)),
+            O=magma.Out(T),
+        )
+
+    def circuit(self):
+        assert self.addr is not None
+        class _ConfigRegisterCircuit(magma.Circuit):
+            name = self.name()
+            IO = self.decl()
+
+            @classmethod
+            def definition(io):
+                reg = mantle.Register(self.width)
+                magma.wire(io.data_in[0:self.width], reg.I)
+                magma.wire(reg.O, io.O)
+
+        return _ConfigRegisterCircuit
+
+    def name(self):
+        return f"ConfigRegister_{self.width}_{self.addr}"
