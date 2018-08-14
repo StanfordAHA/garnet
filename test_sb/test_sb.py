16a091e3ba9b0f2f51888cd3f4a76f25156b2124
Ankita Nayak <ankitan@kiwi.Stanford.EDU>
2018-08-14 15:09:37 -0700

    Fix PEP8 errors

--- test_sb/test_sb.py
+++ test_sb/test_sb.py
@@ -12,10 +12,10 @@ def test_sb_functional_model():
     sb_inst.configure(BitVector(0, 32), BitVector(65535, 32))
     data = [11, 12, 21, 22, 31, 32, 41, 42, 46]
     res = sb_inst(*data)
-    assert res == [[data[8], data[8]], [data[8], data[8]], [data[8], data[8]], [data[8], data[8]]]
-    
+    assert res == [[data[8], data[8]], [data[8], data[8]], [data[8], data[8]], [data[8], data[8]]]    # nopep8
+
     sb_inst2 = sb()
     sb_inst2.reset()
     sb_inst2.configure(BitVector(0, 32), BitVector(43690, 32))
     res2 = sb_inst2(*data)
-    assert res2 == [[data[6], data[7]], [data[6], data[7]], [data[6], data[7]], [data[4], data[5]]]
+    assert res2 == [[data[6], data[7]], [data[6], data[7]], [data[6], data[7]], [data[4], data[5]]]  # nopep8
