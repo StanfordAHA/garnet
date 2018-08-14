c1bcb7381a9bcaa7f6acc18589429e63ca007690
Ankita Nayak <ankitan@kiwi.Stanford.EDU>
2018-08-14 14:59:50 -0700

    added PE output & random output test

--- test_sb/test_sb.py
+++ test_sb/test_sb.py
@@ -4,7 +4,7 @@ from sb.sb import gen_sb
 
 def test_sb_functional_model():
     """
-    Small test to show the usage of the sb functional model.
+    Small tests to show the usage of the sb functional model.
     """
     sb = gen_sb(16, 2, 4, "00", "11", 1, 0, 101010)
     sb_inst = sb()
@@ -12,4 +12,10 @@ def test_sb_functional_model():
     sb_inst.configure(BitVector(0, 32), BitVector(65535, 32))
     data = [11, 12, 21, 22, 31, 32, 41, 42, 46]
     res = sb_inst(*data)
-#   assert res == data[2]
+    assert res == [[data[8], data[8]], [data[8], data[8]], [data[8], data[8]], [data[8], data[8]]]
+    
+    sb_inst2 = sb()
+    sb_inst2.reset()
+    sb_inst2.configure(BitVector(0, 32), BitVector(43690, 32))
+    res2 = sb_inst2(*data)
+    assert res2 == [[data[6], data[7]], [data[6], data[7]], [data[6], data[7]], [data[4], data[5]]]
