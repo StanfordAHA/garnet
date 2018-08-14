16bbda2da00682a79dcf9021cc0fa59737da94cc
Raj Setaluri <raj.setaluri@gmail.com>
2018-08-13 21:00:07 -0700

    Add config logic to tiny cgra

--- experimental/simple_cgra/config_register.py
+++ experimental/simple_cgra/config_register.py
@@ -4,13 +4,14 @@ import mantle
 
 
 class ConfigRegister(generator.Generator):
-    def __init__(self, width, addr_width, data_width):
+    def __init__(self, width, addr_width, data_width, addr, config_en=False):
         super().__init__()
 
         self.width = width
         self.addr_width = addr_width
         self.data_width = data_width
-        self.addr = None
+        self.addr = addr
+        self.config_en = config_en
 
         T = magma.Bits(self.width)
 
@@ -20,6 +21,8 @@ class ConfigRegister(generator.Generator):
             data_in=magma.In(magma.Bits(self.data_width)),
             O=magma.Out(T),
         )
+        if self.config_en:
+            self.add_ports(ce=magma.In(magma.Bit))
 
     def circuit(self):
         assert self.addr is not None
@@ -31,6 +34,8 @@ class ConfigRegister(generator.Generator):
             def definition(io):
                 reg = mantle.Register(self.width, has_ce=True)
                 ce = (io.addr_in == magma.bits(self.addr, self.addr_width))
+                if self.config_en:
+                    ce = ce & io.ce
                 magma.wire(io.data_in[0:self.width], reg.I)
                 magma.wire(ce, reg.CE)
                 magma.wire(reg.O, io.O)
