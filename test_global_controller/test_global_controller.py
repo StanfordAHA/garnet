5505a6170ab188073853830ac563ae207637d861
Alex Carsello <ajcars@stanford.edu>
2018-08-15 16:50:49 -0700

    Random address and data for gc func model tests. More assertions

--- test_global_controller/test_global_controller.py
+++ test_global_controller/test_global_controller.py
@@ -1,16 +1,33 @@
 from bit_vector import BitVector
 from global_controller.global_controller import gen_global_controller
+import fault
+import random
 
 
 def test_global_controller_functional_model():
     """
     Small test to show the usage of the GC functional model.
     """
-    gc = gen_global_controller(32, 32, 5)
+    CONFIG_DATA_WIDTH = 32
+    CONFIG_ADDR_WIDTH = 32
+    CONFIG_OP_WIDTH = 5
+    gc = gen_global_controller(CONFIG_DATA_WIDTH,
+                               CONFIG_ADDR_WIDTH,
+                               CONFIG_OP_WIDTH)
     gc_inst = gc()
-    gc_inst.set_config_data_in(24)
-    gc_inst.config_read(99)
+    max_config_data = (2**CONFIG_DATA_WIDTH) - 1
+    max_config_addr = (2**CONFIG_ADDR_WIDTH) - 1
+    # Set some data to read later
+    random_input = random.randint(1, max_config_data)
+    gc_inst.set_config_data_in(random_input)
+
+    random_addr = random.randint(1, max_config_addr)
+    # read the random input data
+    gc_inst.config_read(random_addr)
     res = gc_inst()
+    # assert read immediately
     assert res.read[0] == 1
-    assert res.config_addr_out[0] == 99
-    assert res.config_data_to_jtag[1] == 24
+    # Read must be deasserted by the end of the top
+    assert res.read[-1] == 0
+    assert res.config_addr_out[0] == random_addr
+    assert res.config_data_to_jtag[-1] == random_input
