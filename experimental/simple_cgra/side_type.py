eb0b6b1d59fbe4bb31a07cd37270c86e7758ae1e
Raj Setaluri <raj.setaluri@gmail.com>
2018-08-16 18:06:10 -0700

    Switch from AOS to SOA for side type
    Previously the side was represented as an array-of-structs for
    representing each layer (e.g. 1-bit, 16-bit). Now we switch to a
    struct-of-arrays. This will make some wiring/indexing tasks easier. It
    will also allow for having different numbers of tracks for each track,
    e.g. only 3 1-bit tracks and 5 16-bit tracks.

--- experimental/simple_cgra/side_type.py
+++ experimental/simple_cgra/side_type.py
@@ -2,9 +2,9 @@ import magma
 
 
 def SideType(num_tracks, layers):
-    layers_dict = {f"layer{l}" : magma.Bits(l) for l in layers}
-    LayersType = magma.Tuple(**layers_dict)
-    T = magma.Array(num_tracks, LayersType)
+    layers_dict = {f"layer{l}" : magma.Array(num_tracks, magma.Bits(l)) \
+                   for l in layers}
+    T = magma.Tuple(**layers_dict)
     return magma.Tuple(I=magma.In(T), O=magma.Out(T))
 
 
