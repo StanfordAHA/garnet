bf5d97277305d08e9971174380fd5e7cf340215f
Raj Setaluri <raj.setaluri@gmail.com>
2018-08-16 19:15:30 -0700

    Implement a clunky switch box
    Implemented a not-very-nice looking version of the switch box. It is
    mostly robust for the set of inputs but looks very unyieldy. Need to
    work on the interfaces to make this nicer.

--- experimental/simple_cgra/sb.py
+++ experimental/simple_cgra/sb.py
@@ -1,11 +1,23 @@
 import magma
 from side_type import SideType
 from configurable import Configurable
+from mux_wrapper import MuxWrapper
+
+
+def get_width(T):
+    if isinstance(T, magma._BitKind):
+        return 1
+    if isinstance(T, magma.BitsKind):
+        return T.N
+    raise NotImplementedError(T, type(T))
 
 
 class SB(Configurable):
-    def __init__(self):
+    def __init__(self, inputs):
         super().__init__()
+
+        self.all_inputs = inputs
+        self.inputs = self.__organize_inputs(inputs)
 
         self.add_ports(
             north=SideType(5, (1, 16)),
@@ -13,11 +25,47 @@ class SB(Configurable):
             south=SideType(5, (1, 16)),
             east=SideType(5, (1, 16)),
         )
+        for i, input_ in enumerate(self.all_inputs):
+            self.add_port(f"core_out_{i}", input_.type())
 
-        self.wire(self.north.I, self.south.O)
-        self.wire(self.west.I, self.east.O)
-        self.wire(self.south.I, self.north.O)
-        self.wire(self.east.I, self.west.O)
+        sides = (self.north, self.west, self.south, self.east)
+        self.muxs = self.__make_muxs(sides)
+        for (side, layer, track), mux in self.muxs.items():
+            idx = 0
+            for side_in in sides:
+                if side_in == side:
+                    continue
+                mux_in = getattr(side.I, f"layer{layer}")[track]
+                self.wire(mux_in, mux.I[idx])
+                idx += 1
+            for input_ in self.inputs[layer]:
+                self.wire(input_, mux.I[idx])
+                idx += 1
+            mux_out = getattr(side.O, f"layer{layer}")[track]
+            self.wire(mux.O, mux_out)
+
+    def __organize_inputs(self, inputs):
+        ret = {1: [], 16: []}
+        for input_ in inputs:
+            width = get_width(input_.type())
+            assert width == 1 or width == 16
+            ret[width].append(input_)
+        return ret
+
+    def __make_muxs(self, sides):
+        height_per_layer = {
+            1: 3 + len(self.inputs[1]),
+            16: 3 + len(self.inputs[16]),
+        }
+        muxs = {}
+        for side in sides:
+            for layer, height in height_per_layer.items():
+                for track in range(5):
+                    muxs[(side, layer, track)] = MuxWrapper(height, layer)
+        return muxs
 
     def name(self):
-        return f"SB"
+        name = "SB"
+        for input_ in self.all_inputs:
+            name += "_" + str(input_.type())
+        return name.replace("(", "$").replace(")", "$")
