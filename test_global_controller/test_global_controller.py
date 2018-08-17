26f1f9ee6a400598ae56864b4f914759bbbab802
Alex Carsello <ajcars@stanford.edu>
2018-08-16 17:16:18 -0700

    Add config_write and rw_delay tests

--- test_global_controller/test_global_controller.py
+++ test_global_controller/test_global_controller.py
@@ -31,5 +31,22 @@ def test_global_controller_functional_model():
     assert res.config_data_to_jtag[-1] == random_input
 
     # Try changing the read_delay
-    new_read_delay = random.randint(1, 20)
-    gc_inst.write_GC_reg(GC_reg_addr.rw_delay_sel_addr, new_read_delay)
+    new_rw_delay = random.randint(1, 20)
+    gc_inst.write_GC_reg(GC_reg_addr.rw_delay_sel_addr, new_rw_delay)
+    res = gc_inst()
+    # assert that the register val was written properly
+    assert res.rw_delay_sel[-1] == new_rw_delay
+
+    # Now, verify that that change actually took effect
+    # Perform a config write
+    random_data = random.randint(1, max_config_data)
+    random_addr = random.randint(1, max_config_addr)
+    gc_inst.config_write(random_addr, random_data)
+    # check that write was asserted and read wasn't
+    assert len(gc_inst.write) == (new_rw_delay+1)
+    assert all(gc_inst.write[0:new_rw_delay])
+    assert gc_inst.write[-1] == 0
+    assert len(gc_inst.read) == (new_rw_delay+1)
+    assert all(rd == 0 for rd in gc_inst.read)
+    assert all(addr == random_addr for addr in gc_inst.config_addr_out)
+    assert all(data == random_data for data in gc_inst.config_data_out)
