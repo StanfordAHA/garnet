dd41cf0c6a953f3dc92dd17f32ef5c302a969314
Ankita Nayak <ankitan@kiwi.Stanford.EDU>
2018-08-17 22:52:37 -0700

    Updated comment for existing test. Add a random input test

--- test_sb/test_sb.py
+++ test_sb/test_sb.py
@@ -1,5 +1,6 @@
 from bit_vector import BitVector
 from sb.sb import gen_sb
+from random import randint
 
 
 def test_sb_functional_model():
@@ -8,8 +9,10 @@ def test_sb_functional_model():
     """
     sb = gen_sb(16, 2, 4, "00", "11", 1, 0, 101010)
 
+    # TEST #1
     # First digit of each element in the array is #side
     # second digit is #track
+    # This particular pattern is used to debug output incase of wrong result
     # Last element is random PE output
     data = [11, 12, 21, 22, 31, 32, 41, 42, 46]
 
@@ -20,9 +23,13 @@ def test_sb_functional_model():
     res = sb_inst(*data)
     assert res == [[data[8], data[8]], [data[8], data[8]], [data[8], data[8]], [data[8], data[8]]]    # nopep8
 
+    # TEST #2
+    # Random integer inputs to the SB
+    data2 = [randint(0, 10000) for j in range(9)]
+
     # This configuration enables the SB to output the 2nd input to the muxes
     sb_inst2 = sb()
     sb_inst2.reset()
     sb_inst2.configure(BitVector(0, 32), BitVector(43690, 32))
-    res2 = sb_inst2(*data)
-    assert res2 == [[data[6], data[7]], [data[6], data[7]], [data[6], data[7]], [data[4], data[5]]]  # nopep8
+    res2 = sb_inst2(*data2)
+    assert res2 == [[data2[6], data2[7]], [data2[6], data2[7]], [data2[6], data2[7]], [data2[4], data2[5]]]  # nopep8
