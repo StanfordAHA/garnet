097f02535e85dc886a7dcbcf85613b5a9874e5c9
Lenny Truong <lenny@stanford.edu>
2018-09-04 15:24:31 -0700

    Preserve pe core logic/test

--- simple_pe/simple_pe_magma.py
+++ simple_pe/simple_pe_magma.py
@@ -3,18 +3,38 @@ import mantle
 from common.configurable_circuit import ConfigInterface
 
 
-def make_name(ops, T, data_width):
-    return f"PE__ops_{'_'.join(op.__name__ for op in ops)}__" \
+def make_name(base_name, ops, T, data_width):
+    return f"{base_name}__ops_{'_'.join(op.__name__ for op in ops)}__" \
            f"{type(T).__name__}_{data_width}"
 
 
+
+def define_pe_core(ops, T=m.UInt, data_width=16):
+    T = T(data_width)
+    opcode_width = m.bitutils.clog2(len(ops))
+
+    class PECore(m.Circuit):
+        name = make_name("PECore", ops, T, data_width)
+        IO = ["opcode", m.In(m.Bits(opcode_width)), "I0", m.In(T), "I1",
+              m.In(T), "O", m.Out(T)]
+
+        @classmethod
+        def definition(io):
+            # Assumes opcode is equal to index in `ops` list, would be nice to
+            # generalize this for different encodings (or opcode orderings)
+            O = mantle.mux([op(io.I0, io.I1) for op in ops], io.opcode)
+            m.wire(O, io.O)
+    return PECore
+
+
 def define_pe(ops, T=m.UInt, data_width=16):
+    PECore = define_pe_core(ops, T, data_width)
     T = T(data_width)
     opcode_width = m.bitutils.clog2(len(ops))
     config_addr_width = 1
 
     class PE(m.Circuit):
-        name = make_name(ops, T, data_width)
+        name = make_name("PE", ops, T, data_width)
         IO = ["I0", m.In(T), "I1", m.In(T), "O", m.Out(T),
               # We use explicit names here instead of m.ClockInterface so it
               # matches the interfaces of the genesis circuits
@@ -36,8 +56,5 @@ def define_pe(ops, T=m.UInt, data_width=16):
             # logic. E.g. it could automatically create the register and wire
             # it up to read_data
             m.wire(opcode, io.read_data)
-            # Assumes opcode is equal to index in `ops` list, would be nice to
-            # generalize this for different encodings (or opcode orderings)
-            O = mantle.mux([op(io.I0, io.I1) for op in ops], opcode)
-            m.wire(O, io.O)
+            m.wire(PECore()(opcode, io.I0, io.I1), io.O)
     return PE
--- test_simple_pe/test_simple_pe_magma.py
+++ test_simple_pe/test_simple_pe_magma.py
@@ -1,5 +1,5 @@
 from bit_vector import BitVector
-from simple_pe.simple_pe_magma import define_pe
+from simple_pe.simple_pe_magma import define_pe, define_pe_core
 from simple_pe.simple_pe import gen_simple_pe
 from common.testers import ResetTester, ConfigurationTester
 from fault.test_vector_generator import generate_test_vectors_from_streams
@@ -8,10 +8,70 @@ import operator
 import magma as m
 import mantle
 import os
+import pytest
+import fault
 
 
-def test_simple_pe():
-    ops = [operator.add, operator.sub, operator.and_, operator.or_]
+simple_ops = [operator.add, operator.sub, operator.and_, operator.or_]
+ops = [simple_ops]
+
+
+@pytest.mark.parametrize("ops", ops)
+def test_simple_pe_core(ops):
+    pe = define_pe_core(ops, T=m.UInt, data_width=16)
+
+    tester = fault.Tester(pe)
+
+    m.compile("test_simple_pe/build/pe_core", pe, output="coreir",
+              coreir_args={"passes": ["rungenerators", "flatten",
+                                      "cullgraph"]})
+
+    # Sanity check each op with random value
+    for i, op in enumerate(ops):
+        tester.poke(pe.opcode, i)
+        I0 = fault.random.random_bv(16)
+        I1 = fault.random.random_bv(16)
+        tester.poke(pe.I0, I0)
+        tester.poke(pe.I1, I1)
+        tester.eval()
+        tester.expect(pe.O, op(I0, I1))
+    tester.compile_and_run(target="coreir")
+    opcode_width = m.bitutils.clog2(len(ops))
+    op_strs = {
+        operator.add: "+",
+        operator.sub: "-",
+        operator.and_: "&",
+        operator.or_: "|"
+    }
+
+    problem = """\
+[GENERAL]
+model_file: pe_core.json
+[DEFAULT]
+bmc_length: 30
+verification: safety
+"""
+    for i, op in enumerate(ops):
+        problem += f"""\
+[PE check {op.__name__}]
+description: "Check opcode={i} corresponds to {op.__name__}"
+formula: (self.opcode = {i}_{opcode_width}) -> ((self.I0 {op_strs[op]} self.I1) = self.O)
+prove: TRUE
+expected: TRUE
+[PE check {op.__name__} is possible]
+description: "Avoid vacuosly true version of above property"
+formula: (self.opcode != {i}_{opcode_width})
+prove: TRUE
+expected: FALSE
+"""  # noqa
+    with open("test_simple_pe/build/problem.txt", "w") as f:
+        f.write(problem)
+    assert not os.system(
+        "CoSA --problem test_simple_pe/build/problem.txt")
+
+
+@pytest.mark.parametrize("ops", ops)
+def test_simple_pe(ops):
     pe = define_pe(ops, T=m.UInt, data_width=16)
 
     class SimplePETester(ResetTester, ConfigurationTester):
