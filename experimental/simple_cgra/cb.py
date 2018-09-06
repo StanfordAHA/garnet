8ff383bf1d3f7e898593c44989d65a8348c8e8f2
Raj Setaluri <raj.setaluri@gmail.com>
2018-09-05 17:07:23 -0700

    Fix cb

--- experimental/simple_cgra/cb.py
+++ experimental/simple_cgra/cb.py
@@ -1,6 +1,6 @@
 import magma
 from mux_wrapper import MuxWrapper
-from configurable import Configurable
+from configurable import Configurable, ConfigurationType
 
 
 class CB(Configurable):
@@ -18,14 +18,21 @@ class CB(Configurable):
         self.add_ports(
             I=magma.In(magma.Array(self.height, T)),
             O=magma.Out(T),
+            clk=magma.In(magma.Clock),
+            config=magma.In(ConfigurationType(8, 32)),
         )
         self.add_configs(
-            S=sel_bits,
+           S=sel_bits,
         )
+        self.registers["S"].addr = 0
 
         self.wire(self.I, self.mux.I)
         self.wire(self.S, self.mux.S)
         self.wire(self.mux.O, self.O)
 
+        self.fanout(self.config.config_addr, self.registers.values())
+        self.fanout(self.config.config_data, self.registers.values())
+        self.fanout(self.clk, self.registers.values())
+
     def name(self):
-        return f"SB_{self.height}_{self.width}"
+        return f"CB_{self.height}_{self.width}"
