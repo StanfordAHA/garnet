7e04e93497d5d139e442649d36ea52d5af3477b7
Lenny Truong <lenny@stanford.edu>
2018-09-06 08:07:10 -0700

    Refactor config ets template into common

--- test_simple_pe/test_simple_pe_magma.py
+++ test_simple_pe/test_simple_pe_magma.py
@@ -1,6 +1,7 @@
 from bit_vector import BitVector
 from simple_pe.simple_pe_magma import define_pe, define_pe_core
 from simple_pe.simple_pe import gen_simple_pe
+from common.configurable_circuit import config_ets_template
 from common.testers import ResetTester, ConfigurationTester
 from fault.test_vector_generator import generate_test_vectors_from_streams
 from fault.random import random_bv
@@ -118,64 +119,10 @@ def test_simple_pe(ops):
 
     for i, op in enumerate(ops):
         with open(f"test_simple_pe/build/conf_{op.__name__}.ets", "w") as ets:
-            ets.write(f"""\
-# INIT
-I: self.reset = 0_1
-I: self.config_en = 0_1
-I: conf_done = 0_1
-I: self.config_addr = 0_1
-I: self.config_data = 0_{opcode_width}
-
-# STATES
-
-
-# S0 -> S0a: reset
-S0: self.config_addr = 0_1
-S0: self.config_data = 0_{opcode_width}
-S0: self.config_en = 0_1
-S0: self.reset = 0_1
-S0: conf_done = 0_1
-
-S0a: self.config_addr = 0_1
-S0a: self.config_data = 0_{opcode_width}
-S0a: self.config_en = 0_1
-S0a: self.reset = 1_1
-S0a: conf_done = 0_1
-
-S1: self.config_addr = 0_1
-S1: self.config_data = 0_{opcode_width}
-S1: self.config_en = 0_1
-S1: self.reset = 0_1
-S1: conf_done = 0_1
-
-# S1a -> S2a: config
-S1a: self.config_addr = 0_1
-S1a: self.config_data = {i}_{opcode_width}
-S1a: self.config_en = 1_1
-S1a: self.reset = 0_1
-S1a: conf_done = 0_1
-
-S2: self.config_addr = 0_1
-S2: self.config_data = {i}_{opcode_width}
-S2: self.config_en = 1_1
-S2: self.reset = 0_1
-S2: conf_done = 0_1
-
-S2a: self.config_addr = 0_1
-S2a: self.config_data = 0_{opcode_width}
-S2a: self.config_en = 0_1
-S2a: self.reset = 0_1
-S2a: conf_done = 1_1
-
-# TRANSITIONS
-I -> S0
-S0 -> S0a
-S0a -> S1
-S1 -> S1a
-S1a -> S2
-S2 -> S2a
-S2a -> S2a
-""")
+            ets.write(config_ets_template.format(config_addr=0,
+                                                 config_addr_width=1,
+                                                 config_data=i,
+                                                 config_data_width=opcode_width))
         problem = f"""\
 [GENERAL]
 model_file: pe.json,conf_{op.__name__}.ets
