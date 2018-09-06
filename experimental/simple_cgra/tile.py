5b38fae959be8454e283ccbe929f5caa08ca5e93
Raj Setaluri <raj.setaluri@gmail.com>
2018-09-05 17:31:33 -0700

    Fix tile

--- experimental/simple_cgra/tile.py
+++ experimental/simple_cgra/tile.py
@@ -3,6 +3,7 @@ import magma
 from sb import SB
 from cb import CB
 from side_type import SideType
+from configurable import ConfigurationType
 
 
 def get_width(T):
@@ -20,13 +21,15 @@ class Tile(generator.Generator):
         self.core = core
         self.sb = SB(self.core.outputs())
         widths = [get_width(i.type()) for i in self.core.inputs()]
-        self.cbs = [CB(10, w) for w in widths]
+        self.cbs = [CB(5, w) for w in widths]
 
         self.add_ports(
             north=SideType(5, (1, 16)),
             west=SideType(5, (1, 16)),
             south=SideType(5, (1, 16)),
             east=SideType(5, (1, 16)),
+            config=magma.In(ConfigurationType(32, 32)),
+            clk=magma.In(magma.Clock),
         )
 
         self.wire(self.north, self.sb.north)
@@ -42,13 +45,25 @@ class Tile(generator.Generator):
         for i, input_ in enumerate(self.core.inputs()):
             self.wire(self.cbs[i].O, input_)
 
+        for i, out in enumerate(self.core.outputs()):
+            port_name = f"core_out_{i}"
+            self.wire(out, getattr(self.sb, port_name))
+
+        for cb in self.cbs:
+            self.wire(self.config.config_addr[24:32], cb.config.config_addr)
+            self.wire(self.config.config_data, cb.config.config_data)
+
     def __wire_cb(self, side, cb):
         if cb.width == 1:
             self.wire(side.I.layer1, cb.I[:5])
-            self.wire(side.O.layer1, cb.I[5:])
+            # TODO(rsetaluri): Create anonymous port to wire to the remaining
+            # inputs.
+            #self.wire(side.O.layer1, cb.I[5:])
         elif cb.width == 16:
             self.wire(side.I.layer16, cb.I[:5])
-            self.wire(side.O.layer16, cb.I[5:])
+            # TODO(rsetaluri): Create anonymous port to wire to the remaining
+            # inputs.
+            #self.wire(side.O.layer16, cb.I[5:])
         else:
             raise NotImplementedError(cb, cb.width)
 
