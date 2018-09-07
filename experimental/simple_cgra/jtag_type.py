4432ae0a3e3cacb100863317dc325f3253307626
Raj Setaluri <raj.setaluri@gmail.com>
2018-09-07 13:37:10 -0700

    Make tck magma.Clock type

--- experimental/simple_cgra/jtag_type.py
+++ experimental/simple_cgra/jtag_type.py
@@ -5,6 +5,6 @@ JTAGType = magma.Tuple(
     tdi=magma.In(magma.Bit),
     tdo=magma.Out(magma.Bit),
     tms=magma.In(magma.Bit),
-    tck=magma.In(magma.Bit),
+    tck=magma.In(magma.Clock),
     trst_n=magma.In(magma.Bit)
 )
