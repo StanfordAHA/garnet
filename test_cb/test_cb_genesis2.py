d2d55a6b623a95b4934142e987222fce6b3802c9
Raj Setaluri <raj.setaluri@gmail.com>
2018-09-07 17:20:47 -0700

    [Issue #99] Fix parsed verilog interface expectations
    Commit phanrahan/magma@18943be changed how we parse verilog. In
    particular, it changed Array(N, Bit) to Bits(N). All tests which
    depended on that output are fixed.

--- test_cb/test_cb_genesis2.py
+++ test_cb/test_cb_genesis2.py
@@ -21,5 +21,5 @@ def test_cb_genesis2(capsys):
     out, _ = capsys.readouterr()
     assert out == f"""\
 Running genesis cmd 'Genesis2.pl -parse -generate -top cb -input cb/genesis/cb.vp -parameter cb.width='16' -parameter cb.num_tracks='10' -parameter cb.feedthrough_outputs='1111101111' -parameter cb.has_constant='1' -parameter cb.default_value='7''
-cb(clk: In(Bit), reset: In(Bit), in_0: Array(16,In(Bit)), in_1: Array(16,In(Bit)), in_2: Array(16,In(Bit)), in_3: Array(16,In(Bit)), in_4: Array(16,In(Bit)), in_6: Array(16,In(Bit)), in_7: Array(16,In(Bit)), in_8: Array(16,In(Bit)), in_9: Array(16,In(Bit)), out: Array(16,Out(Bit)), config_addr: Array(32,In(Bit)), config_data: Array(32,In(Bit)), config_en: In(Bit), read_data: Array(32,Out(Bit)))
+cb(clk: In(Bit), reset: In(Bit), in_0: In(Bits(16)), in_1: In(Bits(16)), in_2: In(Bits(16)), in_3: In(Bits(16)), in_4: In(Bits(16)), in_6: In(Bits(16)), in_7: In(Bits(16)), in_8: In(Bits(16)), in_9: In(Bits(16)), out: Out(Bits(16)), config_addr: In(Bits(32)), config_data: In(Bits(32)), config_en: In(Bit), read_data: Out(Bits(32)))
 """  # nopep8
