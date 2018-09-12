d26bd8a43f78c5a7e7e837ed46944ab7034d6edf
Lenny Truong <lenny@stanford.edu>
2018-09-12 15:02:40 -0700

    Remove unused ncsim code

--- test_pe_core/test_pe_old/test_compute_unit.py
+++ test_pe_core/test_pe_old/test_compute_unit.py
@@ -3,7 +3,6 @@ import subprocess
 import pe
 from testvectors import complete, random, test_output
 from verilator import compile, run_verilator_test
-import ncsim
 import delegator
 import os
 from random import randint
@@ -26,20 +25,6 @@ def teardown_module():
     os.system(f"rm PECOMPtest_pe_comp_unq1")
     os.system(f"rm REGMODEtest_opt_reg")
     os.system(f"rm REGMODEtest_opt_reg_file")
-
-def run_ncsim_test(op, opcode, tests, strategy):
-    return
-    if os.environ.get("TRAVIS", True):
-        # Skip on Travis because cadence tools not available
-        # FIXME: Should check for cadence tools available instead
-        return
-    ncsim.compile(f'test_{op}_{strategy.__name__}', opcode, tests)
-    RTL_FOLDER = "../../hardware/generator_z/top/genesis_verif"
-    result = delegator.run(f"""
-irun -top test_pe_comp_unq1_tb -timescale 1ns/1ps -l irun.log -access +rwc -notimingchecks -input ../../hardware/generator_z/impl/verif/cmd.tcl build/ncsim_test_{op}_{strategy.__name__}_tb.v /nobackup/nikhil3/arm_mems/arm/tsmc/cln40g/sram_sp_hsc_rvt_hvt_rvt/r10p2/sram_512w_16b/sram_512w_16b.v {RTL_FOLDER}/cb_unq1.v {RTL_FOLDER}/cb_unq2.v {RTL_FOLDER}/cb_unq3.v {RTL_FOLDER}/global_signal_tile_unq1.v {RTL_FOLDER}/io1bit_unq1.v {RTL_FOLDER}/io1bit_unq2.v {RTL_FOLDER}/io1bit_unq3.v {RTL_FOLDER}/io1bit_unq4.v {RTL_FOLDER}/jtag_unq1.sv {RTL_FOLDER}/memory_core_unq1.v {RTL_FOLDER}/memory_tile_unq1.v {RTL_FOLDER}/mem_unq1.v {RTL_FOLDER}/pe_tile_new_unq1.v {RTL_FOLDER}/sb_unq1.v {RTL_FOLDER}/sb_unq2.v {RTL_FOLDER}/sb_unq3.v  {RTL_FOLDER}/test_cmpr.sv {RTL_FOLDER}/test_full_add.sv {RTL_FOLDER}/test_lut.sv {RTL_FOLDER}/test_mult_add.sv {RTL_FOLDER}/test_opt_reg.sv {RTL_FOLDER}/test_pe_comp_unq1.sv {RTL_FOLDER}/test_pe_unq1.sv {RTL_FOLDER}/test_shifter_unq1.sv {RTL_FOLDER}/top.v
-    """)
-    assert not result.return_code, result.out + "\n" + result.err
-    assert "Failed!" not in result.out, result.out + "\n" + result.err
 
 ops, signed_ops = [], []
 for name, op in inspect.getmembers(pe, inspect.isfunction):
@@ -103,7 +88,6 @@ def test_op(op, strategy):
             build_dir=build_dir)
     run_verilator_test('test_pe_comp_unq1', 'harness', 'test_pe_comp_unq1',
                        build_dir=build_dir)
-    # run_ncsim_test(op, a.instruction, tests, strategy)
 
 def test_signed_op(signed_op, signed, strategy):
     a = getattr(pe, signed_op)(signed)
@@ -114,4 +98,3 @@ def test_signed_op(signed_op, signed, strategy):
             build_dir=build_dir)
     run_verilator_test('test_pe_comp_unq1', 'harness', 'test_pe_comp_unq1',
                        build_dir=build_dir)
-    # run_ncsim_test(signed_op, a.instruction, tests, strategy)
