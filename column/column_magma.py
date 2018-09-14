bc570e152b4e699e5375644eeb62f9bab4d3611f
Alex Carsello <ajcars@stanford.edu>
2018-09-14 16:26:37 -0700

    Connect tile ID inputs properly

--- column/column_magma.py
+++ column/column_magma.py
@@ -4,6 +4,7 @@ import generator.generator as generator
 from common.side_type import SideType
 from generator.configurable import ConfigurationType
 from generator.from_magma import FromMagma
+from generator.const import Const
 
 
 class Column(generator.Generator):
@@ -21,7 +22,8 @@ class Column(generator.Generator):
             config=magma.In(ConfigurationType(32, 32)),
             clk=magma.In(magma.Clock),
             rst=magma.In(magma.Reset),
-            read_config_data=magma.Out(32)
+            read_config_data=magma.Out(32),
+            column_num=magma.In(8)
         )
 
         self.read_data_OR = FromMagma(mantle.Or(self.height, 32))
@@ -35,6 +37,10 @@ class Column(generator.Generator):
             self.wire(self.ports.east[i], tile.ports.east)
             self.wire(tile.ports.read_config_data,
                       self.read_data_OR.ports["I"+i])
+            # Wire upper 8 bits of tile ID to row number
+            self.wire(tile.ports.tile_id[8:16], Const(magma.bits(i, 8)))
+            # Wire lower 8 bits of tile ID to col number
+            self.wire(tile.ports.tile_id[0:8], self.ports.column_num)
         for i in range(1, self.height):
             t0 = self.tiles[i - 1]
             t1 = self.tiles[i]
