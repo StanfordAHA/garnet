4e25b57670e955d8b733f16a5d5fdc2c597e4a77
Lenny Truong <lenny@stanford.edu>
2018-09-16 14:54:27 -0700

    Refactor old compute unit tests

--- test_pe_core/test_pe_old/test_compute_unit.py
+++ test_pe_core/test_pe_old/test_compute_unit.py
@@ -1,6 +1,8 @@
+import glob
 import pytest
 import subprocess
 import pe
+import fault
 from testvectors import complete, random, test_output
 from verilator import compile, run_verilator_test
 import delegator
@@ -10,17 +12,19 @@ from collections import OrderedDict
 import inspect
 from pe_core import pe_core_genesis2
 import glob
+import itertools
+import magma as m
 
 
-def setup_module():
-    # Generate the PE
-    pe_core_genesis2.pe_core_wrapper.generator()()
+@pytest.fixture
+def pe_core():
+    return pe_core_genesis2.pe_core_wrapper.generator()()
 
 
 def teardown_module():
     # Cleanup PE genesis2 collateral
-    for item in glob.glob('genesis_*'):
-        os.system(f"rm -r {item}")
+    # for item in glob.glob('genesis_*'):
+    #     os.system(f"rm -r {item}")
     os.system(f"rm PEtest_pe")
     os.system(f"rm PECOMPtest_pe_comp_unq1")
     os.system(f"rm REGMODEtest_opt_reg")
@@ -44,7 +48,7 @@ def pytest_generate_tests(metafunc):
     if 'const_value' in metafunc.fixturenames:
         metafunc.parametrize("const_value", range(16))
     if 'strategy' in metafunc.fixturenames:
-        metafunc.parametrize("strategy", [complete, random])
+        metafunc.parametrize("strategy", ["complete", "random"])
 
 
 @pytest.fixture
@@ -86,24 +90,42 @@ def get_tests(pe, strategy, signed=False):
 
 build_dir = "test_pe_core/test_pe_old/build"
 
+def run_test(functional_model, strategy, pe_core, signed):
+    pe_compute_unit = m.DefineFromVerilogFile('genesis_verif/test_pe_comp_unq1.sv')[0]
+    tester = fault.Tester(pe_compute_unit)
+    N = 4
+    _iter = None
+    if strategy == "complete":
+        _iter = itertools.product(range(0, N), range(0, N), range(0, 1))
+    elif strategy == "random":
+        n = 256
+        _iter = [
+            (randint(0, (1 << N) - 1), randint(0, (1 << N) - 1), randint(0, 1))
+            for n in range(256)
+        ]
+    tester.poke(pe_compute_unit.op_code, functional_model.instruction)
+    for op_a, op_b, op_d_p in _iter:
+        tester.poke(pe_compute_unit.op_a, op_a)
+        tester.poke(pe_compute_unit.op_b, op_b)
+        tester.poke(pe_compute_unit.op_d_p, op_d_p)
+        tester.eval()
+        res, res_p = functional_model._alu(op_a=op_a, op_b=op_b, op_d_p=op_d_p)
+        tester.print(pe_compute_unit.op_a)
+        tester.print(pe_compute_unit.op_b)
+        tester.print(pe_compute_unit.op_d_p)
+        tester.expect(pe_compute_unit.res, res)
+        tester.expect(pe_compute_unit.res_p, res_p)
+        tester.eval()
+    tester.compile_and_run(target='verilator', directory=build_dir,
+                           include_directories=["../../../genesis_verif"],
+                           flags=['-Wno-fatal'])
 
-def test_op(op, strategy):
-    a = getattr(pe, op)()
 
-    tests = get_tests(a, strategy)
-
-    compile('harness', 'test_pe_comp_unq1', a.instruction, tests,
-            build_dir=build_dir)
-    run_verilator_test('test_pe_comp_unq1', 'harness', 'test_pe_comp_unq1',
-                       build_dir=build_dir)
+def test_op(op, strategy, pe_core):
+    functional_model = getattr(pe, op)()
+    run_test(functional_model, strategy, pe_core, False)
 
 
 def test_signed_op(signed_op, signed, strategy):
-    a = getattr(pe, signed_op)(signed)
-
-    tests = get_tests(a, strategy, signed)
-
-    compile('harness', 'test_pe_comp_unq1', a.instruction, tests,
-            build_dir=build_dir)
-    run_verilator_test('test_pe_comp_unq1', 'harness', 'test_pe_comp_unq1',
-                       build_dir=build_dir)
+    functional_model = getattr(pe, signed_op)(signed)
+    run_test(functional_model, strategy, pe_core, signed)
