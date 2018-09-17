d6041a0cdcea631b3a3e732f7d3eb9bd57d33c57
Lenny Truong <lenny@stanford.edu>
2018-09-17 10:30:52 -0700

    Update tests for python 3.7

--- test_common/test_model.py
+++ test_common/test_model.py
@@ -1,8 +1,12 @@
 from common.model import Model
+import sys
 
 
 def test_model():
-    EXPECTED_MSG = "TypeError(\"Can't instantiate abstract class Model with abstract methods __call__, __init__\",)"  # nopep8
+    if sys.version_info >= (3,7):
+        EXPECTED_MSG = "TypeError(\"Can't instantiate abstract class Model with abstract methods __call__, __init__\")"  # nopep8
+    else:
+        EXPECTED_MSG = "TypeError(\"Can't instantiate abstract class Model with abstract methods __call__, __init__\",)"  # nopep8
     has_type_error = False
     try:
         my_model = Model()
