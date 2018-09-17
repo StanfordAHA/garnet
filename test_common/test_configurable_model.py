609198b27f57327faf9c087ac6c7c1794fefc67a
Lenny Truong <lenny@stanford.edu>
2018-09-17 10:36:37 -0700

    Use simpler logic based off repr strings

--- test_common/test_configurable_model.py
+++ test_common/test_configurable_model.py
@@ -5,10 +5,7 @@ import sys
 
 
 def test_configurable_model_smoke():
-    if sys.version_info >= (3,7):
-        EXPECTED_MSG = "TypeError(\"Can't instantiate abstract class _ConfigurableModel with abstract methods __call__\")"  # nopep8
-    else:
-        EXPECTED_MSG = "TypeError(\"Can't instantiate abstract class _ConfigurableModel with abstract methods __call__\",)"  # nopep8
+    EXPECTED_MSG = repr(TypeError("Can't instantiate abstract class _ConfigurableModel with abstract methods __call__"))  # nopep8
     has_type_error = False
     try:
         my_model = ConfigurableModel(32, 32)()
@@ -45,10 +42,7 @@ def test_configurable_model_subclass():
         f.config[addr] = data
         assert False
     except ValueError as e:
-        if sys.version_info >= (3,7):
-            assert e.__repr__() == "ValueError('Expected addr to be of width 32')"
-        else:
-            assert e.__repr__() == "ValueError('Expected addr to be of width 32',)"
+        assert repr(e) == repr(ValueError("Expected addr to be of width 32"))
 
     # Check that reading a non-existent address raises a key error. We clear the
     # config by re-initializing foo.
@@ -58,7 +52,4 @@ def test_configurable_model_subclass():
         print(f.config[addr])
         assert False
     except KeyError as e:
-        if sys.version_info >= (3,7):
-            assert e.__repr__() == "KeyError(0)"
-        else:
-            assert e.__repr__() == "KeyError(0,)"
+        assert repr(e) == repr(KeyError(0))
