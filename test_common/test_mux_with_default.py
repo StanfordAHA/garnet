9c4ca200c8b237d7b4809fd98de4bd8579c3d609
Raj Setaluri <raj.setaluri@gmail.com>
2018-09-17 19:05:18 -0700

    Refactor MuxWithDefault generator interface
    The number of select bits coming into the MuxWithDefault module is now
    specified by the user. This module is now similar to a big case
    statement with a default case in Verilog.

--- test_common/test_mux_with_default.py
+++ test_common/test_mux_with_default.py
@@ -1,5 +1,5 @@
 import pytest
-from random import randint
+from random import randint, choice
 import tempfile
 from bit_vector import BitVector
 import fault
@@ -8,30 +8,33 @@ import magma
 from common.mux_with_default import MuxWithDefaultWrapper
 
 
-@pytest.mark.parametrize('height,width,default',
-                         [(randint(2, 10), randint(1, 32), 1)
-                          for _ in range(5)])
-def test_mux_with_default_wrapper(height, width, default):
-    mux = MuxWithDefaultWrapper(height, width, default)
-    assert mux.height == height
+@pytest.mark.parametrize('num_inputs,width,sel_bits,default',
+                         [(randint(2, 10), randint(1, 32), randint(4, 32),
+                           randint(0, 100)) for _ in range(5)])
+def test_mux_with_default_wrapper(num_inputs, width, sel_bits, default):
+    mux = MuxWithDefaultWrapper(num_inputs, width, sel_bits, default)
+    assert mux.num_inputs == num_inputs
     assert mux.width == width
+    assert mux.sel_bits == sel_bits
     assert mux.default == default
-    assert mux.name() == f"MuxWithDefaultWrapper_{height}_{width}_{default}"
+    assert mux.name() == f"MuxWithDefaultWrapper_{num_inputs}_{width}"\
+        f"_{sel_bits}_{default}"
 
     mux_circuit = mux.circuit()
     tester = fault.Tester(mux_circuit)
-    inputs = [fault.random.random_bv(width) for _ in range(height)]
+    inputs = [fault.random.random_bv(width) for _ in range(num_inputs)]
     for i, input_ in enumerate(inputs):
         tester.poke(mux_circuit.I[i], input_)
     tester.poke(mux_circuit.EN, 1)
-    sel_bits = mux.sel_bits
-    for i in range(2 ** sel_bits):
+    for i in range(num_inputs):
         tester.poke(mux_circuit.S, BitVector(i, mux.sel_bits))
         tester.eval()
-        if i < height:
-           tester.expect(mux_circuit.O, inputs[i])
-        else:
-           tester.expect(mux_circuit.O, default)
+        tester.expect(mux_circuit.O, inputs[i])
+    for _ in range(10):
+        sel = choice(range(num_inputs, 2 ** sel_bits))
+        tester.poke(mux_circuit.S, BitVector(sel, mux.sel_bits))
+        tester.eval()
+        tester.expect(mux_circuit.O, default)
     with tempfile.TemporaryDirectory() as tempdir:
         tester.compile_and_run(directory=tempdir,
                                magma_output="coreir-verilog",
