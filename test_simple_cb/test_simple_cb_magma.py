8568a6d0c29192b9bb1a0c4288da179eafdd1661
Raj Setaluri <raj.setaluri@gmail.com>
2018-09-19 19:06:13 -0700

    Update simple cb magma test
    -- Added some more parametrization to test
    -- Moved to using tempdir rather than build/
    -- Removed some unused things

--- test_simple_cb/test_simple_cb_magma.py
+++ test_simple_cb/test_simple_cb_magma.py
@@ -1,15 +1,11 @@
 import pytest
+import tempfile
 from bit_vector import BitVector
-import magma as m
+import fault, fault.random
 from simple_cb.simple_cb_magma import CB
-from simple_cb.simple_cb import gen_simple_cb
-from simple_cb.simple_cb_genesis2 import simple_cb_wrapper
-from common.testers import ResetTester, ConfigurationTester
-from fault.random import random_bv
-from fault import Tester
 
 
-@pytest.mark.parametrize('num_tracks', [10])
+@pytest.mark.parametrize('num_tracks', [2, 5, 10])
 def test_regression(num_tracks):
     params = {
         "width": 16,
@@ -17,37 +13,40 @@ def test_regression(num_tracks):
     }
 
     # Create magma circuit.
-    magma_simple_cb = CB(**params).circuit()
-    m.compile(f"test_simple_cb/build/{magma_simple_cb.name}", magma_simple_cb,
-              output="coreir-verilog")
+    simple_cb = CB(**params)
 
-    simple_cb_functional_model = gen_simple_cb(**params)()
+    # TODO(rsetaluri): Check some basic properties of this generator.
 
-    tester = Tester(magma_simple_cb, magma_simple_cb.clk)
+    simple_cb_circuit = simple_cb.circuit()
+    tester = fault.Tester(simple_cb_circuit, simple_cb_circuit.clk)
 
     def reset():
-        simple_cb_functional_model.reset()
-        tester.poke(magma_simple_cb.reset, 1)
+        tester.poke(simple_cb_circuit.reset, 1)
         tester.eval()
-        tester.poke(magma_simple_cb.reset, 0)
+        tester.poke(simple_cb_circuit.reset, 0)
+
+    # TODO(rsetaluri): Instead of these custom reset(), configure(), and
+    # config_read() methods, use the ResetTester and ConfigTester in
+    # garnet. This also requires bringing in the functional model that exists in
+    # simple_cb/simple_cb.py (see test_simple_cb/test_simple_cb_regression.py).
 
     def configure(addr, data):
-        simple_cb_functional_model.configure(BitVector(addr, 32), data)
-        tester.poke(magma_simple_cb.clk, 0)
-        tester.poke(magma_simple_cb.reset, 0)
-        tester.poke(magma_simple_cb.config.config_addr, addr)
-        tester.poke(magma_simple_cb.config.config_data, data)
-        tester.poke(magma_simple_cb.config.read, 0)
-        # tester.poke(magma_simple_cb.config.write, 1)
+        tester.poke(simple_cb_circuit.clk, 0)
+        tester.poke(simple_cb_circuit.reset, 0)
+        tester.poke(simple_cb_circuit.config.config_addr, addr)
+        tester.poke(simple_cb_circuit.config.config_data, data)
+        tester.poke(simple_cb_circuit.config.read, 0)
+        # TODO(alexcarsello): Once config.write logic is enabled, check that
+        # leaving write=0 does not perform a reconfiguration.
+        #tester.poke(simple_cb_circuit.config.write, 1)
         tester.step(2)
 
     def config_read(addr):
-        # simple_cb_functional_model.config_read(addr)
-        tester.poke(magma_simple_cb.clk, 0)
-        tester.poke(magma_simple_cb.reset, 0)
-        tester.poke(magma_simple_cb.config.config_addr, addr)
-        tester.poke(magma_simple_cb.config.read, 1)
-        tester.poke(magma_simple_cb.config.write, 0)
+        tester.poke(simple_cb_circuit.clk, 0)
+        tester.poke(simple_cb_circuit.reset, 0)
+        tester.poke(simple_cb_circuit.config.config_addr, addr)
+        tester.poke(simple_cb_circuit.config.read, 1)
+        tester.poke(simple_cb_circuit.config.write, 0)
         tester.step(2)
 
     for config_data in [BitVector(x, 32) for x in range(num_tracks)]:
@@ -55,14 +54,15 @@ def test_regression(num_tracks):
         configure(BitVector(0, 8), config_data)
         config_read(BitVector(0, 8))
         tester.eval()
-        tester.expect(magma_simple_cb.read_config_data, config_data)
-        inputs = [random_bv(16) for _ in range(num_tracks)]
+        tester.expect(simple_cb_circuit.read_config_data, config_data)
+        inputs = [fault.random.random_bv(16) for _ in range(num_tracks)]
         for i, input_ in enumerate(inputs):
-            tester.poke(magma_simple_cb.I[i], input_)
+            tester.poke(simple_cb_circuit.I[i], input_)
         tester.eval()
-        tester.expect(magma_simple_cb.O, inputs[config_data.as_uint()])
+        tester.expect(simple_cb_circuit.O, inputs[config_data.as_uint()])
 
-    tester.compile_and_run(target="verilator",
-                           skip_compile=True,
-                           directory="test_simple_cb/build",
-                           flags=["-Wno-fatal"])
+    with tempfile.TemporaryDirectory() as tempdir:
+        tester.compile_and_run(target="verilator",
+                               magma_output="coreir-verilog",
+                               directory=tempdir,
+                               flags=["-Wno-fatal"])
