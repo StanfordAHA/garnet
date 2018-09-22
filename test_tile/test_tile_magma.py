b5a8b282b01768c12b9be4376786c9e7c15e5443
Alex Carsello <ajcars@stanford.edu>
2018-09-21 17:36:55 -0700

    Insantiate BasicTester in test_tile

--- test_tile/test_tile_magma.py
+++ test_tile/test_tile_magma.py
@@ -2,12 +2,16 @@ from common.dummy_core_magma import DummyCore
 import magma
 from bit_vector import BitVector
 from tile.tile_magma import Tile
-from common.testers import ResetTester, ConfigurationTester
+from common.testers import BasicTester
+import fault
 
 
 def test_tile():
     core = DummyCore()
     tile = Tile(core)
     tile_circ = tile.circuit()
-    magma.compile(f"{tile.name}", tile_circ,
-                  output="coreir-verilog")
+    magma.compile(f"{tile.name}", tile_circ, output="coreir-verilog")
+
+    # No functional model for tile yet, so we have to use the
+    # standard fault tester for now
+    tester = BasicTester(tile_circ, tile_circ.clk, tile_circ.reset)
