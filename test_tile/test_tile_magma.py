9699d6f7a0617ed36a8c15f01c773cd9d0259348
Alex Carsello <ajcars@stanford.edu>
2018-09-24 11:37:15 -0700

    Compile and run verilator in tempdir for tile_test

--- test_tile/test_tile_magma.py
+++ test_tile/test_tile_magma.py
@@ -3,15 +3,23 @@ import magma
 from bit_vector import BitVector
 from tile.tile_magma import Tile
 from common.testers import BasicTester
-import fault
+import tempfile
+from fault.random import random_bv
 
 
 def test_tile():
     core = DummyCore()
     tile = Tile(core)
     tile_circ = tile.circuit()
-    magma.compile(f"{tile.name()}", tile_circ, output="coreir-verilog")
-
     # No functional model for tile yet, so we have to use the
     # standard fault tester for now
     tester = BasicTester(tile_circ, tile_circ.clk, tile_circ.reset)
+    # assign the tile a random ID for configuration
+    tile_id = random_bv(16)
+    tester.poke(tile_circ.tile_id, tile_id)
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        tester.compile_and_run(target="verilator",
+                               magma_output="coreir-verilog",
+                               directory=tempdir,
+                               flags=["-Wno-fatal"])
