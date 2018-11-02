63cc0fe5f66fb1b550e4c53abc9e1addba2170d3
Lenny Truong <lenny@stanford.edu>
2018-11-02 10:10:24 -0700

    Have coreir and bit_vector dependencies managed by magma

--- setup.py
+++ setup.py
@@ -13,8 +13,6 @@ setup(
         # magma/mantle not on PYPI yet, installed using requirements.txt
         # "magma",
         # "mantle",
-        "coreir==0.24a",
-        "bit_vector==0.34a",
         "fault==0.28",
         "delegator.py",
     ],
