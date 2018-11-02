886be85e17187c912c0fa29758b30a42d2292757
Lenny Truong <lenny@stanford.edu>
2018-11-02 10:21:18 -0700

    Use pip cosa

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
         # magma/mantle not on PYPI yet, installed using requirements.txt
         # "magma",
         # "mantle",
+        "cosa==0.2.8",
         "fault==0.28",
         "delegator.py",
     ],
