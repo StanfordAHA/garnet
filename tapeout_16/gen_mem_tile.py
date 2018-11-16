cc22850354f2bf20a159861847c2880dc27d25f5
Alex Carsello <ajcars@stanford.edu>
2018-11-16 14:44:30 -0800

    Make tile gen scripts add pass through signals

--- tapeout_16/gen_mem_tile.py
+++ tapeout_16/gen_mem_tile.py
@@ -5,5 +5,10 @@ from memory_core.memory_core_magma import MemCore
 
 core = MemCore(16, 1024)
 tile = Tile(core)
+pass_throughs = [tile.ports.clk, tile.ports.reset,
+                 tile.ports.stall, tile.ports.config]
+for signal in pass_throughs:
+    tile.pass_signal_through(signal)
+tile.read_data_reduction()
 tile_circ = tile.circuit()
 magma.compile("MemTile", tile_circ, output="coreir-verilog")
--- tapeout_16/gen_pe_tile.py
+++ tapeout_16/gen_pe_tile.py
@@ -5,5 +5,10 @@ from pe_core.pe_core_magma import PECore
 
 core = PECore()
 tile = Tile(core)
+pass_throughs = [tile.ports.clk, tile.ports.reset,
+                 tile.ports.stall, tile.ports.config]
+for signal in pass_throughs:
+    tile.pass_signal_through(signal)
+tile.read_data_reduction()
 tile_circ = tile.circuit()
 magma.compile("PETile", tile_circ, output="coreir-verilog")
