651eb7900449888ca48a663ae0a9dda442a5a12b
Daniel Stanley <dstanley@kiwi.Stanford.EDU>
2018-12-03 18:20:50 -0800

    Fixed lint warnings

--- test_simple_sb/test_simple_sb_magma_no_model.py
+++ test_simple_sb/test_simple_sb_magma_no_model.py
@@ -1,14 +1,12 @@
-import pytest
 import tempfile
 import random
-from bit_vector import BitVector
 import magma
 import fault
 import fault.random
 from common.core import Core
 from common.testers import BasicTester
 from simple_sb.simple_sb_magma import SB
-from simple_sb.simple_sb_model import SimpleSBModel, SimpleSBTester, SideModel
+from simple_sb.simple_sb_model import SideModel
 
 
 class DummyCore(Core):
@@ -52,12 +50,12 @@ def test_regression():
     NUM_TRACKS = 5
     LAYERS = (1, 16)
     SIDES = ("north", "west", "south", "east")
-    num_core_outputs = len(dummy_core.outputs())
+    # num_core_outputs = len(dummy_core.outputs())
 
     simple_sb_circuit = simple_sb.circuit()
-    #tester = fault.Tester(simple_sb_circuit, simple_sb_circuit.clk)
-    #model = SimpleSBModel(NUM_TRACKS, LAYERS, num_core_outputs)
-    #simple_sb_tester = SimpleSBTester(
+    # tester = fault.Tester(simple_sb_circuit, simple_sb_circuit.clk)
+    # model = SimpleSBModel(NUM_TRACKS, LAYERS, num_core_outputs)
+    # simple_sb_tester = SimpleSBTester(
     #    NUM_TRACKS, LAYERS, num_core_outputs, model, tester)
 
     config = {side: SideModel(NUM_TRACKS, LAYERS) for side in SIDES}
@@ -67,7 +65,8 @@ def test_regression():
     for side in SIDES:
         for layer in LAYERS:
             for track in range(NUM_TRACKS):
-                config[side].values[layer][track] = (random.randint(0, 3), random.randint(0,1))
+                config[side].values[layer][track] = (random.randint(0, 3),
+                                                     random.randint(0, 1))
                 in1_[side].values[layer][track] = fault.random.random_bv(layer)
                 in2_[side].values[layer][track] = fault.random.random_bv(layer)
 
@@ -80,8 +79,7 @@ def test_regression():
         "bit_out": fault.random.random_bv(1),
     }
 
-
-    # This function taken from the model, with update to look only at sel part of config
+    # This function taken from the model, with minor updates
     # finds mux output before register buffer based on in_ core_outputs config
     def get_output(side, layer, track, in_, core_outputs):
         inputs = []
@@ -94,9 +92,10 @@ def test_regression():
         inputs.append(core_outputs["data_out"] if layer == 16
                       else core_outputs["bit_out"])
         select = config[side].values[layer][track][0]
-        return  inputs[select]
+        return inputs[select]
 
-    basic_tester = BasicTester(simple_sb_circuit, simple_sb_circuit.clk, simple_sb_circuit.reset)
+    basic_tester = BasicTester(simple_sb_circuit, simple_sb_circuit.clk,
+                               simple_sb_circuit.reset)
 
     # configure
     basic_tester.reset()
@@ -107,11 +106,13 @@ def test_regression():
                 this_config = config[side].values[layer][track]
                 basic_tester.configure(addr, this_config[0])
                 basic_tester.config_read(addr)
-                basic_tester.expect(simple_sb_circuit.read_config_data, this_config[0])
+                basic_tester.expect(simple_sb_circuit.read_config_data,
+                                    this_config[0])
                 addr = addr + 1
                 basic_tester.configure(addr, this_config[1])
                 basic_tester.config_read(addr)
-                basic_tester.expect(simple_sb_circuit.read_config_data, this_config[1])
+                basic_tester.expect(simple_sb_circuit.read_config_data,
+                                    this_config[1])
                 addr = addr + 1
 
     # poke inputs
@@ -123,7 +124,8 @@ def test_regression():
         for side in SIDES:
             for layer in LAYERS:
                 for track in range(NUM_TRACKS):
-                    in_port = getattr(simple_sb_circuit.interface.ports[side].I, f"layer{layer}")[track]
+                    side_ports = simple_sb_circuit.interface.ports[side].I
+                    in_port = getattr(side_ports, f"layer{layer}")[track]
                     basic_tester.poke(in_port, in_[side].values[layer][track])
     basic_tester.eval()
 
@@ -133,17 +135,19 @@ def test_regression():
             for track in range(NUM_TRACKS):
                 if config[side].values[layer][track][1] == 0:
                     # not buffered
-                    expected = get_output(side, layer, track, in2_, core_outputs2)
+                    expected = get_output(side, layer, track, in2_,
+                                          core_outputs2)
                 else:
                     # buffered
-                    expected = get_output(side, layer, track, in1_, core_outputs1)
+                    expected = get_output(side, layer, track, in1_,
+                                          core_outputs1)
 
-                out_port = getattr(simple_sb_circuit.interface.ports[side].O, f"layer{layer}")[track]
+                side_ports = simple_sb_circuit.interface.ports[side].O
+                out_port = getattr(side_ports, f"layer{layer}")[track]
                 basic_tester.expect(out_port, expected)
-
 
     with tempfile.TemporaryDirectory() as tempdir:
         basic_tester.compile_and_run(target="verilator",
-                                                magma_output="coreir-verilog",
-                                                directory=tempdir,
-                                                flags=["-Wno-fatal"])
+                                     magma_output="coreir-verilog",
+                                     directory=tempdir,
+                                     flags=["-Wno-fatal"])
