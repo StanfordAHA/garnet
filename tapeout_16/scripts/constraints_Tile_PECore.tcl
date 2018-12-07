b2966645f7d6938de6f6a661209ed5efe7a4d1b2
Alex Carsello <ajcars@stanford.edu>
2018-12-06 17:19:09 -0800

    Make all paths from config registers false paths

--- tapeout_16/scripts/constraints_Tile_PECore.tcl
+++ tapeout_16/scripts/constraints_Tile_PECore.tcl
@@ -7,5 +7,8 @@ set_output_delay -min 0 -clock clk [all_outputs]
 set_load 0.1 [all_outputs]
 set_input_transition 0.2 [all_inputs]
 
+set constant_regs [filter flop true [find / -instance instances_seq/ConfigRegister*]]
+set_false_path -from [get_pins -of $constant_regs -filter "is_clock_pin==true"]
+set_false_path -to [get_pins -of $constant_regs -filter "is_data_pin==true"]
 set_false_path -through [get_pins SB*/* -filter "direction==out"] -through [get_pins CB_*/* -filter "direction==in"]
 set_false_path -through [get_ports {config* tile_id* reset}]  -to [all_outputs]
