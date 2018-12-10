a0bc898152bc49dc91195c0438ec156a7e537af5
Daniel Stanley <dstanley@kiwi.Stanford.EDU>
2018-12-10 15:18:29 -0800

    Improve test_simple_sb_magma_no_model. Debug statements still in place

--- test_simple_sb/test_simple_sb_magma_no_model.py
+++ test_simple_sb/test_simple_sb_magma_no_model.py
@@ -58,93 +58,130 @@ def test_regression():
     # simple_sb_tester = SimpleSBTester(
     #    NUM_TRACKS, LAYERS, num_core_outputs, model, tester)
 
-    config = {side: SideModel(NUM_TRACKS, LAYERS) for side in SIDES}
-    in1_ = {side: SideModel(NUM_TRACKS, LAYERS) for side in SIDES}
-    in2_ = {side: SideModel(NUM_TRACKS, LAYERS) for side in SIDES}
+    configs = []
+    inputs = []
+    core_outputs = []
+    NUM_BATCHES = 3
+    BATCH_LEN = 100
+    for batch in range(NUM_BATCHES):
+        config = {side: SideModel(NUM_TRACKS, LAYERS) for side in SIDES}
+        configs.append(config)
+        in_ = {side: SideModel(NUM_TRACKS, LAYERS) for side in SIDES}
+        inputs.append(in_)
+        core_outputs_dict = {}
+        core_outputs.append(core_outputs_dict)
 
-    for side in SIDES:
-        for layer in LAYERS:
-            for track in range(NUM_TRACKS):
-                config[side].values[layer][track] = (random.randint(0, 3),
-                                                     random.randint(0, 1))
-                in1_[side].values[layer][track] = fault.random.random_bv(layer)
-                in2_[side].values[layer][track] = fault.random.random_bv(layer)
+    for batch in range(NUM_BATCHES):
+        for side in SIDES:
+            for layer in LAYERS:
+                for track in range(NUM_TRACKS):
+                    # decide on which register buffers to enable
+                    if batch == 0:
+                        buffer_config = 0
+                    elif batch == 1:
+                        buffer_config = 1
+                    else:
+                        buffer_config = random.randint(0, 1)
 
-    core_outputs1 = {
-        "data_out": fault.random.random_bv(16),
-        "bit_out": fault.random.random_bv(1),
-    }
-    core_outputs2 = {
-        "data_out": fault.random.random_bv(16),
-        "bit_out": fault.random.random_bv(1),
-    }
+                    configs[batch][side].values[layer][track] = \
+                        (random.randint(0, 3), buffer_config)
+                    batch_inputs = [fault.random.random_bv(layer)
+                                    for _ in range(BATCH_LEN)]
+                    inputs[batch][side].values[layer][track] = batch_inputs
+                    core_outputs_batch_data = \
+                        [fault.random.random_bv(16) for _ in range(BATCH_LEN)]
+                    core_outputs_batch_bits = \
+                        [fault.random.random_bv(1) for _ in range(BATCH_LEN)]
+                    core_outputs[batch]["data_out"] = core_outputs_batch_data
+                    core_outputs[batch]["bit_out"] = core_outputs_batch_bits
 
     # This function taken from the model, with minor updates
-    # finds mux output before register buffer based on in_ core_outputs config
-    def get_output(side, layer, track, in_, core_outputs):
-        inputs = []
+    # finds mux output before register buffer based on:
+    # inputs core_outputs configs
+    def get_output(side, layer, track, batch, step):
+        buffer_config = configs[batch][side].values[layer][track][1]
+        relevant_step = step-1 if buffer_config else step
+        mux_inputs = []
         for other_side in SIDES:
             if other_side == side:
                 continue
-            inputs.append(in_[other_side].values[layer][track])
+            mux_inputs.append(
+                inputs[batch][other_side].values[layer][track][relevant_step])
         # TODO(rsetaluri): Abstract this part out. Right now it is a
         # hard coded hack.
-        inputs.append(core_outputs["data_out"] if layer == 16
-                      else core_outputs["bit_out"])
-        select = config[side].values[layer][track][0]
-        return inputs[select]
+        if layer == 16:
+            mux_inputs.append(core_outputs[batch]["data_out"][relevant_step])
+        else:
+            mux_inputs.append(core_outputs[batch]["bit_out"][relevant_step])
+        select = configs[batch][side].values[layer][track][0]
+        print(side, layer, track, batch, step, 'select is', select, 'returning', mux_inputs[select])
+        return mux_inputs[select]
 
     basic_tester = BasicTester(simple_sb_circuit, simple_sb_circuit.clk,
                                simple_sb_circuit.reset)
 
-    # configure
-    basic_tester.reset()
-    addr = 0
-    for side in SIDES:
-        for layer in LAYERS:
-            for track in range(NUM_TRACKS):
-                this_config = config[side].values[layer][track]
-                basic_tester.configure(addr, this_config[0])
-                basic_tester.config_read(addr)
-                basic_tester.expect(simple_sb_circuit.read_config_data,
-                                    this_config[0])
-                addr = addr + 1
-                basic_tester.configure(addr, this_config[1])
-                basic_tester.config_read(addr)
-                basic_tester.expect(simple_sb_circuit.read_config_data,
-                                    this_config[1])
-                addr = addr + 1
+    print("DEBUG A")
 
-    # poke inputs
-    for core_outputs, in_ in zip([core_outputs1, core_outputs2], [in1_, in2_]):
-        basic_tester.step(2)
-        for name, value in core_outputs.items():
-            port = simple_sb_circuit.interface.ports[name]
-            basic_tester.poke(port, value)
+    ''' TODO fix next line, should be NUM_BATCHES '''
+    for batch in range(1):
+        print("DEBUG B ", batch)
+        # configure
+        basic_tester.reset()
+        addr = 0
         for side in SIDES:
             for layer in LAYERS:
                 for track in range(NUM_TRACKS):
-                    side_ports = simple_sb_circuit.interface.ports[side].I
-                    in_port = getattr(side_ports, f"layer{layer}")[track]
-                    basic_tester.poke(in_port, in_[side].values[layer][track])
-    basic_tester.eval()
+                    this_config = configs[batch][side].values[layer][track]
+                    basic_tester.configure(addr, this_config[0])
+                    basic_tester.config_read(addr)
+                    basic_tester.expect(simple_sb_circuit.read_config_data,
+                                        this_config[0])
+                    addr = addr + 1
+                    basic_tester.configure(addr, this_config[1])
+                    basic_tester.config_read(addr)
+                    basic_tester.expect(simple_sb_circuit.read_config_data,
+                                        this_config[1])
+                    addr = addr + 1
 
-    # expect outputs
-    for side in SIDES:
-        for layer in LAYERS:
-            for track in range(NUM_TRACKS):
-                if config[side].values[layer][track][1] == 0:
-                    # not buffered
-                    expected = get_output(side, layer, track, in2_,
-                                          core_outputs2)
-                else:
-                    # buffered
-                    expected = get_output(side, layer, track, in1_,
-                                          core_outputs1)
+        for step in range(BATCH_LEN):
+            '''
+            if batch == 2:
+                print("DEBUG C", step)
+            '''
+            basic_tester.step(3)
+            # poke inputs
+            for name, values in core_outputs[batch].items():
+                value = values[step]
+                port = simple_sb_circuit.interface.ports[name]
+                basic_tester.poke(port, value)
+                print('Poking core with', value, 'step', step)
+            for side in SIDES:
+                for layer in LAYERS:
+                    for track in range(NUM_TRACKS):
+                        side_ports = simple_sb_circuit.interface.ports[side].I
+                        in_port = getattr(side_ports, f"layer{layer}")[track]
+                        value = inputs[batch][side].values[layer][track][step]
+                        basic_tester.poke(in_port, value)
+                        if layer == 16:
+                            print('Poking', side, layer, track, 'with', value, 'step', step)
+            basic_tester.eval()
 
-                side_ports = simple_sb_circuit.interface.ports[side].O
-                out_port = getattr(side_ports, f"layer{layer}")[track]
-                basic_tester.expect(out_port, expected)
+            if step == 0:
+                # Can't expect on step zero because we don't know what was
+                # sitting in register buffers
+                continue
+
+            # expect outputs
+            for side in SIDES:
+                for layer in LAYERS:
+                    for track in range(NUM_TRACKS):
+                        ''' TODO delete next statement '''
+                        if layer == 1:
+                            continue
+                        expected = get_output(side, layer, track, batch, step)
+                        side_ports = simple_sb_circuit.interface.ports[side].O
+                        out_port = getattr(side_ports, f"layer{layer}")[track]
+                        basic_tester.expect(out_port, expected)
 
     with tempfile.TemporaryDirectory() as tempdir:
         basic_tester.compile_and_run(target="verilator",
