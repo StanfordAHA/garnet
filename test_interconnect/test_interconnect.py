4c69ba8536420d72753e22fe46e677edb41e275a
Keyi Zhang <keyi@stanford.edu>
2018-12-27 14:48:05 -0800

    migrated the tests from simple_sb. seems broken and need to debug

--- test_interconnect/test_interconnect.py
+++ test_interconnect/test_interconnect.py
@@ -2,8 +2,36 @@ from interconnect.interconnect import Interconnect, InterconnectType
 from interconnect.interconnect import Tile, Switch, SwitchManager
 from interconnect.interconnect import SwitchBoxSide, SwitchBoxIO
 from interconnect.interconnect import SBConnectionType
-import pycyclone
-from pe_core.pe_core_magma import PECore
+from common.core import Core
+import magma
+from simple_sb.simple_sb_model import SimpleSBModel, SimpleSBTester, SideModel
+import fault.random
+import random
+import tempfile
+
+
+class DummyCore(Core):
+    def __init__(self):
+        super().__init__()
+
+        # PEP 8 rename
+        t_data = magma.Bits(16)
+
+        self.add_ports(
+            data_in=magma.In(t_data),
+            data_out=magma.Out(t_data),
+        )
+
+        self.wire(self.ports.data_in, self.ports.data_out)
+
+    def inputs(self):
+        return [self.ports.data_in]
+
+    def outputs(self):
+        return [self.ports.data_out]
+
+    def name(self):
+        return "DummyCore"
 
 
 def test_switch_manager():
@@ -85,7 +113,7 @@ def test_tile_core():
     assert str(conn_in[0]) == str(sb)
 
     # test the same thing with out
-    port_name = "res"
+    port_name = "data_out"
     conn = [SBConnectionType(SwitchBoxSide.NORTH, 0, SwitchBoxIO.OUT)]
     interconnect.set_core_connection_out(0, 0, port_name, conn)
     port = interconnect.get_port_node(0, 0, port_name)
@@ -95,23 +123,72 @@ def test_tile_core():
     assert str(nodes[0]) == str(sb)
 
 
-def set_up_interconnect():
+def set_up_interconnect(connect_all=False):
     width = 16
-    port_name = "data0"
+    num_track = 5
+    port_name = "data_in"
     interconnect = Interconnect(width, InterconnectType.Mesh)
     sb_manager = SwitchManager()
-    sb = sb_manager.create_disjoint_switch(0, 0, width, 5)
+    sb = sb_manager.create_disjoint_switch(0, 0, width, num_track)
     switch = Switch(sb)
     tile = Tile(0, 0, 1)
     interconnect.add_tile(tile, switch)
-    interconnect.set_core(0, 0, PECore())
+    interconnect.set_core(0, 0, DummyCore())
     # add connection to its switch boxes
-    conn = [SBConnectionType(SwitchBoxSide.NORTH, 1, SwitchBoxIO.OUT)]
+    if not connect_all:
+        conn = [SBConnectionType(SwitchBoxSide.NORTH, 1, SwitchBoxIO.OUT)]
+    else:
+        conn = []
+        for side in SwitchBoxSide:
+            for track in range(num_track):
+                conn.append((SBConnectionType(side, track, SwitchBoxIO.OUT)))
     interconnect.set_core_connection_in(0, 0, port_name, conn)
     return conn, interconnect, port_name
 
 
 def test_switch_sb():
-    conn, interconnect, port_name = set_up_interconnect()
-    switch = interconnect.get_switch(0, 0)
+    conn, interconnect, port_name = set_up_interconnect(True)
+    sbs, _ = interconnect.realize()
+    assert len(sbs) == 1
+    simple_sb = sbs[0]
 
+    # parameters
+    num_track = 5
+    layers = [16]
+    num_core_outputs = 1
+    sides = ("north", "west", "south", "east")
+
+    # copy the old sb regression test
+    # rename variables to meet PEP 8's standards
+    simple_sb_circuit = simple_sb.circuit()
+    tester = fault.Tester(simple_sb_circuit, simple_sb_circuit.clk)
+    model = SimpleSBModel(num_track, layers, num_core_outputs)
+    simple_sb_tester = SimpleSBTester(
+        num_track, layers, num_core_outputs, model, tester)
+
+    config = {side: SideModel(num_track, layers) for side in sides}
+    in_ = {side: SideModel(num_track, layers) for side in sides}
+
+    for side in sides:
+        for layer in layers:
+            for track in range(num_track):
+                config[side].values[layer][track] = random.randint(0, 3)
+                in_[side].values[layer][track] = fault.random.random_bv(layer)
+
+    core_outputs = {
+        "data_out": fault.random.random_bv(16)
+    }
+
+    simple_sb_tester.reset()
+    simple_sb_tester.configure(config)
+    simple_sb_tester(in_, core_outputs)
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        simple_sb_tester.tester.compile_and_run(target="verilator",
+                                                magma_output="coreir-verilog",
+                                                directory=tempdir,
+                                                flags=["-Wno-fatal"])
+
+
+if __name__ == "__main__":
+    test_switch_sb()
