e2e6df01ea354dc46f701142c1b6cf8f837772be
Taeyoung Kong <kongty0329@gmail.com>
2019-01-08 23:26:54 -0800

    Follow pep8 style

--- io1bit/io1bit_magma.py
+++ io1bit/io1bit_magma.py
@@ -1,12 +1,8 @@
 import magma
-import mantle
 from generator.configurable import ConfigurationType
 from common.core import Core
-from common.coreir_wrap import CoreirWrap
-from generator.const import Const
 from generator.from_magma import FromMagma
-from generator.from_verilog import FromVerilog
-from io1bit import io1bit_genesis2
+from io1bit import tristate_genesis2
 
 
 class IO1bit(Core):
@@ -36,13 +32,10 @@ class IO1bit(Core):
         self.wire(self.ports.pad, self.tristate.ports.pad)
         self.wire(self.ports.f2p_16, self.tristate.ports.f2p_16)
         self.wire(self.ports.f2p_1, self.tristate.ports.f2p_1)
-
-
-        #TODO: Change constant 2 to variables
+        # TODO: Change constant 2 to variables
         self.add_configs(
             S=2,
         )
-
         # IO do not have more than one 32bit configuration register.
         # TODO: Is magma circuit wire zero extended if the number of bits does
         # not match?
@@ -56,16 +49,13 @@ class IO1bit(Core):
             self.wire(self.ports.config.config_data, reg.ports.config_data)
             self.wire(reg.ports.config_en, self.ports.config.write[0])
 
-
-        self.wire(self.f2p_16, self.tristate.ports.f2p_16)
-        self.wire(self.f2p_1, self.tristate.ports.f2p_1)
-        self.wire(self.pad, self.tristate.ports.pad)
-
+        self.wire(self.ports.f2p_16, self.tristate.ports.f2p_16)
+        self.wire(self.ports.f2p_1, self.tristate.ports.f2p_1)
+        self.wire(self.ports.pad, self.tristate.ports.pad)
         self.io_bit = self.registers.S.ports.O[0]
         self.out_bus = self.registers.S.ports.O[1]
         self.wire(self.io_bit, self.tristate.ports.io_bit)
         self.wire(self.out_bus, self.tristate.ports.out_bus)
-
 
     def inputs(self):
         return None
--- io1bit/tristate_genesis2.py
+++ io1bit/tristate_genesis2.py
@@ -4,12 +4,13 @@ from common.generator_interface import GeneratorInterface
 interface = GeneratorInterface()
 
 tristate_wrapper = GenesisWrapper(interface, "tristate",
-                                ["io1bit/genesis/tristate.vp"],
-                                type_map=default_type_map)
+                                  ["io1bit/genesis/tristate.vp"],
+                                  type_map=default_type_map)
 
 """
-This program generates the verilog for the tristate in IO tile and parses it into a
-Magma circuit. The circuit declaration is printed at the end of the program.
+This program generates the verilog for the tristate in IO tile and parses it
+into a Magma circuit. The circuit declaration is printed at the end of the
+program.
 """
 if __name__ == "__main__":
     tristate_wrapper.main()
