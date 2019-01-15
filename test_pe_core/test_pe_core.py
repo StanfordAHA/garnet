f8f9fa0c9600635a8182d4e452ff1c84fd94af07
Lenny Truong <lenny@stanford.edu>
2019-01-15 15:42:08 -0800

    Use global var to prevent PE from being verilated more than once

--- test_pe_core/test_pe_core.py
+++ test_pe_core/test_pe_core.py
@@ -67,15 +67,20 @@ class PECoreTester(fault.Tester):
         self.step(2)
 
 
+_tester = None
+
+
 @pytest.fixture
 def tester(scope="module"):
-    # Generate the PE
-    pe_core = pe_core_genesis2.pe_core_wrapper.generator()()
-    _tester = PECoreTester(pe_core, pe_core.clk)
-    _tester.compile(target='verilator', directory="test_pe_core/build",
-                    include_directories=["../../genesis_verif"],
-                    magma_output="verilog",
-                    flags=['-Wno-fatal'])
+    global _tester
+    if _tester is None:
+        # Generate the PE
+        pe_core = pe_core_genesis2.pe_core_wrapper.generator()()
+        _tester = PECoreTester(pe_core, pe_core.clk)
+        _tester.compile(target='verilator', directory="test_pe_core/build",
+                        include_directories=["../../genesis_verif"],
+                        magma_output="verilog",
+                        flags=['-Wno-fatal'])
     return _tester
 
 
