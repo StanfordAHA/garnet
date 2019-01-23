8c2435c752f7dbd73831fa1ebb620196e389f27a
Alex Carsello <ajcars@stanford.edu>
2019-01-23 14:51:15 -0800

    Update gen_tile_grid script

--- tapeout_16/gen_tile_grid.py
+++ tapeout_16/gen_tile_grid.py
@@ -3,7 +3,6 @@ from tile.tile_magma import Tile
 from memory_core.memory_core_magma import MemCore
 from interconnect.interconnect_magma import Interconnect
 from column.column_magma import ColumnMeso
-from tile.tile_magma import Tile
 
 
 width = 2
