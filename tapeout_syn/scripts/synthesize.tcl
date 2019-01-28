bfb810a43b5b7e132ad26c6e048b2d2a58fa279e
Alex Carsello <ajcars@stanford.edu>
2019-01-28 15:36:45 -0800

    Add target libraries to link libs in synthesis script

--- tapeout_syn/scripts/synthesize.tcl
+++ tapeout_syn/scripts/synthesize.tcl
@@ -11,17 +11,10 @@ $::env(SAED32_PATH)/lib/stdcell_rvt/db_nldm/saed32rvt_ulvl_tt1p05vn40c_i1p05v.db
 set_app_var symbol_library $::env(SAED32_28_PDK)/sym_libs/snpsDefTechLib/tech.db
 
 set_app_var link_library [list \
+* $target_library \
 $::env(SAED32_PATH)/lib/sram/db_nldm/saed32sram_tt1p05vn40c.db]
 
-#create_mw_lib -technology $::env(SAED32_28_PDK)/techfiles/saed32nm_1p9m_mw.tf \
-#-mw_reference_library
-
-
 set design_name $::env(DESIGN)
-
-#set mw_design_library "./work_gemm"
-
-#sh rm -rf work_gemm
 
 sh mkdir -p ./$design_name
 
@@ -34,6 +27,12 @@ sh mkdir -p "./$design_name/WORK"
 sh mkdir -p $report_dir
 
 define_design_lib WORK -path "$report_dir/WORK"
+
+#create_mw_lib -technology $::env(SAED32_PATH)/tech/milkyway/saed32nm_1p9m_mw.tf \
+#-mw_reference_library [list \
+#$::env(SAED32_PATH)/lib/stdcell_rvt/milkyway/saed32nm_rvt_1p9m \
+#$::env(SAED32_PATH)/lib/sram/milkyway/SRAM32NM] \
+#$report_dir/$design_name -open
 
 
 #Read in the RTL
@@ -67,6 +66,7 @@ report_timing -in -net -transition_time  -capacitance  -significant_digits  4 -a
 write -format verilog -hierarchy -output $report_dir/$design_name.sv
 report_qor > $report_dir/$design_name.qor.rpt
 report_power > $report_dir/$design_name.power.top.rpt
+report_area > $report_dir/$design_name.area.rpt
 report_power -hierarchy -levels 3 >  $report_dir/$design_name.power.rpt
 write -format ddc -hier -out $report_dir/$design_name.ddc;
 
