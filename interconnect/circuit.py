3e1807708a61e93afd1477bd29c58884cc2f1523
Keyi Zhang <Kuree@users.noreply.github.com>
2019-01-30 18:10:41 -0800

    Interconnect Rename (#158)
    * improve module naming and remove hacks to uniquify modules
    
    * improve switchbox (SWITCH) naming

--- interconnect/circuit.py
+++ interconnect/circuit.py
@@ -241,8 +241,8 @@ class SB(InterconnectConfigurable):
             self.regs[reg_name] = reg_node, reg
 
     def name(self):
-        return create_name(str(self.switchbox)) + \
-            f"{self.switchbox.x}_{self.switchbox.y}"
+        return f"SB_ID{self.switchbox.id}_{self.switchbox.num_track}TRACKS_" \
+            f"B{self.switchbox.width}"
 
     def __connect_sbs(self):
         # the principle is that it only connects to the nodes within
@@ -569,7 +569,7 @@ class TileCircuit(generator.Generator):
         return config_names.index(mux_sel_name)
 
     def name(self):
-        return create_name(f"TILE {self.x} {self.y}")
+        return f"Tile_{self.core.name()}"
 
 
 class CoreInterface(InterconnectCore):
