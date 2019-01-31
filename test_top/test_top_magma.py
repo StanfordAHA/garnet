3f20767d21e084ab37313c6dcc598e70ed4ebeb7
Keyi Zhang <keyi@stanford.edu>
2019-01-30 23:15:05 -0800

    test new interconnect to top

--- test_top/test_top_magma.py
+++ test_top/test_top_magma.py
@@ -3,6 +3,6 @@ from top.top_magma import CGRA
 
 
 def test_top_magma():
-    cgra = CGRA(4, 4)
+    cgra = CGRA(2, 2)
     cgra_circ = cgra.circuit()
     magma.compile("cgra", cgra_circ, output="coreir-verilog")
