959d5d7bbaa7ad5fa412069b98d8edbb56aa1e05
Alex Carsello <alex.carsello@gmail.com>
2019-01-31 16:56:20 -0800

    Genesis/Verilog Fixes (#157)
    * Change global controller dependencies to .svp to make DC happy
    
    * Fix read_data error in test_lut
    
    * Ensure that the read_data output from test_lut is always defined

--- global_controller/global_controller_genesis2.py
+++ global_controller/global_controller_genesis2.py
@@ -19,10 +19,10 @@ type_map = {
 gc_wrapper = GenesisWrapper(interface,
                             "global_controller",
                             ["global_controller/genesis/global_controller.vp",
-                             "global_controller/genesis/jtag.vp",
-                             "global_controller/genesis/tap.vp",
-                             "global_controller/genesis/flop.vp",
-                             "global_controller/genesis/cfg_and_dbg.vp"],
+                             "global_controller/genesis/jtag.svp",
+                             "global_controller/genesis/tap.svp",
+                             "global_controller/genesis/flop.svp",
+                             "global_controller/genesis/cfg_and_dbg.svp"],
                             type_map=type_map)
 
 if __name__ == "__main__":
