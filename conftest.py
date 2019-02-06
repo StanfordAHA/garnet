5607acbd30d7a9fc2c7ba02044be3d1ce5a406ac
Raj Setaluri <raj.setaluri@gmail.com>
2019-02-06 15:15:17 -0800

    Remove old magma cache clear fns

--- conftest.py
+++ conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from magma.circuit import magma_clear_circuit_cache
 from magma import clear_cachedFunctions
 import magma.backend.coreir_ as coreir_
 
@@ -13,7 +12,6 @@ collect_ignore = [
 
 @pytest.fixture(autouse=True)
 def magma_test():
-    magma_clear_circuit_cache()
     clear_cachedFunctions()
     coreir_.__reset_context()
 
