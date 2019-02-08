f6a213101e52111881ab3db6c97ee799cf59bb72
Raj Setaluri <raj.setaluri@gmail.com>
2019-02-07 16:49:06 -0800

    Refactor mux tests
    Factored out common mux test logic and now tests both MuxWrapper and
    AOIMux.t

--- test_common/test_mux.py
+++ test_common/test_mux.py
@@ -5,21 +5,22 @@ from bit_vector import BitVector
 import fault
 import fault.random
 import magma
+from common.mux_interface import MuxInterface
 from common.mux_wrapper import MuxWrapper
+from common.aoi_mux import AOIMux
 
 
-@pytest.mark.parametrize('height,width', [(randint(2, 10), randint(1, 32))
-                                          for _ in range(5)])
-def test_mux_wrapper(height, width):
+def mux_interface_helper(height, width, mux_class):
     """
-    Test that the mux wrapper circuit works as expected. Specifically, we
-    initialize a mux with random height and width, and check that the output is
-    as expected for select in range [0, height).
+    Test that a class @mux_class which implements the MuxInterface class works
+    as expected. Specifically, we initialize a mux with random height and width,
+    and check that the output is as expected for select in range [0, height).
 
     Note that we do not check the behavior with sel >= height, because this is
     undefined behavior.
     """
-    mux = MuxWrapper(height, width)
+    mux = mux_class(height, width)
+    assert isinstance(mux, MuxInterface)
     assert mux.height == height
     assert mux.width == width
     assert mux.name() == f"MuxWrapper_{height}_{width}"
@@ -37,3 +38,15 @@ def test_mux_wrapper(height, width):
         tester.compile_and_run(directory=tempdir,
                                magma_output="coreir-verilog",
                                flags=["-Wno-fatal"])
+
+
+@pytest.mark.parametrize('height,width', [(randint(2, 10), randint(1, 32))
+                                          for _ in range(5)])
+def test_mux_wrapper(height, width):
+    mux_interface_helper(height, width, MuxWrapper)
+
+
+@pytest.mark.parametrize('height,width', [(randint(2, 10), randint(1, 32))
+                                          for _ in range(5)])
+def test_aoi_mux(height, width):
+    mux_interface_helper(height, width, AOIMux)
