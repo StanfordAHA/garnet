0500c9d0d26d2b177f3dfa68d37562ed3bd181c6
Lenny Truong <lenny@stanford.edu>
2019-02-08 13:29:08 -0800

    update expected instance name output

--- test_generator/test_instance_name.py
+++ test_generator/test_instance_name.py
@@ -7,7 +7,7 @@ from common.util import check_files_equal
 def test_instance_name_tile():
     core = DummyCore()
     circuit = core.circuit()
-    assert str(circuit.instances) == '[inst0 = MuxWithDefaultWrapper_2_32_8_0(), dummy_1 = ConfigRegister_32_8_32_0(name="dummy_1"), dummy_2 = ConfigRegister_32_8_32_1(name="dummy_2")]'  # noqa
+    assert str(circuit.instances) == '[MuxWithDefaultWrapper_2_32_8_0_inst0 = MuxWithDefaultWrapper_2_32_8_0(), dummy_1 = ConfigRegister_32_8_32_0(name="dummy_1"), dummy_2 = ConfigRegister_32_8_32_1(name="dummy_2")]'  # noqa
     with tempfile.TemporaryDirectory() as tempdir:
         m.compile(f"{tempdir}/core", circuit)
         check_files_equal(f"{tempdir}/core.v",
