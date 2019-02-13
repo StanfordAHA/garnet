3f2ba48b8554ed0e36df3d5e16676646b7edd7d7
Taeyoung Kong <kongty@stanford.edu>
2019-02-12 19:09:27 -0800

    Create io1bit pad wrapper and magma. (#141)
    * Add io1bit core with tristate genesis2 wrapper
    
    * Rename io_tristate to tristate
    
    * Add test for tristate genesis2 wrapper
    
    * Follow pep8 style
    
    * Add functional model for io1bit
    
    * Make magma same as genesis2
    
    * Add input and output port to iopad
    
    * Implement io_core for 1bit/16bit
    
    * Add io_core 16bit magma and functional model
    
    * Limit the number of test cases to 100

---
+++ io_core/io16bit_magma.py
@@ -0,0 +1,27 @@
+import magma
+from common.core import Core
+
+
+class IO16bit(Core):
+    def __init__(self):
+        super().__init__()
+        TBit = magma.Bits(16)
+
+        self.add_ports(
+            glb2io=magma.In(TBit),
+            io2glb=magma.Out(TBit),
+            io2f=magma.Out(TBit),
+            f2io=magma.In(TBit)
+        )
+
+        self.wire(self.ports.glb2io, self.ports.io2f)
+        self.wire(self.ports.f2io, self.ports.io2glb)
+
+    def inputs(self):
+        return [self.ports.glb2io, self.ports.f2io]
+
+    def outputs(self):
+        return [self.ports.io2glb, self.ports.io2f]
+
+    def name(self):
+        return "io16bit"
