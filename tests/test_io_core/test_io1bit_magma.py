53714bf6edb66f5ded8cb3b4edf502c2da9d64a0
Keyi <keyi@stanford.edu>
2019-02-28 11:30:18 -0800

    apply patch from #184

--- tests/test_io_core/test_io1bit_magma.py
+++ tests/test_io_core/test_io1bit_magma.py
@@ -13,10 +13,10 @@ def test_regression():
     for _glb2io in [BitVector(x, 1) for x in range(2**1)]:
         for _f2io in [BitVector(x, 1) for x in range(2**1)]:
             tester.poke(io1bit_circuit.glb2io, _glb2io)
-            tester.poke(io1bit_circuit.f2io, _f2io)
+            tester.poke(io1bit_circuit.f2io_1, _f2io)
             tester.eval()
             tester.expect(io1bit_circuit.io2glb, _f2io)
-            tester.expect(io1bit_circuit.io2f, _glb2io)
+            tester.expect(io1bit_circuit.io2f_1, _glb2io)
 
     with tempfile.TemporaryDirectory() as tempdir:
         tester.compile_and_run(target="verilator",
