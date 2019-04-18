26bb8f6da596a9b7304ed5f3a13ed0afec1fee16
Taeyoung Kong <kongty0329@gmail.com>
2019-04-18 14:27:36 -0700

    Change name for ports

--- global_buffer/genesis/global_buffer.sv
+++ global_buffer/genesis/global_buffer.sv
@@ -16,14 +16,14 @@ module global_buffer #(
 (
     input                           clk,
     input                           reset,
+    
+    input                           soc_port_wr_en,
+    input  [GLB_ADDR_WIDTH-1:0]     soc_port_wr_addr,
+    input  [BANK_DATA_WIDTH-1:0]    soc_port_wr_data,
 
-    input                           host_wr_en [NUM_BANKS-1:0],
-    input  [BANK_DATA_WIDTH-1:0]    host_wr_data [NUM_BANKS-1:0],
-    input  [BANK_ADDR_WIDTH-1:0]    host_wr_addr [NUM_BANKS-1:0],
-
-    input                           host_rd_en [NUM_BANKS-1:0],
-    output [BANK_DATA_WIDTH-1:0]    host_rd_data [NUM_BANKS-1:0],
-    input  [BANK_ADDR_WIDTH-1:0]    host_rd_addr [NUM_BANKS-1:0],
+    input                           soc_port_rd_en,
+    input  [GLB_ADDR_WIDTH-1:0]     soc_port_rd_addr,
+    output [BANK_DATA_WIDTH-1:0]    soc_port_rd_data,
 
     input                           cgra_wr_en [NUM_BANKS-1:0],
     input  [BANK_ADDR_WIDTH-1:0]    cgra_wr_addr [NUM_BANKS-1:0],
@@ -45,21 +45,18 @@ module global_buffer #(
 // local parameter declaration
 //============================================================================//
 localparam integer NUM_BANKS_WIDTH = $clog2(NUM_BANKS);
+localparam integer GLB_ADDR_WIDTH = NUM_BANKS_WIDTH + BANK_ADDR_WIDTH;
 
 //============================================================================//
 // configuration signal declaration
 //============================================================================//
-reg [BANK_ADDR_WIDTH-1:0]      top_config_addr_bank [NUM_BANKS-1:0];
-reg                            top_config_en_bank [NUM_BANKS-1:0];
+reg [BANK_ADDR_WIDTH-1:0]       top_config_addr_bank;
+reg                             top_config_en_bank [NUM_BANKS-1:0];
 wire [CONFIG_DATA_WIDTH-1:0]    top_config_rd_data_bank [NUM_BANKS-1:0];
 
-integer j;
-always_comb begin
-    for (j=0; j<NUM_BANKS; j=j+1) begin
-        top_config_addr_bank[j] = top_config_addr[BANK_ADDR_WIDTH-1:0];
-    end
-end
+assign top_config_addr_bank = top_config_addr[BANK_ADDR_WIDTH-1:0];
 
+integer j;
 always_comb begin
     for (j=0; j<NUM_BANKS; j=j+1) begin
         top_config_en_bank[j] = top_config_en_glb && (j == top_config_addr[BANK_ADDR_WIDTH +: NUM_BANKS_WIDTH]);
@@ -72,6 +69,45 @@ always_comb begin
     	top_config_rd_data = top_config_rd_data | top_config_rd_data_bank[j];
     end
 end
+
+//============================================================================//
+// host-bank interface
+//============================================================================//
+host_bank_interface #(
+    .NUM_BANKS(NUM_BANKS),
+    .BANK_ADDR_WIDTH(BANK_ADDR_WIDTH),
+    .BANK_DATA_WIDTH(BANK_DATA_WIDTH)
+) inst_host_bank_interface (
+    .clk(clk),
+    .reset(reset),
+
+    .soc_port_wr_en(soc_port_wr_en),
+    .soc_port_wr_data(soc_port_wr_data),
+    .soc_port_wr_addr(soc_port_wr_addr),
+
+    .soc_port_rd_en(soc_port_rd_en),
+    .soc_port_rd_addr(soc_port_rd_addr),
+    .soc_port_rd_data(soc_port_rd_data),
+
+    .host_wr_en(host_wr_en),
+    .host_wr_data(host_wr_data),
+    .host_wr_addr(host_wr_addr),
+
+    .host_rd_en(host_rd_en),
+    .host_rd_addr(host_rd_addr),
+    .host_rd_data(host_rd_data)
+);
+
+//============================================================================//
+// internal wire declaration
+//============================================================================//
+wire                        host_wr_en [NUM_BANKS-1:0];
+wire [BANK_DATA_WIDTH-1:0]  host_wr_data [NUM_BANKS-1:0];
+wire [BANK_ADDR_WIDTH-1:0]  host_wr_addr [NUM_BANKS-1:0];
+
+wire                        host_rd_en [NUM_BANKS-1:0];
+wire [BANK_DATA_WIDTH-1:0]  host_rd_data [NUM_BANKS-1:0];
+wire [BANK_ADDR_WIDTH-1:0]  host_rd_addr [NUM_BANKS-1:0];
 
 //============================================================================//
 // bank generation
@@ -106,7 +142,7 @@ for (i=0; i<NUM_BANKS; i=i+1) begin: generate_bank
         .config_en(top_config_en_bank[i]),
         .config_wr(top_config_wr),
         .config_rd(top_config_rd),
-        .config_addr(top_config_addr_bank[i]),
+        .config_addr(top_config_addr_bank),
         .config_wr_data(top_config_wr_data),
         .config_rd_data(top_config_rd_data_bank[i])
     );
