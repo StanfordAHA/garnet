23ac0b8734de090da4294e8f1cadd9dc94cf779b
Taeyoung Kong <kongty0329@gmail.com>
2019-04-17 23:14:43 -0700

    Fix pep8 style

--- tests/test_global_buffer/test_global_buffer_verilator_sim.py
+++ tests/test_global_buffer/test_global_buffer_verilator_sim.py
@@ -7,13 +7,13 @@ def verilator_available():
     return shutil.which("verilator") is not None
 
 
-def run_verilator(parameters:dict, top, test_driver):
+def run_verilator(parameters: dict, top, test_driver):
     if not verilator_available():
         raise Exception("Verilator not available")  # pragma: nocover
     param_strs = [f"-G{k}='{str(v)}'"
                   for k, v in parameters.items()]
     preprocessor_strs = [f"{k} '{str(v)}'"
-                  for k, v in parameters.items()]
+                         for k, v in parameters.items()]
     verilator_cmd = f"verilator {top}.sv -cc -I{top}/genesis -O3 -Wno-fatal \
             -exe {test_driver} --trace -CFLAGS \"-std=c++11\""
     verilator_cmd = verilator_cmd + " " + " ".join(param_strs)
@@ -35,7 +35,6 @@ def run_verilator_regression(params, top, test_driver):
 
 @pytest.mark.skipif(not verilator_available(),
                     reason="verilator not available")
-
 @pytest.mark.parametrize('params', [
     {
         "NUM_BANKS": 32,
