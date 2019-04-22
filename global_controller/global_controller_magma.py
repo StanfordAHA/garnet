74cf65ae4d83b80d49f7a7bf0b291244492a5246
Taeyoung Kong <kongty0329@gmail.com>
2019-04-22 01:16:11 -0700

    Modify global controller

--- global_controller/global_controller_magma.py
+++ global_controller/global_controller_magma.py
@@ -8,24 +8,45 @@ from . import global_controller_genesis2
 
 
 class GlobalController(Generator):
-    def __init__(self, addr_width, data_width):
-        super().__init__()
+    def __init__(self, addr_width, data_width, soc_addr_width):
         super().__init__()
 
         self.addr_width = addr_width
         self.data_width = data_width
+        self.soc_addr_width = soc_addr_width
         self.config_type = ConfigurationType(self.addr_width, self.data_width)
+        self.top_config_type = ConfigurationType(self.soc_addr_width,
+                                                 self.data_width)
+        self.glb_config_type = ConfigurationType(self.addr_width,
+                                                 self.data_width)
 
         self.add_ports(
             jtag=JTAGType,
             config=magma.Out(self.config_type),
             read_data_in=magma.In(magma.Bits[self.data_width]),
+            top_config=magma.Out(self.top_config_type),
+            top_read_data_in=magma.In(magma.Bits[self.data_width]),
+            glb_config=magma.Out(self.glb_config_type),
+            glb_read_data_in=magma.In(magma.Bits[self.data_width]),
             clk_in=magma.In(magma.Clock),
             reset_in=magma.In(magma.AsyncReset),
             clk_out=magma.Out(magma.Clock),
             reset_out=magma.Out(magma.AsyncReset),
+            cgra_start_pulse=magma.Out(magma.Bits[1]),
+            cgra_done_pulse=magma.In(magma.Bits[1]),
+            config_start_pulse=magma.Out(magma.Bits[1]),
+            config_done_pulse=magma.In(magma.Bits[1]),
+            #TODO: Make SoC control class just like JTAGType
+            soc_wr_en=magma.In(magma.Bits[1]),
+            soc_wr_addr=magma.In(magma.Bits[self.soc_addr_width]),
+            soc_wr_data=magma.In(magma.Bits[self.data_width]),
+            soc_rd_en=magma.In(magma.Bits[1]),
+            soc_rd_addr=magma.In(magma.Bits[self.soc_addr_width]),
+            soc_rd_data=magma.Out(magma.Bits[self.data_width]),
+            soc_interrupt=magma.Out(magma.Bits[1]),
             # TODO: make number of stall domains a param
-            stall=magma.Out(magma.Bits[4])
+            stall=magma.Out(magma.Bits[4]),
+            glb_stall=magma.Out(magma.Bits[1])
         )
 
         wrapper = global_controller_genesis2.gc_wrapper
@@ -52,5 +73,35 @@ class GlobalController(Generator):
 
         self.wire(self.ports.read_data_in, self.underlying.ports.config_data_in)
 
+        self.wire(self.ports.cgra_start_pulse, self.underlying.ports.cgra_start_pulse)
+        self.wire(self.ports.cgra_done_pulse, self.underlying.ports.cgra_done_pulse)
+        self.wire(self.ports.config_start_pulse, self.underlying.ports.config_start_pulse)
+        self.wire(self.ports.config_done_pulse, self.underlying.ports.config_done_pulse)
+
+        self.wire(self.underlying.ports.top_config_addr_out,
+                  self.ports.top_config.config_addr)
+        self.wire(self.underlying.ports.top_config_data_out,
+                  self.ports.top_config.config_data)
+        self.wire(self.underlying.ports.top_read, self.ports.top_config.read[0])
+        self.wire(self.underlying.ports.top_write, self.ports.top_config.write[0])
+        self.wire(self.ports.top_read_data_in, self.underlying.ports.top_config_data_in)
+
+        self.wire(self.underlying.ports.glb_config_addr_out,
+                  self.ports.glb_config.config_addr)
+        self.wire(self.underlying.ports.glb_config_data_out,
+                  self.ports.glb_config.config_data)
+        self.wire(self.underlying.ports.glb_read, self.ports.glb_config.read[0])
+        self.wire(self.underlying.ports.glb_write, self.ports.glb_config.write[0])
+        self.wire(self.ports.glb_read_data_in, self.underlying.ports.glb_config_data_in)
+
+        self.wire(self.ports.soc_wr_en, self.underlying.ports.soc_control_wr_en)
+        self.wire(self.ports.soc_wr_addr, self.underlying.ports.soc_control_wr_addr)
+        self.wire(self.ports.soc_wr_data, self.underlying.ports.soc_control_wr_data)
+        self.wire(self.ports.soc_rd_en, self.underlying.ports.soc_control_rd_en)
+        self.wire(self.ports.soc_rd_addr, self.underlying.ports.soc_control_rd_addr)
+        self.wire(self.ports.soc_rd_data, self.underlying.ports.soc_control_rd_data)
+        self.wire(self.ports.soc_interrupt, self.underlying.ports.soc_interrupt)
+
+
     def name(self):
-        return f"GlobalController_{self.addr_width}_{self.data_width}"
+        return f"GlobalController_{self.addr_width}_{self.data_width}_{self.soc_addr_width}"
