83eeae0d2069163145bf1c7527ed350b0b39af91
Alex Carsello <alex.carsello@gmail.com>
2019-04-22 12:05:35 -0700

    Start top-level PNR

--- tapeout_16/scripts/place_and_route.tcl
+++ tapeout_16/scripts/place_and_route.tcl
@@ -1,7 +1,9 @@
 ### Tool Settings
-setDesignMode -process 16
+eval_legacy {setDesignMode -process 16}
 
 set_interactive_constraint_modes [all_constraint_modes -active]
+
+eval_legacy {
 setDontUse IOA21D0BWP16P90 true
 setDontUse IOA21D0BWP16P90LVT true
 setDontUse IOA21D0BWP16P90ULVT true
@@ -25,28 +27,27 @@ setNanoRouteMode -routeConcurrentMinimizeViaCountEffort high
 setNanoRouteMode -droutePostRouteSwapVia false
 setNanoRouteMode -routeExpUseAutoVia true
 setNanoRouteMode -drouteExpAdvancedMarFix true
-
+}
 
 ### Place Design
 ##set_multi_cpu_usage -local_cpu 8
 set_interactive_constraint_modes [all_constraint_modes]
 set_multicycle_path -thr */C -setup 3
 set_multicycle_path -thr */C -hold 2
-set_false_path -thr global_controller/*
+set_false_path -thr [get_cells -hier *GlobalController*]
 set_multicycle_path -thr */PAD -setup 2
 set_multicycle_path -thr */PAD -hold 1
 set_dont_touch true [get_nets -of */PAD]
-set_multicycle_path -to [get_pins -hier *gin*] -setup 10
-set_multicycle_path -to [get_pins -hier *gin*] -hold 9
-set_multicycle_path -from [get_pins *_io_bit_reg/CP] -through [get_pins */C] -setup 10
-set_multicycle_path -from [get_pins *_io_bit_reg/CP] -through [get_pins */C] -hold 9
-set_multicycle_path -from [get_pins */clk_in] -setup 2
-set_multicycle_path -from [get_pins */clk_in] -hold 1
-set_multicycle_path -from [get_pins */clk_in] -to [get_pins -hier *gin*] -setup 10
-set_multicycle_path -from [get_pins */clk_in] -to [get_pins -hier *gin*] -hold 9
+#set_multicycle_path -to [get_pins -hier *gin*] -setup 10
+#set_multicycle_path -to [get_pins -hier *gin*] -hold 9
+#set_multicycle_path -from [get_pins *_io_bit_reg/CP] -through [get_pins */C] -setup 10
+#set_multicycle_path -from [get_pins *_io_bit_reg/CP] -through [get_pins */C] -hold 9
+#set_multicycle_path -from [get_pins */clk_in] -setup 2
+#set_multicycle_path -from [get_pins */clk_in] -hold 1
+#set_multicycle_path -from [get_pins */clk_in] -to [get_pins -hier *gin*] -setup 10
+#set_multicycle_path -from [get_pins */clk_in] -to [get_pins -hier *gin*] -hold 9
 set_interactive_constraint_modes {}
 
-eval_novus {create_fence -area {400 4585 450 4720} -name global_controller}
 ##place_connected -attractor [get_property [get_cells -filter "ref_name=~pe*||ref_name=~mem*"] full_name] -level 1
 ##
 set_interactive_constraint_mode [all_constraint_modes]
@@ -57,14 +58,14 @@ set_timing_derate -clock -late 1.03  -delay_corner _default_delay_corner_
 set_timing_derate -data -late 1.05  -delay_corner _default_delay_corner_
 set_interactive_constraint_mode {}
 
-eval_novus {set_db [get_nets rte] .skip_routing true}
-place_opt_design -place
+set_db [get_nets rte] .skip_routing true
+eval_legacy {place_opt_design -place}
 
-eval_novus {write_db place.db -def -sdc -verilog}
+write_db place.db -def -sdc -verilog
 
-place_opt_design -opt
+eval_legacy {place_opt_design -opt}
 
-eval_novus {write_db place_opt.db -def -sdc -verilog}
+write_db place_opt.db -def -sdc -verilog
 setTieHiLoMode -maxDistance 20 -maxFanout 16
 addTieHiLo -cell "TIEHBWP16P90 TIELBWP16P90"
   
@@ -80,7 +81,7 @@ eval_novus {write_db cts.db -def -sdc -verilog}
 
 #### Route Design
 routeDesign
-eval_novus {write_db routed.db -def -sdc -verilog}
+write_db routed.db -def -sdc -verilog
 
 
 ## Chip Finishing
