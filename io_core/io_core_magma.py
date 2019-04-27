f9197e2c5f4e405d3ba02d57eccd629fb8d1a48e
Taeyoung Kong <kongty0329@gmail.com>
2019-04-27 13:53:15 -0700

    Finish implementing iocore

--- io_core/io_core_magma.py
+++ io_core/io_core_magma.py
@@ -7,28 +7,28 @@ class IOCore(Core):
         super().__init__()
 
         self.add_ports(
-            glb2io_16bit=magma.In(magma.Bits[16]),
-            glb2io_1bit=magma.In(magma.Bit),
-            io2glb_16bit=magma.Out(magma.Bits[16]),
-            io2glb_1bit=magma.Out(magma.Bit),
-            f2io_16bit=magma.In(magma.Bits[16]),
-            f2io_1bit=magma.In(magma.Bit),
-            io2f_16bit=magma.Out(magma.Bits[16]),
-            io2f_1bit=magma.Out(magma.Bit),
+            glb2io_16=magma.In(magma.Bits[16]),
+            glb2io_1=magma.In(magma.Bits[1]),
+            io2glb_16=magma.Out(magma.Bits[16]),
+            io2glb_1=magma.Out(magma.Bits[1]),
+            f2io_16=magma.In(magma.Bits[16]),
+            f2io_1=magma.In(magma.Bits[1]),
+            io2f_16=magma.Out(magma.Bits[16]),
+            io2f_1=magma.Out(magma.Bits[1]),
         )
 
-        self.wire(self.ports.glb2io_16bit, self.ports.io2f_16bit)
-        self.wire(self.ports.glb2io_1bit, self.ports.io2f_1bit)
-        self.wire(self.ports.f2io_16bit, self.ports.io2glb_16bit)
-        self.wire(self.ports.f2io_1bit, self.ports.io2glb_1bit)
+        self.wire(self.ports.glb2io_16, self.ports.io2f_16)
+        self.wire(self.ports.glb2io_1, self.ports.io2f_1)
+        self.wire(self.ports.f2io_16, self.ports.io2glb_16)
+        self.wire(self.ports.f2io_1, self.ports.io2glb_1)
 
     def inputs(self):
-        return [self.ports.glb2io_16bit, self.ports.glb2io_1bit,
-                self.ports.f2io_16bit, self.ports.f2io_1bit]
+        return [self.ports.glb2io_16, self.ports.glb2io_1,
+                self.ports.f2io_16, self.ports.f2io_1]
 
     def outputs(self):
-        return [self.ports.io2glb_16bit, self.ports.io2glb_1bit,
-                self.ports.io2f_16bit, self.ports.io2f_1bit]
+        return [self.ports.io2glb_16, self.ports.io2glb_1,
+                self.ports.io2f_16, self.ports.io2f_1]
 
     def name(self):
         return "io_core"
