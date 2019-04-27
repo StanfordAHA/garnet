4eef0ee40dffd1756efd8db18d1ab1ff2bece72f
Taeyoung Kong <kongty0329@gmail.com>
2019-04-27 14:30:09 -0700

    Fix error

--- tests/test_io_core/test_io_core_magma.py
+++ tests/test_io_core/test_io_core_magma.py
@@ -10,21 +10,21 @@ def test_regression():
     io_core_circuit = io_core.circuit()
     tester = Tester(io_core_circuit)
 
-    for _glb2io_16bit, _f2io_16bit in \
+    for _glb2io_16, _f2io_16 in \
             [(random_bv(16), random_bv(16)) for _ in range(100)]:
-        tester.poke(io_core_circuit.glb2io_16bit, _glb2io_16bit)
-        tester.poke(io_core_circuit.f2io_16bit, _f2io_16bit)
+        tester.poke(io_core_circuit.glb2io_16, _glb2io_16)
+        tester.poke(io_core_circuit.f2io_16, _f2io_16)
         tester.eval()
-        tester.expect(io_core_circuit.io2glb_16bit, _f2io_16bit)
-        tester.expect(io_core_circuit.io2f_16bit, _glb2io_16bit)
+        tester.expect(io_core_circuit.io2glb_16, _f2io_16)
+        tester.expect(io_core_circuit.io2f_16, _glb2io_16)
 
-    for _glb2io_1bit, _f2io_1bit in \
+    for _glb2io_1, _f2io_1 in \
             [(random_bv(1), random_bv(1)) for _ in range(100)]:
-        tester.poke(io_core_circuit.glb2io_1bit, _glb2io_1bit)
-        tester.poke(io_core_circuit.f2io_1bit, _f2io_1bit)
+        tester.poke(io_core_circuit.glb2io_1, _glb2io_1)
+        tester.poke(io_core_circuit.f2io_1, _f2io_1)
         tester.eval()
-        tester.expect(io_core_circuit.io2glb_1bit, _f2io_1bit)
-        tester.expect(io_core_circuit.io2f_1bit, _glb2io_1bit)
+        tester.expect(io_core_circuit.io2glb_1, _f2io_1)
+        tester.expect(io_core_circuit.io2f_1, _glb2io_1)
 
     with tempfile.TemporaryDirectory() as tempdir:
         tester.compile_and_run(target="verilator",
