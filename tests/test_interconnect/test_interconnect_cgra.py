ab1165f04a9c7f2ab5000a0077c69a62c282ee62
Taeyoung Kong <kongty0329@gmail.com>
2019-04-27 18:13:47 -0700

    Change 1bit iocore tag to small i

--- tests/test_interconnect/test_interconnect_cgra.py
+++ tests/test_interconnect/test_interconnect_cgra.py
@@ -109,11 +109,11 @@ def test_interconnect_line_buffer(cw_files, add_pd):
         "e0": [("I0", "io2f_16"), ("m0", "data_in"), ("p0", "data0")],
         "e1": [("m0", "data_out"), ("p0", "data1")],
         "e3": [("p0", "alu_res"), ("I1", "f2io_16")],
-        "e4": [("I0", "io2f_1"), ("m0", "wen_in")]
+        "e4": [("i0", "io2f_1"), ("m0", "wen_in")]
     }
     bus = {"e0": 16, "e1": 16, "e3": 16, "e4": 1}
 
-    placement, routing = pnr(interconnect, (netlist, bus), cwd="tmp")
+    placement, routing = pnr(interconnect, (netlist, bus))
     config_data = interconnect.get_route_bitstream(routing)
 
     # in this case we configure m0 as line buffer mode
@@ -143,7 +143,7 @@ def test_interconnect_line_buffer(cw_files, add_pd):
     src = f"glb2io_16_X{src_x}_Y{src_y}"
     dst_x, dst_y = placement["I1"]
     dst = f"io2glb_16_X{dst_x}_Y{dst_y}"
-    wen_x, wen_y = placement["I0"]
+    wen_x, wen_y = placement["i0"]
     wen = f"glb2io_1_X{wen_x}_Y{wen_y}"
 
     tester.poke(circuit.interface[wen], 1)
@@ -180,7 +180,7 @@ def test_interconnect_sram(cw_files, add_pd):
     netlist = {
         "e0": [("I0", "io2f_16"), ("m0", "addr_in")],
         "e1": [("m0", "data_out"), ("I1", "f2io_16")],
-        "e2": [("I0", "io2f_1"), ("m0", "ren_in")]
+        "e2": [("i0", "io2f_1"), ("m0", "ren_in")]
     }
     bus = {"e0": 16, "e1": 16, "e2": 1}
 
@@ -221,7 +221,7 @@ def test_interconnect_sram(cw_files, add_pd):
     src = f"glb2io_16_X{addr_x}_Y{addr_y}"
     dst_x, dst_y = placement["I1"]
     dst = f"io2glb_16_X{dst_x}_Y{dst_y}"
-    ren_x, ren_y = placement["I0"]
+    ren_x, ren_y = placement["i0"]
     ren = f"glb2io_1_X{ren_x}_Y{ren_y}"
 
     tester.step(2)
