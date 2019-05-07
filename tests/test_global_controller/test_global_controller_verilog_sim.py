eaad14287dfbf22f4b72311c762dd97be289c026
Taeyoung Kong <kongty0329@gmail.com>
2019-05-07 14:43:45 -0700

    Change localparam to parameter to make irun happy

--- tests/test_global_controller/test_global_controller_verilog_sim.py
+++ tests/test_global_controller/test_global_controller_verilog_sim.py
@@ -23,8 +23,9 @@ def run_verilog_regression(params):
                                    "tests/test_global_controller/genesis/"
                                    "cfg_ifc.svp",
                                    "global_controller/genesis/"
-                                   "global_controller.vp",
+                                   "global_controller.svp",
                                    "global_controller/genesis/jtag.svp",
+                                   "global_controller/genesis/axi_ctrl.svp",
                                    "global_controller/genesis/tap.svp",
                                    "global_controller/genesis/cfg_and_dbg.svp",
                                    "global_controller/genesis/flop.svp"],
