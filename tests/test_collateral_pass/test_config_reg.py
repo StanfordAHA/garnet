abb78b778a85c16a3b400afb30a1df62f2bdd05e
Keyi Zhang <keyi@stanford.edu>
2019-05-10 18:40:38 -0700

    add register pass

---
+++ tests/test_collateral_pass/test_config_reg.py
@@ -0,0 +1,17 @@
+import tempfile
+import os
+import json
+from cgra.util import create_cgra, IOSide, GlobalSignalWiring
+from collateral_pass.config_register import get_interconnect_regs
+
+
+def test_config_register():
+    sides = IOSide.North
+    ic = create_cgra(2, 2, sides, global_signal_wiring=GlobalSignalWiring.Meso)
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        tempdir = "temp"
+        filename = os.path.join(tempdir, "config.json")
+        result = get_interconnect_regs(ic)
+        with open(filename, "w+") as f:
+            json.dump(result, f)
