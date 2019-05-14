75cbb2d0e4024da34bbfda773e5d7507b4715d9d
mbstrange2 <mstrange@stanford.edu>
2019-05-13 22:27:38 -0700

    auto db function

--- tests/test_memory_core/test_memory_core.py
+++ tests/test_memory_core/test_memory_core.py
@@ -61,8 +61,9 @@ class MemoryCoreTester(ResetTester, BasicTester):
         self.poke(self._circuit.wen_in, 0)
 
     def write_and_observe(self, data, addr=0):
-        self.functional_model.write(addr, data)
         self.functional_model.read(addr)
+        self.functional_model.write(addr, data)
+        self.eval()
         # \_
         self.poke(self._circuit.clk_in, 0)
         self.poke(self._circuit.wen_in, 1)
@@ -72,8 +73,18 @@ class MemoryCoreTester(ResetTester, BasicTester):
 
         # _/
         self.poke(self._circuit.clk_in, 1)
+
+    def observe(self, addr=0):
+        self.functional_model.read(addr)
         self.eval()
-        self.poke(self._circuit.wen_in, 0)
+        # \_
+        self.poke(self._circuit.clk_in, 0)
+        self.poke(self._circuit.wen_in, 1)
+        self.poke(self._circuit.addr_in, addr)
+        self.eval()
+
+        # _/
+        self.poke(self._circuit.clk_in, 1)
 
     def read(self, addr=0):
         # \_
@@ -313,6 +324,7 @@ def test_db_arbitrary_addr():
     tester.poke(Mem.clk_in, 0)
     tester.poke(Mem.switch_db, 0)
     tester.functional_model.switch()
+    tester.functional_model.data_out = fault.AnyValue
 
     for i in range(100):
         randaddr = random.randint(0,99)
@@ -346,21 +358,27 @@ def test_db_arbitrary_addr():
 
 def test_db_auto():
     db_auto(0, 1, 2, 3, 4, 5,
-             1, 3, 9, 0, 0, 0,
-             3, 3, 3, 1, 1, 1,
-             3)
+            1, 3, 9, 0, 0, 0,
+            3, 3, 3, 1, 1, 1,
+            3, 27)
 
+
+def test_db_auto2():
+    db_auto(0, 1, 2, 3, 4, 5,
+            1, 3, 1, 3, 9, 0,
+            2, 2, 2, 2, 3, 1,
+            5, 27)
 
 def db_auto(order0, order1, order2, order3, order4, order5,
              stride0, stride1, stride2, stride3, stride4, stride5,
              size0, size1, size2, size3, size4, size5,
-             dimensionality,
+             dimensionality, input_size,
              start_address=0,
              read_mode=0, manual_switch=0, arbitrary_addr=0):
 
     [Mem, tester] = make_memory_core()
     max_count = size0 * size1 * size2 * size3 * size4 * size5
-    depth = max_count
+    depth = input_size
     ranges = [size0, size1, size2, size3, size4, size5]
     strides = [stride0, stride1, stride2, stride3, stride4, stride5]
     tester.functional_model.config_db(depth, ranges, strides,
@@ -399,12 +417,31 @@ def db_auto(order0, order1, order2, order3, order4, order5,
     for addr, data, feat in config_data:
         tester.configure(addr, data, feat)
 
-    for i in range(max_count):
+    for i in range(depth):
         tester.write(i + 1)
-    for i in range(max_count):
+    for i in range(depth):
         tester.write_and_observe(2*i+1)
-    for i in range(max_count):
+    for i in range(depth, max_count):
+        tester.observe(2*i+1)
+    for i in range(depth):
         tester.write_and_observe(3*i+1)
+
+    tempdir = "tests/test_memory_core/build"
+    for genesis_verilog in glob.glob("genesis_verif/*.*"):
+        shutil.copy(genesis_verilog, tempdir)
+    tester.compile_and_run(directory=tempdir,
+                           magma_output="coreir-verilog",
+                           target="verilator",
+                           flags=["-Wno-fatal", "--trace"])
+
+    #with tempfile.TemporaryDirectory() as tempdir:
+    #    for genesis_verilog in glob.glob("genesis_verif/*.*"):
+    #        shutil.copy(genesis_verilog, tempdir)
+    #    tester.compile_and_run(directory=tempdir,
+    #                           magma_output="coreir-verilog",
+    #                           target="verilator",
+    #                           flags=["-Wno-fatal"])
+
 
 def db_basic(order0, order1, order2, order3, order4, order5,
              stride0, stride1, stride2, stride3, stride4, stride5,
