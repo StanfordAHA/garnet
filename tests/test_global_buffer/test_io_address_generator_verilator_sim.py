fc4a30eaddad3875eb89aebf8f5eb8b75c9681fd
Taeyoung Kong <kongty0329@gmail.com>
2019-05-15 16:50:23 -0700

    Change test name

---
+++ tests/test_global_buffer/test_io_address_generator_verilator_sim.py
@@ -0,0 +1,31 @@
+import glob
+import pytest
+import os
+from gemstone.common.run_genesis import run_genesis
+from verilator_sim import run_verilator, verilator_available
+
+
+def run_verilator_regression(top, test_driver, genesis_params={},
+                             verilog_params={}):
+    # Genesis version of global_controller
+    run_genesis(f"{top}", ["global_buffer/genesis/io_address_generator.svp"],
+                genesis_params)
+    files = glob.glob('genesis_verif/io_address_generator.sv')
+    return run_verilator(verilog_params, top, files, test_driver)
+
+
+@pytest.mark.skipif(not verilator_available(),
+                    reason="verilator not available")
+@pytest.mark.parametrize('verilog_params', [
+    {
+        "BANK_DATA_WIDTH": 64,
+        "GLB_ADDR_WIDTH": 22,
+        "CGRA_DATA_WIDTH": 16
+    }
+])
+def test_address_generator_verilator(verilog_params):
+    test_driver = f"tests/test_global_buffer/verilator/"\
+                  f"test_io_address_generator.cpp"
+    res = run_verilator_regression("io_address_generator", test_driver,
+                                   {}, verilog_params)
+    assert res == 1
