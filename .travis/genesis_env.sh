2e69937d9a3da26cd504f80c09863b85aa7f8fd6
Keyi Zhang <keyi@stanford.edu>
2019-05-16 10:55:11 -0700

    use the old way to deal with env variables

--- .travis/genesis_env.sh
+++
@@ -1,4 +0,0 @@
-# add Genesis to the path
-export GENESIS_HOME=/Genesis2/Genesis2Tools
-export PATH=$GENESIS_HOME/bin:$GENESIS_HOME/gui/bin:$PATH
-export PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$PERL5LIB
