72732e426d256e73fdd7a75487fc9e6dd30d0c71
Keyi Zhang <keyi@stanford.edu>
2019-05-16 10:55:11 -0700

    use the old way to deal with env variables

---
+++ .travis/run.sh
@@ -0,0 +1,14 @@
+# add Genesis to the path
+export GENESIS_HOME=/Genesis2/Genesis2Tools
+export PATH=$GENESIS_HOME/bin:$GENESIS_HOME/gui/bin:$PATH
+export PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$PERL5LIB
+
+cd /garnet/
+
+pytest --codestyle          \
+    --cov global_controller \
+    --cov io_core           \
+    --cov memory_core       \
+    --ignore=filecmp.py     \
+    --ignore=Genesis2/      \
+    -v --cov-report term-missing tests
