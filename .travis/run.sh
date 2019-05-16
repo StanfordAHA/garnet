eedb264e851dbda4c76ba2679d669c3b7f5863ed
Keyi Zhang <keyi@stanford.edu>
2019-05-16 11:01:22 -0700

    force color

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,6 +3,9 @@ export GENESIS_HOME=/Genesis2/Genesis2Tools
 export PATH=$GENESIS_HOME/bin:$GENESIS_HOME/gui/bin:$PATH
 export PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$PERL5LIB
 
+# force color
+export PYTEST_ADDOPTS="--color=yes"
+
 cd /garnet/
 
 pytest --codestyle          \
