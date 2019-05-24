72e555698d003a20fd8cc41b3562b2468ef9aa84
mbstrange2 <mstrange@stanford.edu>
2019-05-24 15:12:24 -0700

    refactor into sv

--- memory_core/memory_core_genesis2.py
+++ memory_core/memory_core_genesis2.py
@@ -21,11 +21,11 @@ interface = GeneratorInterface()\
 
 memory_core_wrapper = GenesisWrapper(
     interface, "memory_core",
-    ["memory_core/genesis_new/linebuffer_control.vp",
-     "memory_core/genesis_new/fifo_control.vp",
-     "memory_core/genesis_new/doublebuffer_control.vp",
+    ["memory_core/genesis_new/linebuffer_control.svp",
+     "memory_core/genesis_new/fifo_control.svp",
+     "memory_core/genesis_new/doublebuffer_control.svp",
      "memory_core/genesis_new/mem.vp",
-     "memory_core/genesis_new/sram_control.vp",
+     "memory_core/genesis_new/sram_control.svp",
      "memory_core/genesis_new/memory_core.vp",
      "memory_core/genesis_new/sram_stub.vp"],
     type_map={"clk": m.In(m.Clock),
