249e49aef21aca5a3fd407b34c802c15d56de858
Alex Carsello <alex.carsello@gmail.com>
2019-05-27 15:36:27 -0700

    update top-level synth script

--- tapeout_16/scripts/synthesize_top.tcl
+++ tapeout_16/scripts/synthesize_top.tcl
@@ -3,9 +3,14 @@ set_attr super_thread_servers [list localhost localhost localhost localhost loca
 set_attr library [list \
 /tsmc16/TSMCHOME/digital/Front_End/timing_power_noise/NLDM/tcbn16ffcllbwp16p90_100a/tcbn16ffcllbwp16p90ssgnp0p72vm40c.lib \
 /tsmc16/TSMCHOME/digital/Front_End/timing_power_noise/NLDM/tphn16ffcllgv18e_110c/tphn16ffcllgv18essgnp0p72v1p62vm40c.lib \
-../Tile_MemCore/pnr.lib \
-../Tile_PECore/pnr.lib \
+/sim/ajcars/mc/ts1n16ffcllsblvtc512x16m8s_130a/NLDM/ts1n16ffcllsblvtc512x16m8s_130a_ssgnp0p72vm40c.lib \
+/sim/ajcars/mc/ts1n16ffcllsblvtc256x32m4sw_130a/NLDM/ts1n16ffcllsblvtc256x32m4sw_130a_ssgnp0p72vm40c.lib \
+/sim/ajcars/mc/ts1n16ffcllsblvtc256x32m8sw_130a/NLDM/ts1n16ffcllsblvtc256x32m8sw_130a_ssgnp0p72vm40c.lib \
+/sim/ajcars/mc/ts1n16ffcllsblvtc2048x32m8sw_130a/NLDM/ts1n16ffcllsblvtc2048x32m8sw_130a_ssgnp0p72vm40c.lib \
+/sim/ajcars/mc/ts1n16ffcllsblvtc2048x64m8sw_130a/NLDM/ts1n16ffcllsblvtc2048x64m8sw_130a_ssgnp0p72vm40c.lib
 ] 
+#../Tile_MemCore/pnr.lib \
+#../Tile_PECore/pnr.lib \
 
 set_attr lef_library [list \
 /tsmc16/download/TECH16FFC/N16FF_PRTF_Cad_1.2a/PR_tech/Cadence/LefHeader/Standard/VHV/N16_Encounter_9M_2Xa1Xd3Xe2Z_UTRDL_9T_PODE_1.2a.tlef \
@@ -15,6 +20,11 @@ set_attr lef_library [list \
 /tsmc16/TSMCHOME/digital/Back_End/lef/tpbn16v_090a/fc/fc_lf_bu/APRDL/lef/tpbn16v.lef \
 /tsmc16/TSMCHOME/digital/Back_End/lef/tphn16ffcllgv18e_110e/mt_1/9m/9M_2XA1XD_H_3XE_VHV_2Z/lef/tphn16ffcllgv18e_9lm.lef \
 /tsmc16/pdk/2016.09.15_MOSIS/FFC/T-N16-CL-DR-032/N16_DTCD_library_kit_20160111/N16_DTCD_library_kit_20160111/lef/topMxyMxe_M9/N16_DTCD_v1d0a.lef \
+/sim/ajcars/mc/ts1n16ffcllsblvtc512x16m8s_130a/LEF/ts1n16ffcllsblvtc512x16m8s_130a_m4xdh.lef \
+/sim/ajcars/mc/ts1n16ffcllsblvtc256x32m4sw_130a/LEF/ts1n16ffcllsblvtc256x32m4sw_130a_m4xdh.lef \
+/sim/ajcars/mc/ts1n16ffcllsblvtc256x32m8sw_130a/LEF/ts1n16ffcllsblvtc256x32m8sw_130a_m4xdh.lef \
+/sim/ajcars/mc/ts1n16ffcllsblvtc2048x32m8sw_130a/LEF/ts1n16ffcllsblvtc2048x32m8sw_130a_m4xdh.lef \
+/sim/ajcars/mc/ts1n16ffcllsblvtc2048x64m8sw_130a/LEF/ts1n16ffcllsblvtc2048x64m8sw_130a_m4xdh.lef \
 /tsmc16/pdk/2016.09.15_MOSIS/FFC/T-N16-CL-DR-032/N16_ICOVL_library_kit_FF+_20150528/N16_ICOVL_library_kit_FF+_20150528/lef/topMxMxaMxc_M9/N16_ICOVL_v1d0a.lef]
 
 set_attr qrc_tech_file [list /tsmc16/download/TECH16FFC/cworst/Tech/cworst_CCworst_T/qrcTechFile]
@@ -46,20 +56,21 @@ source -verbose "../../scripts/constraints_${base_design}.tcl"
 redirect check_design.rpt {check_design -all}
 redirect timing_lint.rpt {report_timing -lint -verbose}
 
-set_attr ungroup_ok false /designs/$::env(DESIGN)/instances_hier/Garnet_inst0/instances_hier/GlobalController_32_32_inst0
-set_attr boundary_opto false [get_attr subdesign /designs/$::env(DESIGN)/instances_hier/Garnet_inst0/instances_hier/GlobalController_32_32_inst0]
+set_attr ungroup_ok false [get_cells -hier GlobalController*]
+set_attr ungroup_ok false [get_cells -hier GlobalBuffer*]
+set_attr boundary_opto false [get_attr subdesign [get_cells -hier GlobalController*]] 
 #set_attr preserve true [get_nets -of [get_pins -of [get_cells -hier -filter "ref_name=~*PDB*"]  -filter "name=~AIO"]]
 #set_attr preserve size_delete_ok [get_cells my_or*]
 #set_attr preserve size_delete_ok [get_cells my_buf*]
 
 remove_assigns_without_optimization
+ungroup -flatten -all
 
 syn_gen 
 write_db -to_file gen.db
 syn_map 
 write_db -to_file map.db
 
-ungroup -flatten -all
 write_snapshot -directory results_syn -tag final
 write_design -innovus -basename results_syn/syn_out
 exit
