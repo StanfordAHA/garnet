44d2b8a0a46347eb3f1ee28d709b017521f03dec
Alex Carsello <alex.carsello@gmail.com>
2019-05-28 15:28:54 -0700

    make mem core clock slower

---
+++ tapeout_16/gen_rtl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ../
+if [ -d "genesis_verif/" ]; then
+  rm -rf genesis_verif
+fi
+
+python garnet.py --width 32 --height 16 -v --no_sram_stub
+
+cp garnet.v genesis_verif/garnet.sv
+
+cp -r genesis_verif/ tapeout_16/
+
+cd tapeout_16/
