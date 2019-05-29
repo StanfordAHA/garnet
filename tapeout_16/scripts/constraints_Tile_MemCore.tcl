4035440431e20d9df584311bfecf53ff3e8a30e6
Alex Carsello <alex.carsello@gmail.com>
2019-05-29 13:06:32 -0700

    Read memcore config regs from file for constraints

--- tapeout_16/scripts/constraints_Tile_MemCore.tcl
+++ tapeout_16/scripts/constraints_Tile_MemCore.tcl
@@ -6,7 +6,14 @@ set_output_delay -min 0 -clock clk [all_outputs]
 
 set_input_transition 0.2 [all_inputs]
 
-set_false_path -from [get_ports {config* tile_id* reset}] -to [all_outputs]
+set_false_path -setup -from [get_ports {config* tile_id* reset}] -to [all_outputs]
+set config_file [open "/sim/ajcars/garnet/mem_synth.txt"]
+set lines [split [read $config_file] "\n"]
+# set false paths from config regs
+foreach line $lines {
+  set_false_path -through [get_pins -hier $line/O*] 
+  echo "setting false path through config reg $line"
+}
 
 set_attribute ungroup_ok false [get_cells *]
 set_attribute ungroup_ok true [get_cells -hier MemCore*]
