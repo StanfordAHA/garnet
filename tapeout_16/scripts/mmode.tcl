6e621fe1ad182fc7847f82b8a666af9535fdf530
Alex Carsello <alex.carsello@gmail.com>
2019-05-31 23:56:24 -0700

    Add sram timing lib to mmode

--- tapeout_16/scripts/mmode.tcl
+++ tapeout_16/scripts/mmode.tcl
@@ -1,6 +1,7 @@
 create_library_set -name default_library_set -timing [list \
 /tsmc16/TSMCHOME/digital/Front_End/timing_power_noise/NLDM/tcbn16ffcllbwp16p90_100a/tcbn16ffcllbwp16p90ssgnp0p72vm40c.lib \
 /tsmc16/TSMCHOME/digital/Front_End/timing_power_noise/NLDM/tphn16ffcllgv18e_110c/tphn16ffcllgv18essgnp0p72v1p62vm40c.lib \
+/sim/ajcars/mc/ts1n16ffcllsblvtc2048x64m8sw_130a/NLDM/ts1n16ffcllsblvtc2048x64m8sw_130a_ssgnp0p72vm40c.lib \
 ../Tile_PE/pnr.lib \
 ../Tile_MemCore/pnr.lib \
 ]
