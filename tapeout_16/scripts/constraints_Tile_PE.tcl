42014a09eecef30baa284eb088e59fd44ddc533d
Alex Carsello <alex.carsello@gmail.com>
2019-06-06 00:20:04 -0700

    tile constraint updates

--- tapeout_16/scripts/constraints_Tile_PE.tcl
+++ tapeout_16/scripts/constraints_Tile_PE.tcl
@@ -1,4 +1,4 @@
-create_clock -name clk -period 2.2 [get_ports clk*]
+create_clock -name clk -period 2.3 [get_ports clk*]
 set_input_delay -max 0.2 -clock clk [all_inputs]
 set_output_delay -max 0.2 -clock clk [all_outputs]
 set_input_delay -min 0 -clock clk [all_inputs]
@@ -9,4 +9,4 @@ set_input_transition 0.2 [all_inputs]
 set_false_path -through [get_ports {config* tile_id* reset}]  -to [all_outputs]
 
 set_attribute ungroup_ok false [get_cells *]
-set_attribute ungroup_ok true [get_cells -hier PE_inst0]
+set_attribute ungroup_ok true [get_cells -hier PE*]
