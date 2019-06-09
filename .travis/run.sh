7806bcbe0ddcd04096ee7eb69e3523365da162ae
Keyi Zhang <keyi@stanford.edu>
2019-06-08 18:40:11 -0700

    I have no idea why travis is failing

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,5 +1,7 @@
 # add Genesis to the path
-pip install genesis2
+export GENESIS_HOME=/Genesis2/Genesis2Tools
+export PATH=$GENESIS_HOME/bin:$GENESIS_HOME/gui/bin:$PATH
+export PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$PERL5LIB
 
 # force color
 export PYTEST_ADDOPTS="--color=yes"
