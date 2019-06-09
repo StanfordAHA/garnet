cb16c689bda531b21a45dfa444b3a9ea237619b8
Keyi Zhang <keyi@stanford.edu>
2019-06-08 18:25:58 -0700

    try a different genesis

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,7 +1,5 @@
 # add Genesis to the path
-export GENESIS_HOME=/Genesis2/Genesis2Tools
-export PATH=$GENESIS_HOME/bin:$GENESIS_HOME/gui/bin:$PATH
-export PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$PERL5LIB
+pip install genesis2
 
 # force color
 export PYTEST_ADDOPTS="--color=yes"
