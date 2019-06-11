155156e9e5dcf1d25a5c4e10a3ec05d98ed4098a
Keyi Zhang <keyi@stanford.edu>
2019-06-11 14:08:17 -0700

    add initial implementation for peak core reg

--- peak_core/peak_core.py
+++ peak_core/peak_core.py
@@ -1,4 +1,3 @@
-import inspect
 import math
 import hwtypes
 import magma
@@ -6,6 +5,7 @@ import peak
 from gemstone.common.core import ConfigurableCore, PnRTag
 from gemstone.common.configurable import ConfigurationType
 from gemstone.generator.from_magma import FromMagma
+from gemstone.generator.generator import Generator
 
 
 class HashableDict(dict):
@@ -69,9 +69,52 @@ class _PeakWrapper(metaclass=_PeakWrapperMeta):
         return self.__asm(instr)
 
 
+class PassThroughReg(Generator):
+    def __init__(self, name: str):
+        super().__init__(name=name)
+        self.add_ports(
+            config_addr=magma.In(magma.Bits[8]),
+            config_addr_out=magma.Out(magma.Bits[8]),
+            config_data=magma.In(magma.Bits[32]),
+            config_data_out=magma.Out(magma.Bits[32]),
+            config_en=magma.In(magma.Bit),
+            config_en_out=magma.Out(magma.Bit),
+            reset=magma.In(magma.AsyncReset),
+            reset_out=magma.Out(magma.AsyncReset),
+            O=magma.Out(magma.Out(magma.Bits[32])),
+            O_in=magma.In(magma.In(magma.Bits[32]))
+        )
+        self.width = 32
+
+        self.wire(self.ports.config_addr, self.ports.config_addr_out)
+        self.wire(self.ports.config_data, self.ports.config_data_out)
+        self.wire(self.ports.config_en, self.ports.config_en_out)
+        self.wire(self.ports.reset, self.ports.reset_out)
+        self.wire(self.ports.O_in, self.ports.O)
+
+        self.addr = 0
+
+    def set_addr(self, addr):
+        self.addr = addr
+
+    def set_global_addr(self, global_addr):
+        pass
+
+    def set_addr_width(self, addr_width):
+        pass
+
+    def set_data_width(self, data_width):
+        pass
+
+    def name(self):
+        return f"PassThroughRegister"
+
+
 class PeakCore(ConfigurableCore):
     def __init__(self, peak_generator):
         super().__init__(8, 32)
+        ignored_ports = {"clk_en", "reset", "config_addr", "config_data",
+                         "config_en"}
 
         self.wrapper = _PeakWrapper(peak_generator)
 
@@ -83,6 +126,8 @@ class PeakCore(ConfigurableCore):
         outputs = self.wrapper.outputs()
         for ports, dir_ in ((inputs, magma.In), (outputs, magma.Out),):
             for i, (name, typ) in enumerate(ports.items()):
+                if name in ignored_ports:
+                    continue
                 magma_type = _convert_type(typ)
                 self.add_port(name, dir_(magma_type))
                 my_port = self.ports[name]
@@ -112,6 +157,17 @@ class PeakCore(ConfigurableCore):
             self.reg_width[name] = len_
             self.wire(self.registers[name].ports.O[:len_],
                       self.peak_circuit.ports[instr_name][lb:ub])
+
+        # connecting the wires
+        #self.wire(self.ports.reset, self.peak_circuit.ports["reset"])
+        # we need to fake registers
+        self.registers["PE_operand16"] = PassThroughReg("PE_operand16")
+        self.registers["PE_operand1"] = PassThroughReg("PE_operand1")
+
+        reg16 = self.registers["PE_operand16"]
+        reg1 = self.registers["PE_operand1"]
+        self.wire(self.ports.config.config_data, reg16.ports.O_in)
+        self.wire(self.ports.config.config_data, reg1.ports.O_in)
 
         self._setup_config()
 
