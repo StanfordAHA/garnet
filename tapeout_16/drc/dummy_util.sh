5c90d246c3bb7efe7388e248f2709e8f22e39073
Taeyoung Kong <kongty0329@gmail.com>
2019-06-18 22:01:14 -0700

    Fix dummy_util output file name

--- tapeout_16/drc/dummy_util.sh
+++ tapeout_16/drc/dummy_util.sh
@@ -17,19 +17,20 @@ cd dummy_util
 cat <<EOF > dummy_util.drc
 LAYOUT PATH "$gds"
 LAYOUT PRIMARY "$toplevel"
-DRC RESULTS DATABASE "dummy_util.gds" GDSII _dummy_only
-DRC SUMMARY REPORT "dummy_util_summary.txt"
+DRC RESULTS DATABASE "${toplevel}_dummy_util.gds" GDSII _dummy_only
+DRC SUMMARY REPORT "${toplevel}_dummy_util_summary.txt"
+
 
 INCLUDE "$d/Dummy_FEOL_CalibreYE_16nmFFP.14c.ip"
 INCLUDE "$d/Dummy_BEOL_CalibreYE_16nmFFP.14c.ip"
 EOF
 
 echo "running Calibre dummy utility"
-calibre -drc -hier -turbo -hyper -64 dummy_util.drc | tee dummy_util.log | grep -P '^ERROR|^DRC RuleCheck'
+calibre -drc -hier -turbo -hyper -64 dummy_util.drc | tee ${toplevel}_dummy_util.log | grep -P '^ERROR|^DRC RuleCheck'
 
 echo "Merging dummy metal gds with original gds"
-mgds=$(basename $1 .gds)_with_dummy.gds
-calibredrv -a layout filemerge -in "$gds" -in dummy_util.gds -out "../$mgds" -map_cell "${toplevel}_dummy_only" "$toplevel" -topcell "$toplevel"
+mgds=${toplevel}_$(basename ( .gds)_with_dummy.gds
+calibredrv -a layout filemerge -in "$gds" -in ${toplevel}_dummy_util.gds -out "../$mgds" -map_cell "${toplevel}_dummy_only" "$toplevel" -topcell "$toplevel")
 
 cd ..
 echo -e "\nFinal gds is $mgds"
