baa2c1e7ad5a41fd4d24bf5d80f731d2c38c2f2b
Alex Carsello <alex.carsello@gmail.com>
2019-06-19 20:29:26 -0700

    set_drive constraints on global signals to force buffer insertion

--- tapeout_16/scripts/constraints_Tile_PE.tcl
+++ tapeout_16/scripts/constraints_Tile_PE.tcl
@@ -1,11 +1,11 @@
 create_clock -name clk -period 2.3 [get_ports clk]
 create_generated_clock -name clk_out -source [get_ports clk] -multiply_by 1 [get_ports clk_out]
-set_input_delay -max 0.2 -clock clk [all_inputs]
-set_output_delay -max 0.2 -clock clk [all_outputs]
+set_input_delay -max 0 -clock clk [all_inputs]
+set_output_delay -max 0 -clock clk [all_outputs]
 set_input_delay -min 0 -clock clk [all_inputs]
 set_output_delay -min 0 -clock clk [all_outputs]
 
-set_input_transition 0.2 [all_inputs]
+set_input_transition 0.05 [all_inputs]
 
 set_false_path -through [get_ports tile_id*]  -to [all_outputs]
 set_false_path -through [get_pins -hier *inst_*]
@@ -17,13 +17,7 @@ set_attribute ungroup_ok true [get_cells -hier PE*]
 set_load 0.02 [get_ports config_out*]
 set_load 0.02 [get_ports stall_out*]
 set_load 0.02 [get_ports reset_out*]
-set_load 0.01 [get_ports clk_out*]
+set_load 0.02 [get_ports clk_out*]
 
-# Manage skew of clk/global signals down column of tiles
-set_max_delay -from [get_ports clk] -to [get_ports clk_out*] 0.05
-set_min_delay -from [get_ports {config_config* config_read* config_write*}] -to [get_ports config_out*] 0.1
-set_min_delay -from [get_ports stall] -to [get_ports stall_out*] 0.1
-set_min_delay -from [get_ports reset] -to [get_ports reset_out*] 0.1
-set_max_delay -from [get_ports {config_config* config_read* config_write*}] -to [get_ports config_out*] 0.12
-set_max_delay -from [get_ports stall] -to [get_ports stall_out*] 0.12
-set_max_delay -from [get_ports reset] -to [get_ports reset_out*] 0.12
+
+set_drive 30 [get_ports {config_config* config_read* config_write* stall reset read_config_data_in*}]
