7b1131f11668ff3661d60bbcea69e2ae9bf6c90a
Alex Carsello <alex.carsello@gmail.com>
2019-06-19 20:26:21 -0700

    Add constraints to manage clk/global signal skew down tile column

---
+++ tapeout_16/scripts/global_signal_tile_constraints.tcl
@@ -0,0 +1,14 @@
+#Make sure that global signal pass throughs toggle after clk_out to avoid hold time problems
+set global_signals [get_ports {config_out* reset_out* stall_out* read_config_data}]
+# Add in the max input and output delays
+set_max_delay -combinational_from_to -from [get_ports {config_config* config_read* config_write*}] -to [get_ports config_out*] 0.2
+set_max_delay -combinational_from_to -from [get_ports stall] -to [get_ports stall_out*] 0.2
+set_max_delay -combinational_from_to -from [get_ports reset] -to [get_ports reset_out*] 0.2
+
+# Very tightly constrain clk skew so we don't have differences between tile types
+set_max_delay -combinational_from_to -from [get_ports clk] -to [get_ports clk_out] 0.13
+set_min_delay -combinational_from_to -from [get_ports clk] -to [get_ports clk_out] 0.13
+foreach_in_collection signal $global_signals {
+  echo "signal: [get_property $signal hierarchical_name]"
+  set_data_check -from [get_ports clk_out] -to $signal -hold 0.05
+}
