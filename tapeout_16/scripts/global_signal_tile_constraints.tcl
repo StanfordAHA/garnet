417ded1058c38857f3d208b36258430fe81f8726
Alex Carsello <alex.carsello@gmail.com>
2019-06-22 14:50:41 -0700

    Tighten max delay on clock pass through

--- tapeout_16/scripts/global_signal_tile_constraints.tcl
+++ tapeout_16/scripts/global_signal_tile_constraints.tcl
@@ -7,8 +7,8 @@ set_max_delay -combinational_from_to -from [get_ports stall] -to [get_ports stal
 set_max_delay -combinational_from_to -from [get_ports reset] -to [get_ports reset_out*] 0.2
 
 # constrain clk skew so we don't have differences between tile types
-set_max_delay -combinational_from_to -from [get_ports clk] -to [get_ports clk_out] 0.17
-set_min_delay -combinational_from_to -from [get_ports clk] -to [get_ports clk_out] 0.155
+set_max_delay -combinational_from_to -from [get_ports clk] -to [get_ports clk_out] 0.16
+set_min_delay -combinational_from_to -from [get_ports clk] -to [get_ports clk_out] 0.15
 foreach_in_collection signal $global_signals {
   echo "signal: [get_property $signal hierarchical_name]"
   set_data_check -clock clk -from [get_ports clk_out] -to $signal -hold 2.35
