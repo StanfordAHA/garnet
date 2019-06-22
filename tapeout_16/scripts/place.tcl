f07ac4be2ad65c1616d004a6b4e47c4d97ad3ea1
Alex Carsello <alex.carsello@gmail.com>
2019-06-21 18:17:13 -0700

    More intermediate checkpoints

--- tapeout_16/scripts/place.tcl
+++ tapeout_16/scripts/place.tcl
@@ -1,20 +1,45 @@
 source ../../scripts/tool_settings.tcl
 #Constraints
 set_interactive_constraint_modes [all_constraint_modes]
-#set_multicycle_path -thr */C -setup 10
-#set_multicycle_path -thr */C -hold 9
-#set_multicycle_path -thr */PAD -setup 2
-#set_multicycle_path -thr */PAD -hold 1
 set_dont_touch true [get_nets -of */PAD]
 
-set_interactive_constraint_mode [all_constraint_modes]
+set_multicycle_path -setup 2 -through [get_nets -hier *Tile*config*]
+set_multicycle_path -hold 1 -through [get_nets -hier *Tile*config*]
+set_multicycle_path -setup 2 -through [get_nets -hier *Tile*reset*]
+set_multicycle_path -hold 1 -through [get_nets -hier *Tile*reset*]
+set_multicycle_path -setup 2 -through [get_nets -hier *Tile*stall*]
+set_multicycle_path -hold 1 -through [get_nets -hier *Tile*stall*]
+
+set ports_out(tlx_fwd_clock)       [list \
+  pad_tlx_fwd_toggle_o \
+  pad_tlx_fwd_tvalid_p_o \
+  pad_tlx_fwd_tdata_lo_p_o[*] \
+  pad_tlx_fwd_tdata_hi_p_o[*] \
+  pad_tlx_fwd_tvalid_t_o \
+  pad_tlx_fwd_tdata_t_o[*] \
+]
+
+set_multicycle_path -setup 1 -to [get_ports $ports_out(tlx_fwd_clock)]
+set_multicycle_path -hold 0 -to [get_ports $ports_out(tlx_fwd_clock)]
+
+set_load 0.1 [all_outputs]
+
+
 set_timing_derate -clock -early 0.97 -delay_corner _default_delay_corner_ 
 set_timing_derate -clock -late 1.03  -delay_corner _default_delay_corner_
 set_timing_derate -data -late 1.05  -delay_corner _default_delay_corner_
+
+set_case_analysis 1 [get_pins -hier *DS0]
+set_case_analysis 1 [get_pins -hier *DS1]
+set_case_analysis 1 [get_pins -hier *DS2]
+
 set_interactive_constraint_mode {}
 
 ### Place Design
 place_opt_design -place
+
+saveDesign init_place.enc -def -tcon -verilog
+
 place_opt_design -opt
 
 setTieHiLoMode -maxDistance 20 -maxFanout 16
--- tapeout_16/scripts/route.tcl
+++ tapeout_16/scripts/route.tcl
@@ -2,4 +2,5 @@ source ../../scripts/tool_settings.tcl
 #### Route Design
 routeDesign
 setAnalysisMode -aocv true
+saveDesign init_route.enc -def -tcon -verilog
 optDesign -postRoute -hold -setup
