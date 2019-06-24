cb29e72db7cc4f6610bd3fd51019fe02ebb33385
Alex Carsello <alex.carsello@gmail.com>
2019-06-23 22:07:05 -0700

    Remove redundant M8 blockage

--- tapeout_16/scripts/floorplan.tcl
+++ tapeout_16/scripts/floorplan.tcl
@@ -293,7 +293,6 @@ gen_power
 
 # M7-M9 power straps
 # vertical
-create_route_blockage -area $core_to_edge 4050 [expr 5000 - $core_to_edge] [expr 5000 - $core_to_edge] -layers M8 -pg_nets
 foreach layer {M7 M8 M9} {
     add_stripes \
         -nets {VDD VSS} \
