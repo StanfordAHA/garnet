2b1ff251a2ddd0a308a354cb078d91aff1851029
Alex Carsello <alex.carsello@gmail.com>
2019-06-25 17:25:44 -0700

    merge in lvt and ulvt gds files

--- tapeout_16/scripts/stream_func.tcl
+++ tapeout_16/scripts/stream_func.tcl
@@ -1,6 +1,8 @@
 proc gen_gds {gds_name} {
   set gds_files [list \
   /tsmc16/TSMCHOME/digital/Back_End/gds/tcbn16ffcllbwp16p90_100a/tcbn16ffcllbwp16p90.gds \
+  /tsmc16/TSMCHOME/digital/Back_End/gds/tcbn16ffcllbwp16p90lvt_100a/tcbn16ffcllbwp16p90lvt.gds \
+  /tsmc16/TSMCHOME/digital/Back_End/gds/tcbn16ffcllbwp16p90ulvt_100a/tcbn16ffcllbwp16p90ulvt.gds \
   /tsmc16/TSMCHOME/digital/Back_End/gds/tpbn16v_090a/fc/fc_lf_bu/APRDL/tpbn16v.gds \
   /tsmc16/TSMCHOME/digital/Back_End/gds/tphn16ffcllgv18e_110e/mt/9m/9M_2XA1XD_H_3XE_VHV_2Z/tphn16ffcllgv18e.gds \
   /tsmc16/pdk/2016.09.15_MOSIS/FFC/T-N16-CL-DR-032/N16_DTCD_library_kit_20160111/N16_DTCD_library_kit_20160111/gds/N16FF_Phantom_v1d0_1a_20140707.gds \
@@ -12,6 +14,7 @@ proc gen_gds {gds_name} {
   ../Tile_MemCore/pnr.gds \
   /sim/ajcars/aha-arm-soc-june-2019/components/butterphy/butterphy_top.gds
   ]
+
   
   
   streamOut $gds_name -uniquifyCellNames -mode ALL -merge ${gds_files} -mapFile /tsmc16/pdk/latest/pnr/innovus/PR_tech/Cadence/GdsOutMap/gdsout_2Xa1Xd_h_3Xe_vhv_2Z_1.2a.map -units 1000
