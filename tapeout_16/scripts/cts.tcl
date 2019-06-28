6b9c40e6effe09f97b02bc699f8b3026959442d8
Alex Carsello <alex.carsello@gmail.com>
2019-06-27 20:27:55 -0700

    Save intermediate database in cts

--- tapeout_16/scripts/cts.tcl
+++ tapeout_16/scripts/cts.tcl
@@ -8,5 +8,6 @@ create_ccopt_clock_tree_spec
 ccopt_design -cts
 write_db cts_only.db
 optDesign -postCTS
+write_db cts_first_opt.db
 optDesign -postCTS -hold
 #eval_novus {write_db cts.db -def -sdc -verilog}
