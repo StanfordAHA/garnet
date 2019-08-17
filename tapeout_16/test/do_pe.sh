fa62392223ac11924f50f6d5b31bac6d15485cf6
steveri <steveri@steveri.com>
2019-08-17 15:59:59 -0700

    hail mary IV

--- tapeout_16/test/do_pe.sh
+++ tapeout_16/test/do_pe.sh
@@ -3,9 +3,10 @@
 VERBOSE=true
 VERBOSE=false
 
+set -x
 cd ..
 source /cad/modules/tcl/init/bash
-module load genesis
+module load genesis2
 module load base
 python3 garnet.py --width 32 --height 16 -v --no_sram_stub
 
