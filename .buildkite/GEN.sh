df0c469831be5b07bdbc0d4081d11d77998c66d3
steveri <steveri@steveri.com>
2019-08-28 09:40:19 -0700

    cleaning the pipeline

--- .buildkite/GEN.sh
+++ .buildkite/GEN.sh
@@ -11,6 +11,11 @@ echo "+++ SETUP AND VERIFY ENVIRONMENT"
 # (Probably could/should skip this step for buildkite)
 #   - set -x; cd tapeout_16; set +x; source test/module_loads.sh; set -x
 
+set +x
+source /cad/modules/tcl/init/bash
+module load genesis2
+set -x
+
 if [ "$VERBOSE" == true ];
   then bin/requirements_check.sh -v
   else bin/requirements_check.sh -q
