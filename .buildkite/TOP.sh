ea0a68f41dc49ae5c767078ff9d3a0c20e40e530
steveri <steveri@steveri.com>
2019-08-29 07:53:41 -0700

    new TOP.sh

---
+++ .buildkite/TOP.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Exit on error from any pipe stage of any command
+set -eo pipefail
+
+# For debugging; echo each command before executing it
+set -x
+
+# (From tapeout_16/README in 'tapeout' branch
+# P&R Flow for Top:
+#     Navigate to garnet/tapeout_16/synth/GarnetSOC_pad_frame
+#     Type innovus -stylus to open the Innovus tool
+#     Type source ../../scripts/top_flow_multi_vt.tcl
+
+
+# Copy in the latest synth info from previous passes (PE, mem synth)
+synth_src=$CACHEDIR/synth/
+cp -rp $synth_src/GarnetSOC_pad_frame/ garnet/tapeout_16/synth
+
+# Navigate to garnet/tapeout_16/synth/GarnetSOC_pad_frame
+mkdir garnet/tapeout_16/synth/GarnetSOC_pad_frame
+
+echo "--- MODULE LOAD REQUIREMENTS"
+echo ""
+set +x; source test/module_loads.sh
+
+set -x
+
+########################################################################
+# Type innovus -stylus to open the Innovus tool
+# Type source ../../scripts/top_flow_multi_vt.tcl
+
+# echo tcl commands as they execute; also, quit when done (!)
+s=../../scripts/top_flow_multi_vt.tcl
+set wrapper=/tmp/top_flow_multi_vt.tcl
+echo "source -verbose $s" > $wrapper
+echo "exit" >> $wrapper
+set s=$wrapper
+
+# PWR_AWARE=1
+nobuf='stdbuf -oL -eL'
+innovus -stylus -no_gui -abort_on_error -replay $s || exit 13
+
+
+
+
+
+
+# # (Most of this is copied from synth/README file in tapeout_sr)
+# 
+# # Copy in a full-chip post-synthesis netlist, see notes below
+# % synth_src=$CACHEDIR/synth/
+# % cp -rp $synth_src/GarnetSOC_pad_frame/ garnet/tapeout_16/synth
+# 
+# # Make your way to the new local synth dir
+# % cd garnet/tapeout_16/synth/GarnetSOC_pad_frame/
