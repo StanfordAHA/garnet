479268f33f7ca1e03e6c5d43821ceb5ba2c5c1d1
steveri <steveri@steveri.com>
2019-09-28 17:12:05 -0700

    adjust icovl cc ONLY; restart from gen_power db

--- tapeout_16/scripts/floorplan.tcl
+++ tapeout_16/scripts/floorplan.tcl
@@ -5,9 +5,11 @@
 ##
 ###################################################################
 
+# FIXME CLEAN THIS UP ***SOON***!!!
+# both in top_garnet_staged.tcl AND in floorplan.tcl (etc?)
 set ::RESTART_FROM_GENPOWER 1
 if $::RESTART_FROM_GENPOWER {
-read_db gen_power.db
+read_db /sim/steveri/garnet/tapeout_16/synth/gpf5_space_out_fail/gen_power.db
 } else {
 delete_global_net_connections
 connect_global_net VDDPST -type pgpin -pin VDDPST -inst *
