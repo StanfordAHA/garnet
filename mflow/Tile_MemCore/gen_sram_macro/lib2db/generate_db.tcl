a33f947d1840e142980402a688b1371add5d01bf
Alex Carsello <alex.carsello@gmail.com>
2019-11-11 17:50:12 -0800

    Update Tile_MemCore gen_sram_macro step to not use BWEB

--- mflow/Tile_MemCore/gen_sram_macro/lib2db/generate_db.tcl
+++ mflow/Tile_MemCore/gen_sram_macro/lib2db/generate_db.tcl
@@ -11,7 +11,7 @@
 # Date   : November 11, 2019
 #
 
-set sram_lib_name "ts1n16ffcllsblvtc$::env(num_words)x$::env(word_size)m$::env(mux_size)sw_$::env(corner)"
+set sram_lib_name "ts1n16ffcllsblvtc$::env(num_words)x$::env(word_size)m$::env(mux_size)s_$::env(corner)"
 enable_write_lib_mode
 read_lib ../../outputs/sram.lib
 write_lib -format db $sram_lib_name -output ../../outputs/sram.db
