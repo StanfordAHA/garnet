21ae0a9e80d28e8a7994e00260eecb699fbaf7a2
Taeyoung Kong <kongty0329@gmail.com>
2019-11-18 11:48:20 -0800

    Temporary push

--- global_buffer/magma/io_controller_magma.py
+++ global_buffer/magma/io_controller_magma.py
@@ -9,24 +9,6 @@ GLB_ADDR_WIDTH = 32
 BANK_ADDR_WIDTH = 17
 BANK_DATA_WIDTH = 64
 
-class _ToBits(Generator):
-    def __init__(self):
-        super().__init__()
-        self.add_ports(
-            I=m.In(m.Bit),
-            O=m.Out(m.Bits[1])
-        )
-        tmp = m.Array[1, self.ports.I]
-        m.wire(self.ports.O, tmp)
-
-    def name(self):
-        return f"ToBits"
-
-def _tobits(parent, I):
-    tobits = _ToBits()
-    parent.wire(I, tobits.ports.I)
-    return tobits.ports.O
-
 class _Ternary(Generator):
     def __init__(self, width):
         super().__init__()
@@ -41,7 +23,7 @@ class _Ternary(Generator):
             out=m.Out(T)
         )
 
-        mux = MuxWrapper(2, self.width)
+        mux = MuxWrapper(2, self.width,)
         self.wire(self.ports.first, mux.ports.I[0])
         self.wire(self.ports.second, mux.ports.I[1])
         self.wire(self.ports.select, mux.ports.S)
@@ -70,20 +52,25 @@ class IoController(Generator):
         TArray = m.Array[self.num_io_channels,
                              m.Array[self.banks_per_io, m.Bits[1]]]
         self.add_ports(
+            clk=m.In(m.Clock),
+            reset=m.In(m.AsyncReset),
+            stall=m.In(m.Bits[1]),
             io_to_bank_wr_en=m.Out(m.Array[self.num_banks, m.Bits[1]]),
             io_to_bank_wr_data=m.Out(m.Array[self.num_banks, m.Bits[BANK_DATA_WIDTH]]),
             io_to_bank_wr_data_bit_sel=m.Out(m.Array[self.num_banks, m.Bits[BANK_DATA_WIDTH]]),
             io_to_bank_wr_addr=m.Out(m.Array[self.num_banks, m.Bits[BANK_ADDR_WIDTH]]),
 
-            # io_to_bank_rd_en=m.Out(m.Array[self.num_banks, 1),
-            # io_to_bank_rd_addr=m.Out(m.Array[self.num_banks, BANK_ADDR_WIDTH),
+            io_to_bank_rd_en=m.Out(m.Array[self.num_banks, m.Bits[1]]),
+            io_to_bank_rd_addr=m.Out(m.Array[self.num_banks, m.Bits[BANK_ADDR_WIDTH]]),
             # bank_to_io_rd_data=m.In(m.Array[self.num_banks, BANK_DATA_WIDTH),
 
             io_ctrl_switch_sel=m.In(TArray),
             adgn_addr=m.In(m.Array[self.num_io_channels, m.Bits[GLB_ADDR_WIDTH]]),
-            adgn_wr_en=m.In(m.Array[self.num_io_channels, m.Bit]),
+            adgn_wr_en=m.In(m.Array[self.num_io_channels, m.Bits[1]]),
             adgn_wr_data=m.In(m.Array[self.num_io_channels, m.Bits[BANK_DATA_WIDTH]]),
             adgn_wr_data_bit_sel=m.In(m.Array[self.num_io_channels, m.Bits[BANK_DATA_WIDTH]]),
+
+            adgn_rd_en=m.In(m.Array[self.num_io_channels, m.Bits[1]]),
         )
 
         # wr_en, wr_data, wr_data_bit_sel channels chain mux
@@ -93,12 +80,18 @@ class IoController(Generator):
         for j in range(self.num_io_channels):
             for k in range(self.banks_per_io):
                 if j == 0 and k == 0:
-                    bank_wr_en_int[0] = _ternary(self, 1, self.ports.adgn_wr_en[0], Const(0), self.ports.io_ctrl_switch_sel[0][0])
+                    bank_wr_en_int[0] = _ternary(self, 1,
+                                                 self.ports.adgn_wr_en[0],
+                                                 Const(0),
+                                                 self.ports.io_ctrl_switch_sel[0][0])
                     bank_wr_data_int[0] = _ternary(self, BANK_DATA_WIDTH, self.ports.adgn_wr_data[0], Const(0), self.ports.io_ctrl_switch_sel[0][0])
                     bank_wr_data_bit_sel_int[0] = _ternary(self, BANK_DATA_WIDTH, self.ports.adgn_wr_data_bit_sel[0], Const(0), self.ports.io_ctrl_switch_sel[0][0])
                 else:
                     idx = j * self.banks_per_io + k
-                    bank_wr_en_int[idx] = _ternary(self, 1, self.ports.adgn_wr_en[j], bank_wr_en_int[idx - 1], self.ports.io_ctrl_switch_sel[j][k])
+                    bank_wr_en_int[idx] = _ternary(self, 1,
+                                                   self.ports.adgn_wr_en[j],
+                                                   bank_wr_en_int[idx - 1],
+                                                   self.ports.io_ctrl_switch_sel[j][k])
                     bank_wr_data_int[idx] = _ternary(self, BANK_DATA_WIDTH, self.ports.adgn_wr_data[j], bank_wr_data_int[idx-1], self.ports.io_ctrl_switch_sel[j][k])
                     bank_wr_data_bit_sel_int[idx] = _ternary(self, BANK_DATA_WIDTH, self.ports.adgn_wr_data_bit_sel[j], bank_wr_data_bit_sel_int[idx-1], self.ports.io_ctrl_switch_sel[j][k])
 
@@ -112,14 +105,60 @@ class IoController(Generator):
                     idx = j * self.banks_per_io + k
                     bank_addr_int[idx] = _ternary(self, GLB_ADDR_WIDTH, self.ports.adgn_addr[j], bank_addr_int[idx - 1], self.ports.io_ctrl_switch_sel[j][k])
 
+        # rd_en channel chain mux
+        bank_rd_en_int = [m.Bit]*self.num_banks
+        for j in range(self.num_io_channels):
+            for k in range(self.banks_per_io):
+                if j == 0 and k == 0:
+                    bank_rd_en_int[0] = _ternary(self, 1,
+                                                 self.ports.adgn_rd_en[0],
+                                                 Const(0),
+                                                 self.ports.io_ctrl_switch_sel[0][0])
+                else:
+                    idx = j * self.banks_per_io + k
+                    bank_rd_en_int[idx] = _ternary(self, 1,
+                                                   self.ports.adgn_rd_en[j],
+                                                   bank_rd_en_int[idx - 1],
+                                                   self.ports.io_ctrl_switch_sel[j][k])
+
+        # clk_en
+        not_ = FromMagma(mantle.DefineNegate(1))
+        self.wire(not_.ports.I, self.ports.stall)
+        # self.wire(not_.ports.O, clk_en)
+
+        # pipeline d1
+        for i in range(self.num_banks):
+            pipeline_reg_d1 = mantle.Register(1, has_ce=True, has_async_reset=False)
+            self.wire(self.ports.clk, pipeline_reg_d1.CLK)
+            self.wire(not_.ports.O, pipeline_reg_d1.CE)
+            self.wire(self.port.io_to_bank_wr_en[i], pipeline_reg_d1.I)
+            self.wire(pipeline_reg_d1.O, io_to_bank_wr_en_d1[i])
+
+        # # rd_data channel chain mux
+        # bank_rd_en_int = [m.Bit]*self.num_banks
+        # for j in range(self.num_io_channels):
+        #     for k in range(self.banks_per_io):
+        #         if j == 0 and k == 0:
+        #             bank_rd_en_int[0] = _ternary(self, 1,
+        #                                          self.ports.adgn_rd_en[0],
+        #                                          Const(0),
+        #                                          self.ports.io_ctrl_switch_sel[0][0])
+        #         else:
+        #             idx = j * self.banks_per_io + k
+        #             bank_rd_en_int[idx] = _ternary(self, 1,
+        #                                            self.ports.adgn_rd_en[j],
+        #                                            bank_rd_en_int[idx - 1],
+        #                                            self.ports.io_ctrl_switch_sel[j][k])
+
+
         # output wr_en
         for i in range(self.num_banks):
             eq = FromMagma(mantle.DefineEQ(GLB_ADDR_WIDTH-BANK_ADDR_WIDTH))
             and_ = FromMagma(mantle.DefineAnd(2, 1))
             self.wire(bank_addr_int[i][BANK_ADDR_WIDTH:GLB_ADDR_WIDTH], eq.ports.I1)
             self.wire(Const(i), eq.ports.I0)
-            self.wire(and_.ports.I0, _tobits(self, eq.ports.O))
-            self.wire(and_.ports.I1, bank_wr_en_int[i])
+            self.wire(and_.ports.I0, bank_wr_en_int[i])
+            self.wire(and_.ports.I1[0], eq.ports.O)
             self.wire(self.ports.io_to_bank_wr_en[i], and_.ports.O)
 
         # output wr_addr
@@ -132,11 +171,18 @@ class IoController(Generator):
             self.wire(self.ports.io_to_bank_wr_data_bit_sel[i], bank_wr_data_bit_sel_int[i])
 
         # output rd_en
-        # for i in range(self.num_banks):
-        #     eq = FromMagma(mantle.DefineEQ(GLB_ADDR_WIDTH-BANK_ADDR_WIDTH))
-        #     self.wire(bank_addr_int[i][BANK_ADDR_WIDTH:GLB_ADDR_WIDTH], eq.ports.I1)
-        #     self.wire(Const(i), eq.ports.I0)
-        #     self.wire(self.ports.io_to_bank_wr_en[i], eq.ports.O)
+        for i in range(self.num_banks):
+            eq = FromMagma(mantle.DefineEQ(GLB_ADDR_WIDTH-BANK_ADDR_WIDTH))
+            and_ = FromMagma(mantle.DefineAnd(2, 1))
+            self.wire(bank_addr_int[i][BANK_ADDR_WIDTH:GLB_ADDR_WIDTH], eq.ports.I1)
+            self.wire(Const(i), eq.ports.I0)
+            self.wire(and_.ports.I0, bank_rd_en_int[i])
+            self.wire(and_.ports.I1[0], eq.ports.O)
+            self.wire(self.ports.io_to_bank_rd_en[i], and_.ports.O)
+
+        # output rd_addr
+        for i in range(self.num_banks):
+            self.wire(self.ports.io_to_bank_rd_addr[i], bank_addr_int[i][0:BANK_ADDR_WIDTH])
 
     def name(self):
         return f"IoController_{self.num_banks}"
