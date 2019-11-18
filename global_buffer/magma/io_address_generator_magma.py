5260664cff680432c06a1f3eb4a0af7ce6b2c05f
Alex Carsello <alex.carsello@gmail.com>
2019-11-18 14:10:42 -0800

    Finish io_addr_gen magma wrapper

--- global_buffer/magma/io_address_generator_magma.py
+++ global_buffer/magma/io_address_generator_magma.py
@@ -41,6 +41,33 @@ class GlobalController(Generator):
         generator = wrapper.generator(mode="declare")
         self.underlying = FromMagma(generator())
 
+        self.wire(self.ports.clk, self.underlying.ports.clk)
+        self.wire(self.ports.clk_en, self.underlying.ports.clk_en)
+        self.wire(self.ports.reset, self.underlying.ports.reset)
+        self.wire(self.ports.cgra_start_pulse, self.underlying.ports.cgra_start_pulse)
+        self.wire(self.ports.cgra_done_pulse, self.underlying.ports.cgra_done_pulse) 
 
+        self.wire(self.ports.start_addr, self.underlying.ports.start_addr)
+        self.wire(self.ports.num_words, self.underlying.ports.num_words)
+        self.wire(self.ports.mode, self.underlying.ports.mode)
+        self.wire(self.ports.done_delay, self.underlying.ports.done_delay)
+
+        self.wire(self.ports.cgra_to_io_wr_en, self.underlying.ports.cgra_to_io_wr_en)
+        self.wire(self.ports.cgra_to_io_rd_en, self.underlying.ports.cgra_to_io_rd_en)
+        self.wire(self.ports.cgra_to_io_wr_data, self.underlying.ports.cgra_to_io_wr_data)
+        self.wire(self.ports.io_to_cgra_rd_data, self.underlying.ports.cgra_to_io_rd_data)
+        self.wire(self.ports.io_to_cgra_rd_data_valid, self.underlying.ports.io_to_cgra_rd_data_valid)
+        self.wire(self.ports.cgra_to_io_addr_high, self.underlying.ports.cgra_to_io_addr_high)
+        self.wire(self.ports.cgra_to_io_addr_low, self.underlying.ports.cgra_to_io_addr_low)
+
+        self.wire(self.ports.io_to_bank_wr_en, self.underlying.ports.io_to_bank_wr_en)
+        self.wire(self.ports.io_to_bank_wr_data, self.underlying.ports.io_to_bank_wr_data)
+        self.wire(self.ports.io_to_bank_wr_data_bit_sel, self.underlying.ports.io_to_bank_wr_data_bit_sel)
+        self.wire(self.ports.io_to_bank_rd_en, self.underlying.ports.io_to_bank_rd_en)
+        self.wire(self.ports.bank_to_io_rd_data, self.underlying.ports.bank_to_io_rd_data)
+        self.wire(self.ports.bank_to_io_rd_data_valid, self.underlying.ports.bank_to_io_rd_data_valid)
+        self.wire(self.ports.io_to_bank_addr, self.underlying.ports.io_to_bank_addr)
+
+    # Wire all 
     def name(self):
         return f"io_address_generator"
