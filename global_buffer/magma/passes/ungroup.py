fe7dd3d225a94589c4d78b40d952d82c551d4831
Alex Carsello <alex.carsello@gmail.com>
2019-11-19 17:05:23 -0800

    First try at ungroup pass

---
+++ global_buffer/magma/passes/ungroup.py
@@ -0,0 +1,83 @@
+import magma
+from gemstone.generator.const import Const
+from gemstone.generator.generator import Generator
+from hwtypes import BitVector
+from collections import defaultdict
+
+def ungroup(top: Generator, *insts):
+    for inst in insts:
+        # Dictionary of wires to remove keyed by inst port name
+        connections_to_replace = defaultdict(lambda: defaultdict(list))
+        for wire in top.wires:
+            # Find any top level wire that goes to the inst we want to ungroup
+            for (ind, port) in enumerate(wire):
+                if port.owner() == inst:
+                    top_port = wire[ind - 1]
+                    port_key = port.qualified_name()
+                    connections_to_replace[port_key]['intermediate'] = port
+                    connections_to_replace[port_key]['external'].append(top_port)
+
+        for wire in inst.wires:
+            # Find any top level wire that goes to the inst we want to ungroup
+            for (ind, port) in enumerate(wire):
+                if port.owner() == inst:
+                    internal_port = wire[ind - 1]
+                    port_key = port.qualified_name()
+                    connections_to_replace[port_key]['internal'].append(internal_port)
+
+        # Now replace all (external <-> intermediate) and (intermediate <-> internal) connections
+        # with (external <-> internal) connections
+
+        # First break the connections
+        for connection in connections_to_replace.values()
+            intermediate = connection['intermediate']
+            # Break external connections
+            for external in connection['external']:
+                top.remove_wire(intermediate, external)
+            # Break internal connections
+            for internal in connection['internal']:
+                inst.remove_wire(intermediate, external)
+      
+        # Now reconnect everything 
+        for connection in connections_to_replace.values()
+            for external in connection['external']:
+                for internal in connection['internal']:
+                    top.wire(external, internal)
+           
+
+            
+                
+
+   # wires_to_remove = []
+   # for wire in top.wires:
+   #     # Find any wire that goes to the insts we want to ungroup
+   #     for (ind, port) in enumerate(wire):
+   #         if port.owner() in insts:
+   #             # The top port must be the other port in the connection
+   #             top_port = wire[ind - 1]
+   #             wires_to_remove.append({'wire': wire, 'intermediate_port': port, 'top_port': top_port})
+   #             # Now find the internal endpoints of this wire
+   #             for internal_wire in port.owner().wires:
+   #                 
+
+   # # Now we have all of the wires entering instances we want to ungroup
+   # for (wire, inst_port) in wires_to_remove:
+   #     internal_wires_to_remove = []
+   #     for internal_wire in port.owner().wires:
+   #         for internal_port in internal_wire:
+   #             if internal_port == inst_port:
+   #                 internal_wires_to_remove.append(internal_wire)
+   #                 break
+   #             
+                
+                
+        
+    #for inst in insts:
+    #    # Get all subinstances 
+    #    # can do this by querying wires
+    #    for wire in inst.wires:
+    #        for port in wire:
+    #            if port.owner() == inst:
+    #                port_to_remove = port
+    #                
+    #    # Recreate all instances and connections in top
