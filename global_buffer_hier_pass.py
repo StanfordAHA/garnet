c856b60c67a9b9560b070a1c0de3993e02b70a86
Alex Carsello <alex.carsello@gmail.com>
2019-11-22 21:25:20 -0800

    Commit full glbuf WIP

--- global_buffer_hier_pass.py
+++ global_buffer_hier_pass.py
@@ -5,10 +5,23 @@ import magma as m
 # Instantiate a global buffer and manipulates the hierarchy
 # to create a "tiled" global buffer for physical design
 global_buffer = GlobalBuffer(32,8,8)
-io_ctrl_channel_Wrappers = []
+io_ctrl_channel_wrappers = []
 # First within the io_controller, group all of the instances into their proper tiles
-for channel_inst_list in io_ctrl.channel_insts:
-    group(io_ctrl, *channel_inst_list)
+for channel_inst_list in global_buffer.io_ctrl.channel_insts:
+    io_ctrl_channel_wrappers.append(group(global_buffer.io_ctrl, *channel_inst_list))
+cfg_ctrl_channel_wrappers = []
+# First within the cfg_controller, group all of the instances into their proper tiles
+for channel_inst_list in global_buffer.cfg_ctrl.channel_insts:
+    cfg_ctrl_channel_wrappers.append(group(global_buffer.cfg_ctrl, *channel_inst_list))
+
+# Now ungroup the controllers
 ungroup(global_buffer, global_buffer.io_ctrl)
 ungroup(global_buffer, global_buffer.cfg_ctrl)
+assert(len(cfg_ctrl_channel_wrappers) == len(io_ctrl_channel_wrappers))
+
+final_tiles = []
+# Finally group the wrappers and corresponding memory banks into tiles
+for channel_num, io_channel, cfg_channel in enumerate(zip(io_ctrl_channel_wrappers, cfg_ctrl_channel_wrappers)):
+    final_tiles.append(group(global_buffer, io_channel, cfg_channel))
+
 m.compile("global_buffer", global_buffer.circuit(), output="coreir-verilog")
