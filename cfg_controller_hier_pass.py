45566fade0e9094a106f5a4a43d33c3fa0a41dc8
Alex Carsello <alex.carsello@gmail.com>
2019-11-22 21:18:45 -0800

    Add cfg_controller hier pass

---
+++ cfg_controller_hier_pass.py
@@ -0,0 +1,11 @@
+from global_buffer.magma.cfg_controller_magma import CfgController
+from hierarchy_passes.ungroup import ungroup
+from hierarchy_passes.group import group
+import magma as m
+
+cfg_ctrl = CfgController(32,8)
+cfg_ctrl_channel_Wrappers = []
+for channel_num, channel_inst_list in enumerate(cfg_ctrl.channel_insts):
+    group(cfg_ctrl, *channel_inst_list)
+#ungroup(cfg_ctrl)
+m.compile("cfg_controller", cfg_ctrl.circuit(), output="coreir-verilog")
