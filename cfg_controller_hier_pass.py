eae151803825fbf2da70b9faef22737b74ba2ab9
Alex Carsello <alex.carsello@gmail.com>
2019-11-26 00:15:41 -0800

    Name instance groups

--- cfg_controller_hier_pass.py
+++ cfg_controller_hier_pass.py
@@ -6,6 +6,6 @@ import magma as m
 cfg_ctrl = CfgController(32,8)
 cfg_ctrl_channel_Wrappers = []
 for channel_num, channel_inst_list in enumerate(cfg_ctrl.channel_insts):
-    group(cfg_ctrl, "Wrapper", *channel_inst_list)
+    group(cfg_ctrl, "cfg_ctrl_tile", *channel_inst_list)
 #ungroup(cfg_ctrl)
 m.compile("cfg_controller", cfg_ctrl.circuit(), output="coreir-verilog")
