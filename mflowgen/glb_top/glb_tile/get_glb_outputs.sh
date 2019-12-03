e4f287e61b2b52b13921683d9e531c72ea342479
James Thomas <jjthomas@stanford.edu>
2019-12-03 12:50:36 -0800

    added glb_top flow and other changes

---
+++ mflowgen/glb_top/glb_tile/get_glb_outputs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+../../configure --design $GARNET_HOME/mflowgen/glb_tile/
+make synopsys-ptpx-genlibdb
+mkdir -p outputs
+cp -L *synopsys-ptpx-genlibdb/outputs/design.lib outputs/glb_tile_tt.lib
+cp -L *synopsys-ptpx-genlibdb/outputs/design.db outputs/glb_tile.db
+cp -L *cadence-innovus-signoff/outputs/design.lef outputs/glb_tile.lef
+
