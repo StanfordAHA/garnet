89f3bd3d106b9220be76e7429b8cc721a594b56a
Alex Carsello <alex.carsello@gmail.com>
2019-12-04 15:09:34 -0800

    fix tcl syntax error

--- mflowgen/glb_tile/cadence-innovus-plugins/plugins/pre_place.tcl
+++ mflowgen/glb_tile/cadence-innovus-plugins/plugins/pre_place.tcl
@@ -62,7 +62,7 @@ if { $M2_direction == "Vertical" } {
 # TSMC16 requires specification of different taps/caps for different
 # locations/orientations, which the foundation flow does not natively support
 source inputs/adk/adk.tcl
-if {[expr ($ADK_END_CAP_CELL == "") && ($ADK_WELL_TAP_CELL == "")]} {
+if {[expr {$ADK_END_CAP_CELL == ""} && {$ADK_WELL_TAP_CELL == ""}]} {
   adk_set_end_cap_mode
   adk_set_well_tap_mode
   adk_add_end_caps
