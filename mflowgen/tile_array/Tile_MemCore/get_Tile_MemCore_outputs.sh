5ba9f19536a3fde5c45b1ebda77490d9647feac5
Alex Carsello <alex.carsello@gmail.com>
2020-01-21 12:49:50 -0800

    Fix bug in mem tile step

--- mflowgen/tile_array/Tile_MemCore/get_Tile_MemCore_outputs.sh
+++ mflowgen/tile_array/Tile_MemCore/get_Tile_MemCore_outputs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-../../configure --design $GARNET_HOME/mflowgen/glb_tile/
+../../configure --design $GARNET_HOME/mflowgen/Tile_MemCore/
 make synopsys-ptpx-genlibdb
 mkdir -p outputs
-cp -L *synopsys-ptpx-genlibdb/outputs/design.lib outputs/glb_tile_tt.lib
-cp -L *synopsys-ptpx-genlibdb/outputs/design.db outputs/glb_tile.db
-cp -L *cadence-innovus-signoff/outputs/design.lef outputs/glb_tile.lef
-cp -L *cadence-innovus-signoff/outputs/design.gds.gz outputs/glb_tile.gds.gz
+cp -L *synopsys-ptpx-genlibdb/outputs/design.lib outputs/Tile_MemCore_tt.lib
+cp -L *synopsys-ptpx-genlibdb/outputs/design.db outputs/Tile_MemCore.db
+cp -L *cadence-innovus-signoff/outputs/design.lef outputs/Tile_MemCore.lef
+cp -L *cadence-innovus-signoff/outputs/design.gds.gz outputs/Tile_MemCore.gds.gz
 
