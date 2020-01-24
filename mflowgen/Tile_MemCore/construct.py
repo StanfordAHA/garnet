9ab2384b4f8dc4d2fe73d95d5127763bdec511c9
Alex Carsello <alex.carsello@gmail.com>
2020-01-24 14:15:30 -0800

    Add extra constraints to genlibdb for loopbreaking

--- mflowgen/Tile_MemCore/construct.py
+++ mflowgen/Tile_MemCore/construct.py
@@ -54,11 +54,12 @@ def construct():
 
   # Custom steps
 
-  rtl          = Step( this_dir + '/../common/rtl'           )
-  constraints  = Step( this_dir + '/constraints'             )
-  gen_sram     = Step( this_dir + '/gen_sram_macro'          )
-  custom_init  = Step( this_dir + '/custom-init'             )
-  custom_power = Step( this_dir + '/custom-power'            )
+  rtl                  = Step( this_dir + '/../common/rtl'                         )
+  genlibdb_constraints = Step( this_dir + '/../common/custom-genlibdb-constraints' )
+  constraints          = Step( this_dir + '/constraints'                           )
+  gen_sram             = Step( this_dir + '/gen_sram_macro'                        )
+  custom_init          = Step( this_dir + '/custom-init'                           )
+  custom_power         = Step( this_dir + '/custom-power'                          )
 
   # Default steps
 
@@ -107,31 +108,36 @@ def construct():
   init.extend_inputs( custom_init.all_outputs() )
   power.extend_inputs( custom_power.all_outputs() )
 
+  # Add extra input edges to genlibdb for loop-breaking constraints
+
+  genlibdb.extend_inputs( genlibdb_constraints.all_outputs() )
+
   #-----------------------------------------------------------------------
   # Graph -- Add nodes
   #-----------------------------------------------------------------------
 
-  g.add_step( info         )
-  g.add_step( rtl          )
-  g.add_step( gen_sram     )
-  g.add_step( constraints  )
-  g.add_step( dc           )
-  g.add_step( iflow        )
-  g.add_step( init         )
-  g.add_step( custom_init  )
-  g.add_step( power        )
-  g.add_step( custom_power )
-  g.add_step( place        )
-  g.add_step( cts          )
-  g.add_step( postcts_hold )
-  g.add_step( route        )
-  g.add_step( postroute    )
-  g.add_step( signoff      )
-  g.add_step( genlibdb     )
-  g.add_step( gdsmerge     )
-  g.add_step( drc          )
-  g.add_step( lvs          )
-  g.add_step( debugcalibre )
+  g.add_step( info                 )
+  g.add_step( rtl                  )
+  g.add_step( gen_sram             )
+  g.add_step( constraints          )
+  g.add_step( dc                   )
+  g.add_step( iflow                )
+  g.add_step( init                 )
+  g.add_step( custom_init          )
+  g.add_step( power                )
+  g.add_step( custom_power         )
+  g.add_step( place                )
+  g.add_step( cts                  )
+  g.add_step( postcts_hold         )
+  g.add_step( route                )
+  g.add_step( postroute            )
+  g.add_step( signoff              )
+  g.add_step( genlibdb_constraints )
+  g.add_step( genlibdb             )
+  g.add_step( gdsmerge             )
+  g.add_step( drc                  )
+  g.add_step( lvs                  )
+  g.add_step( debugcalibre         )
 
   #-----------------------------------------------------------------------
   # Graph -- Add edges
@@ -201,8 +207,9 @@ def construct():
   g.connect_by_name( gdsmerge,     drc          )
   g.connect_by_name( gdsmerge,     lvs          )
 
-  g.connect_by_name( signoff, genlibdb )
-  g.connect_by_name( adk, genlibdb )
+  g.connect_by_name( signoff,              genlibdb )
+  g.connect_by_name( adk,                  genlibdb )
+  g.connect_by_name( genlibdb_constraints, genlibdb )
 
   g.connect_by_name( adk,      debugcalibre )
   g.connect_by_name( dc,       debugcalibre )
@@ -222,6 +229,11 @@ def construct():
                      {'order': "\"main.tcl quality-of-life.tcl floorplan.tcl add-endcaps-welltaps.tcl "\
                                "pin-assignments.tcl make-path-groups.tcl reporting.tcl\""}
                     )
+ 
+  # Adding new input for genlibdb node to run 
+  genlibdb.update_params(
+                         {'order': "\"read_design.tcl genlibdb-constraints.tcl extract_model.tcl\""}
+                        )
 
   return g
 
