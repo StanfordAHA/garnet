32079820af40679dcfcb3841d55d52d30d010be1
Alex Carsello <alex.carsello@gmail.com>
2020-01-27 11:33:44 -0800

    Ensure that gdsmerge gets run for tile_array flow

--- mflowgen/tile_array/Tile_PE/get_Tile_PE_outputs.sh
+++ mflowgen/tile_array/Tile_PE/get_Tile_PE_outputs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ../../configure --design $GARNET_HOME/mflowgen/Tile_PE/
 make synopsys-ptpx-genlibdb
+make mentor-calibre-gdsmerge
 mkdir -p outputs
 cp -L *synopsys-ptpx-genlibdb/outputs/design.lib outputs/Tile_PE_tt.lib
 cp -L *synopsys-ptpx-genlibdb/outputs/design.db outputs/Tile_PE.db
 cp -L *cadence-innovus-signoff/outputs/design.lef outputs/Tile_PE.lef
-cp -L *mentor-calibre-gdsmerge/outputs/design_merged.gds outputs/Tile_PE.gds.gz
+cp -L *mentor-calibre-gdsmerge/outputs/design_merged.gds outputs/Tile_PE.gds
 
