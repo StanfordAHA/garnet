a32584ce4b0e37dcd49949fb02a4caf5f7523417
Alex Carsello <alex.carsello@gmail.com>
2020-01-27 17:00:37 -0800

    move all tile_array pins to top and offset from corners

--- mflowgen/tile_array/custom-init/outputs/pin-assignments.tcl
+++ mflowgen/tile_array/custom-init/outputs/pin-assignments.tcl
@@ -8,28 +8,34 @@
 # Pin Assignments
 #-------------------------------------------------------------------------
 
-# Take all ports and split into halves
-
-set all_ports       [dbGet top.terms.name -v *clk*]
+set all_ports       [lsort [dbGet top.terms.name -v *clk*]]
 
 set num_ports       [llength $all_ports]
 set half_ports_idx  [expr $num_ports / 2]
 
-set pins_left_half  [lrange $all_ports 0               [expr $half_ports_idx - 1]]
-set pins_right_half [lrange $all_ports $half_ports_idx [expr $num_ports - 1]     ]
-
-# Take all clock ports and place them center-left
+# Take all clock ports and place them center-top
 
 set clock_ports     [dbGet top.terms.name *clk*]
-set half_left_idx   [expr [llength $pins_left_half] / 2]
 
 if { $clock_ports != 0 } {
   for {set i 0} {$i < [llength $clock_ports]} {incr i} {
-    set pins_left_half \
-      [linsert $pins_left_half $half_left_idx [lindex $clock_ports $i]]
+    set all_ports \
+      [linsert $all_ports $half_ports_idx [lindex $clock_ports $i]]
   }
 }
 
-set ports_layer M4
-editPin -layer $ports_layer -pin $pins_left_half  -side LEFT  -spreadType SIDE
-editPin -layer $ports_layer -pin $pins_right_half -side RIGHT -spreadType SIDE
+set ports_layer M6
+set offset_pitches 100
+set width [dbGet top.fPlan.box_urx]
+set height [dbGet top.fPlan.box_ury]
+
+set offset [expr $offset_pitches * $savedvars(horiz_pitch)]
+set start $offset
+set end [expr $width - $offset]
+editPin -layer $ports_layer \
+        -pin $all_ports \
+        -side TOP \
+        -start [list $start $height] \
+        -end [list [expr $width - $offset] $height] \
+        -spreadType RANGE \
+        -spreadDirection clockwise
