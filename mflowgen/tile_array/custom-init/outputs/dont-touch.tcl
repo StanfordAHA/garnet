811bcc8d95f9e36c447f99ccdacdf76233e46edd
Alex Carsello <alex.carsello@gmail.com>
2020-01-31 10:54:30 -0800

    update dont-touch comment

--- mflowgen/tile_array/custom-init/outputs/dont-touch.tcl
+++ mflowgen/tile_array/custom-init/outputs/dont-touch.tcl
@@ -8,8 +8,8 @@
 
 # First get all nets connected to CGRA tiles
 set tile_nets [get_nets -of_objects [get_cells -filter {ref_lib_cell_name =~ *Tile*}]]
-# Now iterate over collection and check if net connects 
-# only 2 tiles. If it does, don't try to route it.
+# Now iterate over collection and check if net connects > 1
+# CGRA tile and no other cells. If this is true, don't try to route it.
 foreach_in_collection net $tile_nets {
   set connected_cells [get_cells -of_object $net]
   set connected_tiles [filter_collection $connected_cells {ref_lib_cell_name =~ *Tile*}]
