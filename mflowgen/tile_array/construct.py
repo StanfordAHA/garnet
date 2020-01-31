7a7cd805e39efaf9d4029e09196eb02d9c49de79
Alex Carsello <alex.carsello@gmail.com>
2020-01-30 17:36:23 -0800

    add dont-touch.tcl to init order

--- mflowgen/tile_array/construct.py
+++ mflowgen/tile_array/construct.py
@@ -232,7 +232,7 @@ def construct():
   # that input to the order parameter for that node, so it actually gets run
   init.update_params(
                      {'order': "\"main.tcl quality-of-life.tcl floorplan.tcl add-endcaps-welltaps.tcl "\
-                               "pin-assignments.tcl make-path-groups.tcl reporting.tcl\""}
+                               "pin-assignments.tcl make-path-groups.tcl dont-touch.tcl reporting.tcl\""}
                     )
 
   return g
