f033ca6b1ca2ca648fc9db923c13b1152fc818bd
steveri <steveri@steveri.com>
2020-02-04 08:40:20 -0800

    buildkite script and filter

--- bin/requirements_check.sh
+++ bin/requirements_check.sh
@@ -44,51 +44,49 @@ echo ""
 ##############################################################################
 subheader +++ VERIFY PYTHON PACKAGE REQUIREMENTS
 
+function check_pip {
+  # echo "Verifying existence of python package '$1'..."
+  [ "$VERBOSE" == "true" ] && echo -n "Want $pkg , "
+  pkg="$1"; pkg_found=true
+  # Note package name might have embedded version e.g. 'coreir>=2.0.50'
+  pkg=`echo "$pkg" | awk -F '>' '{print $1}'`
+  # FIXME really should check version number as well...
+  found=`pip3 list | awk '$1=="'$pkg'"{ print "found"}'`
+  if [ $found ] ; then 
+    [ "$VERBOSE" == "true" ] && pip3 list | awk '$1=="'$pkg'"{ print "found pkg "$0}'
+    [ "$VERBOSE" == "true" ] && echo "  Found package '$pkg'"
+    return 0
+  else
+    echo ""
+    echo "  ERROR Cannot find installed python package '$pkg'"
+    exit 13
+  fi
+}
 
-# Uh...what was the point???
-# function check_pip {
-#   # echo "Verifying existence of python package '$1'..."
-#   pkg="$1"; pkg_found=true
-#   # Note package name might have embedded version e.g. 'coreir>=2.0.50'
-#   pkg=`echo "$pkg" | awk -F '>' '{print $1}'`
-#   # FIXME really should check version number as well...
-#   found=`pip3 list | awk '$1=="'$pkg'"{ print "found"}'`
-#   if [ $found ] ; then 
-#     [ $VERBOSE == true ] && echo "  Found package '$pkg'"
-#     return 0
-#   else
-#     echo "  ERROR Cannot find installed python package '$pkg'"
-#     exit 13
-#   fi
-# }
-# 
-# packages=`cat requirements.txt \
-#     | sed 's/.*egg=//' \
-#     | sed 's/==.*//' \
-#     | sed 's/buffer_mapping/buffer-mapping/' \
-#     | sed 's/ordered_set/ordered-set/' \
-#     | sed 's/cosa/CoSA/' \
-#     | awk '{print $1}'
-#   `
-# echo Need python packages $packages
-# found_missing=false
-# for pkg in $packages; do
-#   (check_pip $pkg) || found_missing=true
-# done
-# if [ $found_missing == true ]; then
-#   echo ""
-#   echo "ERROR missing packages, maybe need to do pip3 install -r `pwd`/requirements.txt"
-#   exit 13
-# fi
-# echo Found all packages
-
-
-# This check is probably better than the one up there...
-echo ""
-echo ""
-if ! pip3 check requirements.txt; then
-  echo "ERROR bad packages maybe, might need to do pip3 install -r requirements.txt"
+packages=`cat requirements.txt \
+    | sed 's/.*egg=//' \
+    | sed 's/==.*//' \
+    | sed 's/buffer_mapping/buffer-mapping/' \
+    | sed 's/ordered_set/ordered-set/' \
+    | sed 's/cosa/CoSA/' \
+    | awk '{print $1}'
+  `
+echo Need python packages $packages
+found_missing=false
+for pkg in $packages; do
+    (check_pip $pkg) || found_missing=true
+done
+if [ $found_missing == true ]; then
+  echo ""
+  echo "ERROR missing packages, maybe need to do pip3 install -r `pwd`/requirements.txt"
   exit 13
 fi
-echo ""
-echo ""
+echo Found all packages
+
+########################################################################
+# "pip check" only checks integrity of installed packages;
+# It does not look for or verify requirements.txt packages
+if ! pip3 check; then
+  echo "ERROR bad packages maybe, might need to do pip3 install"
+  exit 13
+fi
