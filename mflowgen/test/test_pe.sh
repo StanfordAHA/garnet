400dd267e5851f4c75e3c0751e7d23c290bd1ffc
steveri <steveri@steveri.com>
2020-02-04 08:40:20 -0800

    buildkite script and filter

--- mflowgen/test/test_pe.sh
+++ mflowgen/test/test_pe.sh
@@ -39,26 +39,42 @@ if test -d $mflowgen/$module; then
     echo "giving up already love ya bye-bye"
     exit 13
 fi
+
 set -x
 mkdir $mflowgen/$module; cd $mflowgen/$module
 ../configure --design $garnet/mflowgen/Tile_PE
 
-
-
-
 # Targets: run "make list" and "make status"
-# make -n mentor-calibre-drc
-echo 'make mentor-calibre-drc |& tee mcdrc.log'
-# make -n debug-mentor-calibre-drc
-echo 'make debug-mentor-calibre-drc |& tee mcdrc-debug.log'
+# make list
+
+# echo "make mentor-calibre-drc \
+#   |& tee mcdrc.log \
+#   | gawk -f $script_home/filter.awk"
+
+nobuf='stdbuf -oL -eL'
+make mentor-calibre-drc \
+  |& $nobuf tee mcdrc.log \
+  |  $nobuf gawk -f $script_home/filter.awk
+
+# Detailed per-cell result
+# CELL Tile_PE ................................................ TOTAL Result Count = 248 (248)
+#     RULECHECK OPTION.COD_CHECK:WARNING ...................... TOTAL Result Count = 1   (1)
+#     RULECHECK IO_CONNECT_CORE_NET_VOLTAGE_IS_CORE:WARNING ... TOTAL Result Count = 1   (1)
+#     RULECHECK G.4:M2 ........................................ TOTAL Result Count = 164 (164)
+#     RULECHECK M2.W.4.1 ...................................... TOTAL Result Count = 82  (82)
+# ----------------------------------------------------------------------------------
+echo ""; sed -n '/^CELL/,/^--- SUMMARY/p' $pe/*/drc.summary \
+  | grep -v SUMM
+
+echo DONE
+
 
 
 ########################################################################
 ########################################################################
 ########################################################################
-echo 'make mentor-calibre-drc |& tee mcdrc.log'
-
-
+# NOTES
+# 
 # Generic Targets:
 # 
 #  - list      -- List all steps
@@ -106,43 +122,3 @@ echo 'make mentor-calibre-drc |& tee mcdrc.log'
 #  - debug-16 : debug-mentor-calibre-gdsmerge
 #  - debug-17 : debug-mentor-calibre-drc
 #  - debug-18 : debug-mentor-calibre-lvs
-
-# OLD
-# # rtl:
-# # python: can't open file 'garnet.py': [Errno 2] No such file or directory
-# # cp: cannot stat ‘garnet.v’: No such file or directory
-# # cat: genesis_verif/*: No such file or directory
-# 
-# # syn .. c-synthesis
-# # Error: Cannot find the design 'Tile_PE' in the library 'WORK'. (LBR-0)
-# 
-# # make -n rtl
-# # rm -rf ./1-rtl
-# # cp -aL ../../soc/components/cgra/garnet/mflowgen/common/rtl 1-rtl || true
-# 
-# # mkdir -p 1-rtl/outputs && \
-# # python ../utils/letters.py -c -t rtl && \
-# # cp -f .mflowgen/1-rtl/mflowgen-run.sh 1-rtl && \
-# # cp -f .mflowgen/1-rtl/mflowgen-debug.sh 1-rtl 2> /dev/null || true && \
-# # cd 1-rtl && sh mflowgen-run.sh 2>&1 | tee mflowgen-run.log && \
-# # cd .. && \
-# # touch -c 1-rtl/outputs/*
-
-# No need, should be done by requirements_check.sh above
-# v=`python -c 'import sys; print(sys.version_info[0]*1000+sys.version_info[1])'`
-# if [ $v -lt 3000 ] ; then
-#   echo ""
-#   echo "WARNING found python version $v -- should be at least 3000"
-#   echo "WARNING I will try to fix this for you"
-#   echo "  ln -s /usr/local/bin/python3 $HOME/bin/python"
-#   echo "  export PATH=$HOME/bin:$PATH"
-#   ln -s /usr/local/bin/python3 $HOME/bin/python
-#   export PATH=$HOME/bin:$PATH
-# fi
-# v=`python -c 'import sys; print(sys.version_info[0]*1000+sys.version_info[1])'`
-# if [ $v -lt 3000 ] ; then
-#   echo ""
-#   echo "ERROR Still seeing python version $v"
-#   echo "giving up already love ya bye-bye"
-#   exit 13
-# fi
