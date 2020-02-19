3aaa76d7950e1eb8d260adcbf70c5899c51d3128
Alex Carsello <alex.carsello@gmail.com>
2020-02-18 23:33:12 -0800

    Allow LVS to run on tile_array. not passing

--- mflowgen/tile_array/Tile_PE/get_Tile_PE_outputs.sh
+++ mflowgen/tile_array/Tile_PE/get_Tile_PE_outputs.sh
@@ -2,9 +2,11 @@
 ../../configure --design $GARNET_HOME/mflowgen/Tile_PE/
 make synopsys-ptpx-genlibdb
 make mentor-calibre-gdsmerge
+make mentor-calibre-lvs
 mkdir -p outputs
 cp -L *synopsys-ptpx-genlibdb/outputs/design.lib outputs/Tile_PE_tt.lib
 cp -L *synopsys-ptpx-genlibdb/outputs/design.db outputs/Tile_PE.db
 cp -L *cadence-innovus-signoff/outputs/design.lef outputs/Tile_PE.lef
 cp -L *mentor-calibre-gdsmerge/outputs/design_merged.gds outputs/Tile_PE.gds
+cp -L *mentor-calibre-lvs/outputs/design.spi outputs/Tile_PE.spi
 
