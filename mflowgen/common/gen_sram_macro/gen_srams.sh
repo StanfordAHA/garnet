cccd9cb2a54755a1614e2e18d47be9c7c2d4e90e
Alex Carsello <alex.carsello@gmail.com>
2020-02-18 18:06:31 -0800

    add partial write param to common gen_sram_macro node

--- mflowgen/common/gen_sram_macro/gen_srams.sh
+++ mflowgen/common/gen_sram_macro/gen_srams.sh
@@ -2,8 +2,18 @@ echo "${num_words}x${word_size}m${mux_size}s" >> config.txt
 
 export MC_HOME=mc/tsn16ffcllhdspsbsram_20131200_130a
 export PATH="${PATH}:mc/MC2_2013.12.00.f/bin"
-sram_name="ts1n16ffcllsblvtc${num_words}x${word_size}m${mux_size}s_130a"
-./mc/tsn16ffcllhdspsbsram_130a.pl -file config.txt -NonBIST -NonSLP -NonDSLP -NonSD -NonBWEB
+sram_name="ts1n16ffcllsblvtc${num_words}x${word_size}m${mux_size}s"
+if [ $partial_write == True ]; then
+  sram_name+="w"
+fi
+
+sram_name+="_130a"
+cmd="./mc/tsn16ffcllhdspsbsram_130a.pl -file config.txt -NonBIST -NonSLP -NonDSLP -NonSD"
+if [ ! $partial_write == True ]; then
+  cmd+=" -NonBWEB"
+fi
+
+eval $cmd
 
 ln -s ../$sram_name/NLDM/${sram_name}_${corner}.lib outputs/sram_tt.lib
 ln -s ../$sram_name/GDSII/${sram_name}_m4xdh.gds outputs/sram.gds
