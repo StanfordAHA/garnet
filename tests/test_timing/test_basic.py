410b22858b22703652e53750ba1a1071382a2e99
Rajsekhar Setaluri <rajsekhar@fb.com>
2020-02-19 10:51:34 -0800

    Pare down test sequence and include IO tiles

--- tests/test_timing/test_basic.py
+++ tests/test_timing/test_basic.py
@@ -23,8 +23,7 @@ def dw_files():
     return result_filenames
 
 
-@pytest.mark.parametrize("batch_size", [100])
-def test_interconnect_point_wise(batch_size: int, dw_files):
+def test_interconnect_point_wise(dw_files):
     """
     Pointwise multiply test on 2x2 fabric, without IO tiles. Routing and
     placement is hand-crafted.
@@ -36,34 +35,11 @@ def test_interconnect_point_wise(batch_size: int, dw_files):
     chip_size = 2
     interconnect = create_cgra(width=chip_size, height=chip_size,
                                io_sides=IOSide.None_, num_tracks=5,
-                               standalone=True, add_pd=True, mem_ratio=(1, 100))
+                               add_pd=True, mem_ratio=(1, 100))
 
-    # Use canal API to get the bitstream for the following route:
-    #   * Tile(0, 0).SB.WEST.0.IN -> Tile(0, 0).PE.data0
-    #   * Tile(0, 0).SB.NORTH.0.IN -> Tile(0, 0).PE.data1
-    #   * Tile(0, 0).PE.alu_res -> Tile(0, 0).SB.EAST.0.OUT
     config_data = []
-    graph_16 = interconnect.get_graph(16)
-    sb_data0 = graph_16[0, 0].get_sb(SwitchBoxSide.WEST, 0, SwitchBoxIO.SB_IN)
-    sb_data1 = graph_16[0, 0].get_sb(SwitchBoxSide.NORTH, 0, SwitchBoxIO.SB_IN)
-    port_data0 = graph_16[0, 0].ports["data0"]
-    port_data1 = graph_16[0, 0].ports["data1"]
-    config_data.append(interconnect.get_node_bitstream_config(sb_data0, port_data0))
-    config_data.append(interconnect.get_node_bitstream_config(sb_data1, port_data1))
-    output_port = graph_16[0, 0].ports["alu_res"]
-    output_sb = graph_16[0, 0].get_sb(SwitchBoxSide.EAST, 0, SwitchBoxIO.SB_OUT)
-    config_data.append(interconnect.get_node_bitstream_config(output_port, output_sb))
 
-    # Now, route from Tile(0, 0).SB.EAST.0.OUT all the way to the east edge of
-    # the CGRA, which is Tile(w, 0).SB.EAST.0.OUT.
-    for x in range(1, chip_size):
-        pre_node = graph_16[x, 0].get_sb(SwitchBoxSide.WEST, 0, SwitchBoxIO.SB_IN)
-        next_node = graph_16[x, 0].get_sb(SwitchBoxSide.EAST, 0, SwitchBoxIO.SB_OUT)
-        config = interconnect.get_node_bitstream_config(pre_node, next_node)
-        assert config is not None
-        config_data.append(config)
-
-    # Now need to configure Tile(0, 0).PE to be umult0.
+    # Configure Tile(0, 0).PE to be umult0.
     pe_tile = interconnect.tile_circuits[(0, 0)]
     mul_conifg_data = pe_tile.core.get_config_bitstream(lassen.asm.umult0())
     for addr, data in mul_conifg_data:
@@ -77,24 +53,18 @@ def test_interconnect_point_wise(batch_size: int, dw_files):
     tester.circuit.clk = 0
     tester.reset()
 
-    # Run configuration sequence and check the configuration output bus.
-    for addr, data in config_data:
+    MAX_CONFIG = 2
+    # Run configuration sequence and check the configuration output bus. Only do
+    # a maximum of 2 config's.
+    for i, (addr, data) in enumerate(config_data):
+        if i > MAX_CONFIG:
+            break
         tester.configure(addr, data)
         tester.config_read(addr)
         tester.eval()
         tester.print(f"Doing config {(addr, data)}\n")
         tester.expect(circuit.read_config_data, data)
     tester.done_config()
-
-    # Drive a bunch of random values and check the output against the expected
-    # value.
-    for _ in range(batch_size):
-        x = hwtypes.BitVector.random(16)
-        y = hwtypes.BitVector.random(16)
-        tester.circuit.SB_T0_WEST_SB_IN_B16_X0_Y0 = x
-        tester.circuit.SB_T0_NORTH_SB_IN_B16_X0_Y0 = y
-        tester.eval()
-        tester.circuit.SB_T0_EAST_SB_OUT_B16_X1_Y0.expect(x * y)
 
     # Compile and run the test using a verilator backend.
     with tempfile.TemporaryDirectory() as tempdir:
