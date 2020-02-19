a4371e7dac03561a26e1622a44f283f81c87fa5c
Alex Carsello <alex.carsello@gmail.com>
2020-02-18 23:33:35 -0800

    Allow LVS to run on glb_top. not passing

--- mflowgen/glb_top/glb_tile/get_glb_outputs.sh
+++ mflowgen/glb_top/glb_tile/get_glb_outputs.sh
@@ -2,9 +2,11 @@
 ../../configure --design $GARNET_HOME/mflowgen/glb_tile/
 make synopsys-ptpx-genlibdb
 make mentor-calibre-gdsmerge
+make mentor-calibre-lvs
 mkdir -p outputs
 cp -L *synopsys-ptpx-genlibdb/outputs/design.lib outputs/glb_tile_tt.lib
 cp -L *synopsys-ptpx-genlibdb/outputs/design.db outputs/glb_tile.db
 cp -L *cadence-innovus-signoff/outputs/design.lef outputs/glb_tile.lef
 cp -L *mentor-calibre-gdsmerge/outputs/design_merged.gds outputs/glb_tile.gds
+cp -L *mentor-calibre-lvs/outputs/design.spi outputs/glb_tile.spi
 
