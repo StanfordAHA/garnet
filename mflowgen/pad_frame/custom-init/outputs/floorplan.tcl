ac11e2ebfcb41a026f236f394b96a9fd4099b53b
steveri <steveri@steveri.com>
2020-02-24 08:01:22 -0800

    floorplan for pad frame

--- mflowgen/pad_frame/custom-init/outputs/floorplan.tcl
+++ mflowgen/pad_frame/custom-init/outputs/floorplan.tcl
@@ -2,58 +2,20 @@
 # floorplan.tcl
 #=========================================================================
 # This script is called from the Innovus init flow step.
-#
-# Author : Christopher Torng
-# Date   : March 26, 2018
 
-#-------------------------------------------------------------------------
-# Floorplan variables
-#-------------------------------------------------------------------------
+# Original `stylus` cmd from 06/2019 tapeout
+# create_floorplan \
+#     -core_margins_by die \
+#     -die_size_by_io_height max \
+#     -site core \
+#     -die_size 4900.0 4900.0 100 100 100 100
 
-# Density target: width will be adjusted to meet this cell density
-set core_density_target 0.70; # Placement density of 70% is reasonable
-# Core height : number of vertical pitches in height of core area
-# We fix this value because the height of the memory and PE tiles
-# must be the same to allow for abutment at the top level
-set core_height 139
+# Translated to legacy cmd
+floorPlan \
+    -coreMarginsBy die \
+    -dieSizeByIoHeight max \
+    -site core \
+    -d 4900.0 4900.0 100 100 100 100
 
-# Make room in the floorplan for the core power ring
-
-set pwr_net_list {VDD VSS}; # List of power nets in the core power ring
-
-set M1_min_width   [dbGet [dbGetLayerByZ 1].minWidth]
-set M1_min_spacing [dbGet [dbGetLayerByZ 1].minSpacing]
-
-set savedvars(p_ring_width)   [expr 48 * $M1_min_width];   # Arbitrary!
-set savedvars(p_ring_spacing) [expr 24 * $M1_min_spacing]; # Arbitrary!
-
-set vert_pitch [dbGet top.fPlan.coreSite.size_y]
-set height [expr $core_height * $vert_pitch]
-
-# Now begin width calculation
-# Get the combined area of all cells in the design
-set cell_areas [get_property [get_cells *] area]
-set total_cell_area 0
-foreach area $cell_areas {
-  set total_cell_area [expr $total_cell_area + $area]
-}
-
-# Calculate FP width that will meet density target given fixed height 
-set width [expr $total_cell_area / $core_density_target / $height]
-
-# Core bounding box margins
-
-set core_margin_t [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $savedvars(p_ring_spacing))) + $savedvars(p_ring_spacing)]
-set core_margin_b [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $savedvars(p_ring_spacing))) + $savedvars(p_ring_spacing)]
-set core_margin_r [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $savedvars(p_ring_spacing))) + $savedvars(p_ring_spacing)]
-set core_margin_l [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $savedvars(p_ring_spacing))) + $savedvars(p_ring_spacing)]
-
-#-------------------------------------------------------------------------
-# Floorplan
-#-------------------------------------------------------------------------
-
-floorPlan -s $width $height \
-             $core_margin_l $core_margin_b $core_margin_r $core_margin_t
-
-setFlipping s
-
+# Dunno what this is...?
+# setFlipping s
