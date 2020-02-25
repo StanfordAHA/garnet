d7b58587d2cb0a29c2a663ddc4bd91d7dfabc0ae
Rajsekhar Setaluri <rajsekhar@fb.com>
2020-02-24 19:47:26 -0800

    Factor out common code

--- tests/test_timing/test_basic.py
+++ tests/test_timing/test_basic.py
@@ -1,30 +1,16 @@
-import glob
-import os
 import pytest
-import shutil
 import tempfile
-from canal.cyclone import SwitchBoxSide, SwitchBoxIO
 from canal.util import IOSide
 from gemstone.common.testers import BasicTester
-import hwtypes
-import lassen.asm
 import magma
 from cgra import create_cgra
+import common
 
 
-@pytest.fixture(scope="module")
-def dw_files():
-    filenames = ["DW_fp_add.v", "DW_fp_mult.v"]
-    dirname = "peak_core"
-    result_filenames = []
-    for name in filenames:
-        filename = os.path.join(dirname, name)
-        assert os.path.isfile(filename)
-        result_filenames.append(filename)
-    return result_filenames
+dw_files = pytest.fixture(scope="module")(common.dw_files)
 
 
-def test_interconnect_point_wise(dw_files):
+def test_basic(dw_files):
     """
     Configuration sequence test on 2x2 fabric + IO tiles.
     """
@@ -32,56 +18,17 @@ def test_interconnect_point_wise(dw_files):
     chip_size = 2
     interconnect = create_cgra(width=chip_size, height=chip_size,
                                io_sides=IOSide.North, num_tracks=5, add_pd=True)
-
-    config_data = []
-
-    # Configure Tile(0, 0).PE to be umult0.
-    pe_tile = interconnect.tile_circuits[(1, 1)]
-    mul_conifg_data = pe_tile.core.get_config_bitstream(lassen.asm.umult0())
-    for addr, data in mul_conifg_data:
-        full_addr = interconnect.get_config_addr(addr, 0, 0, 0)
-        config_data.append((full_addr, data))
-
-    # Construct circuit and setup fault tester.
+    # Poke the circuit with a reset sequence and short configuration sequence.
+    sequence = common.basic_sequence(interconnect)
+    sequence = sequence[:2]  # limit to 2 addr's
     circuit = interconnect.circuit()
-
     tester = BasicTester(circuit, circuit.clk, circuit.reset)
-    tester.circuit.clk = 0
-    tester.print(f"Starting reset\n")
-    tester.reset()
-    tester.print(f"Finished reset\n")
-
-    MAX_CONFIG = 2
-    # Run configuration sequence and check the configuration output bus. Only do
-    # a maximum of 2 config's. Note that we need to use a real sequence to make
-    # sure the configuration addresses and data are valid.
-    for i, (addr, data) in enumerate(config_data):
-        if i > MAX_CONFIG:
-            break
-        tester.configure(addr, data)
-        tester.config_read(addr)
-        tester.eval()
-        tester.print(f"Doing config {(addr, data)}\n")
-        tester.expect(circuit.read_config_data, data)
-    tester.done_config()
+    common.configure(tester, sequence, check_read_data=True)
 
     # Compile and run the test using a verilator backend.
-    ext_srcs = [f"{circuit.name}.sv", "DW_fp_add.v", "DW_fp_mult.v",
-                "AN2D0BWP16P90.sv", "AO22D0BWP16P90.sv"]
     with tempfile.TemporaryDirectory() as tempdir:
-        for genesis_verilog in glob.glob("genesis_verif/*.*"):
-            shutil.copy(genesis_verilog, tempdir)
-        for filename in dw_files:
-            shutil.copy(filename, tempdir)
-        shutil.copy(os.path.join("tests", "test_memory_core",
-                                 "sram_stub.v"),
-                    os.path.join(tempdir, "sram_512w_16b.v"))
-        for aoi_mux in glob.glob("tests/*.sv"):
-            shutil.copy(aoi_mux, tempdir)
+        common.generate_scaffolding(tempdir)
         magma.compile(f"{tempdir}/{circuit.name}", circuit,
-                      output="coreir-verilog", coreir_libs={"float_DW"},
-                      sv=True)
-        tester.compile_and_run(skip_compile=True, target="system-verilog",
-                               simulator="ncsim", directory=tempdir,
-                               ext_model_file=True, ext_srcs=ext_srcs,
-                               skip_run=True)
+                      output="coreir-verilog", coreir_libs={"float_DW"})
+        tester.compile_and_run(skip_compile=True, target="verilator",
+                               directory=tempdir, flags=["-Wno-fatal"])
