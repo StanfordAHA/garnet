f3959449f808d93a691971451c7b6c30fd3009df
steveri <steveri@steveri.com>
2020-03-03 07:55:21 -0800

    updated a couple of comments

--- mflowgen/pad_frame/custom-power/outputs/globalnetconnect.tcl
+++ mflowgen/pad_frame/custom-power/outputs/globalnetconnect.tcl
@@ -6,6 +6,6 @@ connect_global_net VSS -type tielo
 connect_global_net VDD -type pgpin -pin VPP -inst *
 connect_global_net VSS -type pgpin -pin VBB -inst *
 
-# Added for butterphy maybe
+# I/O power nets
 connect_global_net VDDPST -type pgpin -pin VDDPST -inst *
 connect_global_net VSS    -type pgpin -pin VSSPST -inst * 
