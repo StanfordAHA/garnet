8aa945096346faee02011df986b2b5abb3923898
Taeyoung Kong <kongty0329@gmail.com>
2020-03-05 03:45:43 -0800

    Make files clean

--- global_buffer/rtl/glb_bank_memory.sv
+++ global_buffer/rtl/glb_bank_memory.sv
@@ -22,12 +22,20 @@ module glb_bank_memory (
 //===========================================================================//
 // memory-SRAM interface signal declaration
 //===========================================================================//
-logic                                               sram_to_mem_cen;
-logic                                               sram_to_mem_wen;
-logic [BANK_ADDR_WIDTH-BANK_ADDR_BYTE_OFFSET-1:0]   sram_to_mem_addr;
-logic [BANK_DATA_WIDTH-1:0]                         sram_to_mem_data;
-logic [BANK_DATA_WIDTH-1:0]                         sram_to_mem_bit_sel;
-logic [BANK_DATA_WIDTH-1:0]                         mem_to_sram_data;
+logic                                               sram_cen;
+logic                                               sram_wen;
+logic [BANK_ADDR_WIDTH-BANK_ADDR_BYTE_OFFSET-1:0]   sram_addr;
+logic [BANK_DATA_WIDTH-1:0]                         sram_data_in;
+logic [BANK_DATA_WIDTH-1:0]                         sram_bit_sel;
+logic [BANK_DATA_WIDTH-1:0]                         sram_data_out;
+logic                                               ren_d1;
+logic [BANK_DATA_WIDTH-1:0]                         data_out_d1;
+
+assign sram_wen = wen;
+assign sram_cen = wen | ren;
+assign sram_addr = addr[BANK_ADDR_BYTE_OFFSET +: BANK_ADDR_WIDTH-BANK_ADDR_BYTE_OFFSET];
+assign sram_data_in = data_in;
+assign sram_bit_sel = data_in_bit_sel;
 
 //===========================================================================//
 // memory instantiation
@@ -36,13 +44,28 @@ glb_bank_sram_gen #(
     .DATA_WIDTH(BANK_DATA_WIDTH),
     .ADDR_WIDTH(BANK_ADDR_WIDTH-BANK_ADDR_BYTE_OFFSET)
 ) glb_bank_sram_gen (
-    .clk(clk),
-    .clk_en(sram_to_mem_cen),
-    .wen(sram_to_mem_wen),
-    .addr(sram_to_mem_addr),
-    .data_in(sram_to_mem_data),
-    .bit_sel(sram_to_mem_bit_sel),
-    .data_out(mem_to_sram_data)
+    .CLK(clk),
+    .CEB(~sram_cen),
+    .WEB(~sram_wen),
+    .A(sram_addr),
+    .D(sram_data_in),
+    .BWEB(~sram_bit_sel),
+    .Q(sram_data_out)
 );
+
+//===========================================================================//
+// output assignment
+//===========================================================================//
+always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+        ren_d1 <= 0;
+        data_out_d1 <= 0;
+    end
+    else begin
+        ren_d1 <= ren;
+        data_out_d1 <= data_out;
+    end
+end
+assign data_out = ren_d1 ? sram_data_out : data_out_d1;
 
 endmodule
