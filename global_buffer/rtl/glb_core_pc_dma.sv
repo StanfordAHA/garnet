64dda72728e0031fc1e56bf7d1f5971a867c96b4
Taeyoung Kong <kongty0329@gmail.com>
2020-03-08 23:36:34 -0700

    Parallel configuration dma

---
+++ global_buffer/rtl/glb_core_pc_dma.sv
@@ -0,0 +1,35 @@
+/*=============================================================================
+** Module: glb_core_pc_dma.sv
+** Description:
+**              Global Buffer Core PC DMA
+** Author: Taeyoung Kong
+** Change history: 
+**      03/08/2020
+**          - Implement first version of global buffer core parallel config DMA
+**===========================================================================*/
+import  global_buffer_pkg::*;
+
+module glb_core_pc_dma (
+    input  logic                            clk,
+    input  logic                            clk_en,
+    input  logic                            reset,
+
+    // cgra streaming word
+    output cgra_cfg_t                       cgra_cfg_c2sw,
+
+    // read req packet
+    output rdrq_packet_t                    rdrq_packet,
+
+    // read res packet
+    input  rdrs_packet_t                    rdrs_packet,
+
+    // Configuration registers
+    input  logic                            cfg_pc_dma_on,
+    input  dma_pc_header_t                  cfg_pc_dma_header,
+
+    // interrupt pulse
+    input  logic                            cfg_pc_start_pulse,
+    output logic                            pc_done_pulse
+);
+
+endmodule
