ca4a41ef80a2eab924af3f0f0d9e6d014ccace08
Taeyoung Kong <kongty0329@gmail.com>
2020-03-16 21:38:26 -0700

    glb tile pin assignment

--- mflowgen/glb_tile/custom-init/outputs/pin-assignments.tcl
+++ mflowgen/glb_tile/custom-init/outputs/pin-assignments.tcl
@@ -19,5 +19,5 @@ set height [dbGet top.fPlan.box_ury]
 
 editPin -pin [get_property $left hierarchical_name] -start { 0 5 } -end [list 0 [expr {$height - 5}]] -side LEFT -spreadType RANGE -spreadDirection clockwise -layer M6
 editPin -pin [get_property $right hierarchical_name] -start [list $width  5] -end [list $width [expr {$height - 5}]] -side RIGHT -spreadType RANGE -spreadDirection counterclockwise -layer M6
-editPin -pin [get_property $bottom hierarchical_name] -start [list 5 0] -end [list [expr {$width - 5}] 0] -side BOTTOM -spreadType RANGE -spreadDirection clockwise -layer M5
+editPin -pin [get_property $bottom hierarchical_name] -start [list 5 0] -end [list [expr {$width - 5}] 0] -side BOTTOM -spreadType RANGE -spreadDirection counterclockwise -layer M5
 
