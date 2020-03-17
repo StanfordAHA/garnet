110854011ccd3be286ee7090683d70079fa97c55
Alex Carsello <alex.carsello@gmail.com>
2020-03-17 15:27:51 -0700

    Add custom-dc-synth node for fullchip

---
+++ mflowgen/full_chip/custom-dc-synthesis/scripts/tlx_include_paths.tcl
@@ -0,0 +1,10 @@
+# ==============================================================================
+# Include Paths for TLX
+# ==============================================================================
+
+set soc_inc_tlx [concat \
+  rtl/components/nic450/logical/nic400_tlx_aha/amib_M1_m/verilog \
+  rtl/components/nic450/logical/nic400_tlx_aha/cdc_blocks/verilog \
+  rtl/components/nic450/logical/nic400_tlx_aha/reg_slice/verilog \
+  rtl/components/nic450/logical/nic400_tlx_aha/tlx_M1_m_tlx/verilog \
+]
