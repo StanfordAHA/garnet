dfeecd15a69ab078326816c868edec1ab8f27ead
Taeyoung Kong <kongty0329@gmail.com>
2020-03-18 21:50:50 -0700

    Add wrapper to rtl mflowgen step

--- mflowgen/glb_top/rtl/gen_rtl.sh
+++ mflowgen/glb_top/rtl/gen_rtl.sh
@@ -9,3 +9,4 @@ cat $GARNET_HOME/global_buffer/rtl/glb_tile_dummy_axi_s_ctrl.sv >> outputs/desig
 cat $GARNET_HOME/global_buffer/rtl/glb_tile_dummy_intr_ctrl.sv >> outputs/design.v
 cat $GARNET_HOME/global_buffer/rtl/global_buffer.sv >> outputs/design.v
 cat $GARNET_HOME/global_buffer/rtl/glb_tile_stub.sv >> outputs/design.v
+cat $GARNET_HOME/global_buffer/rtl/glb_tile_wrapper.sv >> outputs/design.v
