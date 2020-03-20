a47884bbf7849ac8f129cee925c7e6309265695a
Alex Carsello <alex.carsello@gmail.com>
2020-03-20 09:51:15 -0700

    Update fullchip custom-init node to come after shared pad frame floorplan

---
+++ mflowgen/full_chip/custom-init/outputs/place-macros.tcl
@@ -0,0 +1,13 @@
+#=========================================================================
+# floorplan.tcl
+#=========================================================================
+# This script is called from the Innovus init flow step to place all
+# macros in the full chip floorplan
+#
+# Author : Alex Carsello
+# Date   : March 19,2020
+
+# First, get the sizes of all Garnet macros (Interconnect,
+# global_buffer, and global_controller)
+
+
