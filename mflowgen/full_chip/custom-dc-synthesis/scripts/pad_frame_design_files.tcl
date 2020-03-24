7e99b9d5146ca03a5792910bb441796c8c5d7d52
Alex Carsello <alex.carsello@gmail.com>
2020-03-23 21:15:41 -0700

    Remove soc design scripts from synth node

--- mflowgen/full_chip/custom-dc-synthesis/scripts/pad_frame_design_files.tcl
+++
@@ -1,8 +0,0 @@
-# ==============================================================================
-# loads CGRA design files (RTL)
-# ==============================================================================
-
-# Garnet Files
-set pad_frame_design_files [
-  glob -nocomplain -directory rtl/components/pad_frame/genesis_verif -types f *.v *.sv]
-
