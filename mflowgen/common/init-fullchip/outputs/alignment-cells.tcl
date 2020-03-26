711151d435d5a938845f278a685d5138f66b3a1a
steveri <steveri@steveri.com>
2020-03-26 14:42:32 -0700

    refactoring: halos and blockages

--- mflowgen/common/init-fullchip/outputs/alignment-cells.tcl
+++ mflowgen/common/init-fullchip/outputs/alignment-cells.tcl
@@ -273,6 +273,61 @@ proc get_x_bounds { pos_y core_fp_height } {
     return x_bounds
 }
 
+proc create_grid_route_blockages { fid_name halo_margin } {
+
+    create_route_blockage -name $fid_name -inst $fid_name -cover \
+        -layers {M1 M2 M3 M4 M5 M6 M7 M8 M9} -spacing $halo_margin
+    
+    # sr 1912 FIXME: why via spacing 2u bigger than metal spacing?
+    # sr 1912 FIXME: why halo instead of blockage?
+    # sr 1912 FIXME: why it gotta be so big anyways?
+    # 
+    # sr 2001 got some partial answers maybe
+    # - M1 stripes go up to edge of halo and stop
+    # - endcap for stripes will not place next to blockage
+    # - thus need a bit of halo or no endcaps
+    # - trial and error shows that .05u halo might be enough to get endcaps
+    # - later maybe I'll find out why original blockage has bigger halos
+    #
+    # create_route_blockage -name $fid_name -inst $fid_name -cover \
+        #      -layers {VIA1 VIA2 VIA3 VIA4 VIA5 VIA6 VIA7 VIA8} -spacing [expr $halo_margin + 2]
+    create_route_blockage -name $fid_name -inst $fid_name -cover \
+        -layers {VIA1 VIA2 VIA3 VIA4 VIA5 VIA6 VIA7 VIA8} -spacing $halo_margin
+    
+    # steveri 1912 - HALO NOT GOOD ENOUGH! Router happily installs wires inside the halo :(
+    # Then we get hella DRC errors around the icovl cells.
+    # Solution: need blockages instead and/or as well, nanoroute seems to understand those...
+    # Also need a bit of halo, see comment above about endcaps
+    set inst [get_db insts $fid_name]
+    set name [get_db $inst .name]_bigblockgf
+    set rect [get_db $inst .place_halo_bbox]
+    
+    # Instead of (actually in addition to, for now, but overrides prev)
+    # small blockage with big halo (above), build big blockage w/ tiny halo
+    # Actually new blockage goes ON TOP OF (and overrides) prev for now,
+    # although probably shouldnt :(
+    set halo_metal $halo_margin
+    set new_halo 0.20
+    set llx_metal [expr [get_db $inst .bbox.ll.x] - $halo_metal + $new_halo ]
+    set lly_metal [expr [get_db $inst .bbox.ll.y] - $halo_metal + $new_halo ]
+    set urx_metal [expr [get_db $inst .bbox.ur.x] + $halo_metal - $new_halo ]
+    set ury_metal [expr [get_db $inst .bbox.ur.y] + $halo_metal - $new_halo ]
+    set rect "$llx_metal $lly_metal $urx_metal $ury_metal"
+    create_route_blockage -name $name -rects $rect \
+        -layers {M1 M2 M3 M4 M5 M6 M7 M8 M9} -spacing $new_halo
+    
+    # Originally via halo was bigger than metal halo. but why tho?
+    # set halo_via [expr $halo_margin + 2]
+    set halo_via $halo_margin
+    set llx_via [expr [get_db $inst .bbox.ll.x] - $halo_via + $new_halo ]
+    set lly_via [expr [get_db $inst .bbox.ll.y] - $halo_via + $new_halo ]
+    set urx_via [expr [get_db $inst .bbox.ur.x] + $halo_via - $new_halo ]
+    set ury_via [expr [get_db $inst .bbox.ur.y] + $halo_via - $new_halo ]
+    set rect "$llx_via $lly_via $urx_via $ury_via"
+    create_route_blockage -name $name -rects $rect \
+        -layers {VIA1 VIA2 VIA3 VIA4 VIA5 VIA6 VIA7 VIA8} -spacing $new_halo
+}
+
 proc gen_fiducial_set {pos_x pos_y {id ul} grid {cols 8} {xsepfactor 1.0}} {
     # delete_inst -inst ifid_*
 
@@ -334,67 +389,25 @@ proc gen_fiducial_set {pos_x pos_y {id ul} grid {cols 8} {xsepfactor 1.0}} {
       }
       # FIXME why do this twice? [stever]
       place_inst $fid_name $ix $iy r0; # Overrides/replaces previous placement
+
+      # Halos and blockages for alignment cells
       if {$grid == "true"} {
           set halo_margin_target 15
       } else {
           set halo_margin_target 8
       }
       set halo_margin [snap_to_grid $halo_margin_target 0.09 0]
+
       create_place_halo -insts $fid_name \
-        -halo_deltas $halo_margin $halo_margin $halo_margin $halo_margin -snap_to_site
+          -halo_deltas $halo_margin $halo_margin $halo_margin $halo_margin -snap_to_site
+
       if {$grid == "true"} {
-        create_route_blockage -name $fid_name -inst $fid_name -cover -layers {M1 M2 M3 M4 M5 M6 M7 M8 M9} -spacing $halo_margin
-
-        # sr 1912 FIXME: why via spacing 2u bigger than metal spacing?
-        # sr 1912 FIXME: why halo instead of blockage?
-        # sr 1912 FIXME: why it gotta be so big anyways?
-        # 
-        # sr 2001 got some partial answers maybe
-        # - M1 stripes go up to edge of halo and stop
-        # - endcap for stripes will not place next to blockage
-        # - thus need a bit of halo or no endcaps
-        # - trial and error shows that .05u halo might be enough to get endcaps
-        # - later maybe I'll find out why original blockage has bigger halos
-        #
-        # create_route_blockage -name $fid_name -inst $fid_name -cover -layers {VIA1 VIA2 VIA3 VIA4 VIA5 VIA6 VIA7 VIA8} -spacing [expr $halo_margin + 2]
-        create_route_blockage -name $fid_name -inst $fid_name -cover -layers {VIA1 VIA2 VIA3 VIA4 VIA5 VIA6 VIA7 VIA8} -spacing $halo_margin
-
-          # steveri 1912 - HALO NOT GOOD ENOUGH! Router happily installs wires inside the halo :(
-          # Then we get hella DRC errors around the icovl cells.
-          # Solution: need blockages instead and/or as well, nanoroute seems to understand those...
-          # Also need a bit of halo, see comment above about endcaps
-          set inst [get_db insts $fid_name]
-          set name [get_db $inst .name]_bigblockgf
-          set rect [get_db $inst .place_halo_bbox]
-
-          # Instead of (actually in addition to, for now, but overrides prev)
-          # small blockage with big halo (above), build big blockage w/ tiny halo
-          # Actually new blockage goes ON TOP OF (and overrides) prev for now,
-          # although probably shouldnt :(
-          set halo_metal $halo_margin
-          set new_halo 0.20
-          set llx_metal [expr [get_db $inst .bbox.ll.x] - $halo_metal + $new_halo ]
-          set lly_metal [expr [get_db $inst .bbox.ll.y] - $halo_metal + $new_halo ]
-          set urx_metal [expr [get_db $inst .bbox.ur.x] + $halo_metal - $new_halo ]
-          set ury_metal [expr [get_db $inst .bbox.ur.y] + $halo_metal - $new_halo ]
-          set rect "$llx_metal $lly_metal $urx_metal $ury_metal"
-          create_route_blockage -name $name -rects $rect \
-            -layers {M1 M2 M3 M4 M5 M6 M7 M8 M9} -spacing $new_halo
-
-          # Originally via halo was bigger than metal halo. but why tho?
-          # set halo_via [expr $halo_margin + 2]
-          set halo_via $halo_margin
-          set llx_via [expr [get_db $inst .bbox.ll.x] - $halo_via + $new_halo ]
-          set lly_via [expr [get_db $inst .bbox.ll.y] - $halo_via + $new_halo ]
-          set urx_via [expr [get_db $inst .bbox.ur.x] + $halo_via - $new_halo ]
-          set ury_via [expr [get_db $inst .bbox.ur.y] + $halo_via - $new_halo ]
-          set rect "$llx_via $lly_via $urx_via $ury_via"
-          create_route_blockage -name $name -rects $rect \
-            -layers {VIA1 VIA2 VIA3 VIA4 VIA5 VIA6 VIA7 VIA8} -spacing $new_halo
-
+          create_grid_route_blockages $fid_name $halo_margin
       } else {
-        create_route_blockage -name $fid_name -inst $fid_name -cover -layers {M1 M2 M3 M4 M5 M6 M7 M8 M9} -spacing 2.5
+          create_route_blockage -name $fid_name -inst $fid_name -cover -layers {M1 M2 M3 M4 M5 M6 M7 M8 M9} -spacing 2.5
       }
+
+
       if {$grid == "true"} {
 
           # set ix 12; set pos_x 2; set dx 3; set cols 777;
