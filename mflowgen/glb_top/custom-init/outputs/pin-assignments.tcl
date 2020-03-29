8bb61b52865c47b9ee5c0884bee90f2559c6569c
Taeyoung Kong <kongty0329@gmail.com>
2020-03-28 19:27:07 -0700

    Fix pin-assignment script

--- mflowgen/glb_top/custom-init/outputs/pin-assignments.tcl
+++ mflowgen/glb_top/custom-init/outputs/pin-assignments.tcl
@@ -11,36 +11,39 @@
 set num_glb_tiles 16
 set num_cgra_tiles 2
 
-set all [sort_collection get_ports hierarchical_name]
+set all [sort_collection [get_ports] hierarchical_name]
 
 for {set i 0} {$i < $num_glb_tiles} {incr i} {
     for {set j 0} {$j < $num_cgra_tiles} {incr j} {
-        set tile_ports($i, $j) [sort_collection [get_ports {stream_data_\^(|valid_)% /\^(f2g|g2f)$/\[$i\]\[$j\]* cgra_cfg_g2f\[$i\]\[$j\]*}] hierarchical_name]
+        set tile_ports($i,$j) [sort_collection [get_ports -regexp "(stream_data_(|valid_)(f2g|g2f)(.)($i)(..)($j)(.)*)|(cgra_cfg_g2f(.)($i)(..)($j)(.)*)"] hierarchical_name]
     }
 }
 
+unset cache
 for {set i 0} {$i < $num_glb_tiles} {incr i} {
     for {set j 0} {$j < $num_cgra_tiles} {incr j} {
-        foreach port $tile_ports($i, $j) {
-            lappend cache($port) 1
+        foreach port [get_object_name $tile_ports($i,$j)] {
+            lappend cache $port
         }
     }
 }
 
-foreach a $all {
-    if {![info exists cache($a)]} {
+# left is a list that holds object name that is not in tile_ports
+unset left
+foreach a [get_object_name $all] {
+    if {$a ni $cache} {
         lappend left $a
     }
 }
 
 set width [dbGet top.fPlan.box_urx]
 set height [dbGet top.fPlan.box_ury]
-set tile_width [expr {$width/($num_glb_tiles*$num_cgra_tiles)}]
+set tile_width [dbGet [dbGet -p top.insts.name *glb_tile* -i 0].cell.size_x]
 
-editPin -pin [get_property $left hierarchical_name] -start { 0 5 } -end [list 0 [expr {$height - 5}]] -side LEFT -spreadType RANGE -spreadDirection clockwise -layer M6
+editPin -pin $left -start { 0 5 } -end [list 0 [expr {$height - 5}]] -side LEFT -spreadType RANGE -spreadDirection clockwise -layer M6
 
 for {set i 0} {$i < $num_glb_tiles} {incr i} {
     for {set j 0} {$j < $num_cgra_tiles} {incr j} {
-        editPin -pin [get_property $tile_ports($i, $j) hierarchical_name] -start [list [expr {$tile_width*(2*i+j)+3}] 0] -end [list [expr {$tile_width*(2*i+j+1)-3}] 0] -side BOTTOM -spreadType RANGE -spreadDirection counterclockwise -layer M5
+        editPin -pin [get_property $tile_ports($i,$j) hierarchical_name] -start [list [expr {($tile_width/2)*(2*$i+$j)+3}] 0] -end [list [expr {($tile_width/2)*(2*$i+$j+1)-3}] 0] -side BOTTOM -spreadType RANGE -spreadDirection counterclockwise -layer M5
     }
 }
