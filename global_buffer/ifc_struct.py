3695a851187d9a402442ecd3c63482baabd6b5bd
Taeyoung Kong <kongty0329@gmail.com>
2020-03-30 06:36:30 -0700

    Add wrapper for glb

--- global_buffer/ifc_struct.py
+++ global_buffer/ifc_struct.py
@@ -8,12 +8,12 @@ def ProcPacketIfc(addr_width, data_width):
 
     _Slave = magma.Product.from_fields("ProcPacketIfcSlave", dict(
         wr_en=magma.In(magma.Bit),
-        wr_strb=magma.In(magma.Bits[int(self.data_width/8)]),
-        wr_addr=magma.In(magma.Bits[self.addr_width]),
-        wr_data=magma.In(magma.Bits[self.data_width]),
+        wr_strb=magma.In(magma.Bits[int(data_width/8)]),
+        wr_addr=magma.In(magma.Bits[addr_width]),
+        wr_data=magma.In(magma.Bits[data_width]),
         rd_en=magma.In(magma.Bit),
-        rd_addr=magma.In(magma.Bits[self.addr_width]),
-        rd_data=magma.Out(magma.Bits[self.data_width]),
+        rd_addr=magma.In(magma.Bits[addr_width]),
+        rd_data=magma.Out(magma.Bits[data_width]),
         rd_data_valid=magma.Out(magma.Bit)))
 
     return _Slave
@@ -38,7 +38,8 @@ def GlbCfgIfc(addr_width, data_width):
     return _Slave
 
 def CgraCfgStruct(addr_width, data_width):
-    return magma.Tuple(wr_en=magma.Bits[1],
-                       wr_clk_en=magma.Bits[1],
-                       wr_addr=magma.Bits[addr_width],
-                       wr_data=magma.Bits[data_width])
+    return magma.Product.from_fields("CgraCfgStruct",
+                                     dict(wr_en=magma.Bits[1],
+                                          wr_clk_en=magma.Bits[1],
+                                          wr_addr=magma.Bits[addr_width],
+                                          wr_data=magma.Bits[data_width]))
