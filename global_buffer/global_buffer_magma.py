b3195375b6b4218bcaf8a08aebadc6ade8bc2834
Taeyoung Kong <kongty0329@gmail.com>
2020-03-30 06:05:59 -0700

    Add new magma wrapper

--- global_buffer/global_buffer_magma.py
+++ global_buffer/global_buffer_magma.py
@@ -1,176 +1,67 @@
 import magma
-from gemstone.common.configurable import ConfigurationType
-from gemstone.generator.const import Const
-from gemstone.generator.from_magma import FromMagma
+from gemstone.generator.from_verilog import FromVerilog
 from gemstone.generator.generator import Generator
-from . import global_buffer_genesis2
-from global_buffer.soc_data_type import SoCDataType
-
+from global_buffer.ifc_struct import *
 
 class GlobalBuffer(Generator):
-    def __init__(self, num_banks, num_io, num_cfg, bank_addr_width,
-                 glb_addr_width=32, cfg_addr_width=32, cfg_data_width=32,
-                 axi_addr_width=12):
+    def __init__(self, num_glb_tiles, num_cgra_col, banks_per_tile,
+                 bank_addr_width, bank_data_width, cgra_data_width,
+                 axi_addr_width, axi_data_width,
+                 cfg_addr_width, cfg_data_width):
+
         super().__init__()
 
-        self.num_banks = num_banks
+        self.num_glb_tiles = glb_tiles
+        self.num_cgra_col = num_cgra_col
+        self.col_per_glb = num_cgra_col / num_glb_tiles
+        self.banks_per_tile = banks_per_tile
         self.bank_addr_width = bank_addr_width
-        self.glb_addr_width = glb_addr_width
-        self.num_io = num_io
-        self.num_cfg = num_cfg
-        self.bank_data = 64
-        self.cgra_data = 16
+        self.bank_data_width = bank_data_width
+        self.cgra_data_width = cgra_data_width
+        self.axi_addr_width = axi_addr_width
+        self.axi_data_width = axi_data_width
         self.cfg_addr_width = cfg_addr_width
         self.cfg_data_width = cfg_data_width
-        self.axi_addr_width = axi_addr_width
 
-        self.config_type = ConfigurationType(self.cfg_addr_width,
-                                             self.cfg_data_width)
-        self.axi_config_type = ConfigurationType(self.axi_addr_width,
-                                                 self.cfg_data_width)
+        self.cgra_cfg_type = CgraCfgStruct(self.cfg_addr_width,
+                                           self.cfg_data_width)
 
         self.add_ports(
             clk=magma.In(magma.Clock),
             reset=magma.In(magma.AsyncReset),
+            stall=magma.In(magma.Bits[1]),
 
-            soc_data=SoCDataType(self.glb_addr_width, self.bank_data),
-            cgra_to_io_wr_en=magma.In(magma.Array[self.num_io, magma.Bit]),
-            cgra_to_io_rd_en=magma.In(magma.Array[self.num_io, magma.Bit]),
-            io_to_cgra_rd_data_valid=magma.Out(
-                magma.Array[self.num_io, magma.Bit]),
-            cgra_to_io_wr_data=magma.In(
-                magma.Array[self.num_io, magma.Bits[self.cgra_data]]),
-            io_to_cgra_rd_data=magma.Out(
-                magma.Array[self.num_io, magma.Bits[self.cgra_data]]),
-            cgra_to_io_addr_high=magma.In(
-                magma.Array[self.num_io, magma.Bits[self.cgra_data]]),
-            cgra_to_io_addr_low=magma.In(
-                magma.Array[self.num_io, magma.Bits[self.cgra_data]]),
+            proc_packet=ProcPacketIfc(self.glb_addr_width,
+                                      self.bank_data_width).slave,
+            glb_cfg=GlbCfgIfc(self.axi_addr_width,
+                              self.axi_data_width).slave,
+            sram_cfg=GlbCfgIfc(self.glb_addr_width,
+                               self.axi_data_width).slave,
 
-            glc_to_io_stall=magma.In(magma.Bit),
+            stream_data_f2g=magma.In(
+                magma.Array[self.num_glb_tiles*self.col_per_glb,
+                            magma.Bits[self.cgra_data_width]]),
+            stream_data_valid_f2g=magma.In(
+                magma.Array[self.num_glb_tiles*self.col_per_glb,
+                            magma.Bits[1]]),
+            stream_data_g2f=magma.Out(
+                magma.Array[self.num_glb_tiles*self.col_per_glb,
+                            magma.Bits[self.cgra_data_width]]),
+            stream_data_valid_g2f=magma.Out(
+                magma.Array[self.num_glb_tiles*self.col_per_glb,
+                            magma.Bits[1]]),
 
-            cgra_start_pulse=magma.In(magma.Bit),
-            cgra_done_pulse=magma.Out(magma.Bit),
-            config_start_pulse=magma.In(magma.Bit),
-            config_done_pulse=magma.Out(magma.Bit),
+            cgra_cfg_jtag=magma.In(self.cgra_cfg_type),
+            cgra_cfg_g2f=magma.Out(
+                magma.Array[self.num_glb_tiles*self.col_per_glb,
+                            self.cgra_cfg_type]),
 
-            cgra_config=magma.In(self.config_type),
-            glb_to_cgra_config=magma.Out(
-                magma.Array[self.num_cfg, self.config_type]),
-
-            glb_config=magma.In(self.axi_config_type),
-            glb_config_rd_data=magma.Out(magma.Bits[self.cfg_data_width]),
-            glb_sram_config=magma.In(self.config_type),
-            glb_sram_config_rd_data=magma.Out(magma.Bits[self.cfg_data_width])
+            strm_start_pulse=magma.In(magma.Bits[self.num_glb_tiles]),
+            pc_start_pulse=magma.In(magma.Bits[self.num_glb_tiles]),
+            interrupt_pulse=magma.Out(magma.Bits[3*self.num_glb_tiles]),
         )
 
-        wrapper = global_buffer_genesis2.glb_wrapper
-        param_mapping = global_buffer_genesis2.param_mapping
-        generator = wrapper.generator(param_mapping, mode="declare")
-        circ = generator(num_banks=self.num_banks,
-                         num_io=self.num_io,
-                         num_cfg=self.num_cfg,
-                         bank_addr_width=self.bank_addr_width,
-                         cfg_addr_width=self.cfg_addr_width,
-                         cfg_data=self.cfg_data_width)
-        self.underlying = FromMagma(circ)
-
-        self.wire(self.ports.clk, self.underlying.ports.clk)
-        self.wire(self.ports.reset, self.underlying.ports.reset)
-
-        self.wire(self.ports.soc_data.wr_strb,
-                  self.underlying.ports.host_wr_strb)
-        self.wire(self.ports.soc_data.wr_addr,
-                  self.underlying.ports.host_wr_addr)
-        self.wire(self.ports.soc_data.wr_data,
-                  self.underlying.ports.host_wr_data)
-
-        self.wire(self.ports.soc_data.rd_en,
-                  self.underlying.ports.host_rd_en)
-        self.wire(self.ports.soc_data.rd_addr,
-                  self.underlying.ports.host_rd_addr)
-        self.wire(self.ports.soc_data.rd_data,
-                  self.underlying.ports.host_rd_data)
-
-        for i in range(self.num_io):
-            self.wire(self.ports.cgra_to_io_wr_en[i],
-                      self.underlying.ports.cgra_to_io_wr_en[i])
-            self.wire(self.ports.cgra_to_io_rd_en[i],
-                      self.underlying.ports.cgra_to_io_rd_en[i])
-            self.wire(self.ports.io_to_cgra_rd_data_valid[i],
-                      self.underlying.ports.io_to_cgra_rd_data_valid[i])
-            self.wire(self.ports.cgra_to_io_wr_data[i],
-                      self.underlying.ports.cgra_to_io_wr_data[
-                          i * self.cgra_data:(i + 1) * self.cgra_data])
-            self.wire(self.ports.io_to_cgra_rd_data[i],
-                      self.underlying.ports.io_to_cgra_rd_data[
-                          i * self.cgra_data:(i + 1) * self.cgra_data])
-            self.wire(self.ports.cgra_to_io_addr_high[i],
-                      self.underlying.ports.cgra_to_io_addr_high[
-                          i * self.cgra_data:(i + 1) * self.cgra_data])
-            self.wire(self.ports.cgra_to_io_addr_low[i],
-                      self.underlying.ports.cgra_to_io_addr_low[
-                          i * self.cgra_data:(i + 1) * self.cgra_data])
-
-        for i in range(self.num_cfg):
-            self.wire(self.ports.glb_to_cgra_config[i].write[0],
-                      self.underlying.ports.glb_to_cgra_cfg_wr[i])
-            self.wire(self.ports.glb_to_cgra_config[i].read[0],
-                      self.underlying.ports.glb_to_cgra_cfg_rd[i])
-            self.wire(self.ports.glb_to_cgra_config[i].config_addr,
-                      self.underlying.ports.glb_to_cgra_cfg_addr[
-                          i * self.cfg_addr_width:
-                          (i + 1) * self.cfg_addr_width])
-            self.wire(self.ports.glb_to_cgra_config[i].config_data,
-                      self.underlying.ports.glb_to_cgra_cfg_data[
-                          i * self.cfg_data_width:
-                          (i + 1) * self.cfg_data_width])
-
-        self.wire(self.ports.glc_to_io_stall,
-                  self.underlying.ports.glc_to_io_stall)
-
-        self.wire(self.ports.cgra_config.write[0],
-                  self.underlying.ports.glc_to_cgra_cfg_wr)
-        self.wire(self.ports.cgra_config.read[0],
-                  self.underlying.ports.glc_to_cgra_cfg_rd)
-        self.wire(self.ports.cgra_config.config_addr,
-                  self.underlying.ports.glc_to_cgra_cfg_addr)
-        self.wire(self.ports.cgra_config.config_data,
-                  self.underlying.ports.glc_to_cgra_cfg_data)
-
-        self.wire(self.ports.cgra_start_pulse,
-                  self.underlying.ports.cgra_start_pulse)
-        self.wire(self.ports.cgra_done_pulse,
-                  self.underlying.ports.cgra_done_pulse)
-        self.wire(self.ports.config_start_pulse,
-                  self.underlying.ports.config_start_pulse)
-        self.wire(self.ports.config_done_pulse,
-                  self.underlying.ports.config_done_pulse)
-
-        self.wire(self.ports.glb_config.write[0],
-                  self.underlying.ports.glb_config_wr)
-        self.wire(self.ports.glb_config.read[0],
-                  self.underlying.ports.glb_config_rd)
-        self.wire(self.ports.glb_config.config_data,
-                  self.underlying.ports.glb_config_wr_data)
-        self.wire(self.ports.glb_config.config_addr,
-                  self.underlying.ports.glb_config_addr)
-        self.wire(self.ports.glb_config_rd_data,
-                  self.underlying.ports.glb_config_rd_data)
-
-        self.wire(self.ports.glb_sram_config.write[0],
-                  self.underlying.ports.glb_sram_config_wr)
-        self.wire(self.ports.glb_sram_config.read[0],
-                  self.underlying.ports.glb_sram_config_rd)
-        self.wire(self.ports.glb_sram_config.config_data,
-                  self.underlying.ports.glb_sram_config_wr_data)
-        self.wire(self.ports.glb_sram_config.config_addr,
-                  self.underlying.ports.glb_sram_config_addr)
-        self.wire(self.ports.glb_sram_config_rd_data,
-                  self.underlying.ports.glb_sram_config_rd_data)
+        self.underlying = FromVerilog(file_name="./global_buffer/rtl/global_buffer.sv")
 
     def name(self):
-        return f"GlobalBuffer_{self.num_banks}_{self.num_io}_"\
-               f"{self.num_cfg}_{self.bank_addr_width}_{self.glb_addr_width}_"\
-               f"{self.cfg_addr_width}_{self.cfg_data_width}_"\
-               f"{self.axi_addr_width}"
+        return f"GlobalBuffer_{self.num_glb_tiles}_{self.num_cgra_col}"
