fa60b2d8498a1fb8a74e2a6d722ca5b5ba49b18f
Taeyoung Kong <kongty0329@gmail.com>
2020-03-30 05:46:37 -0700

    Finish pin assignment

--- mflowgen/glb_top/custom-init/outputs/pin-assignments.tcl
+++ mflowgen/glb_top/custom-init/outputs/pin-assignments.tcl
@@ -10,26 +10,79 @@
 
 set num_glb_tiles 16
 set num_cgra_tiles 2
+set cgra_data_width 16
+set cgra_cfg_addr_width 32
+set cgra_cfg_data_width 32
+
 
 set all [sort_collection [get_ports] hierarchical_name]
 
 for {set i 0} {$i < $num_glb_tiles} {incr i} {
     for {set j 0} {$j < $num_cgra_tiles} {incr j} {
-        set tile_ports($i,$j) [sort_collection [get_ports -regexp "(stream_data_(|valid_)(f2g|g2f)(.)($i)(..)($j)(.)*)|(cgra_cfg_g2f(.)($i)(..)($j)(.)*)"] hierarchical_name]
+        for {set k 0} {$k < $cgra_data_width} {incr k} {
+            lappend tile_ports($i,$j) [get_object_name [get_ports "stream_data_f2g[[expr {$i*$num_cgra_tiles*$cgra_data_width+$j*$cgra_data_width+$k}]]"]]
+            lappend tile_ports($i,$j) [get_object_name [get_ports "stream_data_g2f[[expr {$i*$num_cgra_tiles*$cgra_data_width+$j*$cgra_data_width+$k}]]"]]
+        }
     }
 }
 
-unset cache
 for {set i 0} {$i < $num_glb_tiles} {incr i} {
     for {set j 0} {$j < $num_cgra_tiles} {incr j} {
-        foreach port [get_object_name $tile_ports($i,$j)] {
+        lappend tile_ports($i,$j) [get_object_name [get_ports "stream_data_valid_f2g[[expr {$i*$num_cgra_tiles+$j}]]"]]
+        lappend tile_ports($i,$j) [get_object_name [get_ports "stream_data_valid_g2f[[expr {$i*$num_cgra_tiles+$j}]]"]]
+    }
+}
+
+for {set i 0} {$i < $num_glb_tiles} {incr i} {
+    for {set j 0} {$j < $num_cgra_tiles} {incr j} {
+        for {set k 0} {$k < $cgra_cfg_addr_width} {incr k} {
+            lappend tile_ports($i,$j) [get_object_name [get_ports "cgra_cfg_g2f_cfg_addr[[expr {$i*$num_cgra_tiles*$cgra_cfg_addr_width+$j*$cgra_cfg_addr_width+$k}]]"]]
+        }
+    }
+}
+
+for {set i 0} {$i < $num_glb_tiles} {incr i} {
+    for {set j 0} {$j < $num_cgra_tiles} {incr j} {
+        for {set k 0} {$k < $cgra_cfg_data_width} {incr k} {
+            lappend tile_ports($i,$j) [get_object_name [get_ports "cgra_cfg_g2f_cfg_data[[expr {$i*$num_cgra_tiles*$cgra_cfg_data_width+$j*$cgra_cfg_data_width+$k}]]"]]
+        }
+    }
+}
+
+for {set i 0} {$i < $num_glb_tiles} {incr i} {
+    for {set j 0} {$j < $num_cgra_tiles} {incr j} {
+        lappend tile_ports($i,$j) [get_object_name [get_ports "cgra_cfg_g2f_cfg_wr_en[[expr {$i*$num_cgra_tiles+$j}]]"]]
+        lappend tile_ports($i,$j) [get_object_name [get_ports "cgra_cfg_g2f_cfg_rd_en[[expr {$i*$num_cgra_tiles+$j}]]"]]
+    }
+}
+
+for {set i 0} {$i < $num_glb_tiles} {incr i} {
+    for {set k 0} {$k < 3} {incr k} {
+        lappend tile_ports($i,$j) [get_object_name [get_ports "interrupt_pulse[[expr {$i*3+$k}]]"]]
+    }
+}
+
+for {set i 0} {$i < $num_glb_tiles} {incr i} {
+    lappend tile_ports($i,$j) [get_object_name [get_ports "strm_start_pulse[$i]"]]
+    lappend tile_ports($i,$j) [get_object_name [get_ports "pc_start_pulse[$i]"]]
+}
+
+# sorting
+for {set i 0} {$i < $num_glb_tiles} {incr i} {
+    for {set j 0} {$j < $num_cgra_tiles} {incr j} {
+        set tile_ports($i,$j) [lsort $tile_ports($i,$j)]
+    }
+}
+
+for {set i 0} {$i < $num_glb_tiles} {incr i} {
+    for {set j 0} {$j < $num_cgra_tiles} {incr j} {
+        foreach port $tile_ports($i,$j) {
             lappend cache $port
         }
     }
 }
 
 # left is a list that holds object name that is not in tile_ports
-unset left
 foreach a [get_object_name $all] {
     if {$a ni $cache} {
         lappend left $a
@@ -44,6 +97,6 @@ editPin -pin $left -start { 0 5 } -end [list 0 [expr {$height - 5}]] -side LEFT 
 
 for {set i 0} {$i < $num_glb_tiles} {incr i} {
     for {set j 0} {$j < $num_cgra_tiles} {incr j} {
-        editPin -pin [get_property $tile_ports($i,$j) hierarchical_name] -start [list [expr {($tile_width/2)*(2*$i+$j)+3}] 0] -end [list [expr {($tile_width/2)*(2*$i+$j+1)-3}] 0] -side BOTTOM -spreadType RANGE -spreadDirection counterclockwise -layer M5
+        editPin -pin $tile_ports($i,$j) -start [list [expr {($tile_width/2)*(2*$i+$j)+3}] 0] -end [list [expr {($tile_width/2)*(2*$i+$j+1)-3}] 0] -side BOTTOM -spreadType RANGE -spreadDirection counterclockwise -layer M5
     }
 }
