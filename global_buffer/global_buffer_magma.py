ada0b5066e2c61f604300005410fc2121857b777
Taeyoung Kong <kongty0329@gmail.com>
2020-03-30 06:36:30 -0700

    Add wrapper for glb

--- global_buffer/global_buffer_magma.py
+++ global_buffer/global_buffer_magma.py
@@ -1,17 +1,18 @@
 import magma
+from math import log2, ceil
 from gemstone.generator.from_verilog import FromVerilog
 from gemstone.generator.generator import Generator
 from global_buffer.ifc_struct import *
 
 class GlobalBuffer(Generator):
-    def __init__(self, num_glb_tiles, num_cgra_col, banks_per_tile,
-                 bank_addr_width, bank_data_width, cgra_data_width,
-                 axi_addr_width, axi_data_width,
-                 cfg_addr_width, cfg_data_width):
+    def __init__(self, num_glb_tiles, num_cgra_col, banks_per_tile=2,
+                 bank_addr_width=17, bank_data_width=64, cgra_data_width=16,
+                 axi_addr_width=12, axi_data_width=32,
+                 cfg_addr_width=32, cfg_data_width=32):
 
         super().__init__()
 
-        self.num_glb_tiles = glb_tiles
+        self.num_glb_tiles = num_glb_tiles
         self.num_cgra_col = num_cgra_col
         self.col_per_glb = num_cgra_col / num_glb_tiles
         self.banks_per_tile = banks_per_tile
@@ -22,6 +23,9 @@ class GlobalBuffer(Generator):
         self.axi_data_width = axi_data_width
         self.cfg_addr_width = cfg_addr_width
         self.cfg_data_width = cfg_data_width
+        self.glb_addr_width = (self.bank_addr_width
+                               + ceil(log2(self.col_per_glb))
+                               + ceil(log2(self.num_glb_tiles)))
 
         self.cgra_cfg_type = CgraCfgStruct(self.cfg_addr_width,
                                            self.cfg_data_width)
@@ -32,28 +36,28 @@ class GlobalBuffer(Generator):
             stall=magma.In(magma.Bits[1]),
 
             proc_packet=ProcPacketIfc(self.glb_addr_width,
-                                      self.bank_data_width).slave,
+                                      self.bank_data_width),
             glb_cfg=GlbCfgIfc(self.axi_addr_width,
-                              self.axi_data_width).slave,
+                              self.axi_data_width),
             sram_cfg=GlbCfgIfc(self.glb_addr_width,
-                               self.axi_data_width).slave,
+                               self.axi_data_width),
 
             stream_data_f2g=magma.In(
-                magma.Array[self.num_glb_tiles*self.col_per_glb,
+                magma.Array[int(self.num_glb_tiles*self.col_per_glb),
                             magma.Bits[self.cgra_data_width]]),
             stream_data_valid_f2g=magma.In(
-                magma.Array[self.num_glb_tiles*self.col_per_glb,
+                magma.Array[int(self.num_glb_tiles*self.col_per_glb),
                             magma.Bits[1]]),
             stream_data_g2f=magma.Out(
-                magma.Array[self.num_glb_tiles*self.col_per_glb,
+                magma.Array[int(self.num_glb_tiles*self.col_per_glb),
                             magma.Bits[self.cgra_data_width]]),
             stream_data_valid_g2f=magma.Out(
-                magma.Array[self.num_glb_tiles*self.col_per_glb,
+                magma.Array[int(self.num_glb_tiles*self.col_per_glb),
                             magma.Bits[1]]),
 
             cgra_cfg_jtag=magma.In(self.cgra_cfg_type),
             cgra_cfg_g2f=magma.Out(
-                magma.Array[self.num_glb_tiles*self.col_per_glb,
+                magma.Array[int(self.num_glb_tiles*self.col_per_glb),
                             self.cgra_cfg_type]),
 
             strm_start_pulse=magma.In(magma.Bits[self.num_glb_tiles]),
@@ -61,7 +65,14 @@ class GlobalBuffer(Generator):
             interrupt_pulse=magma.Out(magma.Bits[3*self.num_glb_tiles]),
         )
 
-        self.underlying = FromVerilog(file_name="./global_buffer/rtl/global_buffer.sv")
+        self.underlying = FromVerilog(filename="./global_buffer/rtl/global_buffer.sv")
 
     def name(self):
         return f"GlobalBuffer_{self.num_glb_tiles}_{self.num_cgra_col}"
+
+def main():
+    global_buffer = GlobalBuffer(4, 8)
+    print(global_buffer.underlying)
+
+if __name__ == "__main__":
+    main()
