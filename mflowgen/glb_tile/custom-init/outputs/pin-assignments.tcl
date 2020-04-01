0816950334ea0ac831745c7e68621e7be03b05f2
Taeyoung Kong <kongty0329@gmail.com>
2020-04-01 16:23:41 -0700

    Change pin-assignment script

--- mflowgen/glb_tile/custom-init/outputs/pin-assignments.tcl
+++ mflowgen/glb_tile/custom-init/outputs/pin-assignments.tcl
@@ -35,15 +35,64 @@ proc port_compare {a b} {
 }
 
 # Spread the ports for abutment.
+set all [sort_collection [get_ports] hierarchical_name]
+
+set cols_per_tile 2
+set cgra_data_width 16
+set cgra_cfg_addr_width 32
+set cgra_cfg_data_width 32
+
+for {set j 0} {$j < $cols_per_tile} {incr j} {
+    for {set k 0} {$k < $cgra_data_width} {incr k} {
+        lappend bottom_col($j) [get_object_name [get_ports "stream_data_f2g[[expr {$j*$cgra_data_width+$k}]]"]]
+        lappend bottom_col($j) [get_object_name [get_ports "stream_data_g2f[[expr {$j*$cgra_data_width+$k}]]"]]
+    }
+    lappend bottom_col($j) [get_object_name [get_ports "stream_data_valid_f2g[$j]"]]
+    lappend bottom_col($j) [get_object_name [get_ports "stream_data_valid_g2f[$j]"]]
+    for {set k 0} {$k < $cgra_cfg_addr_width} {incr k} {
+        lappend bottom_col($j) [get_object_name [get_ports "cgra_cfg_g2f_cfg_addr[[expr {$j*$cgra_cfg_addr_width+$k}]]"]]
+    }
+    for {set k 0} {$k < $cgra_cfg_data_width} {incr k} {
+        lappend bottom_col($j) [get_object_name [get_ports "cgra_cfg_g2f_cfg_data[[expr {$j*$cgra_cfg_data_width+$k}]]"]]
+    }
+    lappend bottom_col($j) [get_object_name [get_ports "cgra_cfg_g2f_cfg_wr_en[$j]"]]
+    lappend bottom_col($j) [get_object_name [get_ports "cgra_cfg_g2f_cfg_rd_en[$j]"]]
+}
 
 set left [lsort -command port_compare [get_property [get_ports *_wst*] hierarchical_name]]
 set right [lsort -command port_compare [get_property [get_ports *_est*] hierarchical_name]]
-set bottom [lsort -command port_compare [get_property [get_ports -filter {hierarchical_name !~ *_wst* && hierarchical_name !~ *_est*}] hierarchical_name]]
+
+foreach port $left {
+    lappend cache $port
+}
+
+foreach port $right {
+    lappend cache $port
+}
+
+for {set i 0} {$i < $cols_per_tile} {incr i} {
+    foreach port $bottom_col($i) {
+        lappend cache $port
+    }
+}
+
+# bottom is a list that holds object name that is not in bottom_cols, left, or right
+foreach a [get_object_name $all] {
+    if {$a ni $cache} {
+        lappend bottom $a
+    }
+}
+
+set bottom [lsort -command port_compare $bottom]
 
 set width [dbGet top.fPlan.box_urx]
 set height [dbGet top.fPlan.box_ury]
 
 editPin -pin $left -start { 0 5 } -end [list 0 [expr {$height - 5}]] -side LEFT -spreadType RANGE -spreadDirection clockwise -layer M6
 editPin -pin $right -start [list $width  5] -end [list $width [expr {$height - 5}]] -side RIGHT -spreadType RANGE -spreadDirection counterclockwise -layer M6
-editPin -pin $bottom -start [list 5 0] -end [list [expr {$width - 5}] 0] -side BOTTOM -spreadType RANGE -spreadDirection counterclockwise -layer M5
+
+for {set j 0} {$j < $cols_per_tile} {incr j} {
+    editPin -pin $bottom_col($j) -start [list [expr {($width/2)*$j+10}] 0] -end [list [expr {($width/2)*($j+1)-10}] 0] -side BOTTOM -spreadType RANGE -spreadDirection counterclockwise -layer M5
+}
+editPin -pin $bottom -start [list 5 0] -end [list 8 0] -side BOTTOM -spreadType RANGE -spreadDirection counterclockwise -layer M5
 
