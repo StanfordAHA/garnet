50140db35a3278774fd89588180c92c9eb66a743
Taeyoung Kong <kongty0329@gmail.com>
2020-04-01 17:08:24 -0700

    Change spice to verilog for lvs

--- mflowgen/glb_top/glb_tile/get_glb_outputs.sh
+++ mflowgen/glb_top/glb_tile/get_glb_outputs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-../../configure --design $GARNET_HOME/mflowgen/glb_tile/
+mflowgen run --design $GARNET_HOME/mflowgen/glb_tile/
 make synopsys-ptpx-genlibdb
 make mentor-calibre-gdsmerge
 make mentor-calibre-lvs
@@ -7,6 +7,6 @@ mkdir -p outputs
 cp -L *synopsys-ptpx-genlibdb/outputs/design.lib outputs/glb_tile_tt.lib
 cp -L *synopsys-ptpx-genlibdb/outputs/design.db outputs/glb_tile.db
 cp -L *cadence-innovus-signoff/outputs/design.lef outputs/glb_tile.lef
+cp -L *cadence-innovus-signoff/outputs/design.lvs.v outputs/glb_tile.lvs.v
 cp -L *mentor-calibre-gdsmerge/outputs/design_merged.gds outputs/glb_tile.gds
-cp -L *mentor-calibre-lvs/outputs/design.schematic.spi outputs/glb_tile.schematic.spi
 
