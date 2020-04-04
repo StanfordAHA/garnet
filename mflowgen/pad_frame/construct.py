ac41099e4ddb109c429b144962c332c52617d9cd
steveri <steveri@steveri.com>
2020-04-04 11:45:15 -0700

    assertion to check for unconnected bumps

--- mflowgen/pad_frame/construct.py
+++ mflowgen/pad_frame/construct.py
@@ -82,6 +82,12 @@ def construct():
   lvs          = Step( 'mentor-calibre-lvs',            default=True )
   debugcalibre = Step( 'cadence-innovus-debug-calibre', default=True )
 
+  # Die if unconnected bumps
+  init.extend_postconditions([
+    "assert 'STILL UNCONNECTED: bump' not in File( 'mflowgen-run.log' )"
+  ])
+
+
   # Send in the clones
   # 'init' step now gets its own design-rule check
   init_drc = drc.clone()
