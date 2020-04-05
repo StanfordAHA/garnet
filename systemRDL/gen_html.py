e5e914669a89487af3558f3958bd9d5b45f9d49a
Taeyoung Kong <kongty0329@gmail.com>
2020-04-05 16:54:13 -0700

    Add systemRDL files

---
+++ systemRDL/gen_html.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+from systemrdl import RDLCompiler, RDLCompileError
+from ralbot.html import HTMLExporter
+
+# Collect SystemRDL input files from the command line arguments
+input_files = sys.argv[1:]
+
+
+# Create an instance of the compiler
+rdlc = RDLCompiler()
+
+
+try:
+    # Compile all the files provided
+    for input_file in input_files:
+        rdlc.compile_file(input_file)
+
+    # Elaborate the design
+    root = rdlc.elaborate()
+except RDLCompileError:
+    # A compilation error occurred. Exit with error code
+    sys.exit(1)
+
+# Create an HTML exporter
+exporter = HTMLExporter()
+
+# Create HTML documentation
+exporter.export(root, "output/html")
