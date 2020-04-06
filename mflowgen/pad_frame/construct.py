55488e90f6d2d3b46b650c02262861e84e2ced84
steveri <steveri@steveri.com>
2020-04-06 08:08:59 -0700

    factored out separate check-bumps package

--- mflowgen/pad_frame/construct.py
+++ mflowgen/pad_frame/construct.py
@@ -283,7 +283,7 @@ def construct():
       'innovus-foundation-flow/custom-scripts/stream-out.tcl',
       'attach-results-to-outputs.tcl',
       'alignment-cells.tcl',
-      'gen-bumps.tcl', 'route-bumps.tcl'
+      'gen-bumps.tcl', 'check-bumps.tcl', 'route-bumps.tcl'
     ]}
   )
 
