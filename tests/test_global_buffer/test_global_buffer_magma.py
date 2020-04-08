572c644103a351d70b2efc3d0dc7ddc8c1ceeeb7
Taeyoung Kong <kongty@stanford.edu>
2020-04-08 16:12:39 -0700

    Fix pep8

--- tests/test_global_buffer/test_global_buffer_magma.py
+++ tests/test_global_buffer/test_global_buffer_magma.py
@@ -1,6 +1,7 @@
 import magma
 from global_buffer.global_buffer_magma import GlobalBuffer
 
+
 def test_compile():
     global_buffer = GlobalBuffer(16, 32)
     global_buffer_circ = global_buffer.circuit()
