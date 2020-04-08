6a74674187516854f492acebe71435147781aa30
Taeyoung Kong <kongty0329@gmail.com>
2020-04-08 15:13:31 -0700

    Do not use unstable perl preprocessor and change them to fixed value

--- mflowgen/glb_tile/rtl/gen_rtl.sh
+++ mflowgen/glb_tile/rtl/gen_rtl.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+
+# SystemRDL run
+$GARNET_HOME/systemRDL/rdl_prepro.pl $GARNET_HOME/global_buffer/systemRDL/rdl_models/glb.rdl
+java -jar $GARNET_HOME/systemRDL/Ordt.jar -parms $GARNET_HOME/global_buffer/systemRDL/ordt_params/glb.parms -systemverilog $GARNET_HOME/global_buffer/systemRDL/output/ $GARNET_HOME/global_buffer/systemRDL/rdl_models/glb.rdl.pp.final
+cp $GARNET_HOME/global_buffer/systemRDL/output/{glb_jrdl_logic.sv,glb_jrdl_decode.sv,glb_pio.sv} $GARNET_HOME/global_buffer/rtl/
+
 while read F  ; do
   if [[ "$F" =~ "gl" || "$F" =~ "ifc" ]]; then
     echo "Reading design file: $F"
