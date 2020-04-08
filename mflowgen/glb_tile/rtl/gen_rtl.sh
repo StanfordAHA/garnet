8c2a3066911fd9a3770590541b0b7139d968773b
Taeyoung Kong <kongty0329@gmail.com>
2020-04-08 15:16:39 -0700

    Fix width mismatch

--- mflowgen/glb_tile/rtl/gen_rtl.sh
+++ mflowgen/glb_tile/rtl/gen_rtl.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
 # SystemRDL run
-$GARNET_HOME/systemRDL/rdl_prepro.pl $GARNET_HOME/global_buffer/systemRDL/rdl_models/glb.rdl
-java -jar $GARNET_HOME/systemRDL/Ordt.jar -parms $GARNET_HOME/global_buffer/systemRDL/ordt_params/glb.parms -systemverilog $GARNET_HOME/global_buffer/systemRDL/output/ $GARNET_HOME/global_buffer/systemRDL/rdl_models/glb.rdl.pp.final
+java -jar $GARNET_HOME/systemRDL/Ordt.jar -parms $GARNET_HOME/global_buffer/systemRDL/ordt_params/glb.parms -systemverilog $GARNET_HOME/global_buffer/systemRDL/output/ $GARNET_HOME/global_buffer/systemRDL/rdl_models/glb.rdl
 cp $GARNET_HOME/global_buffer/systemRDL/output/{glb_jrdl_logic.sv,glb_jrdl_decode.sv,glb_pio.sv} $GARNET_HOME/global_buffer/rtl/
 
 while read F  ; do
