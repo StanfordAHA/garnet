16e80c3b2b476c458c2b9fd1d554b1f6b8e0e17a
Taeyoung Kong <kongty0329@gmail.com>
2020-04-08 18:12:57 -0700

    Do not need to copy to rtl folder

--- mflowgen/glb_tile/rtl/gen_rtl.sh
+++ mflowgen/glb_tile/rtl/gen_rtl.sh
@@ -2,7 +2,6 @@
 
 # SystemRDL run
 java -jar $GARNET_HOME/systemRDL/Ordt.jar -parms $GARNET_HOME/global_buffer/systemRDL/ordt_params/glb.parms -systemverilog $GARNET_HOME/global_buffer/systemRDL/output/ $GARNET_HOME/global_buffer/systemRDL/rdl_models/glb.rdl
-cp $GARNET_HOME/global_buffer/systemRDL/output/{glb_jrdl_logic.sv,glb_jrdl_decode.sv,glb_pio.sv} $GARNET_HOME/global_buffer/rtl/
 
 while read F  ; do
   if [[ "$F" =~ "gl" || "$F" =~ "ifc" ]]; then
