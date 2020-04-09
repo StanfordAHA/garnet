671512f2f1d1b102a05bf371176d8b2d9e706dfd
Taeyoung Kong <kongty@stanford.edu>
2020-04-09 06:41:41 -0700

    Update gen rtl to run systemrdl

--- mflowgen/glb_tile/rtl/gen_rtl.sh
+++ mflowgen/glb_tile/rtl/gen_rtl.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # SystemRDL run
-java -jar $GARNET_HOME/systemRDL/Ordt.jar -parms $GARNET_HOME/global_buffer/systemRDL/ordt_params/glb.parms -systemverilog $GARNET_HOME/global_buffer/systemRDL/output/ $GARNET_HOME/global_buffer/systemRDL/rdl_models/glb.rdl
+$GARNET_HOME/systemRDL/perlpp.pl $GARNET_HOME/global_buffer/systemRDL/rdl_models/glb.rdl -o $GARNET_HOME/global_buffer/systemRDL/rdl_models/glb.rdl.final
+java -jar $GARNET_HOME/systemRDL/Ordt.jar -parms $GARNET_HOME/global_buffer/systemRDL/ordt_params/glb.parms -systemverilog $GARNET_HOME/global_buffer/systemRDL/output/ $GARNET_HOME/global_buffer/systemRDL/rdl_models/glb.rdl.final
 
 while read F  ; do
   if [[ "$F" =~ "gl" || "$F" =~ "ifc" ]]; then
