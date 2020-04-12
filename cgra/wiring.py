aeed920b5130085051d7eb31833c2062269b87be
Taeyoung Kong <kongty@stanford.edu>
2020-04-12 05:45:33 -0700

    Global controller done

--- cgra/wiring.py
+++ cgra/wiring.py
@@ -59,9 +59,9 @@ def glc_interconnect_wiring(garnet):
     garnet.wire(garnet.global_controller.ports.stall,
                 garnet.interconnect.ports.stall)
 
-    # cgra_soft_reset signal wiring
+    # soft_reset signal wiring
     cgra_soft_reset_port = f"glb2io_1_X{garnet.interconnect.x_max:02X}_Y{0:02X}"
-    garnet.wire(garnet.global_controller.ports.cgra_soft_reset,
+    garnet.wire(garnet.global_controller.ports.soft_reset,
                 garnet.interconnect.ports[cgra_soft_reset_port][0])
     garnet.wire(garnet.interconnect.ports.read_config_data,
                 garnet.global_controller.ports.read_data_in)
