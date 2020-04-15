9423aff286bf5cf21dde7614b03860de2e060525
Taeyoung Kong <kongty@stanford.edu>
2020-04-15 07:40:15 -0700

    Add DW tap
    Add DW

--- mflowgen/global_controller/rtl/gen_rtl.sh
+++ mflowgen/global_controller/rtl/gen_rtl.sh
@@ -4,6 +4,11 @@
 make -C $GARNET_HOME/global_controller rtl
 
 while read F  ; do
+  if [[ "$F" =~ "DW" ]]; then
+    echo "Reading design file: $F"
+    cat $F >> outputs/design.v
+  else
     echo "Reading design file: $F"
     cat $GARNET_HOME/global_controller/$F >> outputs/design.v
+  fi
 done <$GARNET_HOME/global_controller/global_controller.filelist
