f541322e038ca53490d1e47d4eeaef89e128f8f1
Alex Carsello <alex.carsello@gmail.com>
2020-04-21 09:25:41 -0700

    Reduce border sizes for pe and mem tiles

--- mflowgen/Tile_MemCore/custom-init/outputs/floorplan.tcl
+++ mflowgen/Tile_MemCore/custom-init/outputs/floorplan.tcl
@@ -17,18 +17,10 @@ set core_density_target 0.70; # Placement density of 70% is reasonable
 # must be the same to allow for abutment at the top level
 set core_height 139
 
-# Make room in the floorplan for the core power ring
-
-set pwr_net_list {VDD VSS}; # List of power nets in the core power ring
-
-set M1_min_width   [dbGet [dbGetLayerByZ 1].minWidth]
-set M1_min_spacing [dbGet [dbGetLayerByZ 1].minSpacing]
-
-set savedvars(p_ring_width)   [expr 48 * $M1_min_width];   # Arbitrary!
-set savedvars(p_ring_spacing) [expr 24 * $M1_min_spacing]; # Arbitrary!
-
-# Calculate actual height from height param
 set vert_pitch [dbGet top.fPlan.coreSite.size_y]
+set horiz_pitch [dbGet top.fPlan.coreSite.size_x]
+
+# Calculate actual core height from height param
 set height [expr $core_height * $vert_pitch]
 
 # Now begin width calculation
@@ -44,10 +36,10 @@ set width [expr $total_cell_area / $core_density_target / $height]
 
 # Core bounding box margins
 
-set core_margin_t [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $savedvars(p_ring_spacing))) + $savedvars(p_ring_spacing)]
-set core_margin_b [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $savedvars(p_ring_spacing))) + $savedvars(p_ring_spacing)]
-set core_margin_r [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $savedvars(p_ring_spacing))) + $savedvars(p_ring_spacing)]
-set core_margin_l [expr ([llength $pwr_net_list] * ($savedvars(p_ring_width) + $savedvars(p_ring_spacing))) + $savedvars(p_ring_spacing)]
+set core_margin_t $vert_pitch
+set core_margin_b $vert_pitch 
+set core_margin_r [expr 5 * $horiz_pitch]
+set core_margin_l [expr 5 * $horiz_pitch]
 
 #-------------------------------------------------------------------------
 # Floorplan
