149159db3848e187d9ee260c3d8ea54118637817
Alex Carsello <alex.carsello@gmail.com>
2020-04-22 08:54:54 -0700

    Fix bug in tile_array dc dont_touch constraints

--- mflowgen/tile_array/constraints/constraints.tcl
+++ mflowgen/tile_array/constraints/constraints.tcl
@@ -55,15 +55,14 @@ set_max_transition [expr 0.25*${dc_clock_period}] $dc_design_name
 
 # Ensure that no buffers get inserted on abutted nets
 # First get all nets connected to CGRA tiles
-set tile_nets [get_nets -of_objects [get_cells -filter {ref_lib_cell_name =~ *Tile*}]]
+set tile_nets [get_nets -of_objects [get_cells -filter {ref_name =~ *Tile*}]]
 # Now iterate over collection and check if net connects > 1
 # CGRA tile and no other cells. If this is true, don't try to route it.
 foreach_in_collection net $tile_nets {
   set connected_cells [get_cells -of_object $net]
-  set connected_tiles [filter_collection $connected_cells {ref_lib_cell_name =~ *Tile*}]
+  set connected_tiles [filter_collection $connected_cells {ref_name =~ *Tile*}]
   set num_connected_tiles [sizeof_collection $connected_tiles]
   if {($num_connected_tiles > 1) && ([compare_collections $connected_cells $connected_tiles] == 0)} {
-    query_objects $net
     set_dont_touch $net true
   }
 }
@@ -71,5 +70,8 @@ foreach_in_collection net $tile_nets {
 # This can catch nets connected to external ports of tile_array.
 # We must ensure that these nets can be touched
 set ext_port_nets [get_nets -of_objects [get_ports]]
-query_objects $ext_port_nets
 set_dont_touch $ext_port_nets false
+
+# This can catch nets connected to IO tiles, which should be touched
+set io_tile_nets [get_nets -of_objects [get_cells -filter {ref_name =~ Tile_io*}]]
+set_dont_touch $io_tile_nets false
