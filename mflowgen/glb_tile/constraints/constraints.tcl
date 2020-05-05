b45f44d4d60abc7dc811db56cdf6c20469471b16
Taeyoung Kong <kongty0329@gmail.com>
2020-05-05 03:14:16 -0700

    Tile id is constant

--- mflowgen/glb_tile/constraints/constraints.tcl
+++ mflowgen/glb_tile/constraints/constraints.tcl
@@ -35,13 +35,13 @@ set_driving_cell -no_design_rule \
 # set_input_delay constraints for input ports
 #
 # - make this non-zero to avoid hold buffers on input-registered designs
-
 set_input_delay -clock ${clock_name} [expr ${dc_clock_period}*0.2] [all_inputs]
+set_input_delay -clock ${clock_name} 0 glb_tile_id
+# TODO: glb_tile_id is constant
+set_case_analysis 0 glb_tile_id
 
 # set_output_delay constraints for output ports
 set_output_delay -clock ${clock_name} [expr ${dc_clock_period}*0.2] [all_outputs]
-# TODO: these output ports should have longer delay
-# 1. connected to cgra
 
 # set false path
 # glb_tile_id is constant
@@ -53,7 +53,7 @@ set_false_path -from {cfg_pc_tile_connected_wsti}
 set_false_path -to {cfg_tile_connected_esto}
 set_false_path -to {cfg_pc_tile_connected_esto}
 # path from configuration registers are false path
-set_false_path -through [get_cells -hier glb_pio] -through [get_ports glb_tile_int/glb_tile_cfg/cfg_* -filter "direction==out"]
+set_false_path -from [get_cells glb_tile_int/glb_tile_cfg/glb_pio/pio_logic/*] -through [get_ports glb_tile_int/glb_tile_cfg/cfg_* -filter "direction==out"]
 
 # set multi cycle path
 # Macro2Out
