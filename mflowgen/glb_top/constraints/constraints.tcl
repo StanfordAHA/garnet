97f2858f8b17b741645d5bac829004c4123847da
Taeyoung Kong <kongty0329@gmail.com>
2020-05-06 03:11:13 -0700

    glb_top timing constraint

--- mflowgen/glb_top/constraints/constraints.tcl
+++ mflowgen/glb_top/constraints/constraints.tcl
@@ -36,7 +36,8 @@ set_driving_cell -no_design_rule \
 #
 # - make this non-zero to avoid hold buffers on input-registered designs
 
-set_input_delay -clock ${clock_name} [expr ${dc_clock_period}/2.0] [all_inputs]
+set_input_delay -clock ${clock_name} [expr ${dc_clock_period}*0.2] [all_inputs]
+set_false_path -from [get_ports cgra_cfg_jtag_gc2glb_rd_en]
 
 # set_output_delay constraints for output ports
 
