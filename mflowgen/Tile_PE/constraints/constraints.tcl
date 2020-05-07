22820d5d21b6ee29bb68af981e012336afead930
norabarlow <kathleen.feng0@gmail.com>
2020-05-06 20:41:45 -0700

    new pe tile timing constraints with power domains

--- mflowgen/Tile_PE/constraints/constraints.tcl
+++ mflowgen/Tile_PE/constraints/constraints.tcl
@@ -11,52 +11,139 @@
 # is too large the tools will have no trouble but you will get a very
 # conservative implementation.
 
-set clock_net  clk
-set clock_name ideal_clock
-
-create_clock -name ${clock_name} \
-             -period ${dc_clock_period} \
-             [get_ports ${clock_net}]
-
-# This constraint sets the load capacitance in picofarads of the
-# output pins of your design.
-
-set_load -pin_load $ADK_TYPICAL_ON_CHIP_LOAD [all_outputs]
-
-# This constraint sets the input drive strength of the input pins of
-# your design. We specifiy a specific standard cell which models what
-# would be driving the inputs. This should usually be a small inverter
-# which is reasonable if another block of on-chip logic is driving
-# your inputs.
-
-set_driving_cell -no_design_rule \
-  -lib_cell $ADK_DRIVING_CELL [all_inputs]
-
-set_input_delay -clock ${clock_name} [expr ${dc_clock_period}*0.2] [all_inputs]
-
-set_output_delay -clock ${clock_name} [expr ${dc_clock_period}*0.2] [get_ports config_out*]
-set_output_delay -clock ${clock_name} [expr ${dc_clock_period}*0.2] [get_ports read_config_data]
-set_output_delay -clock ${clock_name} [expr ${dc_clock_period}*0.2] [get_ports SB*_OUT_*]
-set_output_delay -clock ${clock_name} [expr ${dc_clock_period}*0.2] [get_ports clk_out]
-set_output_delay -clock ${clock_name} [expr ${dc_clock_period}*0.2] [get_ports clk_pass_through_out]
-set_output_delay -clock ${clock_name} [expr ${dc_clock_period}*0.2] [get_ports reset_out]
-set_output_delay -clock ${clock_name} [expr ${dc_clock_period}*0.2] [get_ports stall_out]
-
-# Make all signals limit their fanout
-
-set_max_fanout 20 $dc_design_name
-
-# Make all signals meet good slew
-
-set_max_transition [expr 0.25*${dc_clock_period}] $dc_design_name
-
-# False paths
-
-set_false_path -to [get_ports hi]
-set_false_path -to [get_ports lo]
-
 if $::env(PWR_AWARE) {
-    source inputs/dc-dont-use-constraints.tcl
     source inputs/pe-constraints.tcl
-    set_dont_touch [get_cells -hierarchical *u_mux_logic*]
 } 
+
+# Case analysis/scenarios
+
+set alu_path PE_inst0/WrappedPE_inst0\$PE_inst0/ALU_inst0\$ALU_comb_inst0
+set fp_add_path $alu_path/magma_BFloat_16_add_inst0
+set fp_mul_path $alu_path/magma_BFloat_16_mul_inst0
+set mul_path $alu_path/magma_Bits_32_mul_inst0
+set add_path $alu_path/magma_Bits_17_add*
+
+set lib_delay 0.03
+
+########################################################################
+# FP ADD
+########################################################################
+create_scenario fp_add
+
+source inputs/common.tcl
+
+set_case_analysis 0 [get_pins $alu_path/alu[7]]
+set_case_analysis 0 [get_pins $alu_path/alu[6]]
+set_case_analysis 0 [get_pins $alu_path/alu[5]]
+set_case_analysis 1 [get_pins $alu_path/alu[4]]
+set_case_analysis 0 [get_pins $alu_path/alu[3]]
+set_case_analysis 1 [get_pins $alu_path/alu[2]]
+set_case_analysis 1 [get_pins $alu_path/alu[1]]
+set_case_analysis 0 [get_pins $alu_path/alu[0]]
+
+# set false path for combinational path directly to i/o
+set_false_path -from [all_inputs] -through [get_pins [list $fp_add_path/*]]
+set_false_path -to [all_outputs] -through [get_pins [list $fp_add_path/*]]
+
+set_false_path -through [get_pins [list $fp_mul_path/*]]
+set_false_path -through [get_pins [list $mul_path/*]]
+set_false_path -through [get_pins [list $add_path/*]]
+
+set fp_add_min [expr 0.95+$lib_delay]
+set fp_add_delay [expr $fp_add_min+0.17]
+set_max_delay $fp_add_delay -through [get_pins [list $fp_add_path/*]]
+
+########################################################################
+# FP MUL
+########################################################################
+create_scenario fp_mul
+
+source inputs/common.tcl
+
+set_case_analysis 0 [get_pins $alu_path/alu[7]]
+set_case_analysis 0 [get_pins $alu_path/alu[6]]
+set_case_analysis 0 [get_pins $alu_path/alu[5]]
+set_case_analysis 1 [get_pins $alu_path/alu[4]]
+set_case_analysis 1 [get_pins $alu_path/alu[3]]
+set_case_analysis 0 [get_pins $alu_path/alu[2]]
+set_case_analysis 0 [get_pins $alu_path/alu[1]]
+set_case_analysis 1 [get_pins $alu_path/alu[0]]
+
+# set false path for combinational path directly to i/o
+set_false_path -from [all_inputs] -through [get_pins [list $fp_mul_path/*]]
+set_false_path -to [all_outputs] -through [get_pins [list $fp_mul_path/*]]
+
+set_false_path -through [get_pins [list $fp_add_path/*]]
+set_false_path -through [get_pins [list $mul_path/*]]
+set_false_path -through [get_pins [list $add_path/*]]
+
+set fp_mul_min [expr 0.75+$lib_delay]
+set fp_mul_delay [expr $fp_mul_min+0.30]
+set_max_delay $fp_mul_delay -through [get_pins [list $fp_mul_path/*]]
+
+########################################################################
+# MUL
+########################################################################
+create_scenario mul
+
+source inputs/common.tcl
+
+set_case_analysis 0 [get_pins $alu_path/alu[7]]
+set_case_analysis 0 [get_pins $alu_path/alu[6]]
+set_case_analysis 0 [get_pins $alu_path/alu[5]]
+set_case_analysis 0 [get_pins $alu_path/alu[4]]
+set_case_analysis 1 [get_pins $alu_path/alu[3]]
+set_case_analysis 0 [get_pins $alu_path/alu[2]]
+set_case_analysis 1 [get_pins $alu_path/alu[1]]
+set_case_analysis 1 [get_pins $alu_path/alu[0]]
+
+set_false_path -through [get_pins [list $fp_add_path/*]]
+set_false_path -through [get_pins [list $fp_mul_path/*]]
+set_false_path -through [get_pins [list $add_path/*]]
+
+set mul_min [expr 0.70+$i_delay+$o_delay]
+set mul_delay [expr $mul_min+0.13]
+set_max_delay $mul_delay -through [get_pins [list $mul_path/*]]
+
+########################################################################
+# ADD
+########################################################################
+create_scenario add
+
+source inputs/common.tcl
+
+set_case_analysis 0 [get_pins $alu_path/alu[7]]
+set_case_analysis 0 [get_pins $alu_path/alu[6]]
+set_case_analysis 0 [get_pins $alu_path/alu[5]]
+set_case_analysis 0 [get_pins $alu_path/alu[4]]
+set_case_analysis 0 [get_pins $alu_path/alu[3]]
+set_case_analysis 0 [get_pins $alu_path/alu[2]]
+set_case_analysis 0 [get_pins $alu_path/alu[1]]
+set_case_analysis 0 [get_pins $alu_path/alu[0]]
+
+set_false_path -through [get_pins [list $fp_add_path/*]]
+set_false_path -through [get_pins [list $fp_mul_path/*]]
+set_false_path -through [get_pins [list $mul_path/*]]
+
+set add_min [expr 0.65+$i_delay+$o_delay]
+set add_delay [expr $add_min+0.00]
+set_max_delay $add_delay -through [get_pins [list $add_path/*]]
+
+########################################################################
+# GENERAL
+########################################################################
+create_scenario general
+
+source inputs/common.tcl
+
+set_false_path -through [get_pins [list $add_path/*]]
+set_false_path -through [get_pins [list $mul_path/*]]
+
+set_false_path -from [all_inputs] -through [get_pins [list $fp_mul_path/*]]
+#set_false_path -to [all_outputs] -through [get_pins [list $fp_mul_path/*]]
+set_false_path -from [all_inputs] -through [get_pins [list $fp_add_path/*]]
+#set_false_path -to [all_outputs] -through [get_pins [list $fp_add_path/*]]
+
+########################################################################
+set_active_scenarios { general mul add}
+########################################################################
