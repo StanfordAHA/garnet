336167db2598ea22ffc77dabd6b843be6d25b2a6
James Thomas <jamesjoethomas@gmail.com>
2020-05-08 15:12:39 -0700

    fixes for full_chip lvs

--- mflowgen/tile_array/Tile_MemCore/get_Tile_MemCore_outputs.sh
+++ mflowgen/tile_array/Tile_MemCore/get_Tile_MemCore_outputs.sh
@@ -8,8 +8,8 @@ cp -L *synopsys-ptpx-genlibdb/outputs/design.lib outputs/Tile_MemCore_tt.lib
 cp -L *synopsys-ptpx-genlibdb/outputs/design.db outputs/Tile_MemCore.db
 cp -L *cadence-innovus-signoff/outputs/design.lef outputs/Tile_MemCore.lef
 cp -L *cadence-innovus-signoff/outputs/design.vcs.v outputs/Tile_MemCore.vcs.v
-cp -L *cadence-innovus-signoff/outputs/design.lvs.v outputs/Tile_MemCore.lvs.v
 cp -L *cadence-innovus-signoff/outputs/design.sdf outputs/Tile_MemCore.sdf
 cp -L *mentor-calibre-gdsmerge/outputs/design_merged.gds outputs/Tile_MemCore.gds
+cp -L *mentor-calibre-lvs/outputs/design_merged.lvs.v outputs/Tile_MemCore.lvs.v
 cp -L *gen_sram_macro/outputs/sram.spi outputs/sram.spi
 
