d84045d2bb20d1411e3d36072c04adb4ec151a4c
Taeyoung Kong <kongty@stanford.edu>
2020-05-12 13:32:33 +0900

    Quick-patch

--- tests/test_global_buffer/test_global_buffer_verilog_sim.py
+++ tests/test_global_buffer/test_global_buffer_verilog_sim.py
@@ -2,6 +2,7 @@ import os
 from gemstone.common.run_verilog_sim import verilog_sim_available
 import pytest
 
+
 @pytest.mark.skipif(not verilog_sim_available(),
                     reason="verilog simulator not available")
 def test_global_buffer_verilog_sim():
