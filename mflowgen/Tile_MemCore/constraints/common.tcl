751595e35d723cf8ea5ff7fb6aa7ed21a22e7fe1
mstrange <mstrange@stanford.edu>
2020-05-16 16:14:43 -0700

    Address Chris' comments about max_transition being a number not relative to clock

--- mflowgen/Tile_MemCore/constraints/common.tcl
+++ mflowgen/Tile_MemCore/constraints/common.tcl
@@ -47,7 +47,7 @@ remove_driving_cell reset
 set_max_fanout 20 $dc_design_name
 # Make all signals meet good slew
 # set_input_delay constraints for input ports
-set_max_transition [expr 0.120*${dc_clock_period}] $dc_design_name
+set_max_transition 0.120 $dc_design_name
 
 # Constrain INPUTS
 # - make this non-zero to avoid hold buffers on input-registered designs
