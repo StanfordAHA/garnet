aa38d2ce406bdced0ff691ccc36570d6ad285b78
Ankita <ankita0805@github.com>
2020-05-17 08:34:40 -0700

    mflowgen/common/power-domains/outputs/dont-touch-constraints.tcl

--- mflowgen/common/power-domains/outputs/dont-touch-constraints.tcl
+++ mflowgen/common/power-domains/outputs/dont-touch-constraints.tcl
@@ -7,6 +7,9 @@
 
 # Set dont_touch constraints on the clamping modules in SB/CBs
 set_dont_touch [get_cells -hierarchical *u_mux_logic*]
+set_dont_touch [get_nets -of_objects [get_ports *SB*]]
+set_dont_touch [get_nets -of_objects [get_pins -of_objects [get_cells -hierarchical *u_mux_logic] -filter name=~*I*] -filter name=~*SB*]
+
 
 # Set this so that the clamping modules in SB/CBs
 # that have dont_touch attribute are uniquified
