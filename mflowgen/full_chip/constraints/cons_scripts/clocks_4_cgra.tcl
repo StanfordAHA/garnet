0dcddd3e6515a6ebc8cbc9cb55996ea1f7b12988
Alex Carsello <alex.carsello@gmail.com>
2020-05-18 20:48:35 -0700

    Add global controller generated clock constraint

--- mflowgen/full_chip/constraints/cons_scripts/clocks_4_cgra.tcl
+++ mflowgen/full_chip/constraints/cons_scripts/clocks_4_cgra.tcl
@@ -34,3 +34,9 @@ create_generated_clock -name cgra_gclk \
     -divide_by 1 \
     -master_clock cgra_by_${cgra_clk_div_factor}_clk \
     [get_pins core/u_aha_platform_ctrl/u_clock_controller/u_cgra_gclk/Q]
+
+## CGRA Clock from global controller
+create_generated_clock -name global_controller_clk \
+    -source [get_pins core/u_aha_platform_ctrl/u_clock_controller/u_cgra_gclk/Q]
+    -divide_by 1 \
+    [get_pins core/u_aha_garnet/u_garnet/GlobalController_cfg_32_32_axi_13_32_inst0_global_controller_inst0/clk_out]
