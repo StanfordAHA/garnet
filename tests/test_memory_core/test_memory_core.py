fe6c1a64a0e70eaaae2a6055669c351f98eba9eb
Kavya Sreedhar <skavya@kiwi.stanford.edu>
2020-05-23 11:54:25 -0700

    delete changes to travis run script, finish porting 1 test to memory_core

--- tests/test_memory_core/test_memory_core.py
+++ tests/test_memory_core/test_memory_core.py
@@ -49,8 +49,10 @@ class MemoryCoreTester(BasicTester):
 
 def test_multiple_output_ports():
     # Regular Bootstrap
-    [Mem, tester, MCore] = make_memory_core()
+    [circuit, tester, MCore] = make_memory_core()
     
+    tester.poke(circuit.stall, 1)
+
     tile_en = 1
     depth = 1024
     chunk = 128
@@ -72,8 +74,8 @@ def test_multiple_output_ports():
     config_data.append((MCore.get_reg_index("strg_ub_app_ctrl_read_depth_0"), 3 * chunk * 4, 0))
     config_data.append((MCore.get_reg_index("strg_ub_app_ctrl_write_depth_wo_0"), 256 * 4, 0))
     config_data.append((MCore.get_reg_index("strg_ub_app_ctrl_write_depth_ss_0"), 256 * 4, 0))
-    config_data.append((MCore.get_reg_index("strg_ub_app_ctrl_coarse_read_depth_0"), int(3 * chunk), 0)
-    config_data.append((MCore.get_reg_index("strg_ub_app_ctrl_coarse_write_depth_wo_0"(, 256, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_app_ctrl_coarse_read_depth_0"), int(3 * chunk), 0))
+    config_data.append((MCore.get_reg_index("strg_ub_app_ctrl_coarse_write_depth_wo_0"), 256, 0))
     config_data.append((MCore.get_reg_index("strg_ub_app_ctrl_coarse_write_depth_ss_0"), 256, 0))
 
     config_data.append((MCore.get_reg_index("strg_ub_input_addr_ctrl_address_gen_0_dimensionality"), 2, 0))
@@ -86,8 +88,8 @@ def test_multiple_output_ports():
     config_data.append((MCore.get_reg_index("strg_ub_input_addr_ctrl_address_gen_0_strides_1"), 256, 0))
     config_data.append((MCore.get_reg_index("strg_ub_input_addr_ctrl_address_gen_0_strides_2"), 0, 0))
     config_data.append((MCore.get_reg_index("strg_ub_input_addr_ctrl_address_gen_0_strides_3"), 0, 0))
-    config_data.append((MCore.get_reg_index("strg_ub_input_addr_ctrl_address_gen_0_strides_4"), 0, 0)
-    config_data.append((MCore.get_reg_index("strg_ub_input_addr_ctrl_address_gen_0_strides_5"), 0, 0)
+    config_data.append((MCore.get_reg_index("strg_ub_input_addr_ctrl_address_gen_0_strides_4"), 0, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_input_addr_ctrl_address_gen_0_strides_5"), 0, 0))
 
     config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_0_dimensionality"), 3, 0))
     config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_0_ranges_0"), 128, 0))
@@ -126,16 +128,86 @@ def test_multiple_output_ports():
     config_data.append((MCore.get_reg_index("chain_idx_output"), 0, 0))
 
     config_data.append((MCore.get_reg_index("strg_ub_app_ctrl_input_port_1"), 0, 0))
-    config_data.append((MCore.get_reg_index(
+    config_data.append((MCore.get_reg_index("strg_ub_app_ctrl_read_depth_1"), 3 * chunk * 4, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_app_ctrl_coarse_read_depth_1"), 3 * chunk, 0))
 
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_dimensionality"), 3, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_ranges_0"), 128, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_ranges_1"), 3, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_ranges_2"), 100, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_ranges_3"), 0, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_ranges_4"), 0, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_ranges_5"), 0, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_starting_addr"), 128, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_strides_0"), 1, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_strides_1"), 0, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_strides_2"), 256, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_strides_3"), 0, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_strides_4"), 0, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_output_addr_ctrl_address_gen_1_strides_5"), 0, 0))
 
+    config_data.append((MCore.get_reg_index("strg_ub_tba_1_tb_0_range_outer"), chunk, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_tba_1_tb_0_starting_addr"), 0, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_tba_1_tb_0_stride"), 1, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_tba_1_tb_0_dimensionality"), 1, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_tba_1_tb_0_indices_merged_0"), 0, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_tba_1_tb_0_range_inner"), 2, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_tba_1_tb_0_tb_height"), 1, 0))
+    
+    config_data.append((MCore.get_reg_index("strg_ub_sync_grp_sync_group_1"), 1, 0))
+    config_data.append((MCore.get_reg_index("strg_ub_pre_fetch_1_input_latency"), 4, 0))
     
     # Configure
     for addr, data, feat in config_data:
         tester.configure(addr, data, feat)
 
+    tester.poke(circuit.stall, 0)
+    tester.eval()
+
+    inputs = []
+    for z in range(6):
+        for i in range(depth):
+            inputs.append(i)
+
+    outputs_0 = []
+    outputs_1 = []
+    for z in range(6):
+        for i in range(depth // 2):
+            outputs_0.append(i)
+            outputs_1.append(i + 512)
+
+    tester.poke(circuit.ren_in_0, 1)
+    tester.poke(circuit.ren_in_1, 1)
+
+    output_idx = 0
+
+    for i in range(4 * depth):
+        # We are just writing sequentially for this sample
+        if(i >= 2 * depth + 4 * chunk):
+            tester.poke(circuit.wen_in_0, 1)
+        elif(i >= 2 * depth):
+            # Write for two rounds
+            tester.poke(circuit.wen_in_0, 0)
+        else:
+            tester.poke(circuit.wen_in_0, 1)
+            tester.poke(circuit.data_in_0, inputs[i])
+
+        tester.eval()
+
+        if (i > depth + startup_delay):
+            tester.expect(circuit.valid_out_0, 1)
+            tester.expect(circuit.valid_out_1, 1)
+            tester.expect(circuit.data_out_0, outputs_0[output_idx])
+            tester.expect(circuit.data_out_1, outputs_1[output_idx])
+            output_idx += 1
+        else:
+            tester.expect(circuit.valid_out_0, 0)
+            tester.expect(circuit.valid_out_1, 0)
+
+        tester.step(2)
+
     with tempfile.TemporaryDirectory() as tempdir:
-        tempdir="dump"
+        tempdir="dump_"
         for genesis_verilog in glob.glob("genesis_verif/*.*"):
             shutil.copy(genesis_verilog, tempdir)
         tester.compile_and_run(directory=tempdir,
