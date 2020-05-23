a6fbabbc1849fac2a579d34b2dfe353ffd445614
Alex Carsello <alex.carsello@gmail.com>
2020-05-23 13:49:31 -0700

    Add dragonphy power nets to globalnetconnect

--- mflowgen/common/custom-power-chip/outputs/globalnetconnect.tcl
+++ mflowgen/common/custom-power-chip/outputs/globalnetconnect.tcl
@@ -9,3 +9,11 @@ globalNetConnect VSS -type pgpin -pin VBB -inst *
 # I/O power nets
 globalNetConnect VDDPST -type pgpin -pin VDDPST -inst *
 globalNetConnect VSS    -type pgpin -pin VSSPST -inst * 
+
+# Dragonphy power nets
+globalNetConnect VDD -type pgpin -pin DVDD -inst iphy -override
+globalNetConnect VSS -type pgpin -pin DVSS -inst iphy -override
+globalNetConnect AVDD -type pgpin -pin AVDD -inst iphy -override
+globalNetConnect AVSS -type pgpin -pin AVSS -inst iphy -override
+globalNetConnect CVDD -type pgpin -pin CVDD -inst iphy -override
+globalNetConnect CVSS -type pgpin -pin CVSS -inst iphy -override
