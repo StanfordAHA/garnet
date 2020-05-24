4b86bd9b9e5f2f8963233036a4d2db92c9da9d74
Alex Carsello <alex.carsello@gmail.com>
2020-05-24 16:26:38 -0700

    Add script to connect dragonphy stripes

--- mflowgen/common/custom-power-chip/outputs/power-strategy-dualmesh.tcl
+++ mflowgen/common/custom-power-chip/outputs/power-strategy-dualmesh.tcl
@@ -199,6 +199,48 @@ addStripe -nets {VSS VDD} -layer $pmesh_bot -direction horizontal \
     -start [expr $pmesh_bot_str_pitch]                            \
     -stop 4000
 
+# Add M8 stripes to connect to dragonphy
+# Stripes for bottom half of phy block
+set bottom 4101.96; set top 4400
+addStripe -nets {VDD VSS } \
+    -layer M8 -direction horizontal -width 2 -spacing 1 \
+    -start $bottom \
+    -set_to_set_distance [expr 12.295 - 0.279 - 0.016]\
+    -switch_layer_over_obs 0 \
+    -max_same_layer_jog_length 2 \
+    -padcore_ring_top_layer_limit AP -padcore_ring_bottom_layer_limit M1 \
+    -block_ring_top_layer_limit M8 -block_ring_bottom_layer_limit M1 \
+    -use_wire_group 0 -snap_wire_center_to_grid none \
+    -stop $top
+
+# Stripes for top half of phy block. There are eight groups of wires,
+# each consisting of four VDD/VSS pairs.
+set bottom 4421.34
+set top [expr $bottom + 34]
+set delta [expr 39.452 - 0.307 + 0.023 ]
+foreach i { 0 1 2 3 4 5 6 7 8 } {
+
+    addStripe -nets {VSS VDD } \
+        -layer M8 -direction horizontal -width 2 -spacing 1 \
+        -start $bottom \
+        -set_to_set_distance [expr 7.15 - 0.15] \
+        -switch_layer_over_obs 0 \
+        -max_same_layer_jog_length 2 \
+        -padcore_ring_top_layer_limit AP -padcore_ring_bottom_layer_limit M1 \
+        -block_ring_top_layer_limit M8 -block_ring_bottom_layer_limit M1 \
+        -use_wire_group 0 -snap_wire_center_to_grid none \
+        -stop $top
+
+    set bottom [expr $bottom + $delta]
+    set top    [expr $bottom + 34]
+}
+
+
+# Delete the two stray wires where pins are missing at the top right.
+set a { 50 4762 1360 4768 }
+editDelete -net VDD -layer M8 -area $a
+editDelete -net VSS -layer M8 -area $a
+
 #-------------------------------------------------------------------------
 # Power mesh top settings (vertical)
 #-------------------------------------------------------------------------
@@ -256,20 +298,4 @@ addStripe -nets {VDD VSS} \
   -start_from left \
   -area {1050.0 1050.0 3850.0 3850.0}
 
-# Now that we've already done our addStripe commands,
-# delete the blockage around the phy
-deleteRouteBlk -name dragonphy
-
-# Connect stripes to dragonphy
-sroute \
-  -inst {iphy} \
-  -connect { blockPin } \
-  -layerChangeRange { M8 M9 } \
-  -blockPinTarget { boundaryWithPin } \
-  -allowJogging 1 \
-  -crossoverViaLayerRange { M8 M7 } \
-  -nets { VDD VSS } \
-  -allowLayerChange 0 \
-  -blockPin useLef \
-  -targetViaLayerRange { M8 M7 }
 
