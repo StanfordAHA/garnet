5a9ba6622d0e146cee43ed7ff224e447b06cca2b
mstrange <mstrange@stanford.edu>
2020-06-23 12:43:00 -0700

    Added module load script for Max's memtile flow setup

---
+++ mflowgen/Tile_MemCore/setup-mem.sh
@@ -0,0 +1,14 @@
+# On ARM machines...
+source /cad/modules/tcl/init/bash
+module load base/1.0
+module load genesis2
+module load icadv/latest
+module load incisive/15.20.022
+module load vcs
+module load genus/latest
+module load innovus/19.10.000
+module load lc
+module load syn/latest
+module load calibre/2019.1
+module load prime/latest
+
