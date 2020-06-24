7a0795185a2d2e6e3790718b1787e5663f44fae7
mstrange <mstrange@stanford.edu>
2020-06-24 13:49:41 -0700

    Killed all latet references, replaced with current tool versions in the systems modulefiles

--- mflowgen/Tile_MemCore/setup-mem.sh
+++ mflowgen/Tile_MemCore/setup-mem.sh
@@ -2,13 +2,13 @@
 source /cad/modules/tcl/init/bash
 module load base/1.0
 module load genesis2
-module load icadv/latest
+module load icadv/12.30.712
 module load incisive/15.20.022
 module load vcs
-module load genus/latest
+module load genus/19.10.000
 module load innovus/19.10.000
 module load lc
-module load syn/latest
+module load syn/P-2019.03
 module load calibre/2019.1
-module load prime/latest
+module load prime/Q-2019.12-SP2
 
