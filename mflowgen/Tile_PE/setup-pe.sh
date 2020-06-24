55610ee3f08326ee4b6f34205e4ec92f300ca626
norabarlow <kathleen.feng0@gmail.com>
2020-06-24 00:54:23 -0700

    add module setup script for pe

---
+++ mflowgen/Tile_PE/setup-pe.sh
@@ -0,0 +1,14 @@
+source /cad/modules/tcl/init/sh
+module load base
+module load lc
+module load vcs/O-2018.09-SP1
+module load syn/latest
+module load pts
+module load genesis2
+module load xcelium
+module load icadv/latest
+module load incisive/15.20.022
+module load genus/latest
+module load innovus/19.10.000
+module load lc
+module load calibre/2019.1
