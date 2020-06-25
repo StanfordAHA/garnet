74e10d5358acb7e340e4391bd08509e8d772be8e
norabarlow <kathleen.feng0@gmail.com>
2020-06-24 18:53:38 -0700

    update modules to include version

--- mflowgen/Tile_PE/setup-pe.sh
+++ mflowgen/Tile_PE/setup-pe.sh
@@ -1,14 +1,13 @@
 source /cad/modules/tcl/init/sh
 module load base
-module load lc
 module load vcs/O-2018.09-SP1
-module load syn/latest
-module load pts
-module load genesis2
-module load xcelium
-module load icadv/latest
+module load syn/P-2019.03
+module load pts/K-2015.12
+module load genesis2/r11879
+module load xcelium/19.03.003
+module load icadv/12.30.712 
 module load incisive/15.20.022
-module load genus/latest
+module load genus/19.10.000 
 module load innovus/19.10.000
-module load lc
+module load lc/M-2017.06-SP3 
 module load calibre/2019.1
