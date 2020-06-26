e0e598b2983df5ddfa6744922db57a75239e8dfb
Alex Carsello <alex.carsello@gmail.com>
2020-06-26 11:25:12 -0700

    Add module load script for all of garnet

---
+++ mflowgen/setup-garnet.sh
@@ -0,0 +1,16 @@
+source /cad/modules/tcl/init/sh
+
+module load base/1.0
+module load genesis2
+module load xcelium/19.03.003
+module load icadv/12.30.712 
+module load incisive/15.20.022
+module load vcs/O-2018.09-SP1
+module load genus/19.10.000 
+module load innovus/19.10.000
+module load lc/M-2017.06-SP3 
+module load syn/P-2019.03
+module load calibre/2019.1
+module load pts/M-2017.06
+module load ext/latest
+
