0310bcb4fb23346bf45b1c3cf3868393ca47df97
Alex Carsello <alex.carsello@gmail.com>
2020-07-06 09:44:48 -0700

    Add tile array size params to cts node instead of cts override node

--- mflowgen/tile_array/custom-cts-overrides/outputs/cts-overrides.tcl
+++ mflowgen/tile_array/custom-cts-overrides/outputs/cts-overrides.tcl
@@ -1,4 +1,4 @@
-for {set x 0} {$x < ${array_width}} {incr x} {
+for {set x 0} {$x < $::env(array_width)} {incr x} {
   set x_hex [format "%02X" $x]
   set tile_name Tile_X${x_hex}_Y01
   set tile_type [get_property [get_cells $tile_name] ref_lib_cell_name]
