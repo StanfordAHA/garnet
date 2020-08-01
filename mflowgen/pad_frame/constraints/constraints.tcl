b488da04e140cf0aa60de0a70bab076fed8e8f25
Alex Carsello <alex.carsello@gmail.com>
2020-07-31 18:14:14 -0700

    fix clk name in pad frame constraints

--- mflowgen/pad_frame/constraints/constraints.tcl
+++ mflowgen/pad_frame/constraints/constraints.tcl
@@ -11,7 +11,7 @@
 # is too large the tools will have no trouble but you will get a very
 # conservative implementation.
 
-set clock_net  clk
+set clock_net  pad_MASTER_CLK
 set clock_name ideal_clock
 
 create_clock -name ${clock_name} \
