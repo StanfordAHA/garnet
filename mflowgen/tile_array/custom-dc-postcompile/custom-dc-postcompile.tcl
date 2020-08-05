c69ccfc946c4d1267cb7cafa595f92f8564a75e6
Alex Carsello <alex.carsello@gmail.com>
2020-08-05 15:09:45 -0700

    comments/cleanup

--- mflowgen/tile_array/custom-dc-postcompile/custom-dc-postcompile.tcl
+++ mflowgen/tile_array/custom-dc-postcompile/custom-dc-postcompile.tcl
@@ -1,2 +1,10 @@
-set_dont_touch [get_references *mantle*] false
-ungroup [get_references *mantle*]
+#=========================================================================
+# Post-compile DC commands file
+#=========================================================================
+
+# We are manually ungrouping all references to mantle_wire* modules after 
+# compilation because doing so before compilation caused hi,lo -> tile_id
+# connection on pe and memory tiles to be optimized away and replaced with 
+# tie cells.
+set_dont_touch [get_references *mantle_wire*] false
+ungroup [get_references *mantle_wire*]
