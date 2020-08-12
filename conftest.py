b04c792648ad0d92671c349dd70406e48ae433cf
Lenny Truong <lenny@stanford.edu>
2020-08-11 18:11:28 -0700

    Use gc.collect

--- conftest.py
+++ conftest.py
@@ -3,6 +3,8 @@ from magma import clear_cachedFunctions
 import magma.backend.coreir_ as coreir_
 from gemstone.generator import clear_generator_cache
 
+import gc
+
 collect_ignore = [
     # TODO(rsetaluri): Remove this once it is moved to canal!
     "experimental",  # directory for experimental projects
@@ -15,10 +17,10 @@ collect_ignore = [
 @pytest.fixture(autouse=True)
 def magma_test():
     clear_cachedFunctions()
-    inst = coreir_.CoreIRContextSingleton().get_instance()
-    del inst
     coreir_.CoreIRContextSingleton().reset_instance()
     clear_generator_cache()
+    # Force garbage collection to free memory and avoid oom
+    gc.collect()
 
 
 def pytest_addoption(parser):
