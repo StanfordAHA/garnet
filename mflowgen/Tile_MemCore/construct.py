431f54022940ed33baafc6fb3ed7b78a92689fc7
Alex Carsello <alex.carsello@gmail.com>
2020-08-24 11:46:58 -0700

    Relax mem tile to 1.2 ns

--- mflowgen/Tile_MemCore/construct.py
+++ mflowgen/Tile_MemCore/construct.py
@@ -26,7 +26,7 @@ def construct():
   parameters = {
     'construct_path'      : __file__,
     'design_name'         : 'Tile_MemCore',
-    'clock_period'        : 1.1,
+    'clock_period'        : 1.2,
     'adk'                 : adk_name,
     'adk_view'            : adk_view,
     # Synthesis
