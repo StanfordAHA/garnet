c1ecd89384b0479cea67c6b88bc97f238502914d
Taeyoung Kong <kongty0329@gmail.com>
2020-08-29 02:17:23 -0700

    Guard import

--- mflowgen/glb_top/rtl/gen_rtl.sh
+++ mflowgen/glb_top/rtl/gen_rtl.sh
@@ -5,7 +5,6 @@ make -C $GARNET_HOME/global_buffer rtl
 
 cat $GARNET_HOME/global_buffer/rtl/global_buffer_param.svh >> outputs/design.v
 cat $GARNET_HOME/global_buffer/rtl/global_buffer_pkg.svh >> outputs/design.v
-cat $GARNET_HOME/global_buffer/rtl/axil_ifc.sv >> outputs/design.v
 cat $GARNET_HOME/global_buffer/rtl/cfg_ifc.sv >> outputs/design.v
 cat $GARNET_HOME/global_buffer/rtl/glb_dummy_start.sv >> outputs/design.v
 cat $GARNET_HOME/global_buffer/rtl/glb_dummy_end.sv >> outputs/design.v
