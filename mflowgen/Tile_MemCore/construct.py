0e3ec1324f31b6107e60bd527e9e597ccd4bf4f6
steveri <steveri@steveri.com>
2020-08-30 08:38:27 -0700

    ta w 250MHz mt

--- mflowgen/Tile_MemCore/construct.py
+++ mflowgen/Tile_MemCore/construct.py
@@ -26,7 +26,8 @@ def construct():
   parameters = {
     'construct_path'      : __file__,
     'design_name'         : 'Tile_MemCore',
-    'clock_period'        : 1.25,
+    'clock_period'        : 4.0,    # 250MHz finishes in three hours
+  # 'clock_period'        : 1.25,   # 800MHz requires > ten hours
     'adk'                 : adk_name,
     'adk_view'            : adk_view,
     # Synthesis
