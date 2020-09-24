07aac090deab28333efe03b871ddbd4c45b9ff9f
Taeyoung Kong <kongty0329@gmail.com>
2020-09-24 14:57:31 -0700

    Exclude ts1n verilog

--- mflowgen/glb_tile/rtl/gen_rtl.sh
+++ mflowgen/glb_tile/rtl/gen_rtl.sh
@@ -3,7 +3,11 @@
 # SystemRDL run
 make -C $GARNET_HOME/global_buffer rtl
 
+rm -f outputs/design.v
+
 while read F  ; do
+  if [[ ! "$F" =~ "TS1N" ]]; then
     echo "Reading design file: $F"
     cat $GARNET_HOME/global_buffer/rtl/$F >> outputs/design.v
+  fi
 done <$GARNET_HOME/global_buffer/rtl/glb_tile.filelist
