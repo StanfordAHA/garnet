519bdd24da912e46d80c721341b40fd2267eda76
Lenny Truong <lenny@stanford.edu>
2020-09-25 10:36:24 -0700

    Use convert API

--- memory_core/memory_core_magma.py
+++ memory_core/memory_core_magma.py
@@ -395,7 +395,8 @@ class MemCore(ConfigurableCore):
         # Need to invert this
         self.resetInverter = FromMagma(mantle.DefineInvert(1))
         self.wire(self.resetInverter.ports.I[0], self.ports.reset)
-        self.wire(self.as_async_reset(self.resetInverter.ports.O[0]),
+        self.wire(self.convert(self.resetInverter.ports.O[0],
+                               magma.asyncreset),
                   self.underlying.ports.rst_n)
         self.wire(self.ports.clk, self.underlying.ports.clk)
 
