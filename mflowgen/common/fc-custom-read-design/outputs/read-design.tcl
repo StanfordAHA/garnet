5c258e5b3761aa8d15caa17d5ab7aa1ad5ae96b1
Alex Carsello <alex.carsello@gmail.com>
2020-09-28 16:09:43 -0700

    Add first pass at genus fc read-design script

--- mflowgen/common/fc-custom-read-design/outputs/read-design.tcl
+++ mflowgen/common/fc-custom-read-design/outputs/read-design.tcl
@@ -8,14 +8,16 @@ set design_files [concat $soc_design_files $cgra_design_files $pad_frame_design_
 echo "DA WIDTH: ${::env(TLX_REV_DATA_LO_WIDTH)}"
 
 if { $::env(soc_only) } {
-  if { ![analyze -define {ARM_CODEMUX=1 NO_CGRA=1 TLX_FWD_DATA_LO_WIDTH=16 TLX_REV_DATA_LO_WIDTH=45} -format sverilog $design_files] } { exit 1 }
+  if { ![read_hdl -define {ARM_CODEMUX=1 NO_CGRA=1 TLX_FWD_DATA_LO_WIDTH=16 TLX_REV_DATA_LO_WIDTH=45} -sv $design_files] } { exit 1 }
 } else {
-  if { ![analyze -define {ARM_CODEMUX=1 TLX_FWD_DATA_LO_WIDTH=16 TLX_REV_DATA_LO_WIDTH=45} -format sverilog $design_files] } { exit 1 }
+  if { ![read_hdl -define {ARM_CODEMUX=1 TLX_FWD_DATA_LO_WIDTH=16 TLX_REV_DATA_LO_WIDTH=45} -sv $design_files] } { exit 1 }
 }
 
 if {[file exists [which setup-design-params.txt]]} {
-  elaborate $dc_design_name -file_parameters setup-design-params.txt
-  rename_design $dc_design_name* $dc_design_name
+  #elaborate $dc_design_name -file_parameters setup-design-params.txt
+  #rename_design $dc_design_name* $dc_design_name
+  puts "Error: parameter files not supported"
+  exit 1  
 } else {
-  elaborate $dc_design_name
+  elaborate $design_name
 }
