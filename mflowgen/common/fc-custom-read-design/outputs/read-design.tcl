332c00c08e09ccbdcb4736ba4dae19a96b35c990
Alex Carsello <alex.carsello@gmail.com>
2020-09-29 13:36:07 -0700

    Remove if statements around read_hdl since it doens't return anything

--- mflowgen/common/fc-custom-read-design/outputs/read-design.tcl
+++ mflowgen/common/fc-custom-read-design/outputs/read-design.tcl
@@ -10,9 +10,9 @@ set design_files [concat $soc_design_files $cgra_design_files $pad_frame_design_
 echo "DA WIDTH: ${::env(TLX_REV_DATA_LO_WIDTH)}"
 
 if { $::env(soc_only) } {
-  if { ![read_hdl -define {ARM_CODEMUX=1 NO_CGRA=1 TLX_FWD_DATA_LO_WIDTH=16 TLX_REV_DATA_LO_WIDTH=45} -sv $design_files] } { exit 1 }
+  read_hdl -define {ARM_CODEMUX=1 NO_CGRA=1 TLX_FWD_DATA_LO_WIDTH=16 TLX_REV_DATA_LO_WIDTH=45} -sv $design_files
 } else {
-  if { ![read_hdl -define {ARM_CODEMUX=1 TLX_FWD_DATA_LO_WIDTH=16 TLX_REV_DATA_LO_WIDTH=45} -sv $design_files] } { exit 1 }
+  read_hdl -define {ARM_CODEMUX=1 TLX_FWD_DATA_LO_WIDTH=16 TLX_REV_DATA_LO_WIDTH=45} -sv $design_files
 }
 
 if {[file exists [which setup-design-params.txt]]} {
