1d8b452c60e29dc7226de0999602e4b205bcb741
Alex Carsello <alex.carsello@gmail.com>
2020-09-30 20:56:45 -0700

    design_innovus_merged -> design-merged in all get_output scripts

--- mflowgen/tile_array/Tile_MemCore/get_Tile_MemCore_outputs.sh
+++ mflowgen/tile_array/Tile_MemCore/get_Tile_MemCore_outputs.sh
@@ -9,7 +9,7 @@ cp -L *synopsys-ptpx-genlibdb/outputs/design.db outputs/Tile_MemCore.db
 cp -L *cadence-innovus-signoff/outputs/design.lef outputs/Tile_MemCore.lef
 cp -L *cadence-innovus-signoff/outputs/design.vcs.v outputs/Tile_MemCore.vcs.v
 cp -L *cadence-innovus-signoff/outputs/design.sdf outputs/Tile_MemCore.sdf
-cp -L *cadence-innovus-signoff/outputs/design_innovus_merged.gds outputs/Tile_MemCore.gds
+cp -L *cadence-innovus-signoff/outputs/design-merged.gds outputs/Tile_MemCore.gds
 cp -L *mentor-calibre-lvs/outputs/design_merged.lvs.v outputs/Tile_MemCore.lvs.v
 cp -L *gen_sram_macro/outputs/sram.spi outputs/sram.spi
 
