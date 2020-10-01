09e4eb7abe4fc2a9e012fc9cd9fc37610781596f
Taeyoung Kong <kongty@stanford.edu>
2020-09-30 23:16:50 -0700

    Just copy all sv files to rtl

--- mflowgen/glb_top/rtl/gen_rtl.sh
+++ mflowgen/glb_top/rtl/gen_rtl.sh
@@ -3,9 +3,9 @@
 # SystemRDL run
 make -C $GARNET_HOME/global_buffer rtl
 
-cat $GARNET_HOME/global_buffer/rtl/global_buffer_param.svh >> outputs/design.v
-cat $GARNET_HOME/global_buffer/rtl/global_buffer_pkg.svh >> outputs/design.v
-cat $GARNET_HOME/global_buffer/rtl/cfg_ifc.sv >> outputs/design.v
-cat $GARNET_HOME/global_buffer/rtl/glb_dummy_start.sv >> outputs/design.v
-cat $GARNET_HOME/global_buffer/rtl/glb_dummy_end.sv >> outputs/design.v
-cat $GARNET_HOME/global_buffer/rtl/global_buffer.sv >> outputs/design.v
+rm -f outputs/design.v
+
+while read F  ; do
+    echo "Reading design file: $F"
+    cat $GARNET_HOME/global_buffer/rtl/$F >> outputs/design.v
+done <$GARNET_HOME/global_buffer/rtl/global_buffer.filelist
