4068352b9bd13aed916e2a0533c043464fe65bb8
Alex Carsello <alex.carsello@gmail.com>
2020-09-30 18:05:46 -0700

    update fullchip output getters to use signoff gds

--- mflowgen/full_chip/glb_top/get_glb_top_outputs.sh
+++ mflowgen/full_chip/glb_top/get_glb_top_outputs.sh
@@ -9,7 +9,7 @@ cp -L *synopsys-ptpx-genlibdb/outputs/design.db outputs/glb_top.db
 cp -L *cadence-innovus-signoff/outputs/design.lef outputs/glb_top.lef
 cp -L *cadence-innovus-signoff/outputs/design.vcs.v outputs/glb_top.vcs.v
 cp -L *cadence-innovus-signoff/outputs/design.sdf outputs/glb_top.sdf
-cp -L *mentor-calibre-gdsmerge/outputs/design_merged.gds outputs/glb_top.gds
+cp -L *cadence-innovus-signoff/outputs/design_innovus_merged.gds outputs/glb_top.gds
 cp -L *mentor-calibre-lvs/outputs/design_merged.lvs.v outputs/glb_top.lvs.v
 cp -L *glb_tile/outputs/sram.spi outputs/glb_top.sram.spi
 
