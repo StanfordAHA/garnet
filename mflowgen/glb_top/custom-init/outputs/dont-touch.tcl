9cbb14f04b121b19fe6cb0202d473eb4c69a6fcb
Taeyoung Kong <kongty@stanford.edu>
2020-09-30 17:12:11 -0700

    Some changes

--- mflowgen/glb_top/custom-init/outputs/dont-touch.tcl
+++ mflowgen/glb_top/custom-init/outputs/dont-touch.tcl
@@ -1,7 +1,7 @@
 set num_tiles [sizeof_collection [get_cells *glb_tile_gen*]]
 for {set i 1} {$i < $num_tiles} {incr i} {
   # only need to set west since east pins are connected to same nets
-  set nets [get_nets -of_objects [get_pins -of_objects [get_cells *glb_tile_gen_$i*] -filter {name =~ *_wst*}]]
+  set nets [get_nets -of_objects [get_pins -of_objects [get_cells *glb_tile_gen\[$i\]*] -filter {name =~ *_wst*}]]
   query_objects $nets
   set_dont_touch $nets true
 }
