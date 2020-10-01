134f57787704899971b3e534b2978e4b22645795
Alex Carsello <alex.carsello@gmail.com>
2020-10-01 10:50:46 -0700

    Convert glb to genus-genlib

--- mflowgen/glb_top/glb_tile/get_glb_outputs.sh
+++ mflowgen/glb_top/glb_tile/get_glb_outputs.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 mflowgen run --design $GARNET_HOME/mflowgen/glb_tile/
-make synopsys-ptpx-genlibdb
+make cadence-genus-genlib
 make mentor-calibre-gdsmerge
 make mentor-calibre-lvs
 mkdir -p outputs
-cp -L *synopsys-ptpx-genlibdb/outputs/design.lib outputs/glb_tile_tt.lib
-cp -L *synopsys-ptpx-genlibdb/outputs/design.db outputs/glb_tile.db
+cp -L *cadence-genus-genlib/outputs/design.lib outputs/glb_tile_tt.lib
 cp -L *cadence-innovus-signoff/outputs/design.lef outputs/glb_tile.lef
 cp -L *cadence-innovus-signoff/outputs/design-merged.gds outputs/glb_tile.gds
 cp -L *mentor-calibre-lvs/outputs/design_merged.lvs.v outputs/glb_tile.lvs.v
