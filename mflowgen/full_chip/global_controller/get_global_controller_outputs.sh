7b2d25b1c94696306d3f78b44b1e969c4ac572c9
Alex Carsello <alex.carsello@gmail.com>
2020-09-30 20:56:45 -0700

    design_innovus_merged -> design-merged in all get_output scripts

--- mflowgen/full_chip/global_controller/get_global_controller_outputs.sh
+++ mflowgen/full_chip/global_controller/get_global_controller_outputs.sh
@@ -9,6 +9,6 @@ cp -L *synopsys-ptpx-genlibdb/outputs/design.db outputs/global_controller.db
 cp -L *cadence-innovus-signoff/outputs/design.lef outputs/global_controller.lef
 cp -L *cadence-innovus-signoff/outputs/design.vcs.v outputs/global_controller.vcs.v
 cp -L *cadence-innovus-signoff/outputs/design.sdf outputs/global_controller.sdf
-cp -L *cadence-innovus-signoff/outputs/design_innovus_merged.gds outputs/global_controller.gds
+cp -L *cadence-innovus-signoff/outputs/design-merged.gds outputs/global_controller.gds
 cp -L *mentor-calibre-lvs/outputs/design_merged.lvs.v outputs/global_controller.lvs.v
 
