49a6bd6e573d59fcddf34d853d738a8d1252e063
Ankita Nayak <ankita0805@gmail.com>
2020-09-30 20:42:00 -0700

    use PondCore instead of get_pond

--- cgra/util.py
+++ cgra/util.py
@@ -7,7 +7,7 @@ from canal.interconnect import Interconnect
 from passes.power_domain.pd_pass import add_power_domain, add_aon_read_config_data
 from lassen.sim import PE_fc
 from io_core.io_core_magma import IOCore
-from memory_core.memory_core_magma import MemCore, get_pond
+from memory_core.memory_core_magma import MemCore, get_pond, PondCore 
 from peak_core.peak_core import PeakCore
 from typing import Tuple, Dict, List, Tuple
 from passes.tile_id_pass.tile_id_pass import tile_id_physical
@@ -88,7 +88,8 @@ def create_cgra(width: int, height: int, io_sides: IOSide,
                 else:
                     core = PeakCore(PE_fc)
                     if add_pond:
-                        additional_core[(x, y)] = get_pond()
+                        additional_core[(x, y)] = PondCore()
+                        #additional_core[(x, y)] = get_pond() 
 
             cores[(x, y)] = core
 
@@ -123,8 +124,6 @@ def create_cgra(width: int, height: int, io_sides: IOSide,
 
             outputs.remove("data_out_pond")
             outputs.remove("valid_out_pond")
-            outputs.remove("chain_data_out_pond")
-            outputs.remove("chain_valid_out_pond")
 
     # This is slightly different from the original CGRA. Here we connect
     # input to every SB_IN and output to every SB_OUT.
