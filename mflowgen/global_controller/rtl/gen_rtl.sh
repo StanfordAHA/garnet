94db8d855d8ba7d8460fe3876422e382b6ed267d
Taeyoung Kong <kongty@stanford.edu>
2020-10-05 15:13:41 -0700

    No need of DW

--- mflowgen/global_controller/rtl/gen_rtl.sh
+++ mflowgen/global_controller/rtl/gen_rtl.sh
@@ -6,14 +6,9 @@ if [ -f ../inputs/design.v ]; then
 else
   # SystemRDL run
   make -C $GARNET_HOME/global_controller rtl
-  
+
   while read F  ; do
-    if [[ "$F" =~ "DW" ]]; then
-      echo "Reading design file: $F"
-      cat $F >> outputs/design.v
-    else
-      echo "Reading design file: $F"
-      cat $GARNET_HOME/global_controller/$F >> outputs/design.v
-    fi
+    echo "Reading design file: $F"
+    cat $GARNET_HOME/global_controller/$F >> outputs/design.v
   done <$GARNET_HOME/global_controller/global_controller.filelist
 fi
