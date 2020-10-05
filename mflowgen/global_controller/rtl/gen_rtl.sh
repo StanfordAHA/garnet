0f4005968fccd1a3f52fc1200edfc713a552398a
Alex Carsello <alex.carsello@gmail.com>
2020-10-05 12:22:30 -0700

    pass rtl from full_chip -> glc

--- mflowgen/global_controller/rtl/gen_rtl.sh
+++ mflowgen/global_controller/rtl/gen_rtl.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-
-# SystemRDL run
-make -C $GARNET_HOME/global_controller rtl
-
-while read F  ; do
-  if [[ "$F" =~ "DW" ]]; then
-    echo "Reading design file: $F"
-    cat $F >> outputs/design.v
-  else
-    echo "Reading design file: $F"
-    cat $GARNET_HOME/global_controller/$F >> outputs/design.v
-  fi
-done <$GARNET_HOME/global_controller/global_controller.filelist
+if [ -f ../inputs/design.v ]; then
+  echo "Using RTL from parent graph"
+  mkdir -p outputs
+  (cd outputs; ln -s ../../inputs/design.v)
+else
+  # SystemRDL run
+  make -C $GARNET_HOME/global_controller rtl
+  
+  while read F  ; do
+    if [[ "$F" =~ "DW" ]]; then
+      echo "Reading design file: $F"
+      cat $F >> outputs/design.v
+    else
+      echo "Reading design file: $F"
+      cat $GARNET_HOME/global_controller/$F >> outputs/design.v
+    fi
+  done <$GARNET_HOME/global_controller/global_controller.filelist
+fi
