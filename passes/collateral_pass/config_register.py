d9774a415cf13b32f9a97469a550c29b04282063
Keyi Zhang <keyi@stanford.edu>
2020-10-07 19:58:08 -0700

    update config reg pass to generate info for additional cores as well

--- passes/collateral_pass/config_register.py
+++ passes/collateral_pass/config_register.py
@@ -1,7 +1,7 @@
 from canal.circuit import get_mux_sel_name
 from canal.interconnect import Interconnect
 from peak_core.peak_core import PeakCore
-from memory_core.memory_core_magma import MemCore
+from memory_core.memtile_util import LakeCoreBase
 
 
 def get_interconnect_regs(interconnect: Interconnect):
@@ -74,56 +74,59 @@ def get_core_registers(interconnect: Interconnect):
     result = []
     for x, y in interconnect.tile_circuits:
         tile = interconnect.tile_circuits[(x, y)]
-        core = tile.core
-        if core is None:
-            continue
-        if isinstance(core, PeakCore):
-            # extract out PE's configuration
-            # notice that peak core is a black box. we have no idea what
-            # the registers' semantic meaning
-            feat_addr = tile.features().index(core)
-            regs = list(core.registers.keys())
-            regs.sort()
-            for idx, reg_name in enumerate(regs):
-                reg_addr = idx
-                width = core.reg_width[reg_name]
-                addr = interconnect.get_config_addr(reg_addr, feat_addr, x, y)
-                result.append({
-                    "name": reg_name,
-                    "addr": addr,
-                    "range": (1 << width) - 1,
-                    "lo": 0,
-                    "hi": width + 1
-                })
-        elif isinstance(core, MemCore):
-            # memory has couple features
-            base_feat_addr = tile.features().index(core)
-            regs = list(core.registers.keys())
-            for reg_name in regs:
-                reg_addr, lo, hi = core.get_reg_info(reg_name)
-                width = core.registers[reg_name].width
-                addr = interconnect.get_config_addr(reg_addr, base_feat_addr,
-                                                    x, y)
-                result.append({
-                    "name": reg_name,
-                    "addr": addr,
-                    "range": 1 << width,
-                    "lo": lo,
-                    "hi": hi
-                })
-
-            # SRAM
-            num_sram = core.num_sram_features
-            repeat = core.mem_width // core.data_width
-            for sram_index in range(num_sram):
-                for reg_addr in range(256):
-                    addr = \
-                        interconnect.get_config_addr(
-                            reg_addr, base_feat_addr + sram_index, x, y)
+        cores = [tile.core] + tile.additional_cores
+        for core in cores:
+            if core is None:
+                continue
+            if isinstance(core, PeakCore):
+                # extract out PE's configuration
+                # notice that peak core is a black box. we have no idea what
+                # the registers' semantic meaning
+                feat_addr = tile.features().index(core)
+                regs = list(core.registers.keys())
+                regs.sort()
+                for idx, reg_name in enumerate(regs):
+                    reg_addr = idx
+                    width = core.reg_width[reg_name]
+                    addr = interconnect.get_config_addr(reg_addr, feat_addr, x, y)
                     result.append({
-                        "name": f"SRAM_{sram_index}_{reg_addr}",
+                        "name": reg_name,
                         "addr": addr,
-                        "range": (1 << 16) - 1,
-                        "repeat": repeat
+                        "range": (1 << width) - 1,
+                        "lo": 0,
+                        "hi": width + 1
                     })
+            elif isinstance(core, LakeCoreBase):
+                # memory has couple features
+                base_feat_addr = tile.features().index(core)
+                regs = list(core.registers.keys())
+                for reg_name in regs:
+                    reg_addr, lo, hi = core.get_reg_info(reg_name)
+                    width = core.registers[reg_name].width
+                    addr = interconnect.get_config_addr(reg_addr, base_feat_addr,
+                                                        x, y)
+                    result.append({
+                        "name": reg_name,
+                        "addr": addr,
+                        "range": 1 << width,
+                        "lo": lo,
+                        "hi": hi
+                    })
+
+                # SRAM
+                num_sram = core.num_sram_features
+                if not hasattr(core, "mem_width"):
+                    continue
+                repeat = core.mem_width // core.data_width
+                for sram_index in range(num_sram):
+                    for reg_addr in range(256):
+                        addr = \
+                            interconnect.get_config_addr(
+                                reg_addr, base_feat_addr + sram_index, x, y)
+                        result.append({
+                            "name": f"SRAM_{sram_index}_{reg_addr}",
+                            "addr": addr,
+                            "range": (1 << 16) - 1,
+                            "repeat": repeat
+                        })
     return result
