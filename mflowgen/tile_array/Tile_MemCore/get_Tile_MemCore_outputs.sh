373a1a467f1ce6a0aa497464c3cd2ad83aa7bec7
Alex Carsello <alex.carsello@gmail.com>
2020-10-22 11:22:40 -0700

    Update output getters to work with either pegasus or calibre lvs

--- mflowgen/tile_array/Tile_MemCore/get_Tile_MemCore_outputs.sh
+++ mflowgen/tile_array/Tile_MemCore/get_Tile_MemCore_outputs.sh
@@ -9,6 +9,6 @@ cp -L *cadence-innovus-signoff/outputs/design.lef outputs/Tile_MemCore.lef
 cp -L *cadence-innovus-signoff/outputs/design.vcs.v outputs/Tile_MemCore.vcs.v
 cp -L *cadence-innovus-signoff/outputs/design.sdf outputs/Tile_MemCore.sdf
 cp -L *cadence-innovus-signoff/outputs/design-merged.gds outputs/Tile_MemCore.gds
-cp -L *mentor-calibre-lvs/outputs/design_merged.lvs.v outputs/Tile_MemCore.lvs.v
+cp -L *-lvs/outputs/design_merged.lvs.v outputs/Tile_MemCore.lvs.v
 cp -L *gen_sram_macro/outputs/sram.spi outputs/sram.spi
 
