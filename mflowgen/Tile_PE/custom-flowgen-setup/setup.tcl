f8353baa6cd2b1b242367ee123ffa6897cc290ce
Alex Carsello <alex.carsello@gmail.com>
2020-10-30 16:07:39 -0700

    update pe to use fast corner setup

--- mflowgen/Tile_PE/custom-flowgen-setup/setup.tcl
+++ mflowgen/Tile_PE/custom-flowgen-setup/setup.tcl
@@ -12,24 +12,32 @@ foreach s ${active_scenarios} {
     set vars(constraints_${s},post_cts_sdc)   inputs/sdc/${s}.sdc
 }
 
-set a_views {}
+set setup_a_views {}
+set hold_a_views {}
 foreach s ${active_scenarios} {
-    lappend a_views analysis_${s}
+    lappend setup_a_views analysis_setup_${s}
+    lappend hold_a_views analysis_hold_${s}
 }
 
-set vars(analysis_views)                         [join ${a_views}]
+set vars(analysis_views)                         [concat ${setup_a_views} ${hold_a_views}]
 
 foreach s ${active_scenarios} {
-    set vars(analysis_${s},delay_corner)          delay_default
-    set vars(analysis_${s},constraint_mode)       constraints_${s}
+    set vars(analysis_setup_${s},delay_corner)          delay_default
+    set vars(analysis_setup_${s},constraint_mode)       constraints_${s}
+    if {"delay_best" in $vars(delay_corners)} {
+      set vars(analysis_hold_${s},delay_corner)          delay_best
+    } else {
+      set vars(analysis_hold_${s},delay_corner)          delay_default
+    }
+    set vars(analysis_hold_${s},constraint_mode)       constraints_${s}
 }
 
-set vars(default_setup_view)                   "analysis_default_c"
-set vars(setup_analysis_views)                 "[join ${a_views}]"
-set vars(active_setup_views)                   "[join ${a_views}]"
+set vars(default_setup_view)                   "analysis_setup_default_c"
+set vars(setup_analysis_views)                 "[join ${setup_a_views}]"
+set vars(active_setup_views)                   "[join ${setup_a_views}]"
 
-set vars(default_hold_view)                    "analysis_default_c"
-set vars(hold_analysis_views)                  "[join ${a_views}]"
-set vars(active_hold_views)                    "[join ${a_views}]"
+set vars(default_hold_view)                    "analysis_hold_default_c"
+set vars(hold_analysis_views)                  "[join ${hold_a_views}]"
+set vars(active_hold_views)                    "[join ${hold_a_views}]"
 
-set vars(power_analysis_view)                  analysis_default_c
+set vars(power_analysis_view)                  analysis_setup_default_c
