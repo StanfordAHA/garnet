20d9ae5a11a741a1953b45e8705902ecdfd5563a
Alex Carsello <alex.carsello@gmail.com>
2020-11-04 17:30:10 -0800

    Pick drc rule deck based on available drc tool

--- mflowgen/full_chip/construct.py
+++ mflowgen/full_chip/construct.py
@@ -22,6 +22,13 @@ def construct():
 
   adk_name = 'tsmc16'
   adk_view = 'stdview'
+  
+  if which("calibre") is not None:
+      drc_rule_deck = 'calibre-drc-chip.rule' 
+      antenna_drc_rule_deck = 'calibre-drc-antenna.rule'
+  else:
+      drc_rule_deck = 'pegasus-drc-chip.rule' 
+      antenna_drc_rule_deck = 'pegasus-drc-antenna.rule'
 
   parameters = {
     'construct_path'    : __file__,
@@ -67,8 +74,8 @@ def construct():
     'TLX_FWD_DATA_LO_WIDTH' : 16,
     'TLX_REV_DATA_LO_WIDTH' : 45,
     # DRC rule deck
-    'drc_rule_deck'         : 'calibre-drc-chip.rule',
-    'antenna_drc_rule_deck' : 'calibre-drc-antenna.rule',
+    'drc_rule_deck'         : drc_rule_deck,
+    'antenna_drc_rule_deck' : antenna_drc_rule_deck,
     'nthreads'              : 16
   }
 
