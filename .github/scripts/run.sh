1d0167574bcc78b6fea8b57f71203a378623778f
Keyi Zhang <Kuree@users.noreply.github.com>
2020-11-06 11:11:44 -0800

    migrate CI tests to github actions (#734)
    * migrate CI tests to github actions
    
    * Used maintained version of coveralls instead
    
    * give up on coverall; no idea who owns the coverall garnet repo
    
    * Update linux.yml
    
    * Set token inside docker
    
    * Add comment about where token is stored
    
    * Update coverage badge
    
    Co-authored-by: Leonard (Lenny) Truong <lenny@stanford.edu>

--- .github/scripts/run.sh
+++ .github/scripts/run.sh
@@ -1,7 +1,3 @@
-# add Genesis to the path
-export GENESIS_HOME=/Genesis2/Genesis2Tools
-export PATH=$GENESIS_HOME/bin:$GENESIS_HOME/gui/bin:$PATH
-export PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$PERL5LIB
 cd / && git clone https://github.com/dillonhuff/clockwork && cd clockwork && git checkout lower_ubuffer
 export LAKE_CONTROLLERS="/clockwork/lake_controllers/"
 export LAKE_STREAM="/clockwork/lake_stream/"
