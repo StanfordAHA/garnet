93f996c51899fb3ea9219c591cdc820a303ca814
Alex Carsello <alex.carsello@gmail.com>
2020-11-11 13:00:53 -0800

    Add glb-tile ff lib to glbtile outputs

--- mflowgen/glb_top/glb_tile/get_glb_outputs.sh
+++ mflowgen/glb_top/glb_tile/get_glb_outputs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 mflowgen run --design $GARNET_HOME/mflowgen/glb_tile/
 make cadence-genus-genlib
+if [ $multicorner = True ]; then
+  make genlib-ff
+fi
 if command -v calibre &> /dev/null
 then
     make mentor-calibre-lvs
@@ -10,6 +13,9 @@ fi
 
 mkdir -p outputs
 cp -L *cadence-genus-genlib/outputs/design.lib outputs/glb_tile_tt.lib
+if [ $multicorner = True ]; then
+  cp -L *genlib-ff/outputs/design.lib outputs/glb_tile_tt.lib
+fi
 cp -L *cadence-innovus-signoff/outputs/design.lef outputs/glb_tile.lef
 cp -L *cadence-innovus-signoff/outputs/design-merged.gds outputs/glb_tile.gds
 cp -L *-lvs/outputs/design_merged.lvs.v outputs/glb_tile.lvs.v
