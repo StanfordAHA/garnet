ee214ef77b827f969e4b5f056f5d866cf391be7a
Taeyoung Kong <kongty@stanford.edu>
2020-11-11 13:47:36 -0800

    Add pipeline register at the input

--- global_buffer/rtl/glb_core_proc_router.sv
+++ global_buffer/rtl/glb_core_proc_router.sv
@@ -33,11 +33,12 @@ import global_buffer_param::*;
 // Internal Logic
 //============================================================================//
 // packet pipeline
-packet_t packet_w2e_esto_int;
-packet_t packet_e2w_wsto_int;
+packet_t packet_w2e_wsti_d1;
+packet_t packet_e2w_esti_d1;
 
 wr_packet_t wr_packet_pr2sw_filtered, wr_packet_pr2sw_muxed;
 rdrq_packet_t rdrq_packet_pr2sw_filtered, rdrq_packet_pr2sw_muxed;
+rdrs_packet_t rdrs_packet_sw2pr_d1; 
 
 // is_even indicates If tile_id is even or not
 // Warning: Tile id starts from 0
@@ -45,13 +46,37 @@ logic is_even;
 assign is_even = (glb_tile_id[0] == 0);
 
 //============================================================================//
+// packet pipeline register
+//============================================================================//
+always_ff @ (posedge clk or posedge reset) begin
+    if (reset) begin
+        packet_w2e_wsti_d1 <= '0;
+        packet_e2w_esti_d1 <= '0;
+    end
+    else begin
+        packet_w2e_wsti_d1 <= packet_w2e_wsti;
+        packet_e2w_esti_d1 <= packet_e2w_esti;
+    end
+end
+
+// response
+always_ff @ (posedge clk or posedge reset) begin
+    if (reset) begin
+        rdrs_packet_sw2pr_d1 <= '0;
+    end
+    else begin
+        rdrs_packet_sw2pr_d1 <= rdrs_packet_sw2pr;
+    end
+end
+
+//============================================================================//
 // request packet
 //============================================================================//
-assign packet_w2e_esto_int.wr = packet_w2e_wsti.wr;
-assign packet_e2w_wsto_int.wr = packet_e2w_esti.wr;
+assign packet_w2e_esto.wr = packet_w2e_wsti_d1.wr;
+assign packet_e2w_wsto.wr = packet_e2w_esti_d1.wr;
 
-assign packet_w2e_esto_int.rdrq = packet_w2e_wsti.rdrq;
-assign packet_e2w_wsto_int.rdrq = packet_e2w_esti.rdrq;
+assign packet_w2e_esto.rdrq = packet_w2e_wsti_d1.rdrq;
+assign packet_e2w_wsto.rdrq = packet_e2w_esti_d1.rdrq;
 
 // packet router to core
 assign wr_packet_pr2sw_muxed = (is_even == 1'b1)
@@ -59,37 +84,24 @@ assign wr_packet_pr2sw_muxed = (is_even == 1'b1)
 assign rdrq_packet_pr2sw_muxed = (is_even == 1'b1)
                                ? packet_w2e_esto.rdrq : packet_e2w_wsto.rdrq;
 
-assign wr_packet_pr2sw_filtered = (wr_packet_pr2sw_muxed.wr_addr[BANK_ADDR_WIDTH + BANK_SEL_ADDR_WIDTH +: TILE_SEL_ADDR_WIDTH] == glb_tile_id)
-                                ? wr_packet_pr2sw_muxed : 0;
-assign rdrq_packet_pr2sw_filtered = (rdrq_packet_pr2sw_muxed.rd_addr[BANK_ADDR_WIDTH + BANK_SEL_ADDR_WIDTH +: TILE_SEL_ADDR_WIDTH] == glb_tile_id)
-                                  ? rdrq_packet_pr2sw_muxed : 0;
+assign wr_packet_pr2sw_filtered = (wr_packet_pr2sw_muxed.wr_addr[BANK_ADDR_WIDTH + BANK_SEL_ADDR_WIDTH +: TILE_SEL_ADDR_WIDTH] == glb_tile_id) ? wr_packet_pr2sw_muxed : 0;
+assign rdrq_packet_pr2sw_filtered = (rdrq_packet_pr2sw_muxed.rd_addr[BANK_ADDR_WIDTH + BANK_SEL_ADDR_WIDTH +: TILE_SEL_ADDR_WIDTH] == glb_tile_id) ? rdrq_packet_pr2sw_muxed : 0;
 
 assign wr_packet_pr2sw = wr_packet_pr2sw_filtered;
 assign rdrq_packet_pr2sw = rdrq_packet_pr2sw_filtered;
+
 
 //============================================================================//
 // response packet
 //============================================================================//
 // packet core to router switch
-assign packet_w2e_esto_int.rdrs = (is_even == 1'b1)
-                                ? (rdrs_packet_sw2pr.rd_data_valid == 1) 
-                                ? rdrs_packet_sw2pr : packet_w2e_wsti.rdrs
-                                : packet_w2e_wsti.rdrs;
+assign packet_w2e_esto.rdrs = (is_even == 1'b1)
+                        ? (rdrs_packet_sw2pr_d1.rd_data_valid == 1)
+                        ? rdrs_packet_sw2pr_d1 : packet_w2e_wsti_d1.rdrs
+                        : packet_w2e_wsti_d1.rdrs;
 
-assign packet_e2w_wsto_int.rdrs = (is_even == 1'b0)
-                                ? (rdrs_packet_sw2pr.rd_data_valid == 1) 
-                                ? rdrs_packet_sw2pr : packet_e2w_esti.rdrs
-                                : packet_e2w_esti.rdrs;
-
-always_ff @(posedge clk or posedge reset) begin
-    if (reset) begin
-        packet_w2e_esto <= 0;
-        packet_e2w_wsto <= 0;
-    end
-    else begin
-        packet_w2e_esto <= packet_w2e_esto_int;
-        packet_e2w_wsto <= packet_e2w_wsto_int;
-    end
-end
-
+assign packet_e2w_wsto.rdrs = (is_even == 1'b0)
+                        ? (rdrs_packet_sw2pr_d1.rd_data_valid == 1)
+                        ? rdrs_packet_sw2pr_d1 : packet_e2w_esti_d1.rdrs
+                        : packet_e2w_esti_d1.rdrs;
 endmodule
