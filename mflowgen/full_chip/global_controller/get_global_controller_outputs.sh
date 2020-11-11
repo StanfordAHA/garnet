f9f93f314f87705e162f6fa0eca63f3663d201c4
Alex Carsello <alex.carsello@gmail.com>
2020-11-11 13:12:03 -0800

    Add glc_ff.lib

--- mflowgen/full_chip/global_controller/get_global_controller_outputs.sh
+++ mflowgen/full_chip/global_controller/get_global_controller_outputs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 mflowgen run --design $GARNET_HOME/mflowgen/global_controller/
 make cadence-genus-genlib
+if [ $multicorner = True ]; then
+  make genlib-ff
+fi
 if command -v calibre &> /dev/null
 then
     make mentor-calibre-lvs
@@ -10,6 +13,9 @@ fi
 
 mkdir -p outputs
 cp -L *cadence-genus-genlib/outputs/design.lib outputs/global_controller_tt.lib
+if [ $multicorner = True ]; then
+  cp -L *genlib-ff/outputs/design.lib outputs/global_controller_ff.lib
+fi
 cp -L *cadence-innovus-signoff/outputs/design.lef outputs/global_controller.lef
 cp -L *cadence-innovus-signoff/outputs/design.vcs.v outputs/global_controller.vcs.v
 cp -L *cadence-innovus-signoff/outputs/design.sdf outputs/global_controller.sdf
