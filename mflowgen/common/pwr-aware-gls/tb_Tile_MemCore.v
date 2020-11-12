57d4597e6fc46d6eeaafdd8f9304f62624f7081a
Ankita <ankita0805@github.com>
2020-11-12 01:57:58 -0800

    update

--- mflowgen/common/pwr-aware-gls/tb_Tile_MemCore.v
+++ mflowgen/common/pwr-aware-gls/tb_Tile_MemCore.v
@@ -303,8 +303,8 @@ module tb_Tile_MemCore();
          else $error("tie low cells are off");
       #1 assert (hi == 9'h1FF && dut.PowerDomainConfigReg_inst0.ps_en_out == 1'b1) $display ("PASS: tie hi cells are active");
  
-      #1 assert (clk_out == clk && dut.PowerDomainConfigReg_inst0.ps_en_out == 1'b1) $display ("PASS: Clk is ON");
-         else $error("Clk is OFF"); 
+      //#1 assert (clk_out == clk && dut.PowerDomainConfigReg_inst0.ps_en_out == 1'b1) $display ("PASS: Clk is ON");
+      //   else $error("Clk is OFF"); 
       #1 assert (reset_out == reset && dut.PowerDomainConfigReg_inst0.ps_en_out == 1'b1) $display ("PASS: Reset is ON");
          else $error("Reset is OFF"); 
       #1 assert (config_out_config_data == config_config_data && dut.PowerDomainConfigReg_inst0.ps_en_out == 1'b1) $display ("PASS: config_out_config_data is ON");
