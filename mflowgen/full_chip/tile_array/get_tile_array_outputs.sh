b21b2a398cd5f09aab03f1149224d9c09ceeddf3
Taeyoung Kong <kongty@stanford.edu>
2020-11-21 23:03:34 -0800

    Add sram db file as output for ptpx

--- mflowgen/full_chip/tile_array/get_tile_array_outputs.sh
+++ mflowgen/full_chip/tile_array/get_tile_array_outputs.sh
@@ -13,6 +13,9 @@ cp -L *cadence-innovus-signoff/outputs/design.lef outputs/tile_array.lef
 cp -L *cadence-innovus-signoff/outputs/design.vcs.v outputs/tile_array.vcs.v
 cp -L *cadence-innovus-signoff/outputs/design.sdf outputs/tile_array.sdf
 cp -L *cadence-innovus-signoff/outputs/design-merged.gds outputs/tile_array.gds
-cp -L *Tile_MemCore/outputs/sram.spi outputs/tile_array.sram.spi
 cp -L *-lvs/outputs/design_merged.lvs.v outputs/tile_array.lvs.v
+cp -L *Tile_MemCore/outputs/sram.spi outputs/tile_array.sram.spi
+cp -L *Tile_MemCore/outputs/sram.v outputs/tile_array.sram.v
+cp -L *Tile_MemCore/outputs/sram-pwr.v outputs/tile_array.sram.pwr.v
+cp -L *Tile_MemCore/outputs/sram_tt.db outputs/tile_array.sram_tt.db
 
