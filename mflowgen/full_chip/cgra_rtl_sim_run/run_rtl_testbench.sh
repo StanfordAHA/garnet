e2c2aefaa57ab1bb4b4bccbbf7739ba1cacf205a
Taeyoung Kong <kongty@stanford.edu>
2021-01-05 22:00:38 -0800

    Add rtl simulation node

--- mflowgen/full_chip/cgra_rtl_sim_run/run_rtl_testbench.sh
+++ mflowgen/full_chip/cgra_rtl_sim_run/run_rtl_testbench.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
 
 #-----------------------------------------------------------------------
-# Parameters
-#-----------------------------------------------------------------------
-
-#-----------------------------------------------------------------------
-# get placement file
-#-----------------------------------------------------------------------
-
-#-----------------------------------------------------------------------
 # Design
 #-----------------------------------------------------------------------
 # Grab all design/testbench files
 ln -s inputs/xcelium.d xcelium.d
+if [ $waveform = "True" ]; then
+    VCD_FLAG="+VCD_ON"
+fi
+
 for app in $(echo $cgra_apps | sed "s/,/ /g")
 do
     set -xe;
-    # TODO: add application directory to mflowgen
-    xrun -R  -sv_lib inputs/libcgra.so +APP0=inputs/meta/${app};
+    xrun -R  -sv_lib inputs/libcgra.so +APP0=inputs/meta/${app} $VCD_FLAG;
     cp xrun.log outputs/xrun.${app}.log;
 done
