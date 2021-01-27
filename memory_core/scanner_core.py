070885768bfd4d65e183f02a693e9015112dace2
mbstrange2 <mstrange@stanford.edu>
2021-01-26 19:40:43 -0800

    initial scanner test - can't create CGRA?

---
+++ memory_core/scanner_core.py
@@ -0,0 +1,105 @@
+import magma
+from gemstone.generator.from_magma import FromMagma
+from typing import List
+from canal.interconnect import Interconnect
+from lake.top.extract_tile_info import *
+import kratos as kts
+from gemstone.generator.from_magma import FromMagma
+from typing import List
+from lake.top.pond import Pond
+from lake.top.extract_tile_info import *
+from gemstone.common.core import PnRTag
+from lake.modules.scanner import *
+
+import kratos as kts
+
+if __name__ == "__main__":
+    from memtile_util import LakeCoreBase
+else:
+    from .memtile_util import LakeCoreBase
+
+
+def config_scan_tile(interconnect: Interconnect, full_cfg, new_config_data, x_place, y_place, score_cfg):
+    for config_reg, val, feat in new_config_data:
+        idx, value = score_cfg.get_config_data(config_reg, val)
+        full_cfg.append((interconnect.get_config_addr(
+                         idx,
+                         feat, x_place, y_place), value))
+
+
+class ScannerCore(LakeCoreBase):
+
+    def __init__(self,
+                 data_width=16,  # CGRA Params
+                 config_data_width=32,
+                 config_addr_width=8):
+
+        scan_name = "Scanner"
+
+        print("Creating scanner core...")
+
+        super().__init__(config_data_width=config_data_width,
+                         config_addr_width=config_addr_width,
+                         data_width=data_width,
+                         name="ScannerCore")
+
+        # Capture everything to the tile object
+        self.data_width = data_width
+        self.config_data_width = config_data_width
+        self.config_addr_width = config_addr_width
+
+        cache_key = (self.data_width,
+                     self.config_data_width, self.config_addr_width)
+
+        # Check for circuit caching
+        if cache_key not in LakeCoreBase._circuit_cache:
+            # Instantiate core object here - will only use the object representation to
+            # query for information. The circuit representation will be cached and retrieved
+            # in the following steps.
+            self.dut = Scanner(data_width=data_width)
+
+            circ = kts.util.to_magma(self.dut,
+                                     flatten_array=True,
+                                     check_multiple_driver=False,
+                                     optimize_if=False,
+                                     check_flip_flop_always_ff=False)
+            LakeCoreBase._circuit_cache[cache_key] = (circ, self.dut)
+        else:
+            circ, self.dut = LakeCoreBase._circuit_cache[cache_key]
+
+        # Save as underlying circuit object
+        self.underlying = FromMagma(circ)
+
+        self.wrap_lake_core()
+
+        conf_names = list(self.registers.keys())
+        conf_names.sort()
+        with open("scanner_cfg.txt", "w+") as cfg_dump:
+            for idx, reg in enumerate(conf_names):
+                write_line = f"(\"{reg}\", 0),  # {self.registers[reg].width}\n"
+                cfg_dump.write(write_line)
+        with open("scanner_synth.txt", "w+") as cfg_dump:
+            for idx, reg in enumerate(conf_names):
+                write_line = f"{reg}\n"
+                cfg_dump.write(write_line)
+
+    def get_config_bitstream(self, length):
+        configs = []
+        config_scanner = [("tile_en", 1)]
+        for name, v in config_scanner:
+            configs = [self.get_config_data(name, v)] + configs
+
+        print("Getting SCAN bitstream...")
+        config_extra = self.get_config_bitstream(length)
+
+        for name, v in config_extra:
+            configs = [self.get_config_data(name, v)] + configs
+
+        return configs
+
+    def pnr_info(self):
+        return PnRTag("s", self.DEFAULT_PRIORITY, 1)
+
+
+if __name__ == "__main__":
+    sc = ScannerCore()
