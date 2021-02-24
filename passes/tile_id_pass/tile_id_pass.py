673eb06fa39e5bb97244dae183a75dcfb473e66c
Alex Carsello <alex.carsello@gmail.com>
2021-02-24 15:54:52 -0800

    First pass at switching tile id pass

--- passes/tile_id_pass/tile_id_pass.py
+++ passes/tile_id_pass/tile_id_pass.py
@@ -1,7 +1,6 @@
 from io_core.io_core_magma import IOCoreValid
 from canal.interconnect import Interconnect
-import magma
-from gemstone.generator.const import Const
+import magma as m
 from hwtypes import BitVector
 
 # We create constant tie hi and tie lo outputs on each tile
@@ -23,14 +22,14 @@ def tile_id_physical(interconnect: Interconnect):
         tile_core = tile.core
         if isinstance(tile_core, IOCoreValid) or tile_core is None:
             continue
-        tile.add_ports(
-            hi=magma.Out(magma.Bits[tie_hi_width]),
-            lo=magma.Out(magma.Bits[tie_lo_width])
+        tile.io += m.IO(
+            hi=m.Out(magma.Bits[tie_hi_width]),
+            lo=m.Out(magma.Bits[tie_lo_width])
         )
         # wire all hi bits high
-        tile.wire(tile.ports.hi, Const((2 ** tie_hi_width) - 1))
+        m.wire(((2 ** tie_hi_width) - 1), tile.io.hi)
         # wire all lo bits low
-        tile.wire(tile.ports.lo, Const(0))
+        m.wire(0, tile.io.lo)
         # Get the correct tile_id value
         x_bv = BitVector[tile_id_width / 2](x)
         y_bv = BitVector[tile_id_width / 2](y)
@@ -39,7 +38,7 @@ def tile_id_physical(interconnect: Interconnect):
         tile_id_port = tile.ports.tile_id
         for wire in interconnect.wires:
             if tile_id_port in wire:
-                interconnect.remove_wire(wire[0], wire[1])
+                interconnect.unwire(wire[0], wire[1])
                 break
 
         # Actually connect hi/lo outputs to tile_id at top level
@@ -49,8 +48,8 @@ def tile_id_physical(interconnect: Interconnect):
                 hi_index = i // 2
             else:
                 hi_index = (i // 2) + 1
-            hi_port = tile.ports.hi[hi_index]
-            lo_port = tile.ports.lo[lo_index]
+            hi_port = tile.io.hi[hi_index]
+            lo_port = tile.io.lo[lo_index]
             tie_port = hi_port if (tile_id_bv[i] == 1) else lo_port
             # Connect tile_id ports to hi/lo outputs instead of constant
-            interconnect.wire(tile.ports.tile_id[i], tie_port)
+            m.wire(tile.io.tile_id[i], tie_port)
