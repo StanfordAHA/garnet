75ae0e8b94e4236e8a1ad3404cb3bdab0b1a0b59
Alex Carsello <alex.carsello@gmail.com>
2021-02-25 12:07:25 -0800

    First pass at glc conversion

--- global_controller/global_controller_magma.py
+++ global_controller/global_controller_magma.py
@@ -1,4 +1,4 @@
-import magma
+import magma as m
 from gemstone.common.jtag_type import JTAGType
 from gemstone.common.configurable import ConfigurationType
 from gemstone.generator.from_magma import FromMagma
@@ -8,50 +8,53 @@ from .global_controller_genesis2 import gen_wrapper, GlobalControllerParams
 from cgra.ifc_struct import *
 
 
-class GlobalController(Generator):
+class GlobalController(m.Generator2):
     def __init__(self, addr_width=32, data_width=32,
                  axi_addr_width=12, axi_data_width=32,
                  num_glb_tiles=16, glb_addr_width=22,
                  block_axi_addr_width=12):
-        super().__init__()
-
+        
         self.addr_width = addr_width
         self.data_width = data_width
         self.axi_addr_width = axi_addr_width
         self.axi_data_width = axi_data_width
         self.num_glb_tiles = num_glb_tiles
         self.block_axi_addr_width = block_axi_addr_width
+        
+        self.name = f"GlobalController_cfg_{self.addr_width}_" \
+                    f"{self.data_width}_axi_{self.axi_addr_width}_" \
+                    f"{self.axi_data_width}"
         # Control logic assumes cgra config_data_width is same as axi_data_width
         assert self.axi_data_width == self.data_width
 
         self.glb_addr_width = glb_addr_width
         self.config_type = ConfigurationType(self.addr_width, self.data_width)
 
-        self.add_ports(
-            clk_in=magma.In(magma.Clock),
-            reset_in=magma.In(magma.AsyncReset),
+        self.io = m.IO(
+            clk_in=m.In(m.Clock),
+            reset_in=m.In(m.AsyncReset),
 
-            clk_out=magma.Out(magma.Clock),
-            reset_out=magma.Out(magma.AsyncReset),
-            cgra_stall=magma.Out(magma.Bits[self.num_glb_tiles]),
-            glb_stall=magma.Out(magma.Bits[self.num_glb_tiles]),
-            soft_reset=magma.Out(magma.Bit),
+            clk_out=m.Out(m.Clock),
+            reset_out=m.Out(m.AsyncReset),
+            cgra_stall=m.Out(m.Bits[self.num_glb_tiles]),
+            glb_stall=m.Out(m.Bits[self.num_glb_tiles]),
+            soft_reset=m.Out(m.Bit),
 
             glb_cfg=GlbCfgIfc(self.block_axi_addr_width,
                               self.axi_data_width).master,
             sram_cfg=GlbCfgIfc(self.glb_addr_width, self.axi_data_width).master,
 
-            strm_start_pulse=magma.Out(magma.Bits[self.num_glb_tiles]),
-            pc_start_pulse=magma.Out(magma.Bits[self.num_glb_tiles]),
-            strm_g2f_interrupt_pulse=magma.In(magma.Bits[self.num_glb_tiles]),
-            strm_f2g_interrupt_pulse=magma.In(magma.Bits[self.num_glb_tiles]),
-            pcfg_g2f_interrupt_pulse=magma.In(magma.Bits[self.num_glb_tiles]),
+            strm_start_pulse=m.Out(m.Bits[self.num_glb_tiles]),
+            pc_start_pulse=m.Out(m.Bits[self.num_glb_tiles]),
+            strm_g2f_interrupt_pulse=m.In(m.Bits[self.num_glb_tiles]),
+            strm_f2g_interrupt_pulse=m.In(m.Bits[self.num_glb_tiles]),
+            pcfg_g2f_interrupt_pulse=m.In(m.Bits[self.num_glb_tiles]),
 
-            cgra_config=magma.Out(self.config_type),
-            read_data_in=magma.In(magma.Bits[self.data_width]),
+            cgra_config=m.Out(self.config_type),
+            read_data_in=m.In(m.Bits[self.data_width]),
             jtag=JTAGType,
             axi4_slave=AXI4LiteIfc(self.axi_addr_width, self.data_width).slave,
-            interrupt=magma.Out(magma.Bit)
+            interrupt=m.Out(m.Bit)
         )
 
         params = GlobalControllerParams(cfg_data_width=self.data_width,
@@ -67,125 +70,122 @@ class GlobalController(Generator):
         self.underlying = FromMagma(generator())
 
         # wire clk and reset
-        self.wire(self.ports.clk_in, self.underlying.ports.clk_in)
-        self.wire(self.ports.reset_in, self.underlying.ports.reset_in)
+        m.wire(self.io.clk_in, self.underlying.ports.clk_in)
+        m.wire(self.io.reset_in, self.underlying.ports.reset_in)
 
         # cgra control signals
-        self.wire(self.underlying.ports.clk_out, self.ports.clk_out)
-        self.wire(self.underlying.ports.reset_out, self.ports.reset_out)
-        self.wire(self.underlying.ports.cgra_stall, self.ports.cgra_stall)
-        self.wire(self.underlying.ports.glb_stall, self.ports.glb_stall)
-        self.wire(self.underlying.ports.soft_reset,
-                  self.ports.soft_reset)
+        m.wire(self.underlying.ports.clk_out, self.io.clk_out)
+        m.wire(self.underlying.ports.reset_out, self.io.reset_out)
+        m.wire(self.underlying.ports.cgra_stall, self.io.cgra_stall)
+        m.wire(self.underlying.ports.glb_stall, self.io.glb_stall)
+        m.wire(self.underlying.ports.soft_reset,
+                  self.io.soft_reset)
 
         # global buffer configuration
-        self.wire(self.ports.glb_cfg.wr_en,
+        m.wire(self.io.glb_cfg.wr_en,
                   self.underlying.ports.glb_cfg_wr_en)
-        self.wire(self.ports.glb_cfg.wr_clk_en,
+        m.wire(self.io.glb_cfg.wr_clk_en,
                   self.underlying.ports.glb_cfg_wr_clk_en)
-        self.wire(self.ports.glb_cfg.wr_addr,
+        m.wire(self.io.glb_cfg.wr_addr,
                   self.underlying.ports.glb_cfg_wr_addr)
-        self.wire(self.ports.glb_cfg.wr_data,
+        m.wire(self.io.glb_cfg.wr_data,
                   self.underlying.ports.glb_cfg_wr_data)
-        self.wire(self.ports.glb_cfg.rd_en,
+        m.wire(self.io.glb_cfg.rd_en,
                   self.underlying.ports.glb_cfg_rd_en)
-        self.wire(self.ports.glb_cfg.rd_clk_en,
+        m.wire(self.io.glb_cfg.rd_clk_en,
                   self.underlying.ports.glb_cfg_rd_clk_en)
-        self.wire(self.ports.glb_cfg.rd_addr,
+        m.wire(self.io.glb_cfg.rd_addr,
                   self.underlying.ports.glb_cfg_rd_addr)
-        self.wire(self.underlying.ports.glb_cfg_rd_data,
-                  self.ports.glb_cfg.rd_data)
-        self.wire(self.underlying.ports.glb_cfg_rd_data_valid,
-                  self.ports.glb_cfg.rd_data_valid)
+        m.wire(self.underlying.ports.glb_cfg_rd_data,
+                  self.io.glb_cfg.rd_data)
+        m.wire(self.underlying.ports.glb_cfg_rd_data_valid,
+                  self.io.glb_cfg.rd_data_valid)
 
         # global buffer sram configuration
-        self.wire(self.ports.sram_cfg.wr_en,
+        m.wire(self.io.sram_cfg.wr_en,
                   self.underlying.ports.sram_cfg_wr_en)
-        self.wire(self.ports.sram_cfg.wr_clk_en,
+        m.wire(self.io.sram_cfg.wr_clk_en,
                   self.underlying.ports.sram_cfg_wr_clk_en)
-        self.wire(self.ports.sram_cfg.wr_addr,
+        m.wire(self.io.sram_cfg.wr_addr,
                   self.underlying.ports.sram_cfg_wr_addr)
-        self.wire(self.ports.sram_cfg.wr_data,
+        m.wire(self.io.sram_cfg.wr_data,
                   self.underlying.ports.sram_cfg_wr_data)
-        self.wire(self.ports.sram_cfg.rd_en,
+        m.wire(self.io.sram_cfg.rd_en,
                   self.underlying.ports.sram_cfg_rd_en)
-        self.wire(self.ports.sram_cfg.rd_clk_en,
+        m.wire(self.io.sram_cfg.rd_clk_en,
                   self.underlying.ports.sram_cfg_rd_clk_en)
-        self.wire(self.ports.sram_cfg.rd_addr,
+        m.wire(self.io.sram_cfg.rd_addr,
                   self.underlying.ports.sram_cfg_rd_addr)
-        self.wire(self.underlying.ports.sram_cfg_rd_data,
-                  self.ports.sram_cfg.rd_data)
-        self.wire(self.underlying.ports.sram_cfg_rd_data_valid,
-                  self.ports.sram_cfg.rd_data_valid)
+        m.wire(self.underlying.ports.sram_cfg_rd_data,
+                  self.io.sram_cfg.rd_data)
+        m.wire(self.underlying.ports.sram_cfg_rd_data_valid,
+                  self.io.sram_cfg.rd_data_valid)
 
         # start/done pulse
-        self.wire(self.underlying.ports.strm_f2g_interrupt_pulse,
-                  self.ports.strm_f2g_interrupt_pulse)
-        self.wire(self.underlying.ports.strm_g2f_interrupt_pulse,
-                  self.ports.strm_g2f_interrupt_pulse)
-        self.wire(self.underlying.ports.pcfg_g2f_interrupt_pulse,
-                  self.ports.pcfg_g2f_interrupt_pulse)
-        self.wire(self.ports.strm_start_pulse,
+        m.wire(self.underlying.ports.strm_f2g_interrupt_pulse,
+                  self.io.strm_f2g_interrupt_pulse)
+        m.wire(self.underlying.ports.strm_g2f_interrupt_pulse,
+                  self.io.strm_g2f_interrupt_pulse)
+        m.wire(self.underlying.ports.pcfg_g2f_interrupt_pulse,
+                  self.io.pcfg_g2f_interrupt_pulse)
+        m.wire(self.io.strm_start_pulse,
                   self.underlying.ports.strm_start_pulse)
-        self.wire(self.ports.pc_start_pulse,
+        m.wire(self.io.pc_start_pulse,
                   self.underlying.ports.pc_start_pulse)
 
         # cgra configuration interface
-        self.wire(self.underlying.ports.cgra_cfg_addr,
-                  self.ports.cgra_config.config_addr)
-        self.wire(self.underlying.ports.cgra_cfg_wr_data,
-                  self.ports.cgra_config.config_data)
-        self.wire(self.underlying.ports.cgra_cfg_read,
-                  self.ports.cgra_config.read[0])
-        self.wire(self.underlying.ports.cgra_cfg_write,
-                  self.ports.cgra_config.write[0])
-        self.wire(self.ports.read_data_in,
+        m.wire(self.underlying.ports.cgra_cfg_addr,
+                  self.io.cgra_config.config_addr)
+        m.wire(self.underlying.ports.cgra_cfg_wr_data,
+                  self.io.cgra_config.config_data)
+        m.wire(self.underlying.ports.cgra_cfg_read,
+                  self.io.cgra_config.read[0])
+        m.wire(self.underlying.ports.cgra_cfg_write,
+                  self.io.cgra_config.write[0])
+        m.wire(self.io.read_data_in,
                   self.underlying.ports.cgra_cfg_rd_data)
 
         # axi4-lite slave interface
-        self.wire(self.ports.axi4_slave.awaddr,
-                  self.underlying.ports.axi_awaddr)
-        self.wire(self.ports.axi4_slave.awvalid,
-                  self.underlying.ports.axi_awvalid)
-        self.wire(self.ports.axi4_slave.awready,
-                  self.underlying.ports.axi_awready)
-        self.wire(self.ports.axi4_slave.wdata,
-                  self.underlying.ports.axi_wdata)
-        self.wire(self.ports.axi4_slave.wvalid,
-                  self.underlying.ports.axi_wvalid)
-        self.wire(self.ports.axi4_slave.wready,
-                  self.underlying.ports.axi_wready)
-        self.wire(self.ports.axi4_slave.bready,
-                  self.underlying.ports.axi_bready)
-        self.wire(self.ports.axi4_slave.bvalid,
-                  self.underlying.ports.axi_bvalid)
-        self.wire(self.ports.axi4_slave.bresp,
-                  self.underlying.ports.axi_bresp)
-        self.wire(self.ports.axi4_slave.araddr,
-                  self.underlying.ports.axi_araddr)
-        self.wire(self.ports.axi4_slave.arvalid,
-                  self.underlying.ports.axi_arvalid)
-        self.wire(self.ports.axi4_slave.arready,
-                  self.underlying.ports.axi_arready)
-        self.wire(self.ports.axi4_slave.rdata,
-                  self.underlying.ports.axi_rdata)
-        self.wire(self.ports.axi4_slave.rresp,
-                  self.underlying.ports.axi_rresp)
-        self.wire(self.ports.axi4_slave.rvalid,
-                  self.underlying.ports.axi_rvalid)
-        self.wire(self.ports.axi4_slave.rready,
-                  self.underlying.ports.axi_rready)
+        m.wire(self.io.axi4_slave.awaddr,
+               self.underlying.axi_awaddr)
+        m.wire(self.io.axi4_slave.awvalid,
+               self.underlying.axi_awvalid)
+        m.wire(self.io.axi4_slave.awready,
+               self.underlying.axi_awready)
+        m.wire(self.io.axi4_slave.wdata,
+               self.underlying.axi_wdata)
+        m.wire(self.io.axi4_slave.wvalid,
+               self.underlying.axi_wvalid)
+        m.wire(self.io.axi4_slave.wready,
+               self.underlying.axi_wready)
+        m.wire(self.io.axi4_slave.bready,
+               self.underlying.axi_bready)
+        m.wire(self.io.axi4_slave.bvalid,
+               self.underlying.axi_bvalid)
+        m.wire(self.io.axi4_slave.bresp,
+               self.underlying.axi_bresp)
+        m.wire(self.io.axi4_slave.araddr,
+               self.underlying.axi_araddr)
+        m.wire(self.io.axi4_slave.arvalid,
+               self.underlying.axi_arvalid)
+        m.wire(self.io.axi4_slave.arready,
+               self.underlying.axi_arready)
+        m.wire(self.io.axi4_slave.rdata,
+               self.underlying.axi_rdata)
+        m.wire(self.io.axi4_slave.rresp,
+               self.underlying.axi_rresp)
+        m.wire(self.io.axi4_slave.rvalid,
+               self.underlying.axi_rvalid)
+        m.wire(self.io.axi4_slave.rready,
+               self.underlying.axi_rready)
 
         # interrupt
-        self.wire(self.ports.interrupt, self.underlying.ports.interrupt)
+        m.wire(self.io.interrupt, self.underlying.interrupt)
 
         # jtag interface signals
-        self.wire(self.ports.jtag.tdi, self.underlying.ports.tdi)
-        self.wire(self.ports.jtag.tdo, self.underlying.ports.tdo)
-        self.wire(self.ports.jtag.tms, self.underlying.ports.tms)
-        self.wire(self.ports.jtag.tck, self.underlying.ports.tck)
-        self.wire(self.ports.jtag.trst_n, self.underlying.ports.trst_n)
+        m.wire(self.io.jtag.tdi, self.underlying.tdi)
+        m.wire(self.io.jtag.tdo, self.underlying.tdo)
+        m.wire(self.io.jtag.tms, self.underlying.tms)
+        m.wire(self.io.jtag.tck, self.underlying.tck)
+        m.wire(self.io.jtag.trst_n, self.underlying.trst_n)
 
-    def name(self):
-        return f"GlobalController_cfg_{self.addr_width}_{self.data_width}" \
-               f"_axi_{self.axi_addr_width}_{self.axi_data_width}"
