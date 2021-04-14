3acd3c85a82ef06bd09c6f2d98d1b1149e06a560
steveri <steveri@steveri.com>
2021-04-14 12:42:31 -0700

    debugged

--- .buildkite/bin/prh-twostep.sh
+++ .buildkite/bin/prh-twostep.sh
@@ -2,7 +2,7 @@
 
 set -eu ; # exit immediately on failure
 
-notes="
+cat <<EOF>/dev/null
     Want final directory structure to look something like:
 
 
@@ -27,23 +27,23 @@ notes="
         # Repeat five times for ten total tests including retries
 
         - command: '$$buildpipe | buildkite-agent pipeline upload'
-          label: ":pipeline: Upload"
+          label: "gensteps"
         - wait: { continue_on_failure: true }
 
         - command: '$$buildpipe | buildkite-agent pipeline upload'
-          label: ":pipeline: Upload"
+          label: "gensteps"
         - wait: { continue_on_failure: true }
 
         - command: '$$buildpipe | buildkite-agent pipeline upload'
-          label: ":pipeline: Upload"
+          label: "gensteps"
         - wait: { continue_on_failure: true }
 
         - command: '$$buildpipe | buildkite-agent pipeline upload'
-          label: ":pipeline: Upload"
+          label: "gensteps"
         - wait: { continue_on_failure: true }
 
         - command: '$$buildpipe | buildkite-agent pipeline upload'
-          label: ":pipeline: Upload"
+          label: "gensteps"
 
     ------------------------------------------------------------------
     prh-pipeline.sh generates the next two pipe steps, something like
@@ -58,39 +58,39 @@ notes="
     
       njobs=0
       for i in 0 1 2 3 4 5 6 7 8 9; do
-          rundir=$1/run$i ; # e.g. '/build/prh3555/run0'
-          test -e $rundir && echo "ooh something borked" && continue
-          test -e ${rundir}-pass  && continue; # run passed
-          test -e ${rundir}-fail2 && continue; # run reached max retries
-          if test -e ${rundir}-fail1; then
-              build_step prh.sh $rundir run$i fail2 ; # failed once; retry
+          rundir=\$1/run\$i ; # e.g. '/build/prh3555/run0'
+          test -e \$rundir && echo "ooh something borked" && continue
+          test -e \${rundir}-pass  && continue; # run passed
+          test -e \${rundir}-fail2 && continue; # run reached max retries
+          if test -e \${rundir}-fail1; then
+              build_step prh.sh \$rundir run\$i fail2 ; # failed once; retry
           else
-              build_step prh.sh $rundir run$i fail1 ; # first try
+              build_step prh.sh \$rundir run\$i fail1 ; # first try
           fi
           njobs++
-          if ($njobs==2); then break; else continue; fi
+          if (\$njobs==2); then break; else continue; fi
       done
       echo " - wait:     { continue_on_failure: true }"
       exit
 
     prh.sh /build/prh3555/run0 does this:
       build and run in dir rundir=/build/prh3555/run0
-      if PASS then mv $rundir ${rundir}-pass
+      if PASS then mv \$rundir \${rundir}-pass
       else for i in 1 2 3 4 5 6 7 8 9; do
-        failname=${rundir}-fail$i
-        if ! -e $failname; then mv $rundir $failname; break; fi
+        failname=\${rundir}-fail\$i
+        if ! -e \$failname; then mv \$rundir \$failname; break; fi
       fi
 
-"
+EOF
 
 date=`date +%H%M`
 
 # test 1
 cat <<EOF
 steps:
-  - label: fail begin $date
+  - label: $date fail
     command: 'echo fail 10; sleep 10; exit 13'
-  - label: pass begin $date
+  - label: $date pass
     command: 'echo pass 20; sleep 20; exit 0'
 EOF
 exit
@@ -112,8 +112,3 @@ function build_step {
     label: 'run0' or 'run0a'
     command: "prh.sh $rundir"; # failed once; retry
 }
-
-
-
-
-
