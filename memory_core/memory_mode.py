1044c1f887141592b21387e334cf64084e611a23
mbstrange2 <mstrange@stanford.edu>
2021-04-27 13:08:51 -0700

    refactor memtile configuration to use explicit modes which allows preloading in any mode

--- memory_core/memory_mode.py
+++ memory_core/memory_mode.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 
-class Mode(Enum):
-    LINE_BUFFER = 0
-    FIFO = 1
+class MemoryMode(Enum):
+    UNIFIED_BUFFER = "lake"
+    ROM = 1
     SRAM = 2
-    DB = 3
+    FIFO = 3
