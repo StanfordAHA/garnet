9a159f89fd80e8e59aac0c281c144f498a2a1b6d
Taeyoung Kong <kongty@stanford.edu>
2021-06-19 22:06:04 -0700

    Parsing part is done

--- tests/test_app/test.c
+++ tests/test_app/test.c
@@ -17,8 +17,20 @@ int main() {
     assert(info->input_info[0]->io == Input);
     assert(strcmp(info->input_info[0]->filename, "bin/input.pgm") == 0);
     assert(info->input_info[0]->num_io_tiles == 1);
+    assert(info->input_info[0]->io_tiles[0].pos.x == 0);
+    assert(info->input_info[0]->io_tiles[0].pos.y == 0);
+    assert(info->input_info[0]->io_tiles[0].loop_dim == 1);
+    assert(info->input_info[0]->io_tiles[0].stride[0] == 1);
+    assert(info->input_info[0]->io_tiles[0].extent[0] == 4096);
 
     assert(info->output_info[0]->io == Output);
     assert(strcmp(info->output_info[0]->filename, "bin/gold.pgm") == 0);
     assert(info->output_info[0]->num_io_tiles == 1);
+    assert(info->output_info[0]->io_tiles[0].pos.x == 1);
+    assert(info->output_info[0]->io_tiles[0].pos.y == 0);
+    assert(info->output_info[0]->io_tiles[0].loop_dim == 2);
+    assert(info->output_info[0]->io_tiles[0].stride[0] == 1);
+    assert(info->output_info[0]->io_tiles[0].stride[1] == 64);
+    assert(info->output_info[0]->io_tiles[0].extent[0] == 63);
+    assert(info->output_info[0]->io_tiles[0].extent[1] == 64);
 }
