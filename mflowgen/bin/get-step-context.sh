ac8952f8affbe2fb5f4a31f455ceab109663d4bb
steveri <steveri@steveri.com>
2021-07-02 08:40:31 -0700

    clean up get-step script output

--- mflowgen/bin/get-step-context.sh
+++ mflowgen/bin/get-step-context.sh
@@ -2,40 +2,86 @@
 
 # Usage: $0 <refdir>
 
-# Build the necessary context to run postroute_hold step only.
-# TODO/LATER maybe want this to work for any step.
+# Using reference/cached build in <refdir>, find and link to the
+# necessary context for running a single target step in an mflowgen
+# build
 
-REF=$1
-
-# Want to link two steps from <refdir> into current (build) dir
-#     *-cadence-innovus-postroute
-#     *-cadence-innovus-flowsetup
-
-# Example usage
+# E.g. for cadence-innovus-postroute_hold we would link two steps from
+# <refdir> into current (build) dir
 # 
-#     RUNDIR=/build/run230/full_chip
+#     ln -s 23-cadence-innovus-flowsetup <refdir>/23-cadence-innovus-postroute
+#     ln -s 31-cadence-innovus-postroute <refdir>/30-cadence-innovus-postroute
+
+# Example usage: run standalone step "cadence-innovus-postroute_hold"
+# in $RUNDIR using cached information in $REFDIR":
+# 
+#     # 1. Designate a reference directory from which we will pull context.
 #     REFDIR=/build/gold
-# 
-#     # Note setup script leaves you in RUNDIR as a side effect
-#     source mflowgen/bin/setup-buildkite.sh --dir $RUNDIR
-#     mflowgen/bin/get-step-context.sh $REFDIR
+#
+#     # 2. Build the design framework
+#     mflowgen run --design $GARNET_HOME/mflowgen/full_chip
+#
+#     # 3. Build standalone context and execute target step "postroute_hold"
+#     get-step-context.sh $REFDIR cadence-innovus-postroute_hold
 #     make cadence-innovus-postroute_hold
 
-DBG=1
+REF=$1
+target_step=$2
+target_step=cadence-innovus-postroute_hold
+
+
+DBG=0
 
 echo "--- BEGIN $0 $*" | sed "s,$GARNET_HOME,\$GARNET_HOME,"
-echo "  GARNET_HOME=$GARNET_HOME"
+echo "  where GARNET_HOME=$GARNET_HOME"
 
-echo "--- PRH TEST RIG SETUP - symlink to steps in $REF/full_chip";
+echo "+++ STANDALONE TEST RIG SETUP - build symlinks to steps in $REF/full_chip";
+
+function find_dependences {
+    # Example: 
+    #    find_dependences cadence-innovus-postroute_hold
+    # 
+    # Returns:
+    #      adk
+    #      cadence-innovus-postroute
+    #      cadence-innovus-flowsetup
+
+    stepname="$1"
+    stepnum=`make list | awk '$NF == "'$step'"{print $2; exit}'`
+    echo "FOUND $stepnum $stepname"
+
+    # Note for some reason it ignores adk dependences.
+    # Seems to work anyway I guess?
+    deps=`make info-$stepnum |& grep -v warning | sed 's/|//g' \
+         | awk '$NF ~ /^[0-9]/ {print $NF}; /^Parameters/{exit}' \
+         | sed "/$step\$/,\\$d" \
+         | egrep -v 'freepdk|tsmc' \
+         | sed 's/^[0-9]*[-]//'`
+
+    echo $deps
+}
+
+# Find-dependences came up with this:
+
+set +x
+find_dependences cadence-innovus-postroute_hold
+set -x
+echo ""
+echo ""
+echo ""
+
+
+
+
 for step in cadence-innovus-postroute cadence-innovus-flowsetup; do
 
-    echo Processing step $step
+    [ "$DBG" ] && echo Processing step $step
 
     # Find name of step in local dir; bug out if exists already
     [ "$DBG" ] && (make list |& egrep ": $step\$")
     stepnum=$(make list |& egrep ": $step\$" | awk '{print $2}')
     local_step=$stepnum-$step
-    [ "$DBG" ] && echo Want local step $local_step; echo ''
+    [ "$DBG" ] && (echo "Want local step $local_step"; echo '')
     if test -e $local_step; then
         echo "Looks like '$local_step' exists already"
         echo "Doing nothing for '$local_step'"
@@ -45,7 +91,7 @@ for step in cadence-innovus-postroute cadence-innovus-flowsetup; do
     # Find name of step in ref dir
     stepnum=$(cd $REF/full_chip; make list |& egrep ": $step\$" | awk '{print $2}')
     ref_step=$REF/full_chip/$stepnum-$step
-    [ "$DBG" ] && echo Found ref step $ref_step
+    [ "$DBG" ] && echo "Found ref step $ref_step"
     if ! test -d $ref_step; then
         echo "hm look like $ref_step don't exist after all..."
         exit 13
@@ -56,14 +102,82 @@ for step in cadence-innovus-postroute cadence-innovus-flowsetup; do
     touch $ref_step/.prebuilt
 
     # Link local to ref
-    [ "$DBG" ] && echo "Ready to do: ln -s $ref_step $local_step"
+    [ "$DBG" ] && echo "ln -s $ref_step $local_step"
     ln -s $ref_step $local_step
     echo "$local_step => $ref_step"
-    echo ''
+    [ "$DBG" ] && echo ''
 
 done
+
+echo "+++ TODO LIST";
+echo "make -n cadence-innovus-postroute_hold"
+make -n cadence-innovus-postroute_hold
+|& egrep "^mkdir.*output" | sed "s/output.*//" | egrep -v ^Make'
+
 
 # # Did we get away with it? Example of how to check:
 # echo "+++ TODO LIST"
 # function make-n-filter { egrep '^mkdir.*output' | sed 's/output.*//' | egrep -v ^Make ;}
 # make -n cadence-innovus-postroute_hold |& make-n-filter
+
+
+
+# NOTES
+
+
+exit
+########################################################################
+# To run locally on kiwi can do this:
+do_this=OFF
+if [ "$do_this" == "ON" ]; then
+
+    # 1. Set up run and ref dirs
+    T=/tmp/get-step-context; cd $T
+    REFDIR=/build/gold; RUNDIR=$T/full_chip
+
+    # 2. Set up to execute target step in $RUNDIR
+    export GARNET_HOME=/nobackup/steveri/github/garnet
+    sbk=$GARNET_HOME/mflowgen/bin/setup-buildkite.sh
+    source $sbk --dir $RUNDIR |& tee log | less
+
+    # 3. Jimmy up a fake tsmc16 library
+    (cd /tmp/steveri/mflowgen.master/adks; ln -s freepdk-45nm tsmc16)
+
+    # 4. Build the design framework
+    mflowgen run --design $GARNET_HOME/mflowgen/full_chip
+
+    ### >>> GOOD TO HERE <<< ###
+
+    # 3. Build standalone context and execute target step "postroute_hold"
+    mflowgen/bin/get-step-context.sh $REFDIR
+    make cadence-innovus-postroute_hold
+fi
+
+exit
+########################################################################
+# Buildkite on arm machine should be able to do this:
+do_this=OFF
+if [ "$do_this" == "ON" ]; then
+
+    # 1. Set up run and ref dirs
+    T=/tmp/get-step-context; cd $T
+    REFDIR=/build/gold; RUNDIR=$T/full_chip
+
+    # 2. Set up to execute target step in $RUNDIR
+    export GARNET_HOME=/sim/steveri/soc/components/cgra/garnet
+    sbk=$GARNET_HOME/mflowgen/bin/setup-buildkite.sh
+    source $sbk --dir $RUNDIR |& tee log | less
+
+    # 3. No need to jimmy up a fake tsmc16 library
+    # (cd /tmp/steveri/mflowgen.master/adks; ln -s freepdk-45nm tsmc16)
+
+    # 4. Build the design framework
+    mflowgen run --design $GARNET_HOME/mflowgen/full_chip
+
+    ### >>> GOOD TO HERE <<< ###
+
+    # 3. Build standalone context and execute target step "postroute_hold"
+    mflowgen/bin/get-step-context.sh $REFDIR
+    make cadence-innovus-postroute_hold
+fi
+
