f81411c5276a22462cc6f72cc6e0a8a542953ef1
steveri <steveri@steveri.com>
2021-08-18 21:10:40 -0700

    ready to watch it fail...

--- mflowgen/full_chip/construct.py
+++ mflowgen/full_chip/construct.py
@@ -69,7 +69,8 @@ def construct():
     'array_height'      : 16,
     'interconnect_only' : False,
     # glb tile memory size (unit: KB)
-    'glb_tile_mem_size' : 64,
+    # 'glb_tile_mem_size' : 64,
+    'glb_tile_mem_size' : 256,
     # Power Domains
     'PWR_AWARE'         : True,
     # Include Garnet?
