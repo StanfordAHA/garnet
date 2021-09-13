3423b44b170c548c2b558f96792cd59204c79781
steveri <steveri@steveri.com>
2021-09-13 13:10:17 -0700

    auto-detect mc failure + fallback to cached srams

--- mflowgen/common/gen_sram_macro/gen_srams.sh
+++ mflowgen/common/gen_sram_macro/gen_srams.sh
@@ -9,18 +9,34 @@ fi
 
 sram_name+="_130a"
 
-##############################################################################
-USE_CACHED=True
-USE_CACHED=False
-if [ $USE_CACHED == True ]; then
+# Compile the memories and capture the output in a log.
+# Note that the compiler itself apparently never exits with nonzero status,
+# regardless of errors flagged during execution (??)
 
-    # Mar 2021 Temporary fix to get around expired memory-compiler license
-    echo '+++ HACK TIME! Using cached srams...'; set -x
+cmd="./inputs/adk/mc/${mc_name}/tsn16ffcllhdspsbsram_130a.pl -file config.txt -NonBIST -NonSLP -NonDSLP -NonSD"
+if [ ! $partial_write == True ]; then
+    cmd+=" -NonBWEB"
+fi
+eval $cmd |& tee mc.log
+
+# Because compiler does not fail properly, we have to look in the log
+# for success/failure. If compiler failed, try and find cached srams to use.
+# In particular, we currently are getting this failure (sep 2021):
+# 
+#    FATAL (MC, 044): Failed to get license for feature
+#    MC2-tsn16ffcllhdspsbsram-CLV or MC2-tsn16ffcllhdspsbsram-CPV 
+
+if grep FATAL mc.log; then
+    echo "**ERROR Looks like memory compiler flagged a fatal error"
+    echo "Will try using cached SRAMs instead :("
+    echo '+++ HACK TIME! Using cached srams because mem compiler failed.'
+    set -x
 
     # Use latest gold build as the cache (if hack were less temporary,
     # would use parm / env var here).
     # The -P option says to use the physical directory structure
     # instead of following symbolic links
+
     gold=/sim/buildkite-agent/gold
     if ! test -e $gold; then
         echo "**ERROR Cannot find gold directory for cached srams"
@@ -32,20 +48,57 @@ if [ $USE_CACHED == True ]; then
     echo "Using gold cache '$g'"
 
     # Use first sram found in cache, so long as it has the right name (e.g. sram_name='ts1n16ffcllsblvtc2048x64m8sw_130a')
+
     sram_dir=$(find $g -name ${sram_name} | head -1)
     echo "Using srams found in dir '$sram_dir'"
     cp -rp ${sram_dir} .
 
-    # Done w/hack
-    set +x; echo '--- continue...'
+    # Done with hack
 
-else
-    cmd="./inputs/adk/mc/${mc_name}/tsn16ffcllhdspsbsram_130a.pl -file config.txt -NonBIST -NonSLP -NonDSLP -NonSD"
-    if [ ! $partial_write == True ]; then
-        cmd+=" -NonBWEB"
-    fi
-    eval $cmd
+    set +x; echo '--- continue...'
 fi
+
+
+
+
+# ##############################################################################
+# USE_CACHED=True
+# USE_CACHED=False
+# if [ $USE_CACHED == True ]; then
+# 
+#     # Mar 2021 Temporary fix to get around expired memory-compiler license
+#     echo '+++ HACK TIME! Using cached srams...'; set -x
+# 
+#     # Use latest gold build as the cache (if hack were less temporary,
+#     # would use parm / env var here).
+#     # The -P option says to use the physical directory structure
+#     # instead of following symbolic links
+#     gold=/sim/buildkite-agent/gold
+#     if ! test -e $gold; then
+#         echo "**ERROR Cannot find gold directory for cached srams"
+#         echo "I.e. '$gold' does not exist"
+#         echo "Also see $0"
+#         exit 13
+#     fi
+#     g=$(cd -P $gold; pwd)
+#     echo "Using gold cache '$g'"
+# 
+#     # Use first sram found in cache, so long as it has the right name (e.g. sram_name='ts1n16ffcllsblvtc2048x64m8sw_130a')
+#     sram_dir=$(find $g -name ${sram_name} | head -1)
+#     echo "Using srams found in dir '$sram_dir'"
+#     cp -rp ${sram_dir} .
+# 
+#     # Done w/hack
+#     set +x; echo '--- continue...'
+# 
+# else
+#     cmd="./inputs/adk/mc/${mc_name}/tsn16ffcllhdspsbsram_130a.pl -file config.txt -NonBIST -NonSLP -NonDSLP -NonSD"
+#     if [ ! $partial_write == True ]; then
+#         cmd+=" -NonBWEB"
+#     fi
+#     eval $cmd |& tee mc.log
+#     
+# fi
 
 ln -s ../$sram_name/NLDM/${sram_name}_${corner}.lib outputs/sram_tt.lib
 ln -s ../$sram_name/NLDM/${sram_name}_${bc_corner}.lib outputs/sram_ff.lib
