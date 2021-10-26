c20ba7a9976200d249e32a5bcbc9d8d2c4d507b7
Taeyoung Kong <kongty@stanford.edu>
2021-10-25 18:49:34 -0700

    [WIP] SRAM controller kratos conversion is done

--- global_buffer/design/glb_cfg_ifc.py
+++ global_buffer/design/glb_cfg_ifc.py
@@ -1,28 +1,21 @@
 from kratos import Interface
-from global_buffer.design.global_buffer_parameter import GlobalBufferParams
 
 
 class GlbConfigInterface(Interface):
-    def __init__(self, params: GlobalBufferParams):
-        Interface.__init__(self, "glb_cfg_ifc")
-        self.params = params
-
-        addr_width = params.axi_addr_width
-        data_width = params.axi_data_width
+    def __init__(self, addr_width: int, data_width: int):
+        Interface.__init__(self, f"glb_cfg_ifc_A_{addr_width}_D_{data_width}")
 
         # Local variables
         wr_en = self.var("wr_en", 1)
-        wr_clk_en = self.var("wr_clk_en", 1)
         wr_addr = self.var("wr_addr", addr_width)
         wr_data = self.var("wr_data", data_width)
         rd_en = self.var("rd_en", 1)
-        rd_clk_en = self.var("rd_clk_en", 1)
         rd_addr = self.var("rd_addr", addr_width)
         rd_data = self.var("rd_data", data_width)
         rd_data_valid = self.var("rd_data_valid", 1)
 
-        m_to_s = [wr_en, wr_clk_en, wr_addr,
-                  wr_data, rd_en, rd_clk_en, rd_addr]
+        m_to_s = [wr_en, wr_addr,
+                  wr_data, rd_en, rd_addr]
         s_to_m = [rd_data, rd_data_valid]
 
         self.master = self.modport("master")
