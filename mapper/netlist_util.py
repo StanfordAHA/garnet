d062266fa1aded8c0efc4ebd3b811748797e58db
Jack <melchert@stanford.edu>
2021-11-01 08:36:56 -0700

    Fixed aha glb

--- mapper/netlist_util.py
+++ mapper/netlist_util.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from hwtypes.adt import Tuple, Product
 from metamapper.node import Dag, Input, Output, Combine, Select, DagNode, IODag, Constant, Sink
 from metamapper.common_passes import AddID
-from DagVisitor import Visitor
+from DagVisitor import Visitor, Transformer
 from hwtypes import Bit, BitVector
 from peak.mapper.utils import Unbound
 from graphviz import Digraph
@@ -105,9 +105,13 @@ class CreateBuses(Visitor):
     def visit_Output(self, node: Output):
         Visitor.generic_visit(self, node)
         child_bid = self.node_to_bid[node.child]
-        assert isinstance(child_bid, dict)
-        for field, bid in child_bid.items():
-            self.netlist[bid].append((node, field))
+        if node.child.field == "stencil_valid":
+            port = 'f2io_1'
+        else:
+            port = 'f2io_16'
+        self.netlist[child_bid].append((node, port))
+        #for field, bid in child_bid.items():
+        #    self.netlist[bid].append((node, field))
 
 
 class CreateInstrs(Visitor):
@@ -188,12 +192,12 @@ class CreateIDs(Visitor):
     def visit_Output(self, node: Output):
         Visitor.generic_visit(self, node)
         child = list(node.children())[0]
-        assert isinstance(child, Combine)
-        c_children = list(child.children())
-        if isinstance(c_children[0], Constant):
-            is_bit = True
-        else:
+        #assert isinstance(child, Combine)
+        #c_children = list(child.children())
+        if "io16" in node.iname:
             is_bit = False
+        else:
+            is_bit = True
 
         if is_bit:
             id = f"i{self.i}"
@@ -294,7 +298,7 @@ class FlattenIO(Visitor):
         isel = lambda t: "io2f_1" if t==Bit else "io2f_16"
         real_inputs = [Input(type=IO_Input_t, iname="_".join(str(field) for field in path)) for path in ipath_to_type]
         self.inputs = {path: inode.select(isel(t)) for inode, (path, t) in zip(real_inputs, ipath_to_type.items())}
-
+        #breakpoint()
         self.outputs = {}
         self.run(dag)
         real_sources = [self.node_map[s] for s in dag.sources[1:]]
@@ -363,35 +367,6 @@ class FlattenIO(Visitor):
 
 
 
-class PlacementHelper(Visitor):
-    def __init__(self):
-        pass
-    def doit(self, dag: IODag):
-        self.pe_paths = []
-        self.run(dag)
-        for path in self.pe_paths:
-            path.reverse()
-        return self.pe_paths
-    def generic_visit(self, node: DagNode):
-        if node.node_name == "global.PE":
-            found = False
-            for path in self.pe_paths:
-                if node.iname in path:
-                    found = True
-                    break
-            if not found:
-                path = []
-                path.append(node.iname)
-                self.pe_paths.append(path)
-            for child in node.children():
-                print(child)
-                for chil in child.children():
-                    if chil.node_name == "global.PE":
-                        path.append(chil.iname)
-        Visitor.generic_visit(self, node)
-
-
-
 def print_netlist_info(info, filename):
 
     outfile = open(filename, 'w')
@@ -427,7 +402,6 @@ class DagToPdf(Visitor):
         self.nodes_to_ids = nodes_to_ids
 
     def doit(self, dag: Dag):
-        AddID().run(dag)
         self.graph = Digraph()
         self.run(dag)
         return self.graph
@@ -435,12 +409,9 @@ class DagToPdf(Visitor):
     def generic_visit(self, node):
         Visitor.generic_visit(self, node)
         def n2s(node):
-            return f"{str(node)}_{node._id_}"
+            return f"{str(node)}_{str(node.iname)}"
         if self.no_unbound and not is_unbound_const(node):
-            if node in self.nodes_to_ids:
-                self.graph.node(n2s(node), label=self.nodes_to_ids[node])
-            else:
-                self.graph.node(n2s(node))
+            self.graph.node(n2s(node))
         for i, child in enumerate(node.children()):
             if self.no_unbound and not is_unbound_const(child):
                 self.graph.edge(n2s(child), n2s(node), label=str(i))
@@ -448,17 +419,85 @@ class DagToPdf(Visitor):
 def gen_dag_img(dag, file, info, no_unbound=True):
     DagToPdf(info, no_unbound).doit(dag).render(filename=file)
 
+
+class RemoveInputsOutputs(Visitor):
+    def __init__(self):
+        pass
+
+    def doit(self, dag: Dag):
+        self.node_map = {}
+        self.inputs = []
+        self.outputs = []
+        self.run(dag)
+        real_sources = [self.node_map[s] for s in dag.sources[1:]]
+        real_sinks = [self.node_map[s] for s in dag.sinks[1:]]
+        #breakpoint()
+        return IODag(inputs=self.inputs, outputs=self.outputs, sources=real_sources, sinks=real_sinks)
+
+
+    def visit_Select(self, node: DagNode):
+        Visitor.generic_visit(self, node)
+        if not ("hw_output" in [child.iname for child in node.children()] or "self" in [child.iname for child in node.children()]):
+            new_children = [self.node_map[child] for child in node.children()]
+            io_child = new_children[0]
+            if "io16in" in io_child.iname:
+                new_node = new_children[0].select("io2f_16")
+            elif "io1in" in io_child.iname:
+                new_node = new_children[0].select("io2f_1")
+            else:
+                new_node = node.copy()
+            new_node.set_children(*new_children)
+            self.node_map[node] = new_node
+
+    def generic_visit(self, node: DagNode):
+        Visitor.generic_visit(self, node)
+        if node.node_name == "global.IO" or node.node_name == "global.BitIO":
+            if "write" in node.iname:
+                new_node = Output(type=IO_Output_t, iname=node.iname)
+                new_children = [self.node_map[child] for child in node.children()]
+                new_node.set_children(*new_children)
+                self.outputs.append(new_node)
+            else:
+                new_node = Input(type=IO_Input_t, iname=node.iname)
+                self.inputs.append(new_node)
+
+            self.node_map[node] = new_node
+        else:
+            if not(node.node_name == "Input" or "Input" in [child.node_name for child in node.children()]):
+                new_children = [self.node_map[child] for child in node.children()]
+                new_node = node.copy()
+                new_node.set_children(*new_children)
+                self.node_map[node] = new_node
+
+
+
+
 from lassen.sim import PE_fc as lassen_fc
 from metamapper. common_passes import print_dag
 
 def create_netlist_info(dag: Dag, tile_info: dict, load_only = False, id_to_name = None):
-    fdag = FlattenIO().doit(dag)
-    # gen_dag_img(fdag, f"img/foo")
+    gen_dag_img(dag, "dag", None)
+    # Extract IO metadata
+    # Inline IO tiles
+
+    #fdag = FlattenIO().doit(dag)
+
+    fdag = RemoveInputsOutputs().doit(dag)
+
+
+    print_dag(fdag)
+    gen_dag_img(fdag, "dag_no_io", None)
+
     def tile_to_char(t):
         if t.split(".")[1]=="PE":
             return "p"
         elif t.split(".")[1]=="MEM":
             return "m"
+        elif t.split(".")[1] == "IO":
+            return "I"
+        elif t.split(".")[1] == "BitIO":
+            return "i"
+
     node_info = {t:tile_to_char(t) for t in tile_info}
     nodes_to_ids = CreateIDs(node_info).doit(fdag)
    
@@ -486,172 +525,6 @@ def create_netlist_info(dag: Dag, tile_info: dict, load_only = False, id_to_name
     info["netlist"] = {}
     for bid, ports in netlist.items():
         info["netlist"][bid] = [(nodes_to_ids[node], field) for node, field in ports]
-
-    return info
-    #gen_dag_img(fdag, "placement_dag", nodes_to_ids)
-
-    paths = PlacementHelper().doit(fdag)
-
-    placements = []
-    placed_tiles = set()
-
-    pe_x = 0
-
-    ic = 30
-    oc = 30
-    output_mem_y = 20
-    grid_x = 64
-    grid_y = 24
-
-    for bid, tiles in info["netlist"].items():
-        if len(tiles) == ic + 1:
-            input_y = -1
-            pe_name = None
-            pe_added = False
-            for tile in tiles:
-                if tile[0] not in placed_tiles:
-                    placed_tiles.add(tile[0])
-                    if 'm' in tile[0]:
-                        mem_placement = {}
-                        mem_placement["name"] = info["id_to_name"][tile[0]]
-                        mem_placement["id"] = tile[0]
-                        mem_placement["x"] = 3
-                        mem_placement["y"] = input_y
-                        placements.append(mem_placement)
-                    elif 'p' in tile[0]:
-                        pe_added = True
-                        pe_placement = {}
-                        pe_placement["name"] = info["id_to_name"][tile[0]]
-                        pe_placement["id"] = tile[0]
-                        pe_placement["x"] = pe_x
-                        if (pe_x + 2) % 4 != 0:
-                            pe_x += 1
-                        else:
-                            pe_x += 2
-                        pe_placement["y"] = input_y
-                        placements.append(pe_placement)
-                    else:
-                        print("Error, not pe or mem tile")
-                    # print(tile[0])
-                pe_name = info["id_to_name"][tile[0]]
-
-            for path in paths:
-                if pe_name in path:
-                    input_y = path.index(pe_name) 
-            print(input_y + 1)
-            if pe_added:
-                for placement in placements:
-                    if placement["y"] == -1:
-                        placement["y"] = (input_y + 1)
-            else:
-                for placement in placements:
-                    if placement["y"] == -1:
-                        placement["y"] = (input_y + 1)
-                        placement["x"] = 7
-
-            pe_x = 0
-        
-    mem_x = 11
-    mem_y = 1
-    for idx, (id, name) in enumerate(info["id_to_name"].items()): 
-        if id not in placed_tiles:
-            if "m" in id and "conv_stencil$ub_conv_stencil_BANK" in name:
-                mem_placement = {}
-                mem_placement["name"] = name
-                mem_placement["id"] = id
-                mem_placement["x"] = mem_x
-                mem_placement["y"] = mem_y
-                if mem_y >= grid_y:
-                    mem_y = 1
-                    mem_x += 4
-                else:
-                    mem_y += 1
-                placements.append(mem_placement)
-                # print(mem_placement)
-
-
-    pe_x = 1
-    pe_y = 18
-    mem_x = 19
-    mem_y = 1
-
-    io_x_in = 0
-    io_x_out = 1
-    bit_io_x_in = 0
-    bit_io_x_out = 1
-
-    last_idx = -1
-    for idx, (id, name) in enumerate(info["id_to_name"].items()): 
-        if id not in placed_tiles:
-            if "p" in id:
-                pe_placement = {}
-                pe_placement["name"] = name
-                pe_placement["id"] = id
-                pe_placement["x"] = pe_x
-                if (pe_x + 2) % 4 != 0:
-                    pe_x += 1
-                else:
-                    pe_x += 2
-                if pe_x >= 40:
-                    pe_y += 1
-                    pe_x = 1
-                pe_placement["y"] = pe_y
-                placements.append(pe_placement)
-                last_idx = idx
-            elif "m" in id:
-                mem_placement = {}
-                mem_placement["name"] = name
-                mem_placement["id"] = id
-                mem_placement["x"] = mem_x
-                mem_placement["y"] = mem_y
-                if mem_y >= output_mem_y - 1:
-                    mem_y = 1
-                    mem_x += 4
-                else:
-                    mem_y += 1
-                placements.append(mem_placement)
-            elif "I" in id:
-                io_placement = {}
-                io_placement["name"] = name
-                io_placement["id"] = id
-                io_placement["y"] = 0
-                if "input" in name:
-                    io_placement["x"] = io_x_in
-                    io_x_in += 2
-                else:
-                    io_placement["x"] = io_x_out
-                    io_x_out += 2
-                placements.append(io_placement)
-            elif "i" in id:
-                io_placement = {}
-                io_placement["name"] = name
-                io_placement["id"] = id
-                io_placement["y"] = 0
-                if "input" in name or "reset" in name:
-                    io_placement["x"] = bit_io_x_in
-                    bit_io_x_in += 2
-                else:
-                    io_placement["x"] = bit_io_x_out
-                    bit_io_x_out += 2
-                placements.append(io_placement)
-            placed_tiles.add(id)
-
-
-    used_tiles = set()
-
-    outfile = open("resnet_one_input_dse_large/design.place", 'w')
-    print("Block Name \t X \t Y \t #Block ID", file = outfile)
-    print("---------------------------", file = outfile)
-    for p in placements:
-        if (p['x'], p['y']) in used_tiles and "i" not in p["id"] and "I" not in p["id"]:
-            print("Tile used twice:", p['x'], p['y'])
-            print(p)
-        used_tiles.add((p['x'], p['y']))
-        print(f"{p['name']}\t{p['x']}\t{p['y']}\t#{p['id']}", file = outfile)
-
-    outfile.close()
-
-    # breakpoint()
 
     return info
 
