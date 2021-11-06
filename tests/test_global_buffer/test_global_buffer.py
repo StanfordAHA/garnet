0aa05cd4c0d702bf84d86718ddfb4aec71f60ec5
Taeyoung Kong <kongty@stanford.edu>
2021-11-06 02:55:29 -0700

    Update glb unit test

--- tests/test_global_buffer/test_global_buffer.py
+++ tests/test_global_buffer/test_global_buffer.py
@@ -6,5 +6,5 @@ import pytest
 @pytest.mark.skipif(not verilog_sim_available(),
                     reason="verilog simulator not available")
 def test_global_buffer_verilog_sim():
-    result = os.system("make -C global_buffer rtl; make -C global_buffer sim")
+    result = os.system("make -C global_buffer rtl; make -C global_buffer run")
     assert result == 0
