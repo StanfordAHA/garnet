b16b285326fdd5ca0dd9c6a3f9307739aa531f44
Taeyoung Kong <kongty@stanford.edu>
2021-11-26 00:03:04 -0800

    Parse power report and dump csv by module breakdown using regex

--- mflowgen/glb_top/synopsys-ptpx-gl/run_step.sh
+++ mflowgen/glb_top/synopsys-ptpx-gl/run_step.sh
@@ -4,3 +4,10 @@ mkdir -p reports logs
 pt_shell -f ptpx.tcl | tee logs/pt.log
 cp reports/${design_name}.power.hier.rpt outputs/power.hier
 cp reports/${design_name}.power.cell.rpt outputs/power.cell
+
+
+for inst in $(echo $instances | sed "s/,/ /g")
+do
+    python parse_report.py --filename reports/${inst}.power.cell.rpt --instances $(echo $parse_modules | sed "s/,/ /g") --csv reports/${inst}.csv
+done
+
