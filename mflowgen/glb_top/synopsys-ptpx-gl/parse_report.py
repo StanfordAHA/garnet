a9067f6a9edaf39c897dd583e0fd12adbd8a3bbd
Taeyoung Kong <kongty@stanford.edu>
2021-11-26 00:03:04 -0800

    Parse power report and dump csv by module breakdown using regex

--- mflowgen/glb_top/synopsys-ptpx-gl/parse_report.py
+++ mflowgen/glb_top/synopsys-ptpx-gl/parse_report.py
@@ -1,5 +1,6 @@
 import argparse
 import pandas as pd
+import re
 
 
 def gen_power_df(filename: str, instances: list):
@@ -9,10 +10,20 @@ def gen_power_df(filename: str, instances: list):
         line_start = False
         for line in f.readlines():
             if line_start is True:
+                if line.startswith( '---------------------' ):
+                    break
+                is_counted = False
+                line_list = line.split()
+                cell_name = line_list[0]
+                power_list = line_list[1:5]
                 for inst in instances:
-                    if inst in line:
-                        line_list = line.split()
-                        power_df.loc[inst] += list(map(float, line_list[1:5]))
+                    if bool(re.search(inst, cell_name)) is True:
+                        power_df.loc[inst] += list(map(float, power_list))
+                        is_counted = True
+                        break  # inst should be included in only one index
+                if is_counted is False:  # If not counted, add it to misc.
+                    line_list = line.split()
+                    power_df.loc['misc'] += list(map(float, power_list))
             elif line.startswith( '---------------------' ):
                 line_start = True
     
@@ -26,7 +37,9 @@ def main():
     parser.add_argument('--csv', '-c', type=str, default="power_by_module.csv")
     args = parser.parse_args()
 
-    power_df = gen_power_df(args.filename, args.instances)
+    if 'misc' not in args.instances:
+        instances = args.instances + ['misc']
+    power_df = gen_power_df(args.filename, instances)
     power_df.to_csv(args.csv)
 
 
