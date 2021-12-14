6e9c2f9818f4c853838607ea487e90b6c7684222
Taeyoung Kong <kongty@stanford.edu>
2021-12-13 22:13:00 -0800

    Optimization done

--- global_buffer/design/glb_bank_ctrl.py
+++ global_buffer/design/glb_bank_ctrl.py
@@ -20,7 +20,6 @@ class GlbBankCtrl(Generator):
         self.packet_wr_data_bit_sel = self.input("packet_wr_data_bit_sel", self._params.bank_data_width)
 
         self.packet_rd_en = self.input("packet_rd_en", 1)
-        self.packet_rd_type = self.input("packet_rd_type", self.header.PacketEnumWidth)
         self.packet_rd_src_tile = self.input("packet_rd_src_tile", self._params.tile_sel_addr_width)
         self.packet_rd_addr = self.input("packet_rd_addr", self._params.bank_addr_width)
         self.packet_rd_data = self.output("packet_rd_data", self._params.bank_data_width)
@@ -129,10 +128,9 @@ class GlbBankCtrl(Generator):
                        out_=self.sram_cfg_rd_en_d)
 
         self.packet_rd_en_d = self.var("packet_rd_en_d", 1)
-        self.packet_rd_type_d = self.var("packet_rd_type_d", self.header.PacketEnumWidth)
         self.packet_rd_src_tile_d = self.var("packet_rd_src_tile_d", self._params.tile_sel_addr_width)
-        self.packet_pipeline_in = concat(self.packet_rd_en, self.packet_rd_type, self.packet_rd_src_tile)
-        self.packet_pipeline_out = concat(self.packet_rd_en_d, self.packet_rd_type_d, self.packet_rd_src_tile_d)
+        self.packet_pipeline_in = concat(self.packet_rd_en, self.packet_rd_src_tile)
+        self.packet_pipeline_out = concat(self.packet_rd_en_d, self.packet_rd_src_tile_d)
         self.packet_rdrq_pipeline = Pipeline(width=self.packet_pipeline_in.width, depth=self.bank_ctrl_pipeline_depth)
         self.add_child("packet_rdrq_pipeline",
                        self.packet_rdrq_pipeline,
