c6b883f2467944faea1c9760c6da18d9e05b64e5
Taeyoung Kong <kongty@stanford.edu>
2021-12-16 04:21:56 -0800

    Fix sram configuration read/write bug

--- global_buffer/design/glb_tile_sram_cfg_ctrl.py
+++ global_buffer/design/glb_tile_sram_cfg_ctrl.py
@@ -29,64 +29,123 @@ class GlbTileSramCfgCtrl(Generator):
             self.if_sram_cfg_ctrl2bank_m.append(self.interface(
                 self.bank_cfg_ifc.master, f"if_sram_cfg_ctrl2bank_m_{i}", is_port=True))
 
-        self.tile_id_match = self.var("tile_id_match", 1)
+        # local variables
+        self.if_sram_cfg_est_m_wr_en_w = self.var("if_sram_cfg_est_m_wr_en_w", 1)
+        self.if_sram_cfg_est_m_wr_addr_w = self.var("if_sram_cfg_est_m_wr_addr_w", self._params.glb_addr_width)
+        self.if_sram_cfg_est_m_wr_data_w = self.var("if_sram_cfg_est_m_wr_data_w", self._params.axi_data_width)
+        self.if_sram_cfg_est_m_rd_en_w = self.var("if_sram_cfg_est_m_rd_en_w", 1)
+        self.if_sram_cfg_est_m_rd_addr_w = self.var("if_sram_cfg_est_m_rd_addr_w", self._params.glb_addr_width)
 
-        self.bank2ctrl_rd_data_valid_w = self.var("bank2ctrl_rd_data_valid_w", 1, size=self._params.banks_per_tile)
-        self.bank2ctrl_rd_data_w = self.var(
-            "bank2ctrl_rd_data_w", self._params.axi_data_width, size=self._params.banks_per_tile)
+        self.ctrl2bank_m_wr_en_w = self.var("ctrl2bank_m_wr_en_w", 1, size=self._params.banks_per_tile)
+        self.ctrl2bank_m_wr_addr_w = self.var(
+            "ctrl2bank_m_wr_addr_w", self._params.bank_addr_width, size=self._params.banks_per_tile)
+        self.ctrl2bank_m_wr_data_w = self.var(
+            "ctrl2bank_m_wr_data_w", self._params.axi_data_width, size=self._params.banks_per_tile)
+        self.ctrl2bank_m_rd_en_w = self.var("ctrl2bank_m_rd_en_w", 1, size=self._params.banks_per_tile)
+        self.ctrl2bank_m_rd_addr_w = self.var(
+            "ctrl2bank_m_rd_addr_w", self._params.bank_addr_width, size=self._params.banks_per_tile)
+
+        self.wr_tile_id_match = self.var("wr_tile_id_match", 1)
+        self.rd_tile_id_match = self.var("rd_tile_id_match", 1)
+        self.wr_bank_sel = self.var("wr_bank_sel", self._params.bank_sel_addr_width)
+        self.rd_bank_sel = self.var("rd_bank_sel", self._params.bank_sel_addr_width)
         self.rd_data_valid_w = self.var("rd_data_valid_w", 1)
         self.rd_data_w = self.var("rd_data_w", self._params.axi_data_width)
 
-        self.add_always(self.tile_id_match_logic)
-        for i in range(self._params.banks_per_tile):
-            self.add_always(self.if_sram_cfg_ctrl2bank_logic, i=i)
-        self.add_always(self.rd_data_mux)
-        self.add_always(self.sram_cfg_pipeline)
+        # local pararmeters
+        self.tile_id_lsb = self._params.bank_addr_width + self._params.bank_sel_addr_width
+        self.tile_id_msb = self.tile_id_lsb + self._params.tile_sel_addr_width - 1
+        self.bank_sel_lsb = self._params.bank_addr_width
+        self.bank_sel_msb = self.bank_sel_lsb + self._params.bank_sel_addr_width - 1
+
+        self.add_always(self.tile_id_match)
+        self.add_always(self.wr_logic)
+        self.add_always(self.rdrq_logic)
+        self.add_always(self.rdrs_logic)
+        self.add_always(self.pipeline)
 
     @always_comb
-    def tile_id_match_logic(self):
-        self.tile_id_match = (self.if_sram_cfg_wst_s.wr_addr[self._params.bank_addr_width
-                                                             + self._params.bank_sel_addr_width
-                                                             + self._params.tile_sel_addr_width - 1,
-                                                             self._params.bank_addr_width
-                                                             + self._params.bank_sel_addr_width]
-                              == self.glb_tile_id)
+    def tile_id_match(self):
+        self.wr_tile_id_match = self.glb_tile_id == self.if_sram_cfg_wst_s.wr_addr[self.tile_id_msb, self.tile_id_lsb]
+        self.rd_tile_id_match = self.glb_tile_id == self.if_sram_cfg_wst_s.rd_addr[self.tile_id_msb, self.tile_id_lsb]
 
     @always_comb
-    def if_sram_cfg_ctrl2bank_logic(self, i):
-        if self.tile_id_match:
-            self.if_sram_cfg_ctrl2bank_m[i].wr_en = ((self.if_sram_cfg_wst_s.wr_addr[self._params.bank_addr_width
-                                                                                     + self._params.bank_sel_addr_width
-                                                                                     - 1, self._params.bank_addr_width]
-                                                      == i) & self.if_sram_cfg_wst_s.wr_en)
-            self.if_sram_cfg_ctrl2bank_m[i].wr_addr = self.if_sram_cfg_wst_s.wr_addr[self._params.bank_addr_width - 1,
-                                                                                     0]
-            self.if_sram_cfg_ctrl2bank_m[i].wr_data = self.if_sram_cfg_wst_s.wr_data
-            self.if_sram_cfg_ctrl2bank_m[i].rd_en = ((self.if_sram_cfg_wst_s.rd_addr[self._params.bank_addr_width
-                                                                                     + self._params.bank_sel_addr_width
-                                                                                     - 1,
-                                                                                     self._params.bank_addr_width] == i)
-                                                     & self.if_sram_cfg_wst_s.rd_en)
-            self.if_sram_cfg_ctrl2bank_m[i].rd_addr = self.if_sram_cfg_wst_s.rd_addr[self._params.bank_addr_width - 1,
-                                                                                     0]
+    def wr_logic(self):
+        self.wr_bank_sel = self.if_sram_cfg_wst_s.wr_addr[self.bank_sel_msb, self.bank_sel_lsb]
+        if self.if_sram_cfg_wst_s.wr_en:
+            if self.wr_tile_id_match:
+                self.if_sram_cfg_est_m_wr_en_w = 0
+                self.if_sram_cfg_est_m_wr_addr_w = 0
+                self.if_sram_cfg_est_m_wr_data_w = 0
+                for i in range(self._params.banks_per_tile):
+                    if self.wr_bank_sel == i:
+                        self.ctrl2bank_m_wr_en_w[i] = self.if_sram_cfg_wst_s.wr_en
+                        self.ctrl2bank_m_wr_addr_w[i] = self.if_sram_cfg_wst_s.wr_addr[self._params.bank_addr_width - 1,
+                                                                                       0]
+                        self.ctrl2bank_m_wr_data_w[i] = self.if_sram_cfg_wst_s.wr_data
+                    else:
+                        self.ctrl2bank_m_wr_en_w[i] = 0
+                        self.ctrl2bank_m_wr_addr_w[i] = 0
+                        self.ctrl2bank_m_wr_data_w[i] = 0
+            else:
+                self.if_sram_cfg_est_m_wr_en_w = self.if_sram_cfg_wst_s.wr_en
+                self.if_sram_cfg_est_m_wr_addr_w = self.if_sram_cfg_wst_s.wr_addr
+                self.if_sram_cfg_est_m_wr_data_w = self.if_sram_cfg_wst_s.wr_data
+                for i in range(self._params.banks_per_tile):
+                    self.ctrl2bank_m_wr_en_w[i] = 0
+                    self.ctrl2bank_m_wr_addr_w[i] = 0
+                    self.ctrl2bank_m_wr_data_w[i] = 0
         else:
-            self.if_sram_cfg_ctrl2bank_m[i].wr_en = 0
-            self.if_sram_cfg_ctrl2bank_m[i].wr_addr = 0
-            self.if_sram_cfg_ctrl2bank_m[i].wr_data = 0
-            self.if_sram_cfg_ctrl2bank_m[i].rd_en = 0
-            self.if_sram_cfg_ctrl2bank_m[i].rd_addr = 0
+            self.if_sram_cfg_est_m_wr_en_w = 0
+            self.if_sram_cfg_est_m_wr_addr_w = 0
+            self.if_sram_cfg_est_m_wr_data_w = 0
+            for i in range(self._params.banks_per_tile):
+                self.ctrl2bank_m_wr_en_w[i] = 0
+                self.ctrl2bank_m_wr_addr_w[i] = 0
+                self.ctrl2bank_m_wr_data_w[i] = 0
 
     @always_comb
-    def rd_data_mux(self):
-        self.rd_data_w = self.if_sram_cfg_est_m.rd_data
-        self.rd_data_valid_w = self.if_sram_cfg_est_m.rd_data_valid
+    def rdrq_logic(self):
+        self.rd_bank_sel = self.if_sram_cfg_wst_s.rd_addr[self.bank_sel_msb, self.bank_sel_lsb]
+        if self.if_sram_cfg_wst_s.rd_en:
+            if self.rd_tile_id_match:
+                self.if_sram_cfg_est_m_rd_en_w = 0
+                self.if_sram_cfg_est_m_rd_addr_w = 0
+                for i in range(self._params.banks_per_tile):
+                    if self.rd_bank_sel == i:
+                        self.ctrl2bank_m_rd_en_w[i] = self.if_sram_cfg_wst_s.rd_en
+                        self.ctrl2bank_m_rd_addr_w[i] = self.if_sram_cfg_wst_s.rd_addr[self._params.bank_addr_width - 1,
+                                                                                       0]
+                    else:
+                        self.ctrl2bank_m_rd_en_w[i] = 0
+                        self.ctrl2bank_m_rd_addr_w[i] = 0
+            else:
+                self.if_sram_cfg_est_m_rd_en_w = self.if_sram_cfg_wst_s.rd_en
+                self.if_sram_cfg_est_m_rd_addr_w = self.if_sram_cfg_wst_s.rd_addr
+                for i in range(self._params.banks_per_tile):
+                    self.ctrl2bank_m_rd_en_w[i] = 0
+                    self.ctrl2bank_m_rd_addr_w[i] = 0
+        else:
+            self.if_sram_cfg_est_m_rd_en_w = 0
+            self.if_sram_cfg_est_m_rd_addr_w = 0
+            for i in range(self._params.banks_per_tile):
+                self.ctrl2bank_m_rd_en_w[i] = 0
+                self.ctrl2bank_m_rd_addr_w[i] = 0
+
+    @always_comb
+    def rdrs_logic(self):
+        self.rd_data_w = 0
+        self.rd_data_valid_w = 0
         for i in range(self._params.banks_per_tile):
             if self.if_sram_cfg_ctrl2bank_m[i].rd_data_valid == 1:
                 self.rd_data_w = self.if_sram_cfg_ctrl2bank_m[i].rd_data
                 self.rd_data_valid_w = self.if_sram_cfg_ctrl2bank_m[i].rd_data_valid
+        if self.if_sram_cfg_est_m.rd_data_valid == 1:
+            self.rd_data_w = self.if_sram_cfg_est_m.rd_data
+            self.rd_data_valid_w = self.if_sram_cfg_est_m.rd_data_valid
 
     @always_ff((posedge, "clk"), (posedge, "reset"))
-    def sram_cfg_pipeline(self):
+    def pipeline(self):
         if self.reset:
             self.if_sram_cfg_est_m.wr_en = 0
             self.if_sram_cfg_est_m.wr_addr = 0
@@ -95,11 +154,23 @@ class GlbTileSramCfgCtrl(Generator):
             self.if_sram_cfg_est_m.rd_addr = 0
             self.if_sram_cfg_wst_s.rd_data = 0
             self.if_sram_cfg_wst_s.rd_data_valid = 0
+            for i in range(self._params.banks_per_tile):
+                self.if_sram_cfg_ctrl2bank_m[i].wr_en = 0
+                self.if_sram_cfg_ctrl2bank_m[i].wr_addr = 0
+                self.if_sram_cfg_ctrl2bank_m[i].wr_data = 0
+                self.if_sram_cfg_ctrl2bank_m[i].rd_en = 0
+                self.if_sram_cfg_ctrl2bank_m[i].rd_addr = 0
         else:
-            self.if_sram_cfg_est_m.wr_en = self.if_sram_cfg_wst_s.wr_en
-            self.if_sram_cfg_est_m.wr_addr = self.if_sram_cfg_wst_s.wr_addr
-            self.if_sram_cfg_est_m.wr_data = self.if_sram_cfg_wst_s.wr_data
-            self.if_sram_cfg_est_m.rd_en = self.if_sram_cfg_wst_s.rd_en
-            self.if_sram_cfg_est_m.rd_addr = self.if_sram_cfg_wst_s.rd_addr
+            self.if_sram_cfg_est_m.wr_en = self.if_sram_cfg_est_m_wr_en_w
+            self.if_sram_cfg_est_m.wr_addr = self.if_sram_cfg_est_m_wr_addr_w
+            self.if_sram_cfg_est_m.wr_data = self.if_sram_cfg_est_m_wr_data_w
+            self.if_sram_cfg_est_m.rd_en = self.if_sram_cfg_est_m_rd_en_w
+            self.if_sram_cfg_est_m.rd_addr = self.if_sram_cfg_est_m_rd_addr_w
             self.if_sram_cfg_wst_s.rd_data = self.rd_data_w
             self.if_sram_cfg_wst_s.rd_data_valid = self.rd_data_valid_w
+            for i in range(self._params.banks_per_tile):
+                self.if_sram_cfg_ctrl2bank_m[i].wr_en = self.ctrl2bank_m_wr_en_w[i]
+                self.if_sram_cfg_ctrl2bank_m[i].wr_addr = self.ctrl2bank_m_wr_addr_w[i]
+                self.if_sram_cfg_ctrl2bank_m[i].wr_data = self.ctrl2bank_m_wr_data_w[i]
+                self.if_sram_cfg_ctrl2bank_m[i].rd_en = self.ctrl2bank_m_rd_en_w[i]
+                self.if_sram_cfg_ctrl2bank_m[i].rd_addr = self.ctrl2bank_m_rd_addr_w[i]
