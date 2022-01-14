38801704cc538ebbc6ec232123ffe2b06958e3e8
Alex Carsello <ajcars@stanford.edu>
2022-01-14 15:17:11 -0800

    rename mem compiler nodes, deprecate old one

--- mflowgen/common/gen_sram_macro_old/lib2db/generate_db.tcl
+++ mflowgen/common/gen_sram_macro_old/lib2db/generate_db.tcl
@@ -11,7 +11,11 @@
 # Date   : November 11, 2019
 #
 
-set sram_lib_name "$::env(sram_name)_$::env(corner)"
+set sram_lib_name "ts1n16ffcllsblvtc$::env(num_words)x$::env(word_size)m$::env(mux_size)s"
+if {$::env(partial_write)} {
+  set sram_lib_name "${sram_lib_name}w"
+}
+set sram_lib_name "${sram_lib_name}_$::env(corner)"
 enable_write_lib_mode
 read_lib ../../outputs/sram_tt.lib
 write_lib -format db $sram_lib_name -output ../../outputs/sram_tt.db
