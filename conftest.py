c5ca73f4564d381adb5b7faadf86cb28842000c6
Maxwell Strange <mstrange@stanford.edu>
2022-01-27 15:56:34 -0800

    update spMspM test, update scanner core with new config

--- conftest.py
+++ conftest.py
@@ -57,7 +57,7 @@ def fp_files(use_dw=True):
 
 
 def run_tb_fn(tester, cwd=None, trace=False, **magma_args):
-    use_verilator = True
+    use_verilator = False
     use_dw = False
     root_dir = os.path.dirname(__file__)
     with tempfile.TemporaryDirectory() as tempdir:
