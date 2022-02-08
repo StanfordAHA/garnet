5d2961bdc2e45ba0b37437e9ab6834a93f6ea3fa
Alex Carsello <ajcars@stanford.edu>
2022-02-08 15:54:50 -0800

    change soc-rtl-v2 to clone local armip repo

--- mflowgen/common/soc-rtl-v2/run.sh
+++ mflowgen/common/soc-rtl-v2/run.sh
@@ -7,7 +7,7 @@ CUR_DIR=$(pwd)
 git clone https://github.com/StanfordAHA/AhaM3SoC.git aham3soc
 
 # Clone the ARM IP Repo
-git clone git@r7arm-aha:nyengele/aham3soc_armip.git aham3soc_armip
+git clone /sim/repos/aham3soc_armip aham3soc_armip
 
 # Generate Pad Frame
 mkdir -p aham3soc_pad_frame
