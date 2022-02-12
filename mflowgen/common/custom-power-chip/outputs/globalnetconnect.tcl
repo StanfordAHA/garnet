f1f4559e66441f7b9bf0978d4c3fb316129c324a
Alex Carsello <ajcars@stanford.edu>
2022-02-11 17:03:32 -0800

    Update power pin names in for chip-level scripts

--- mflowgen/common/custom-power-chip/outputs/globalnetconnect.tcl
+++ mflowgen/common/custom-power-chip/outputs/globalnetconnect.tcl
@@ -3,17 +3,13 @@ globalNetConnect VDD -type pgpin -pin VDD -inst *
 globalNetConnect VDD -type tiehi
 globalNetConnect VSS -type pgpin -pin VSS -inst *
 globalNetConnect VSS -type tielo
-globalNetConnect VDD -type pgpin -pin VPP -inst *
-globalNetConnect VSS -type pgpin -pin VBB -inst *
+globalNetConnect VDD -type pgpin -pin VPW -inst *
+globalNetConnect VSS -type pgpin -pin VNW -inst *
 
-# I/O power nets
-globalNetConnect VDDPST -type pgpin -pin VDDPST -inst *
-globalNetConnect VSS    -type pgpin -pin VSSPST -inst * 
+# I/O pads
+globalNetConnect VDDPST -type pgpin -pin VDDIO -inst *
+globalNetConnect VSS    -type pgpin -pin VSSIO -inst * 
+globalNetConnect VSS    -type pgpin -pin VSSC -inst * 
+globalNetConnect VSS    -type pgpin -pin VSS_CM -inst * 
+globalNetConnect VDD    -type pgpin -pin VDDC -inst * 
 
-# Dragonphy power nets
-globalNetConnect VDD -type pgpin -pin DVDD -inst iphy -override
-globalNetConnect VSS -type pgpin -pin DVSS -inst iphy -override
-globalNetConnect AVDD -type pgpin -pin AVDD -inst iphy -override
-globalNetConnect AVSS -type pgpin -pin AVSS -inst iphy -override
-globalNetConnect CVDD -type pgpin -pin CVDD -inst iphy -override
-globalNetConnect CVSS -type pgpin -pin CVSS -inst iphy -override
