0b8780ae3ff45f596de7c48669e297b4bd28eb25
Alex Carsello <ajcars@stanford.edu>
2022-02-16 11:42:12 -0800

    set pe tile units

--- mflowgen/Tile_PE/constraints/constraints.tcl
+++ mflowgen/Tile_PE/constraints/constraints.tcl
@@ -16,6 +16,8 @@
 # set_attribute [get_lib *lvt*] default_threshold_voltage_group LVT
 # set_attribute [get_lib *ulvt*] default_threshold_voltage_group ULVT
 
+set_units -time ns -capacitance pF
+
 if $::env(PWR_AWARE) {
     source inputs/pe-constraints.tcl
 } 
