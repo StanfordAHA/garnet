9695310e584f2e4da592f79a16abdbee7c094031
Alex Carsello <ajcars@stanford.edu>
2022-04-17 22:00:39 -0700

    update glc output getter to get lib from innovus

--- mflowgen/full_chip/global_controller/get_global_controller_outputs.sh
+++ mflowgen/full_chip/global_controller/get_global_controller_outputs.sh
@@ -9,7 +9,7 @@ else
 fi
 
 mkdir -p outputs
-cp -L *cadence-genus-genlib/outputs/design.lib outputs/global_controller_tt.lib
+cp -L *cadence-innovus-genlib/outputs/design.lib outputs/global_controller_tt.lib
 cp -L *synopsys-dc-lib2db/outputs/design.db outputs/global_controller_tt.db
 cp -L *cadence-innovus-signoff/outputs/design.lef outputs/global_controller.lef
 cp -L *cadence-innovus-signoff/outputs/design.vcs.v outputs/global_controller.vcs.v
