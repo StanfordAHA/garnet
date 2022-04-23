67b97e3ddb0995d1a3dabb0fdb6a27a720712f2f
steveri <steveri@steveri.com>
2022-04-23 10:30:18 -0700

    respect the locks!

--- mflowgen/bin/setup-buildkite.sh
+++ mflowgen/bin/setup-buildkite.sh
@@ -390,6 +390,13 @@ fi
 
 echo "Install mflowgen using repo in dir '$mflowgen'"
 pushd $mflowgen
+
+  # See https://buildkite.com/tapeout-aha/mflowgen/builds/5084
+  while [ test -f .git/index.lock ]; do
+      echo "Found lock $mflowgen/.git/index.lock; wait a half sec now..."
+      sleep 0.5
+  done
+
   git checkout $mflowgen_branch; git pull
   TOP=$PWD; pip install -e .; which mflowgen; pip list | grep mflowgen
 
