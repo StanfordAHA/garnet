ba077b63fed9f5b3310106b46f2c8fdcb854f756
Alex Carsello <ajcars@stanford.edu>
2022-04-28 00:12:39 -0700

    Decrease pe clk period to 1.1ns

--- mflowgen/Tile_PE/construct.py
+++ mflowgen/Tile_PE/construct.py
@@ -44,7 +44,7 @@ def construct():
   parameters = {
     'construct_path'    : __file__,
     'design_name'       : 'Tile_PE',
-    'clock_period'      : 2,
+    'clock_period'      : 1.1,
     'adk'               : adk_name,
     'adk_view'          : adk_view,
     # Synthesis
