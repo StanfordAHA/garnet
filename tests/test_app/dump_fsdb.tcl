8e0b1b9fc0eb93854320091bc08625bf2db4125d
Jack Melchert <melchert@stanford.edu>
2022-05-05 09:02:16 -0700

    Updates

--- tests/test_app/dump_fsdb.tcl
+++ tests/test_app/dump_fsdb.tcl
@@ -1,6 +1,7 @@
+
 if { $::env(WAVEFORM) != "0" } {
   dump -file cgra.fsdb -type FSDB
-  dump -add top -fsdb_opt +mda+packedmda+struct
+  dump -add top -fsdb_opt +mda+packedmda+struct+cell
 } elseif { $::env(WAVEFORM_GLB_ONLY) != "0" } {
   dump -file global_buffer.fsdb -type FSDB
   dump -add "top.dut.global_buffer*" -fsdb_opt +mda+packedmda+struct
