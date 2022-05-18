a8020a6aea41fe385f0e4062167055ea42390837
steveri <steveri@steveri.com>
2022-05-18 15:00:37 -0700

    try again to avoid "hold" violation

--- mflowgen/global_controller/constraints/constraints.tcl
+++ mflowgen/global_controller/constraints/constraints.tcl
@@ -68,5 +68,5 @@ set_max_transition [expr 0.05*${clock_period}] $design_name
 # Don't care if clock passess through global controller too quickly.
 # Also we don't use this clock anyway.
 
-set_min_delay 0 -from [ get_ports clk_in ] -to [ get_ports clk_out ]
+set_false_path -hold -from [ get_ports clk_in ] -to [ get_ports clk_out ]
 
