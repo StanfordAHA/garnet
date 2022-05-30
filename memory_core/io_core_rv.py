28c0730e19a3416021cdfb9598c35856a7aa17cc
mbstrange2 <mstrange@stanford.edu>
2022-05-30 13:31:26 -0700

    push io core rv

---
+++ memory_core/io_core_rv.py
@@ -0,0 +1,96 @@
+import magma
+from gemstone.generator.from_magma import FromMagma
+from typing import List
+from canal.interconnect import Interconnect
+from lake.top.extract_tile_info import *
+import kratos as kts
+from gemstone.generator.from_magma import FromMagma
+from typing import List
+from lake.top.pond import Pond
+from lake.top.extract_tile_info import *
+from gemstone.common.core import PnRTag
+from lake.modules.io_core import *
+
+import kratos as kts
+
+if __name__ == "__main__":
+    from memtile_util import LakeCoreBase
+else:
+    from .memtile_util import LakeCoreBase
+
+
+class IOCoreReadyValid(LakeCoreBase):
+
+    def __init__(self,
+                 data_width=16,  # CGRA Params
+                 config_data_width=32,
+                 config_addr_width=8,
+                 tracks_supported=[1, 16, 17]):
+
+        buffet_name = "IOCoreReadyValid"
+        super().__init__(config_data_width=config_data_width,
+                         config_addr_width=config_addr_width,
+                         data_width=data_width,
+                         name="IOCoreReadyValid")
+
+        # Capture everything to the tile object
+        self.data_width = data_width
+        self.config_data_width = config_data_width
+        self.config_addr_width = config_addr_width
+        self.tracks_supported = tracks_supported
+
+        cache_key = (self.data_width,
+                     self.config_data_width,
+                     self.config_addr_width,
+                     "IOCoreReadyValid")
+
+        # Check for circuit caching
+        if cache_key not in LakeCoreBase._circuit_cache:
+            # Instantiate core object here - will only use the object representation to
+            # query for information. The circuit representation will be cached and retrieved
+            # in the following steps.
+            self.dut = IOCore(data_width=data_width,
+                              tracks_supported=self.tracks_supported)
+
+            circ = kts.util.to_magma(self.dut,
+                                     flatten_array=True,
+                                     check_multiple_driver=False,
+                                     optimize_if=False,
+                                     check_flip_flop_always_ff=False)
+            LakeCoreBase._circuit_cache[cache_key] = (circ, self.dut)
+        else:
+            circ, self.dut = LakeCoreBase._circuit_cache[cache_key]
+
+        # Save as underlying circuit object
+        self.underlying = FromMagma(circ)
+
+        self.wrap_lake_core()
+
+        conf_names = list(self.registers.keys())
+        conf_names.sort()
+        with open("iocorerv_cfg.txt", "w+") as cfg_dump:
+            for idx, reg in enumerate(conf_names):
+                write_line = f"(\"{reg}\", 0),  # {self.registers[reg].width}\n"
+                cfg_dump.write(write_line)
+        with open("iocorerv_synth.txt", "w+") as cfg_dump:
+            for idx, reg in enumerate(conf_names):
+                write_line = f"{reg}\n"
+                cfg_dump.write(write_line)
+
+    def get_config_bitstream(self, config_tuple):
+        capacity_0, capacity_1 = config_tuple
+        configs = []
+        config_lower = [("tile_en", 1)]
+        config_lower += self.dut.get_bitstream(capacity_0, capacity_1)
+        for name, v in config_lower:
+            configs = [self.get_config_data(name, v)] + configs
+        return configs
+
+    def pnr_info(self):
+        return [PnRTag("I", 2, self.DEFAULT_PRIORITY),
+                PnRTag("i", 1, self.DEFAULT_PRIORITY),
+                    ]
+
+
+if __name__ == "__main__":
+    rviocore = IOCoreReadyValid()
