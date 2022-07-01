2a39aba8b729abf77e1ec98ab39c2d5e4f05e6b6
Alex Carsello <ajcars@stanford.edu>
2022-07-01 11:24:59 -0700

    increase tile array clk frequency

--- mflowgen/tile_array/construct.py
+++ mflowgen/tile_array/construct.py
@@ -27,7 +27,7 @@ def construct():
   parameters = {
     'construct_path'    : __file__,
     'design_name'       : 'Interconnect',
-    'clock_period'      : 2,
+    'clock_period'      : 1.1,
     'adk'               : adk_name,
     'adk_view'          : adk_view,
     # Synthesis
