7ee236444121926fb1e026586a9db8f262e16e3f
Alex Carsello <ajcars@stanford.edu>
2022-07-01 11:21:24 -0700

    Add AON taps in core area if AON tap specified in ADK

--- mflowgen/common/power-domains/outputs/add-endcaps-welltaps-setup.tcl
+++ mflowgen/common/power-domains/outputs/add-endcaps-welltaps-setup.tcl
@@ -28,17 +28,18 @@ if {[expr {$ADK_END_CAP_CELL == ""} && {$ADK_WELL_TAP_CELL == ""}]} {
     set M3_str_offset           [expr $M3_str_pitch + $M3_route_pitchX/2 - $M3_str_width/2]
 
     set core_llx [dbGet top.fPlan.coreBox_llx]
+        
+    # Align tap cells with M3 pitch so that the M1 VPP pin is directly aligned with the M3 VDD net
+ 
+    # Pitch is a multiple of the M3 VDD stripe pitch 
+    set horiz_tap_pitch [expr $stripes_per_tap * $M3_str_interset_pitch]
+    set horiz_switch_pitch [expr $stripes_per_switch * $M3_str_interset_pitch]
     
     # Only add boundary taps if ADK specifies an AON boundary tap cell
     if {[info exists ADK_AON_BOUNDARY_TAP_CELL] && [expr {$ADK_AON_BOUNDARY_TAP_CELL ne ""}]} {
         set aon_tap_cell $ADK_AON_BOUNDARY_TAP_CELL
         set aon_tap_cell_width [dbGet [dbGetCellByName $aon_tap_cell].size_x]
-
-        # Align tap cells with M3 pitch so that the M1 VPP pin is directly aligned with the M3 VDD net
  
-        # Pitch is a multiple of the M3 VDD stripe pitch 
-        set horiz_tap_pitch [expr $stripes_per_tap * $M3_str_interset_pitch]
-        
         # Line up AON tap VDDC pin with M3 VDD stripe (3rd stripe in set of {VDD_SW VSS VDD}
         # We subtract (aon_tap_cell_with / 3) from offset to center of cell aligns with VDD, not edge of cell.
         # And cell's VDDC pin is slightly offset from center.
@@ -62,6 +63,25 @@ if {[expr {$ADK_END_CAP_CELL == ""} && {$ADK_WELL_TAP_CELL == ""}]} {
         }
     } else {
         echo "WARNING: Not adding boundary taps because ADK doesn't specify ADK_AON_BOUNDARY_TAP_CELL"
+    }
+    # Only add taps in core area if ADK specifies an AON tap cell
+    if {[info exists ADK_AON_TAP_CELL] && [expr {$ADK_AON_TAP_CELL ne ""}]} {
+       set aon_tap_cell $ADK_AON_TAP_CELL
+       set tap_width [dbGet [dbGetCellByName $aon_tap_cell].size_x]
+       # We really shouldn't have to add the polypitch here, but in practice
+       # the pitch was off by 1 for some reason
+       set tap_interval [expr $horiz_tap_pitch + $polypitch_x]
+       # Start the taps 2 stripe set intervals to the left of the pwr switches
+       set tap_edge_offset [expr $M3_str_offset + (2 * $M3_str_intraset_spacing + $M3_str_width) - ($tap_width / 2) + $horiz_switch_pitch - (2 * $M3_str_interset_pitch)]
+       # Try adding well tap to SD domain
+       addWellTap -cell $aon_tap_cell \
+                  -prefix WELLTAP \
+                  -cellInterval $tap_interval \
+                  -inRowOffset $tap_edge_offset \
+                  -fixedGap \
+                  -powerDomain TOP
+    } else {
+       echo "WARNING: Not adding core area tap cells because ADK doesn't specity ADK_AON_TAP_CELL"
     }
 }
 
