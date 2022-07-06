d298302fd685fca15c1d868d6abba33161cd6f53
steveri <steveri@steveri.com>
2022-07-06 14:25:49 -0700

    try making mtile look more like previous gold

--- mflowgen/common/power-domains/outputs/mem-pd-params.tcl
+++ mflowgen/common/power-domains/outputs/mem-pd-params.tcl
@@ -19,14 +19,24 @@ set stripes_per_tap 12
 # with M3 VDD stripes. stripes_per switch controls 
 # space betwen power switches as a multiple of the M3
 # power stripe pitch.
-set stripes_per_switch 12
+
+# Original mtile had six columns of power switches.
+# New default balue 12 yields fourteen columns.
+# Let's try increasing 12=>26, see what happens
+# set stripes_per_switch 12
+set stripes_per_switch 26
 
 
 # AON box floorplanning params
 
 # Sets width of AON region as a multiple of the 
 # unit stdcell width
-set aon_width 160
+# set aon_width 160
+
+# Original box was skinnier, let's try 100 and see what happens
+set aon_width 110
+
+
 
 # Sets height of AON region as a multiple of the
 # unit stdcell height.
@@ -36,7 +46,16 @@ set aon_height 24
 
 # Sets AON box horizontal offset from center
 # We went to move this to the right of the SRAM macros for mem tile.
-set aon_horiz_offset 80
+# set aon_horiz_offset 80
+
+# Can this be negative? I guess we'll find out!
+# Hoping -80 will bring it back to where it was before
+set aon_horiz_offset -80
+
+
 
 # Sets AON box vertical offset from center
-set aon_vert_offset 15
+# set aon_vert_offset 15
+
+# Original position was higher than 15, let's try 30 see how that goes
+set aon_vert_offset 30
