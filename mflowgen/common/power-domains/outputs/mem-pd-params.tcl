4e9f33f62dd95aae321f7d553ec0ddac4cd46b20
steveri <steveri@steveri.com>
2022-07-07 06:47:37 -0700

    sps 12=>26 to reduce ps columns 14=>6

--- mflowgen/common/power-domains/outputs/mem-pd-params.tcl
+++ mflowgen/common/power-domains/outputs/mem-pd-params.tcl
@@ -20,9 +20,6 @@ set stripes_per_tap 12
 # space betwen power switches as a multiple of the M3
 # power stripe pitch.
 
-# Original mtile had six columns of power switches.
-# New default balue 12 yields fourteen columns.
-# Let's try increasing 12=>26, see what happens
 # set stripes_per_switch 12
 set stripes_per_switch 26
 
@@ -31,12 +28,8 @@ set stripes_per_switch 26
 
 # Sets width of AON region as a multiple of the 
 # unit stdcell width
-# set aon_width 160
-
-# Original box was skinnier, let's try 100 and see what happens
-set aon_width 110
-
-
+set aon_width 160
+# set aon_width 110
 
 # Sets height of AON region as a multiple of the
 # unit stdcell height.
@@ -46,16 +39,9 @@ set aon_height 24
 
 # Sets AON box horizontal offset from center
 # We went to move this to the right of the SRAM macros for mem tile.
-# set aon_horiz_offset 80
-
-# Can this be negative? I guess we'll find out!
-# Hoping -80 will bring it back to where it was before
-set aon_horiz_offset -80
-
-
+set aon_horiz_offset 80
+# set aon_horiz_offset -80
 
 # Sets AON box vertical offset from center
-# set aon_vert_offset 15
-
-# Original position was higher than 15, let's try 30 see how that goes
-set aon_vert_offset 30
+set aon_vert_offset 15
+# set aon_vert_offset 30
