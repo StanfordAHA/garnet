e0cdf0165e27ee8f756a5bc7db2ef7252ea9b2fb
steveri <steveri@steveri.com>
2022-07-12 12:34:05 -0700

    trying to be smarter about switch placement etc.

--- mflowgen/common/power-domains/outputs/mem-pd-params.tcl
+++ mflowgen/common/power-domains/outputs/mem-pd-params.tcl
@@ -16,9 +16,11 @@ set vdd_m3_stripe_sparsity 3
 # Boundary AON TAP params
 
 # AON boundary taps must line up with M3 VDD stripes.
-# stripes_per_tap controls the space between AON taps
-# as a multiple of the M3 power stripe pitch
-set stripes_per_tap [ expr 12 / $vdd_m3_stripe_sparsity ]
+# 'stripes_per_tap' controls the space between AON taps
+# as a multiple of the M3 power stripe pitch.
+# Note that 'stripes_per_tap' must be a multiple of vdd sparsity.
+set vdd_stripes_per_tap [ expr 12 / $vdd_m3_stripe_sparsity ]
+set stripes_per_tap [ expr $vdd_stripes_per_tap * $vdd_m3_stripe_sparsity ]
 
 
 # Power switch params
@@ -26,10 +28,12 @@ set stripes_per_tap [ expr 12 / $vdd_m3_stripe_sparsity ]
 # Like the taps, power switches must line up with M3 VDD stripes.
 # 'stripes_per switch' controls space betwen power switches as a
 # multiple of the M3 power stripe pitch.
+# Note that 'stripes_per_switch' must be a multiple of vdd sparsity.
 
 # sps12/vmss (original default) yields 14 columns of switches and 4.5 hr runtime
 # sps26/vmss yields six columns and finishes in 2.5 hr
-set stripes_per_switch [ expr 30 / $vdd_m3_stripe_sparsity ]
+set vdd_stripes_per_switch [ expr 30 / $vdd_m3_stripe_sparsity ]
+set stripes_per_switch [ $vdd_stripes_per_switch * $vdd_m3_stripe_sparsity ]
 
 
 # AON box floorplanning params
