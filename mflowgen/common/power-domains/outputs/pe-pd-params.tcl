d5cf87aff4cf57b6710a2a4fb25efc82c2866ae3
steveri <steveri@steveri.com>
2022-07-14 09:28:10 -0700

    code cleanup

--- mflowgen/common/power-domains/outputs/pe-pd-params.tcl
+++ mflowgen/common/power-domains/outputs/pe-pd-params.tcl
@@ -18,8 +18,9 @@ set vdd_m3_stripe_sparsity 3
 # AON boundary taps must line up with M3 VDD stripes.
 # 'stripes_per_tap' controls the space between AON taps
 # as a multiple of the M3 power stripe pitch.
-# Note that 'stripes_per_tap' must be a multiple of vdd sparsity.
 set stripes_per_tap 18
+
+# Note that 'stripes_per_tap' must be a multiple of vdd sparsity.
 set vdd_stripes_per_tap [ expr $stripes_per_tap / $vdd_m3_stripe_sparsity ]
 set stripes_per_tap [ expr $vdd_stripes_per_tap * $vdd_m3_stripe_sparsity ]
 
@@ -29,17 +30,18 @@ set stripes_per_tap [ expr $vdd_stripes_per_tap * $vdd_m3_stripe_sparsity ]
 # Like the taps, power switches must line up with M3 VDD stripes.
 # 'stripes_per switch' controls space betwen power switches as a
 # multiple of the M3 power stripe pitch.
-# Note that 'stripes_per_switch' must be a multiple of vdd sparsity.
-
+#
 # If set to 12 in Amber (TSMC) design, get five columns of switches.
 # If set to 18, get 3 cols symmetrically placed, center col at center chip.
 set stripes_per_switch 18
+
+# Note that 'stripes_per_switch' must be a multiple of vdd sparsity.
 set vdd_stripes_per_switch [ expr $stripes_per_switch / $vdd_m3_stripe_sparsity ]
 set stripes_per_switch [ expr $vdd_stripes_per_switch * $vdd_m3_stripe_sparsity ]
 
 # AON box floorplanning params
 
-# Sets width of AON region as a multiple of the unit stdcell width
+# Sets width of AON region as a multiple of the unit stdcell width.
 set aon_width 160
 
 # Sets height of AON region as a multiple of the unit stdcell height.
@@ -47,8 +49,8 @@ set aon_width 160
 # AON region can start an end on an even-numbered row.
 set aon_height 24
 
-# Sets AON box horizontal offset from center in # of unit stdcell widths
+# Sets AON box horizontal offset from center in # of unit stdcell widths.
 set aon_horiz_offset 0
 
-# Sets AON box vertical offset from center in # of unit std cell heights
+# Sets AON box vertical offset from center in # of unit stdcell heights.
 set aon_vert_offset 30
