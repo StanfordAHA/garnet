b0874506f6e6fe59c2624486cb1d55d75b766c4e
steveri <steveri@steveri.com>
2022-07-14 07:49:14 -0700

    update vs. power-domains-update branch

--- mflowgen/common/power-domains/outputs/mem-pd-params.tcl
+++ mflowgen/common/power-domains/outputs/mem-pd-params.tcl
@@ -5,21 +5,21 @@
 # Date: 3/7/21
 
 
-# Boundary AON TAP Params
+# Boundary AON TAP params
 
 # AON boundary taps must line up with M3 VDD stripes.
-# stripes_per_tap controls the space between AON taps
-# as a multiple of the M3 power stripe pitch
+# 'stripes_per_tap' controls the space between AON taps
+# as a multiple of the M3 power stripe pitch.
 set stripes_per_tap 18
 
 
-# Power Switch Params
+# Power switch params
 
-# Like the taps, power switches must also line up
-# with M3 VDD stripes. stripes_per switch controls 
-# space betwen power switches as a multiple of the M3
+# Like the taps, power switches must line up with M3 VDD stripes.
+# 'stripes_per switch' controls space betwen power switches as a
+# multiple of the M3 power stripe pitch.
 # power stripe pitch.
-
+# 
 # sps12 (original default) yields 14 columns of switches and 4.5 hr runtime
 # sps26 yields six columns and finishes in 2.5 hr
 # now using sps18 to match pe tile, at alex's request
@@ -27,19 +27,21 @@ set stripes_per_switch 18
 
 # AON box floorplanning params
 
-# Sets width of AON region as a multiple of the 
-# unit stdcell width
+# Sets width of AON region as a multiple of the unit stdcell width.
 set aon_width 160
 
-# Sets height of AON region as a multiple of the
-# unit stdcell height.
+# Sets height of AON region as a multiple of the unit stdcell height.
 # This should always be an even number so that the
 # AON region can start an end on an even-numbered row.
 set aon_height 24
 
-# Sets AON box horizontal offset from center
-# We went to move this to the right of the SRAM macros for mem tile.
+# Sets AON box horizontal offset from center in # of unit stdcell widths.
+# We want to move this to the right of the SRAM macros for mem tile.
+# ---
+# RESOLVEME - SR: I moved it back to the left b/c runtime was slightly faster.
+# Does this matter? I can move it back.
+# ---
 set aon_horiz_offset -80
 
-# Sets AON box vertical offset from center
+# Sets AON box vertical offset from center in # of unit stdcell heights.
 set aon_vert_offset 15
--- mflowgen/common/power-domains/outputs/pe-pd-params.tcl
+++ mflowgen/common/power-domains/outputs/pe-pd-params.tcl
@@ -5,20 +5,19 @@
 # Date: 3/7/21
 
 
-# Boundary AON TAP Params
+# Boundary AON TAP params
 
 # AON boundary taps must line up with M3 VDD stripes.
-# stripes_per_tap controls the space between AON taps
-# as a multiple of the M3 power stripe pitch
+# 'stripes_per_tap' controls the space between AON taps
+# as a multiple of the M3 power stripe pitch.
 set stripes_per_tap 18
 
 
-# Power Switch Params
+# Power switch params
 
-# Like the taps, power switches must also line up
-# with M3 VDD stripes. stripes_per switch controls 
-# space betwen power switches as a multiple of the M3
-# power stripe pitch.
+# Like the taps, power switches must line up with M3 VDD stripes.
+# 'stripes_per switch' controls space betwen power switches as a
+# multiple of the M3 power stripe pitch.
 #
 # If set to 12 in Amber (TSMC) design, get five columns of switches.
 # If set to 18, get 3 cols symmetrically placed, center col at center chip.
@@ -26,18 +25,16 @@ set stripes_per_switch 18
 
 # AON box floorplanning params
 
-# Sets width of AON region as a multiple of the 
-# unit stdcell width
+# Sets width of AON region as a multiple of the unit stdcell width.
 set aon_width 160
 
-# Sets height of AON region as a multiple of the
-# unit stdcell height.
+# Sets height of AON region as a multiple of the unit stdcell height.
 # This should always be an even number so that the
 # AON region can start an end on an even-numbered row.
 set aon_height 24
 
-# Sets AON box horizontal offset from center in # of unit stdcell widths
+# Sets AON box horizontal offset from center in # of unit stdcell widths.
 set aon_horiz_offset 0
 
-# Sets AON box vertical offset from center in # of unit std cell heights
+# Sets AON box vertical offset from center in # of unit stdcell heights.
 set aon_vert_offset 30
