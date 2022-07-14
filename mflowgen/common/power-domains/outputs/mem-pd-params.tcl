729576b4bd31ff31bff708f3a4b3d3fd2bd813c4
steveri <steveri@steveri.com>
2022-07-14 09:21:02 -0700

    update vs. 'power-domains-update' branch

--- mflowgen/common/power-domains/outputs/mem-pd-params.tcl
+++ mflowgen/common/power-domains/outputs/mem-pd-params.tcl
@@ -21,7 +21,6 @@ set stripes_per_tap 18
 # 
 # sps12 (original default) yields 14 columns of switches and 4.5 hr runtime
 # sps26 yields six columns and finishes in 2.5 hr
-# now using sps18 to match pe tile, at alex's request
 set stripes_per_switch 18
 
 # AON box floorplanning params
