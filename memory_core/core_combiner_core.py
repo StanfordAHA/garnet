2851bcaa6c69c9a470d8f32c792ed8c71ac3d02d
mbstrange2 <mstrange@stanford.edu>
2022-07-19 11:09:44 -0700

    Const import from gemstone

--- memory_core/core_combiner_core.py
+++ memory_core/core_combiner_core.py
@@ -41,7 +41,8 @@ class CoreCombinerCore(LakeCoreBase):
         super().__init__(config_data_width=config_data_width,
                          config_addr_width=config_addr_width,
                          data_width=data_width,
-                         name=cc_core_name)
+                         name=cc_core_name,
+                         ready_valid=True)
 
         # Capture everything to the tile object
         self.data_width = data_width
