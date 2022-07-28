074278fb7fcc4c15ff8c59fafe9989e76d84210f
Taeyoung Kong <kongty@stanford.edu>
2022-07-28 16:20:45 -0700

    Change variable to parameter

--- global_buffer/design/global_buffer.py
+++ global_buffer/design/global_buffer.py
@@ -345,6 +345,7 @@ class GlobalBuffer(Generator):
 
     def add_proc_clk_en(self):
         self.wr_clk_en_gen = GlbClkEnGen(cnt=self._params.tile2sram_wr_delay + self._params.proc_clk_en_margin)
+        self.wr_clk_en_gen.p_cnt.value = self._params.tile2sram_wr_delay + self._params.proc_clk_en_margin
         self.proc_wr_clk_en = self.var("proc_wr_clk_en", 1)
         self.add_child("proc_wr_clk_en_gen",
                        self.wr_clk_en_gen,
@@ -356,6 +357,8 @@ class GlobalBuffer(Generator):
         self.wire(self.if_proc_list[0].wr_clk_en, self.proc_wr_clk_en)
         self.rd_clk_en_gen = GlbClkEnGen(cnt=2 * self._params.num_glb_tiles
                                          + self._params.tile2sram_rd_delay + self._params.proc_clk_en_margin)
+        self.rd_clk_en_gen.p_cnt.value = 2 * self._params.num_glb_tiles + \
+            self._params.tile2sram_rd_delay + self._params.proc_clk_en_margin
         self.proc_rd_clk_en = self.var("proc_rd_clk_en", 1)
         self.add_child("proc_rd_clk_en_gen",
                        self.rd_clk_en_gen,
