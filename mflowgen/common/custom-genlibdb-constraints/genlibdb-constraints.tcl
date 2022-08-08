e53c29d47bb659f6cb2914d63c39b061b1109b6e
steveri <steveri@steveri.com>
2022-08-08 14:02:38 -0700

    RMUX select instances name change (#901)
    * handle the case where RMUX cells don't exist
    
    * better fix for changed RMUX select cell names

--- mflowgen/common/custom-genlibdb-constraints/genlibdb-constraints.tcl
+++ mflowgen/common/custom-genlibdb-constraints/genlibdb-constraints.tcl
@@ -13,7 +13,7 @@
 # These RMUX instances have been marked dont_touch
 # in the synthesis constraints so we can more easily
 # do set_case_analysis on them here
-set rmuxes [get_cells -hier *RMUX_*_sel_inst0]
+set rmuxes [get_cells -hier *RMUX_*_sel_*]
 set rmux_outputs [get_pins -of_objects $rmuxes -filter "direction==out"]
 set_case_analysis 1 $rmux_outputs
 
