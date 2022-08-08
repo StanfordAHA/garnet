8fddc75b7f0cf67d442bce5562ba7a47d711ca91
steveri <steveri@steveri.com>
2022-08-08 11:49:31 -0700

    better fix for changed RMUX select cell names

--- mflowgen/common/synopsys-ptpx-gl/loop_break_Interconnect.tcl
+++ mflowgen/common/synopsys-ptpx-gl/loop_break_Interconnect.tcl
@@ -1,10 +1,8 @@
 # Loop break constraints
 foreach_in_collection tile [get_cells -hier Tile* ] {
   current_instance $tile
-  set rmuxes [get_cells -hier *RMUX_*_sel_inst0]
-  if { "$rmux_cells" != "" } {
-    set rmux_outputs [get_pins -of_objects $rmuxes -filter "direction==out"]
-    set_case_analysis 1 $rmux_outputs
-  }
+  set rmuxes [get_cells -hier *RMUX_*_sel_*]
+  set rmux_outputs [get_pins -of_objects $rmuxes -filter "direction==out"]
+  set_case_analysis 1 $rmux_outputs
   current_instance
 }
