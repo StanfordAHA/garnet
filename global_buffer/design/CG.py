5b21fd89f0a9b9870bf89eb49c48b2b9a1ce4c1d
steveri <steveri@steveri.com>
2022-08-24 10:43:01 -0700

    merge CG.py

--- global_buffer/design/CG.py
+++ global_buffer/design/CG.py
@@ -1,9 +1,27 @@
 from kratos import Generator, always_latch
+from global_buffer.design.global_buffer_parameter import GlobalBufferParams
 
-
+# CG for clock-gate, I guess
 class CG(Generator):
-    def __init__(self, name: str):
-        super().__init__(name)
+
+    # TSMC cell 'CKLNQD1BWP16P90' has ports CP, Q
+    # GF cell 'SC7P5T_CKGPRELATNX1_SSC14R' has ports CLK, Z
+
+    def __init__(self, _params: GlobalBufferParams):
+
+      if self._params.process == "TSMC":
+        super().__init__(_self._params.tsmc_icg_name)
+        self.E = self.input("E", 1)
+        self.CLK = self.clock("CP")
+        self.TE = self.input("TE", 1)
+        self.Q = self.output("Q", 1)
+
+        self.enable_latch = self.var("enable_latch", 1)
+        self.add_always(self.clk_en_latch)
+        self.wire(self.Q, (self.CLK & self.enable_latch))
+
+      elif self._params.process == "GF":
+        super().__init__(_self._params.gf_icg_name)
         self.E = self.input("E", 1)
         self.CLK = self.clock("CLK")
         self.TE = self.input("TE", 1)
