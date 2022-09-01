e087df3bfdb19937a00a4f0b1462f6759812e6e3
steveri <steveri@steveri.com>
2022-09-01 06:48:23 -0700

    Gfmerge master (#910)
    * process-dependent code in global buffer
    * merged in a couple more glb parameters
    * eliminate unsed parameter in glb
    * merged two glb sram files w/conversion branch
    * finished first round of merges for glb
    * now, finished first round of merges for glb
    * clock gate port naming different tsmc vs. gf
    * quick refactor of CG.py
    * final(?) merge CG.py
    * no need for self._params in CG.py
    * remove unused file gf_sram.py

--- global_buffer/design/CG.py
+++ global_buffer/design/CG.py
@@ -1,19 +1,37 @@
 from kratos import Generator, always_latch
+from global_buffer.design.global_buffer_parameter import GlobalBufferParams
 
-
+# CG for clock-gate, I guess
 class CG(Generator):
-    def __init__(self, name: str):
-        super().__init__(name)
+
+    # TSMC cell 'CKLNQD1BWP16P90' has ports CP, Q
+    # GF cell 'SC7P5T_CKGPRELATNX1_SSC14R' has ports CLK, Z
+
+    def __init__(self, _params: GlobalBufferParams):
+
+      if _params.process == "TSMC":
+        super().__init__(_params.tsmc_icg_name)
         self.E = self.input("E", 1)
-        self.CP = self.clock("CP")
+        self.CLK = self.clock("CP")
         self.TE = self.input("TE", 1)
         self.Q = self.output("Q", 1)
 
         self.enable_latch = self.var("enable_latch", 1)
         self.add_always(self.clk_en_latch)
-        self.wire(self.Q, (self.CP & self.enable_latch))
+        self.wire(self.Q, (self.CLK & self.enable_latch))
+
+      elif _params.process == "GF":
+        super().__init__(_params.gf_icg_name)
+        self.E = self.input("E", 1)
+        self.CLK = self.clock("CLK")
+        self.TE = self.input("TE", 1)
+        self.Z = self.output("Z", 1)
+
+        self.enable_latch = self.var("enable_latch", 1)
+        self.add_always(self.clk_en_latch)
+        self.wire(self.Z, (self.CLK & self.enable_latch))
 
     @always_latch
     def clk_en_latch(self):
-        if (~self.CP):
+        if (~self.CLK):
             self.enable_latch = self.E
