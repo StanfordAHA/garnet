0d42d4a05e39f8173eae43d6550c1b1aac2588e9
steveri <steveri@steveri.com>
2022-09-03 11:14:10 -0700

    refactor/explain/simplify

--- mflowgen/Tile_PE/construct.py
+++ mflowgen/Tile_PE/construct.py
@@ -193,11 +193,10 @@ def construct():
       signoff.extend_inputs(['conn-aon-cells-vdd.tcl', 'pd-generate-lvs-netlist.tcl', 'check-clamp-logic-structure.tcl'] )
       pwr_aware_gls.extend_inputs(['design.vcs.pg.v'])
   
-      # Fix and repair PowerDomainConfigReg whenever magma decides to renumber it :(
-
-      synth.pre_extend_commands(         ['inputs/check-pdcr-address.sh'] )
-      init.pre_extend_commands(          ['inputs/check-pdcr-address.sh'] )
-      pwr_aware_gls.pre_extend_commands( ['inputs/check-pdcr-address.sh'] )
+      # Fix and repair PowerDomainConfigReg when/if magma decides to renumber it :(
+      synth.pre_extend_commands( ['./inputs/check-pdcr-address.sh'] )
+      init.pre_extend_commands(  ['./inputs/check-pdcr-address.sh'] )
+      pwr_aware_gls.pre_extend_commands( ['./assign-pdcr-address.sh'] )
 
   # Add short_fix script(s) to list of available postroute scripts
   postroute.extend_inputs( short_fix.all_outputs() )
