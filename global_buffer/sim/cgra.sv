523ce3c3aea6501d53c3da9fa9c3253a42923455
Taeyoung Kong <kongty@stanford.edu>
2022-09-10 03:50:33 -0700

    Revert testbench for static schedule

--- global_buffer/sim/cgra.sv
+++ global_buffer/sim/cgra.sv
@@ -67,17 +67,22 @@ module cgra (
     // ---------------------------------------
     bit [NUM_PRR-1:0] is_glb2prr_on;
     bit [NUM_PRR-1:0] is_prr2glb_on;
+    bit [NUM_PRR-1:0] is_glb2prr_count_on;
     bit [NUM_PRR-1:0][1:0] glb2prr_valid_mode;
     bit [NUM_PRR-1:0][1:0] prr2glb_valid_mode;
     bit [NUM_PRR-1:0] is_prr2glb_done;
+    bit [NUM_PRR-1:0] is_glb2prr_done;
 
     bit [NUM_PRR-1:0][99:0] prr2glb_cnt;
     bit [NUM_PRR-1:0] prr2glb_valid;
+    bit [NUM_PRR-1:0][99:0] glb2prr_cnt;
+    bit [NUM_PRR-1:0] glb2prr_valid;
 
     bit [15:0] glb2prr_q[int][$];
     bit [15:0] glb2prr_fifo[int][$];
     bit [15:0] prr2glb_q[int][$];
     bit [99:0] prr2glb_valid_cnt_q[int][$];
+    bit [99:0] glb2prr_valid_cnt_q[int][$];
 
     bit flush;
 
@@ -89,6 +94,7 @@ module cgra (
         if (reset) begin
             for (int i = 0; i < NUM_PRR; i++) begin
                 glb2prr_q[i] = {};
+                is_glb2prr_count_on[i] <= 0;
             end
         end else begin
             for (int i = 0; i < NUM_PRR; i++) begin
@@ -105,6 +111,16 @@ module cgra (
                             // valid mode
                             if (io1_g2io[i][0] == 1) begin
                                 glb2prr_q[i].push_back(io16_g2io[i][0]);
+                            end
+                        end else if (glb2prr_valid_mode[i] == LD_DMA_VALID_MODE_STATIC) begin
+                            if (strm_data_flush_g2f[i/2] == 1) begin
+                                is_glb2prr_count_on[i] <= 1;
+                            end else if (is_glb2prr_count_on[i] == 1) begin
+                                if (glb2prr_valid[i] == 1) begin
+                                    glb2prr_q[i].push_back(io16_g2io[i][0]);
+                                end
+                            end else if (is_glb2prr_done[i] == 1) begin
+                                is_glb2prr_count_on[i] <= 0;
                             end
                         end
                     end
@@ -233,7 +249,7 @@ module cgra (
                     if ((is_prr2glb_on[i] == 1) && (is_prr2glb_done[i] == 0)) begin
                         if (prr2glb_cnt[i] == prr2glb_valid_cnt_q[i][0]) begin
                             prr2glb_valid[i] <= 1;
-                            void'(iterate_valid_cnt(i));
+                            void'(iterate_prr2glb_valid_cnt(i));
                         end else begin
                             prr2glb_valid[i] <= 0;
                         end
@@ -246,11 +262,69 @@ module cgra (
         end
     end
 
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) begin
+            glb2prr_valid <= 0;
+            glb2prr_cnt <= 0;
+            is_glb2prr_done <= '0;
+        end else begin
+            for (int i = 0; i < NUM_PRR; i++) begin
+                if (flush) begin
+                    glb2prr_cnt <= 0;
+                    is_glb2prr_done <= '0;
+                    glb2prr_valid <= 0;
+                end else if (!stall[i]) begin
+                    if ( ((strm_data_flush_g2f[i/1] == 1) | (is_glb2prr_count_on[i] == 1)) && (is_glb2prr_done[i] == 0)) begin
+                        if (glb2prr_cnt[i] == glb2prr_valid_cnt_q[i][0]) begin
+                            glb2prr_valid[i] <= 1;
+                            void'(iterate_glb2prr_valid_cnt(i));
+                        end else begin
+                            glb2prr_valid[i] <= 0;
+                        end
+                        glb2prr_cnt[i] <= glb2prr_cnt[i] + 1;
+                    end else begin
+                        glb2prr_valid[i] <= 0;
+                    end
+                end
+            end
+        end
+    end
+
     initial begin
         foreach (prr2glb_valid_cnt_q[i]) begin
             prr2glb_valid_cnt_q[i] = {};
         end
+        foreach (glb2prr_valid_cnt_q[i]) begin
+            glb2prr_valid_cnt_q[i] = {};
+        end
     end
+
+    function glb2prr_configure(int prr_id, int dim, int extent[LOOP_LEVEL],
+                               int cycle_stride[LOOP_LEVEL]);
+        bit [99:0] cnt;
+        bit done;
+        int i_extent[LOOP_LEVEL];
+        glb2prr_valid_cnt_q[prr_id] = {};
+        done = 0;
+        i_extent = '{LOOP_LEVEL{0}};
+        while (1) begin
+            cnt = 0;
+            for (int i = 0; i < dim; i++) begin
+                cnt += i_extent[i] * cycle_stride[i];
+            end
+            glb2prr_valid_cnt_q[prr_id].push_back(cnt);
+            for (int i = 0; i < dim; i++) begin
+                i_extent[i] += 1;
+                if (i_extent[i] == extent[i]) begin
+                    i_extent[i] = 0;
+                    if (i == dim - 1) done = 1;
+                end else begin
+                    break;
+                end
+            end
+            if (done == 1) break;
+        end
+    endfunction
 
     function prr2glb_configure(int prr_id, int dim, int extent[LOOP_LEVEL],
                                int cycle_stride[LOOP_LEVEL]);
@@ -303,10 +377,18 @@ module cgra (
         is_prr2glb_on[prr_id] = '0;
     endfunction
 
-    function iterate_valid_cnt(int prr_id);
+    function iterate_prr2glb_valid_cnt(int prr_id);
         void'(prr2glb_valid_cnt_q[prr_id].pop_front());
         if (prr2glb_valid_cnt_q[prr_id].size() == 0) begin
             is_prr2glb_done[prr_id] = 1;
+            // void'(prr2glb_off(prr_id));
+        end
+    endfunction
+
+    function iterate_glb2prr_valid_cnt(int prr_id);
+        void'(glb2prr_valid_cnt_q[prr_id].pop_front());
+        if (glb2prr_valid_cnt_q[prr_id].size() == 0) begin
+            is_glb2prr_done[prr_id] = 1;
             // void'(prr2glb_off(prr_id));
         end
     endfunction
