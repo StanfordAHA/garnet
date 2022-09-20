d4ce3366e5d373cf614f9fc2f942e972ef5f1878
Alex Carsello <ajcars@stanford.edu>
2022-09-20 12:49:23 -0700

    Remove cts routing rules from glb_top, add insertion delay parms to glb_tile pins

--- mflowgen/glb_top/custom-cts/skew-groups.tcl
+++ mflowgen/glb_top/custom-cts/skew-groups.tcl
@@ -1,5 +1,12 @@
 # Put all GLB tiles in a single skew group
-#create_ccopt_skew_group -name glb_tiles -exclusive_sinks [get_object_name [get_pins -hier glb_tile_gen*/clk]]
+#create_ccopt_skew_group \
+#    -name glb_tiles \
+#    -exclusive_sinks [get_object_name [get_pins -hier glb_tile_gen*/clk]] \
+#    -target_skew 0.1ns \
+#    -target_insertion_delay 0.4ns
+#
+#set_ccopt_property -skew_group glb_tiles constrains ccopt
 
-
-#set_ccopt_property auto_limit_insertion_delay_factor 1.2
+foreach_in_collection clk_pin [get_pins -hier glb_tile_gen*/clk] {
+    set_ccopt_property -pin [get_object_name $clk_pin] insertion_delay 300ps
+}
