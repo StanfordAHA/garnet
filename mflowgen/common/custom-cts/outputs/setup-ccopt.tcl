e0d91840465523e6a6e471a4761bd62ec2bf62df
Alex Carsello <ajcars@stanford.edu>
2022-09-23 12:06:45 -0700

    Revert "Get rid of CTS NDRs"
    This reverts commit e31e8a1aab60adfca4d12b0a1c300bcfa460b197.

--- mflowgen/common/custom-cts/outputs/setup-ccopt.tcl
+++ mflowgen/common/custom-cts/outputs/setup-ccopt.tcl
@@ -6,54 +6,54 @@
 
 # Add non-default rules for route types
 
-#add_ndr -name CTS_2W1S \
-#        -width_multiplier {7:9 2} \
-#        -generate_via
-#        # -min_cut {M4:M5 2}
-#
-#add_ndr -name CTS_2W2S \
-#        -width_multiplier {10:12 2} \
-#        -spacing_multiplier {10:12 2} \
-#        -generate_via
-#        # -min_cut {M6:M9 2}
-#
-## Define route types (from Quick Start Example)
-#
-#create_route_type -name leaf_rule \
-#                  -non_default_rule CTS_2W1S \
-#                  -top_preferred_layer 8 \
-#                  -bottom_preferred_layer 7 \
-#                  -prefer_multi_cut_via \
-#                  -preferred_routing_layer_effort medium
-#
-#create_route_type -name trunk_rule \
-#                  -non_default_rule CTS_2W2S \
-#                  -top_preferred_layer 10 \
-#                  -bottom_preferred_layer 9 \
-#                  -shield_net VSS \
-#                  -shield_side both_side \
-#                  -prefer_multi_cut_via \
-#                  -preferred_routing_layer_effort medium
-#
-#create_route_type -name top_rule \
-#                  -non_default_rule CTS_2W2S \
-#                  -top_preferred_layer 12 \
-#                  -bottom_preferred_layer 11 \
-#                  -shield_net VSS \
-#                  -shield_side both_side \
-#                  -prefer_multi_cut_via \
-#                  -preferred_routing_layer_effort medium
-#
-## Specify the route types will be used for leaf, trunk, and top nets,
-## respectively
-#
-#set_ccopt_property -net_type leaf route_type leaf_rule
-#set_ccopt_property -net_type trunk route_type trunk_rule
-#set_ccopt_property -net_type top route_type top_rule
-#
-## Setting top
-#
-#set_ccopt_property routing_top_min_fanout 10000
+add_ndr -name CTS_2W1S \
+        -width_multiplier {7:9 2} \
+        -generate_via
+        # -min_cut {M4:M5 2}
+
+add_ndr -name CTS_2W2S \
+        -width_multiplier {10:12 2} \
+        -spacing_multiplier {10:12 2} \
+        -generate_via
+        # -min_cut {M6:M9 2}
+
+# Define route types (from Quick Start Example)
+
+create_route_type -name leaf_rule \
+                  -non_default_rule CTS_2W1S \
+                  -top_preferred_layer 8 \
+                  -bottom_preferred_layer 7 \
+                  -prefer_multi_cut_via \
+                  -preferred_routing_layer_effort medium
+
+create_route_type -name trunk_rule \
+                  -non_default_rule CTS_2W2S \
+                  -top_preferred_layer 10 \
+                  -bottom_preferred_layer 9 \
+                  -shield_net VSS \
+                  -shield_side both_side \
+                  -prefer_multi_cut_via \
+                  -preferred_routing_layer_effort medium
+
+create_route_type -name top_rule \
+                  -non_default_rule CTS_2W2S \
+                  -top_preferred_layer 12 \
+                  -bottom_preferred_layer 11 \
+                  -shield_net VSS \
+                  -shield_side both_side \
+                  -prefer_multi_cut_via \
+                  -preferred_routing_layer_effort medium
+
+# Specify the route types will be used for leaf, trunk, and top nets,
+# respectively
+
+set_ccopt_property -net_type leaf route_type leaf_rule
+set_ccopt_property -net_type trunk route_type trunk_rule
+set_ccopt_property -net_type top route_type top_rule
+
+# Setting top
+
+set_ccopt_property routing_top_min_fanout 10000
 
 set_ccopt_property buffer_cells {SC7P5T_CKBUFX1_SSC14SL SC7P5T_CKBUFX0P5_SSC14SL SC7P5T_CKBUFX10_SSC14SL SC7P5T_CKBUFX12_SSC14SL SC7P5T_CKBUFX14_SSC14SL SC7P5T_CKBUFX16_SSC14SL SC7P5T_CKBUFX20_SSC14SL SC7P5T_CKBUFX24_SSC14SL SC7P5T_CKBUFX2_SSC14SL SC7P5T_CKBUFX3_SSC14SL  SC7P5T_CKBUFX4_SSC14SL SC7P5T_CKBUFX5_SSC14SL SC7P5T_CKBUFX6_SSC14SL SC7P5T_CKBUFX8_SSC14SL SC7P5T_CKBUFX1_SSC14L SC7P5T_CKBUFX0P5_SSC14L SC7P5T_CKBUFX10_SSC14L SC7P5T_CKBUFX12_SSC14L SC7P5T_CKBUFX14_SSC14L SC7P5T_CKBUFX16_SSC14L SC7P5T_CKBUFX20_SSC14L SC7P5T_CKBUFX24_SSC14L SC7P5T_CKBUFX2_SSC14L SC7P5T_CKBUFX3_SSC14L  SC7P5T_CKBUFX4_SSC14L SC7P5T_CKBUFX5_SSC14L SC7P5T_CKBUFX6_SSC14L SC7P5T_CKBUFX8_SSC14L SC7P5T_CKBUFX1_SSC14R SC7P5T_CKBUFX0P5_SSC14R SC7P5T_CKBUFX10_SSC14R SC7P5T_CKBUFX12_SSC14R SC7P5T_CKBUFX14_SSC14R SC7P5T_CKBUFX16_SSC14R SC7P5T_CKBUFX20_SSC14R SC7P5T_CKBUFX24_SSC14R SC7P5T_CKBUFX2_SSC14R SC7P5T_CKBUFX3_SSC14R  SC7P5T_CKBUFX4_SSC14R SC7P5T_CKBUFX5_SSC14R SC7P5T_CKBUFX6_SSC14R SC7P5T_CKBUFX8_SSC14R}
 
