93a7e284a74835bcc3c887ab4b7450da3a16aa7e
Alex Carsello <ajcars@stanford.edu>
2022-10-02 22:16:15 -0700

    propagate glb multicycle paths to top level constraints

--- mflowgen/full_chip/constraints/cons_scripts/garnet_constraints.tcl
+++ mflowgen/full_chip/constraints/cons_scripts/garnet_constraints.tcl
@@ -9,25 +9,25 @@
 #------------------------------------------------------------------------------
 
 # Paths involved with reading configuration data can be relaxed
-set_multicycle_path 10 -setup -to [get_pins -hier *global_controller*/cgra_cfg_rd_data*]
-set_multicycle_path 9 -hold -to [get_pins -hier *global_controller*/cgra_cfg_rd_data*]
+set_multicycle_path 10 -setup -through [get_pins -hier *global_controller*/cgra_cfg_rd_data*]
+set_multicycle_path 9 -hold -through [get_pins -hier *global_controller*/cgra_cfg_rd_data*]
 
-set_multicycle_path 10 -setup -to [get_pins -hier *global_controller*/sram_cfg_rd_data*]
-set_multicycle_path 9 -hold -to [get_pins -hier *global_controller*/sram_cfg_rd_data*]
+set_multicycle_path 10 -setup -through [get_pins -hier *global_controller*/sram_cfg_rd_data*]
+set_multicycle_path 9 -hold -through [get_pins -hier *global_controller*/sram_cfg_rd_data*]
 
-#set_multicycle_path 3 -setup -to [get_pins -hier *global_controller*/glb_cfg_rd_data*]
-#set_multicycle_path 2 -hold -to [get_pins -hier *global_controller*/glb_cfg_rd_data*]
+#set_multicycle_path 3 -setup -through [get_pins -hier *global_controller*/glb_cfg_rd_data*]
+#set_multicycle_path 2 -hold -through [get_pins -hier *global_controller*/glb_cfg_rd_data*]
 
 set_multicycle_path 5 -setup -through [get_pins -hier *global_buffer*/*interrupt_pulse*]
 set_multicycle_path 4 -hold  -through [get_pins -hier *global_buffer*/*interrupt_pulse*]
-set_multicycle_path -setup 10 -to [get_pins -hier global_buffer/pcfg_broadcast_stall]  
-set_multicycle_path -hold 9 -to [get_pins -hier global_buffer/pcfg_broadcast_stall]    
-set_multicycle_path -setup 10 -to [get_pins -hier global_buffer/glb_clk_en_master]     
-set_multicycle_path -hold 9 -to [get_pins -hier global_buffer/glb_clk_en_master]       
-set_multicycle_path -setup 10 -to [get_pins -hier global_buffer/glb_clk_en_bank_master]
-set_multicycle_path -hold 9 -to [get_pins -hier global_buffer/glb_clk_en_bank_master]  
-set_multicycle_path -setup 10 -to [get_pins -hier global_buffer/flush_crossbar_sel]    
-set_multicycle_path -hold 9 -to [get_pins -hier global_buffer/flush_crossbar_sel]
+set_multicycle_path -setup 10 -to [get_pins -hier global_buffer/pcfg_broadcast_stall*]
+set_multicycle_path -hold 9 -to [get_pins -hier global_buffer/pcfg_broadcast_stall*]
+set_multicycle_path -setup 10 -to [get_pins -hier global_buffer/glb_clk_en_master*]
+set_multicycle_path -hold 9 -to [get_pins -hier global_buffer/glb_clk_en_master*]
+set_multicycle_path -setup 10 -to [get_pins -hier global_buffer/glb_clk_en_bank_master*]
+set_multicycle_path -hold 9 -to [get_pins -hier global_buffer/glb_clk_en_bank_master*]
+set_multicycle_path -setup 10 -to [get_pins -hier global_buffer/flush_crossbar_sel*]
+set_multicycle_path -hold 9 -to [get_pins -hier global_buffer/flush_crossbar_sel*]
 
 # Don't consider xgcd timing since it's not done yet.
 set_false_path -through [get_cells -hier u_xgcd_wrapper_top]
