a7ff6b386e12b9ef3ad809d2701ada03d1572bd5
steveri <steveri@steveri.com>
2022-10-15 12:17:00 -0700

    limit pe width to 110 (112)

--- mflowgen/Tile_PE/custom-init/outputs/floorplan.tcl
+++ mflowgen/Tile_PE/custom-init/outputs/floorplan.tcl
@@ -36,8 +36,23 @@ foreach area $cell_areas {
 # Calculate FP width that will meet density target given fixed height 
 set width [expr $total_cell_area / $core_density_target / $height]
 
-# Core bounding box margins
+# 10/2022 steveri
+# Latest RTL gives width 124, builds array bigger than the chip :(
+# PE width 124 routes in 20 min, but then array is too big.
+# PE width 105 takes over 5 hours to route. So that's too small.
+# PE width 110 routes in ???. Let's use that as a max width, see what happens.
+# (Even though I set it to 110, it comes out 112 in the report, not sure why?)
+set max_width 110
+if { $width > $max_width } {
+    set new_density [expr $total_cell_area / $width / $height]
+    echo "+++ PE too wide"
+    echo "Wanted width $width for density target $core_density_target"
+    echo "But width > 110 disallowed (Tile_PE/custom-init/outputs/floorplan.tcl)"
+    echo "So now using width $width for new density $new_density"
+    set width $max_width
+}
 
+# Core bounding box margins
 set core_margin_t $vert_pitch
 set core_margin_b $vert_pitch 
 set core_margin_r [expr 10 * $horiz_pitch]
