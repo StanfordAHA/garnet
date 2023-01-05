3716a9c545ec86bc9e3603c63efcd5334002ac5a
steveri <steveri@steveri.com>
2023-01-05 13:59:43 -0800

    Spv merge to spv 3 (#938)
    * creeping toward a common merged master branch
    * restore original mflowgen tests
    * conditional pipeline IV: expand number of branch cases
    * (re)fixing the run_sim scripts
    * setup-buildkite: merging good changes from both branches
    * merging gen_rtl
    * oops not reday for ANAIOPADs yet
    * tsmc portion of clk_gate was wrong for some reason...
    * if TSMC / elif GF ....
    * streamout is different for tsmc
    * Merged Tile_Memcore/contruct.py
    * Merged Tile_PE/contruct.py
    * merged fullchip construct.py
    * Merged mflowgen/pad_frame/construct.py
    * Merged mflowgen/glb_tile/construct.py
    * tweak mflowgen/full_chip/construct.py to match dense version
    * common mflowgen/tile_array/construct.py for amber, onyx
    * common mflowgen/glb_top/construct.py for amber, onyx
    * common mflowgen/global_controller/construct.py for amber, onyx
    * final tweak on fc/construct.py -- this one passed full build
    * added a check to early-out TSMC build failures
    * updated vs. latest garnet.py from spVspV branch
    * emergency(?) fix for CI
    * try again with previously-merged rtl-only pipeline
    * merged latest set of files from $smd

--- global_buffer/design/glb_bank_sram_gen.py
+++ global_buffer/design/glb_bank_sram_gen.py
@@ -8,11 +8,11 @@ from global_buffer.design.global_buffer_parameter import GlobalBufferParams
 class GlbBankSramGen(Generator):
     def __init__(self, addr_width, _params: GlobalBufferParams):
 
-        # FIXME this TSMC/GF fix might goo away after the smoke
+        # FIXME this TSMC/GF fix might go away after the smoke
         # clears, but for now it gets us closer to a common master
-        if self._params.process == "TSMC":
+        if _params.process == "TSMC":
             super().__init__("glb_bank_sram_gen")
-        elif self._params.process == "GF":
+        elif _params.process == "GF":
             super().__init__(f"glb_bank_sram_gen_{addr_width}")
 
         self._params = _params
