95034360ceb9a88482fa6b67daccca36dbfad192
root <kalhankoul96@gmail.com>
2023-03-21 13:37:19 -0700

    working on sparse tiles

---
+++ SPARSE_TESTS/copy_formatted.py
@@ -0,0 +1,23 @@
+import shutil
+
+
+shutil.copy("../matmul_ijk_rel5/formatted/tensor_B_tile_0_0_0_0/B0_crd.txt", "./MAT_TMP_DIR/tile1/tensor_B_mode_0_crd")
+shutil.copy("../matmul_ijk_rel5/formatted/tensor_B_tile_0_0_0_0/B0_seg.txt", "./MAT_TMP_DIR/tile1/tensor_B_mode_0_seg")
+
+shutil.copy("../matmul_ijk_rel5/formatted/tensor_B_tile_0_0_0_0/B1_crd.txt", "./MAT_TMP_DIR/tile1/tensor_B_mode_1_crd")
+shutil.copy("../matmul_ijk_rel5/formatted/tensor_B_tile_0_0_0_0/B1_seg.txt", "./MAT_TMP_DIR/tile1/tensor_B_mode_1_seg")
+
+shutil.copy("../matmul_ijk_rel5/formatted/tensor_B_tile_0_0_0_0/B_vals.txt", "./MAT_TMP_DIR/tile1/tensor_B_mode_vals")
+
+shutil.copy("../matmul_ijk_rel5/formatted/tensor_B_tile_0_0_0_0/B_shape.txt", "./MAT_TMP_DIR/tile1/tensor_B_mode_shape")
+
+shutil.copy("../matmul_ijk_rel5/formatted/tensor_C_tile_0_0_0_1/C0_crd.txt", "./MAT_TMP_DIR/tile1/tensor_C_mode_1_crd")
+shutil.copy("../matmul_ijk_rel5/formatted/tensor_C_tile_0_0_0_1/C0_seg.txt", "./MAT_TMP_DIR/tile1/tensor_C_mode_1_seg")
+
+shutil.copy("../matmul_ijk_rel5/formatted/tensor_C_tile_0_0_0_1/C1_crd.txt", "./MAT_TMP_DIR/tile1/tensor_C_mode_0_crd")
+shutil.copy("../matmul_ijk_rel5/formatted/tensor_C_tile_0_0_0_1/C1_seg.txt", "./MAT_TMP_DIR/tile1/tensor_C_mode_0_seg")
+
+shutil.copy("../matmul_ijk_rel5/formatted/tensor_C_tile_0_0_0_1/C_vals.txt", "./MAT_TMP_DIR/tile1/tensor_C_mode_vals")
+
+shutil.copy("../matmul_ijk_rel5/formatted/tensor_C_tile_0_0_0_1/C_shape.txt", "./MAT_TMP_DIR/tile1/tensor_C_mode_shape")
+
