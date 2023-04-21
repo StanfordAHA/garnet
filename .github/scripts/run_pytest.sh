ad47210e374005c8ddbc785611a2eff46ff49448
steveri <steveri@steveri.com>
2023-04-21 10:15:41 -0700

    must run pytest from garnet dir, duh

--- .github/scripts/run_pytest.sh
+++ .github/scripts/run_pytest.sh
@@ -1,3 +1,15 @@
+# LAKE_DIR=$(dirname $(dirname `python -c "import lake; print(lake.__file__)"`))
+# source ${LAKE_DIR}/scripts/setenv.sh
+
+# force color
+export PYTEST_ADDOPTS="--color=yes"
+
+# get the garnet root
+# .github/scripts/run.sh
+# ROOT=$(dirname $(dirname $(dirname $BASH_SOURCE)))
+# cd ${ROOT}
+cd /aha/garnet
+
 pytest --pycodestyle           \
        --cov global_controller \
        --cov io_core           \
