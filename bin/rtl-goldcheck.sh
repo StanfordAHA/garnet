5dcc95e7fa854a9d64ffa3e4b3cc5dd2e815ce07
steveri <steveri@steveri.com>
2023-06-27 13:03:11 -0700

    new/updated rtl-goldcheck utilities

--- bin/rtl-goldcheck.sh
+++ bin/rtl-goldcheck.sh
@@ -120,31 +120,16 @@ cp $refdir/$ref.gz . || exit 13
 gunzip $ref.gz
 f1=design.v; f2=$ref
 
-# Need 'sed s/unq...' to handle the case where both designs are
-# exactly the same but different "unq" suffixes e.g.
-#     < Register_unq3 Register_inst0 (
-#     > Register_unq2 Register_inst0 (
-#
-# Need 's/_O._value_O/...' because generator seems e.g. to randomly assign
-# the equivalent values 'PE_onyx_inst_onyxpeintf_O3_value_O' and '...O4_value_O' :(
-
-function vcompare {
-    cat $1 |
-    sed 's/_O._value_O/_Ox_value_O/g' | # Treat all zeroes as equivalent
-    sed 's/,$//'           | # No trailing commas
-    sed 's/unq[0-9*]/unq/' | # Canonicalize unq's
-    sed '/^\s*$/d'         | # No blank lines
-    sort                   | # Out-of-order is okay
-    cat
-}
 printf "\n"
-echo "Comparing `vcompare $f1 | wc -l` lines of $f1"
-echo "versus    `vcompare $f2 | wc -l` lines of $f2"
+echo "Comparing `cat $f1 | wc -l` lines of $f1"
+echo "versus    `cat $f2 | wc -l` lines of $f2"
 printf "\n"
 
-echo "diff $f1 $f2"
-ndiffs=`diff -Bb -I Date <(vcompare $f1) <(vcompare $f2) | wc -l`
+alias vcompare=/aha/garnet/bin/rtl-goldcompare.sh
 
+echo "/aha/garnet/bin/rtl-goldcompare.sh $f1 $f2"
+# ndiffs=`diff -Bb -I Date $f1 $f2 | wc -l`
+ndiffs=`vcompare $f1 $f2 | wc -l`
 if [ "$ndiffs" != "0" ]; then
 
     # ------------------------------------------------------------------------
@@ -152,7 +137,8 @@ if [ "$ndiffs" != "0" ]; then
 
     printf "Test FAILED with $ndiffs diffs\n\n"
     printf "Top 40 diffs:"
-    diff -I Date <(vcompare $f1) <(vcompare $f2) | head -40
+    # diff -I Date <(vcompare $f1) <(vcompare $f2) | head -40
+    vcompare $f1 $f2 | head -40
     exit 13
 fi
 
