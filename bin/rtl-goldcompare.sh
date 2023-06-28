338848a9ea37eee8db73a56cd432a9d9c35a7715
steveri <steveri@stanford.edu>
2023-06-28 07:19:26 -0700

    Mflowgen fix (#969)
    * better way of tracking adks
    * found adk repo on gf machine
    * decided to do parens instead of pushd
    * final cleanup
    * ---------------------------------------------------
    * Fixed CI test, no longer fails on bad gold-rtl check
    * CI test should flag failing gold step but not fail pipeline
    * trying something wacky; complete revamp of gold check
    * narrowing in on better gold checks
    * try getting fancy with emojis i guess
    * quick test of formats
    * final final cleanup
    * last minute tweak: prune unused docker images
    * change ownership of RTL gold check from aha repo to garnet repo
    * new/updated rtl-goldcheck utilities
    * oops using wrong rtl-goldcheck script
    * update to new onyx gold rtl

---
+++ bin/rtl-goldcompare.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cmd=$0; HELP="
+DESCRIPTION
+  Compare two verilog files, ignoring non-functional diffs mostly ish.
+
+EXAMPLES
+  $cmd file1.v file2.v
+  $cmd <(gunzip -c file1.v.gz) <(gunzip -c file2.v.gz)
+"
+[ "$1" == "--help" ] && echo "$HELP" && exit
+[ "$1" == "" ] && echo "$HELP" && exit
+
+# Need 'sed s/unq...' to handle the case where both designs are
+# exactly the same but different "unq" suffixes e.g.
+#     < Register_unq3 Register_inst0 (
+#     > Register_unq2 Register_inst0 (
+#
+# Need 's/_O._value_O/...' because generator seems e.g. to randomly assign
+# the equivalent values 'PE_onyx_inst_onyxpeintf_O3_value_O' and '...O4_value_O' :(
+
+function vcompare {
+    cat $1 |
+    sed 's/_O._value_O/_Ox_value_O/g' | # Treat all zeroes as equivalent
+    sed 's/,$//'           | # No trailing commas
+    sed 's/unq[0-9*]/unq/' | # Canonicalize unq's
+    sed '/^\s*$/d'         | # No blank lines
+    sort                   | # Out-of-order is okay
+    cat
+}
+diff -Bb -I Date <(vcompare $1) <(vcompare $2)
