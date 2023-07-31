e4075b51817aee465015214f84d46a52555691cf
steveri <steveri@steveri.com>
2023-07-31 15:14:19 -0700

    cleanup

--- TEMP/custom-checkout.sh
+++ TEMP/custom-checkout.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 
 # FIXME/NOTE! can skip a lot of stuff by checking to see if
 # $FLOW_REPO / $FLOW_HEAD_SHA already been set
@@ -13,11 +12,11 @@ fi
 
 git submodule foreach --recursive "git clean -ffxdq"
 git clean -ffxdq
-set -x; echo git fetch -v --prune -- origin $BUILDKITE_COMMIT
 
 unset FAIL
 # FAIL means build was triggered by foreign (non-aha) repo, i.e. one of the submods
-set -x; git fetch -v --prune -- origin $BUILDKITE_COMMIT || FAIL=true
+echo git fetch -v --prune -- origin $BUILDKITE_COMMIT
+git fetch -v --prune -- origin $BUILDKITE_COMMIT || FAIL=true
 [ "$FAIL" ] && git fetch -v --prune -- origin master || echo okay
 
 git checkout -f FETCH_HEAD
@@ -25,22 +24,24 @@ git submodule sync --recursive
 git submodule update --init --recursive --force
 git submodule foreach --recursive "git reset --hard"
 
+[ "$FAIL" ] && echo "--- Handle PR"
 echo "--- Looking for submod commit $BUILDKITE_COMMIT"
 unset FOUND_SUBMOD
 [ "$FAIL" ] && for submod in garnet Halide-to-Hardware lassen gemstone canal lake; do
     echo "--- - " Looking in submod $submod
     # (cd $submod; git checkout $BUILDKITE_COMMIT && echo UPDATE SUBMOD $submod || echo NOT $submod);
-    (cd $submod; git fetch origin && git checkout $BUILDKITE_COMMIT) && FOUND_SUBMOD=true || echo "--- -- NOT " Ssubmod
+    (set -x; cd $submod; git fetch origin && git checkout $BUILDKITE_COMMIT) && FOUND_SUBMOD=true || echo "--- -- NOT " Ssubmod
     [ "$FOUND_SUBMOD" ] && echo "--- -- FOUND " $submod
     [ "$FOUND_SUBMOD" ] && break
 done || echo okay no submod updates needed
 
-# want?
+# # want?
 if [ "$FOUND_SUBMOD" ]; then
-    export FLOW_REPO=$submod
-    export FLOW_REPO_SHA=$BUILDKITE_COMMIT
-    export BUILDKITE_COMMIT=master
-    export BUILDKITE_COMMIT_MESSAGE="hello woild"
+    # These are used later by pipeline.xml
+    # BUT NOT as global env vars; this script must
+    # be sourced in same scope as var usage, see?
+    FLOW_REPO=$submod
+    FLOW_REPO_SHA=$BUILDKITE_COMMIT
 fi
 
 
