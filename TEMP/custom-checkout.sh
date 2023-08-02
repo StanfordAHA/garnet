15470a98b576a3aba4499e137fe5b7b282f3e463
steveri <steveri@steveri.com>
2023-08-02 08:49:10 -0700

    big changes, much will break

--- TEMP/custom-checkout.sh
+++ TEMP/custom-checkout.sh
@@ -8,76 +8,90 @@ RESTORE_SHELLOPTS="$(set +o)"
 set +u # nounset? not on my watch!
 set +x # debug OFF
 
+pwd # We are in root dir (/) !!!oh no!!!
+echo cd $BUILDKITE_BUILD_PATH
+cd $BUILDKITE_BUILD_PATH
 pwd
-env
-cd $BUILDKITE_BUILD_PATH 
 
-########################################################################
-echo "+++ checkout.sh trash"
-echo '-------------'
-ls -l /tmp/ahaflow-custom-checkout* || echo nope
-echo '-------------'
-ls -ld /var/lib/buildkite-agent/builds/*/stanford-aha/aha-flow/ || echo nope
-echo '-------------'
-ls -ld /var/lib/buildkite-agent/builds/*/stanford-aha/aha-flow/aha || echo nope
-echo '-------------'
-echo I am `whoami`
-
-# No!
-f='/tmp/ahaflow-custom-checkout-$BUILDKITE_BUILD_NUMBER.sh'
-test -f $f && /bin/rm $f
-
-
-echo "--- CONTINUE"
-########################################################################
+# ########################################################################
+# echo "+++ checkout.sh trash"
+# echo '-------------'
+# ls -l /tmp/ahaflow-custom-checkout* || echo nope
+# echo '-------------'
+# ls -ld /var/lib/buildkite-agent/builds/*/stanford-aha/aha-flow/ || echo nope
+# echo '-------------'
+# ls -ld /var/lib/buildkite-agent/builds/*/stanford-aha/aha-flow/aha || echo nope
+# echo '-------------'
+# echo I am `whoami`
+# 
+# # No!
+# f='/tmp/ahaflow-custom-checkout-$BUILDKITE_BUILD_NUMBER.sh'
+# test -f $f && /bin/rm $f
+# 
+# 
+# echo "--- CONTINUE"
+# ########################################################################
 
 # FIXME/NOTE! can skip a lot of stuff by checking to see if
 # $FLOW_REPO / $FLOW_HEAD_SHA already been set
 
-# git remote set-url origin https://github.com/hofstee/aha
-if ! git remote set-url origin https://github.com/hofstee/aha 2> /dev/null; then
-  test -e aha || git clone https://github.com/hofstee/aha
-  cd aha
-  git remote set-url origin https://github.com/hofstee/aha
-fi
+# set -x
+# # git remote set-url origin https://github.com/hofstee/aha
+# if ! git remote set-url origin https://github.com/hofstee/aha 2> /dev/null; then
+#   test -e aha || git clone https://github.com/hofstee/aha
+#   cd aha
+#   git remote set-url origin https://github.com/hofstee/aha
+# fi
+# set +x
 
+# This is what I SHOULD do...
+echo "--- CLONE AHA REPO"
+cd $BUILDKITE_BUILD_PATH
+test -e aha && /bin/rm -rf aha
+git clone https://github.com/hofstee/aha
+cd aha
+
+git remote set-url origin https://github.com/hofstee/aha
 git submodule foreach --recursive "git clean -ffxdq"
 git clean -ffxdq
 
-unset FAIL
-# FAIL means build was triggered by foreign (non-aha) repo, i.e. one of the submods
+unset PR_FROM_SUBMOD
+# PR_FROM_SUBMOD means build was triggered by foreign (non-aha) repo, i.e. one of the submods
 echo git fetch -v --prune -- origin $BUILDKITE_COMMIT
-git fetch -v --prune -- origin $BUILDKITE_COMMIT || FAIL=true
+git fetch -v --prune -- origin $BUILDKITE_COMMIT || PR_FROM_SUBMOD=true
 
 # AMBER_DEFAULT_BRANCH=no-heroku
 AMBER_DEFAULT_BRANCH=master
-[ "$FAIL" ] && git fetch -v --prune -- origin $AMBER_DEFAULT_BRANCH || echo okay
+[ "$PR_FROM_SUBMOD" ] && git fetch -v --prune -- origin $AMBER_DEFAULT_BRANCH || echo okay
 
 git checkout -f FETCH_HEAD
 git submodule sync --recursive
 git submodule update --init --recursive --force
 git submodule foreach --recursive "git reset --hard"
 
-[ "$FAIL" ] && echo "--- Handle PR"
-echo "--- Looking for submod commit $BUILDKITE_COMMIT"
-unset FOUND_SUBMOD
-[ "$FAIL" ] && for submod in garnet Halide-to-Hardware lassen gemstone canal lake; do
-    echo "--- - " Looking in submod $submod
-    # (cd $submod; git checkout $BUILDKITE_COMMIT && echo UPDATE SUBMOD $submod || echo NOT $submod);
-    (set -x; cd $submod; git fetch origin && git checkout $BUILDKITE_COMMIT) && FOUND_SUBMOD=true || echo "--- -- NOT " Ssubmod
-    [ "$FOUND_SUBMOD" ] && echo "--- -- FOUND " $submod
-    [ "$FOUND_SUBMOD" ] && break
-done || echo okay no submod updates needed
+if [ "$PR_FROM_SUBMOD" ]; then
+    echo "--- Handle PR"
+    echo "--- Looking for submod commit $BUILDKITE_COMMIT"
+    unset FOUND_SUBMOD
+    for submod in garnet Halide-to-Hardware lassen gemstone canal lake; do
+        echo "--- - " Looking in submod $submod
+        # (cd $submod; git checkout $BUILDKITE_COMMIT && echo UPDATE SUBMOD $submod || echo NOT $submod);
+        (set -x; cd $submod; git fetch origin && git checkout $BUILDKITE_COMMIT) && FOUND_SUBMOD=true || echo "--- -- NOT " Ssubmod
+        [ "$FOUND_SUBMOD" ] && echo "--- -- FOUND " $submod
+        [ "$FOUND_SUBMOD" ] && break
+    done
 
-# # want?
-if [ "$FOUND_SUBMOD" ]; then
-    # These are used later by pipeline.xml
-    # BUT NOT as global env vars; this script must
-    # be sourced in same scope as var usage, see?
-    FLOW_REPO=$submod
-    FLOW_REPO_SHA=$BUILDKITE_COMMIT
+    # # want?
+    if [ "$FOUND_SUBMOD" ]; then
+        # These are used later by pipeline.xml
+        # BUT NOT as global env vars; this script must
+        # be sourced in same scope as var usage, see?
+        FLOW_REPO=$submod
+        FLOW_REPO_SHA=$BUILDKITE_COMMIT
+    else
+        echo "ERROR could not find requesting submod"; exit 13
+    fi
 fi
-
 
 # https://github.com/StanfordAHA/garnet/blob/aha-flow-no-heroku/TEMP/custom-checkout.sh
 # https://raw.githubusercontent.com/StanfordAHA/garnet/aha-flow-no-heroku/TEMP/custom-checkout.sh
@@ -98,5 +112,5 @@ echo "AFTER: " `ls -l .buildkite/pipeline.yml`
 # echo "+++ Got hooks?"; ls -l .buildkite/hooks || echo nop
 
 echo "--- RESTORE SHELLOPTS"
-set -vx; eval "$RESTORE_SHELLOPTS"
+eval "$RESTORE_SHELLOPTS"
 pwd
