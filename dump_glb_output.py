8bf73bdcdb3cf55bd31dba6fc79c0b2f6c840d8c
Jayashree Adivarahan <jayashree.v.adivarahan@gmail.com>
2023-08-02 08:34:44 -0700

    revised dump_glb_outputs.py script, uses packet length metadata

--- dump_glb_output.py
+++ dump_glb_output.py
@@ -22,39 +22,91 @@ def dump_files_to_header(directory, output_file, app_name, line_length=80):
 
     with open(output_file, 'a') as header_file:
         for file_name in files:
-            mode = file_name.split('_')[3]  
+            mode = file_name.split('_')[3]  #for a matrix - the mode variable here can be 0, 1, or vals
             array_name = f'app_tensor_X_mode_{mode}_0_data'
             array_name = array_name.replace('.txt', '')
             header_file.write(f'uint16_t {array_name}[] = {{\n\t')
-            with open(os.path.join(directory, file_name), 'r') as file:
-                lines = [line.strip() for line in file.readlines()]
+            print("mode is", mode)
+            if mode=="vals.txt":
+                with open(os.path.join(directory, file_name), 'r') as file:
+                    lines = [line.strip() for line in file.readlines()]
+                    # print("Lines:",lines)
 
-                hex_values = []
-                lastvalue = 0
-                for line in lines:
-                    values = line.split()
-                    while values and values[-1] == '0000' and (len(values) == 1 or values[-2] == '0000'):
-                        values.pop()
-                    hex_values.extend([value for value in values])
-                del hex_values[-1]
+                    hex_values = []
+                    hex_values_to_print = []
+                    for line in lines:
+                        values = line.split()
+                        for value in values:
+                            hex_values.extend([value for value in values])
+
+                    first_val = int(hex_values[0])
+                    # print("hex vals:", hex_values)
+                    # print("first_val: ", first_val)
+                    hex_values_to_print.append(hex_values[0])
+
+                    for i in range(1,first_val+1):
+                        hex_values_to_print.append(hex_values[i])
+                    print("hex_values_to_print", hex_values_to_print)
 
                 # write nicely on single line
-                current_line_length = 0
-                if (hex_values):
-                    # print(hex_values)
-                    for index, hex_value in enumerate(hex_values):
-                        value_length = len(hex_value) + 4  
-                        if current_line_length + value_length > line_length:
-                            header_file.write('\n\t')
-                            current_line_length = 4 
-                        header_file.write(f'0x{hex_value}')
-                        if index < len(hex_values) - 1:
-                            header_file.write(',')
-                        current_line_length += value_length
-                header_file.write('\n};\n\n')
+                    current_line_length = 0
+                    if (hex_values_to_print):
+                        # print(hex_values)
+                        for index, hex_value in enumerate(hex_values_to_print):
+                            value_length = len(hex_value) + 4  
+                            if current_line_length + value_length > line_length:
+                                header_file.write('\n\t')
+                                current_line_length = 4 
+                            header_file.write(f'0x{hex_value}')
+                            if index < len(hex_values_to_print) - 1:
+                                header_file.write(',')
+                            current_line_length += value_length
+                    header_file.write('\n};\n\n')
 
-            array_size_name = f'{array_name}_size'
-            header_file.write(f'const unsigned int {array_size_name} = {len(hex_values)};\n\n')
+                array_size_name = f'{array_name}_size'
+                header_file.write(f'const unsigned int {array_size_name} = {len(hex_values_to_print)};\n\n')
+
+            else:
+                with open(os.path.join(directory, file_name), 'r') as file:
+                    lines = [line.strip() for line in file.readlines()]
+                    # print("Lines:",lines)
+
+                    hex_values = []
+                    hex_values_to_print = []
+                    for line in lines:
+                        values = line.split()
+                        for value in values:
+                            # hex_values.extend([value for value in values])
+                            hex_values.extend([value])
+                    print("hex vals:", hex_values)
+
+                    first_val = int(hex_values[0])
+                    crd_seg_divider = int(hex_values[first_val+1])
+                    # print("hex vals:", hex_values)
+                    # print("first_val: ", first_val)
+                    hex_values_to_print.append(hex_values[0])
+
+                    for i in range(1,first_val+crd_seg_divider+1+1):
+                        hex_values_to_print.append(hex_values[i])
+                    print("hex_values_to_print", hex_values_to_print)
+
+                # write nicely on single line
+                    current_line_length = 0
+                    if (hex_values_to_print):
+                        # print(hex_values)
+                        for index, hex_value in enumerate(hex_values_to_print):
+                            value_length = len(hex_value) + 4  
+                            if current_line_length + value_length > line_length:
+                                header_file.write('\n\t')
+                                current_line_length = 4 
+                            header_file.write(f'0x{hex_value}')
+                            if index < len(hex_values_to_print) - 1:
+                                header_file.write(',')
+                            current_line_length += value_length
+                    header_file.write('\n};\n\n')
+
+                array_size_name = f'{array_name}_size'
+                header_file.write(f'const unsigned int {array_size_name} = {len(hex_values_to_print)};\n\n')
         
         header_file.write('\n#endif\n')
 
