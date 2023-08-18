9ed1722db9dd1e784506ee0d3497a2276a38e9a2
Po-Han Chen <pohan@stanford.edu>
2023-08-18 14:48:57 -0700

    update the constraints

--- mflowgen/Tile_PE/constraints/common.tcl
+++ mflowgen/Tile_PE/constraints/common.tcl
@@ -1,58 +1,40 @@
-# Which SoC are we targeting?
+#=========================================================================
+# Which SoC
+#=========================================================================
 if { [info exists ::env(WHICH_SOC)] } {
     set WHICH_SOC $::env(WHICH_SOC)
 } else {
     set WHICH_SOC "onyx"
 }
 
+#=========================================================================
+# General
+#=========================================================================
+# -- clock
 set clock_net  clk
 set clock_name ideal_clock
-
 create_clock -name ${clock_name} \
              -period ${clock_period} \
              [get_ports ${clock_net}]
 
-# Make all signals limit their fanout
+# -- load and drive
+set_load -pin_load ${ADK_TYPICAL_ON_CHIP_LOAD} [all_outputs]
+set_driving_cell -no_design_rule -lib_cell ${ADK_DRIVING_CELL} [all_inputs]
 
-set_max_fanout 20 $design_name
+# -- fanout
+set_max_fanout 10 ${design_name}
 
-# Make all signals meet good slew
+# -- transition
+set_max_transition 50 ${design_name}
 
-set_max_transition [expr 0.1*1000] $design_name
-
-# This constraint sets the load capacitance in picofarads of the
-# output pins of your design.
-
-set_load -pin_load $ADK_TYPICAL_ON_CHIP_LOAD [all_outputs]
-
-# This constraint sets the input drive strength of the input pins of
-# your design. We specifiy a specific standard cell which models what
-# would be driving the inputs. This should usually be a small inverter
-# which is reasonable if another block of on-chip logic is driving
-# your inputs.
-
-set_driving_cell -no_design_rule \
-  -lib_cell $ADK_DRIVING_CELL [all_inputs]
-
-# Make all signals limit their fanout
-
-set_max_fanout 10 $design_name
-
-# Make all signals meet good slew
-
-set_max_transition [expr 0.050*1000] $design_name
-
-########################################################################
-# FROM MEM TILE (mostly)
-########################################################################
-
-# Passthrough clock inputs
+#=========================================================================
+# Define Passthrough Signals
+#=========================================================================
+# -- Passthrough clock
 set pt_clk_in [get_ports clk_pass_through]
-
-# Passthrough clock outputs
 set pt_clk_out [get_ports clk*out*]
 
-# Other Passthrough inputs
+# -- Passthrough others
 set pt_inputs [get_ports " \
     stall \
     config_config_data* \
@@ -61,8 +43,6 @@ set pt_inputs [get_ports " \
     config_write* \
     reset \
     flush"]
-
-# Other Passthrough Outputs
 set pt_outputs [get_ports " \
     stall_out \
     config_out_config_data* \
@@ -72,107 +52,121 @@ set pt_outputs [get_ports " \
     reset_out \
     flush_out"]
 
-# read_config_data passthrough signals
-# List these seprately because the constraints are slightly different
-# (more relaxed) than for the other pt signals
+# -- Passthrough read_configread_config_data passthrough signals
+#    More relaxed than the other passthrough signals
 set pt_read_data_inputs [get_ports read_config_data_in]
 set pt_read_data_outputs [get_ports read_config_data]
 
-# Now rip this driving cell off of our passthrough signals
-# We are going to use an input/output slew and tighten a bit
-remove_driving_cell $pt_clk_in
-remove_driving_cell $pt_inputs
-remove_driving_cell $pt_read_data_inputs
+#=========================================================================
+# Remove the driving cell from the passthrough signals
+# (We are going to use an input/output slew and tighten a bit)
+#=========================================================================
+remove_driving_cell ${pt_clk_in}
+remove_driving_cell ${pt_inputs}
+remove_driving_cell ${pt_read_data_inputs}
 
-# Drive passthru ports with a particular buffer
-#set_driving_cell -lib_cell BUFFD2BWP16P90 clk_pass_through
-# set_input_delay constraints for input ports
-#
-# Constrain INPUTS
-# - make this non-zero to avoid hold buffers on input-registered designs
+#=========================================================================
+# Input/Output Delay
+#=========================================================================
+# -- IO delay (Normal signals)
 if { $WHICH_SOC == "amber" } {
-set i_delay [expr 0.2 * ${clock_period}]
+    set i_delay [expr 0.2 * ${clock_period}]
 } else {
-set i_delay [expr 0.3 * ${clock_period}]
+    set i_delay [expr 0.3 * ${clock_period}]
 }
-set_input_delay -clock ${clock_name} ${i_delay} [all_inputs -no_clocks]
-# Pass through should have no input delay
-# Fix config input delay to specific value
-set pt_i_delay [expr 0.700*1000]
-set_input_delay -clock ${clock_name} ${pt_i_delay} $pt_clk_in
-set_input_delay -clock ${clock_name} ${pt_i_delay} $pt_inputs
-set_input_delay -clock ${clock_name} ${pt_i_delay} $pt_read_data_inputs
-
-# Constrain OUTPUTS
-# set_output_delay constraints for output ports
 set o_delay [expr 0.0 * ${clock_period}]
+
+# -- IO delay (Passthrough signals)
+# [input delay] Worst case of input delay
+#               = max_diff(pt_clk, pt_data) * (Array Height-1)
+#               This is a result after trail & error
+# [output delay] For passthrough signals there is no point to add output delay
+#                because we will constraint them using max/min delay
+set pt_i_delay 700
+set pt_o_delay 0
+
+# -- Apply IO delay to general signals
+set_input_delay  -clock ${clock_name} ${i_delay} [all_inputs -no_clocks]
 set_output_delay -clock ${clock_name} ${o_delay} [all_outputs]
-# Pass through should have no output delay
-set_output_delay -clock ${clock_name} 0 $pt_clk_out
-set_output_delay -clock ${clock_name} 0 $pt_outputs
-set_output_delay -clock ${clock_name} 0 $pt_read_data_outputs
 
-# Set timing on pass through clock
-# Set clock min delay and max delay
-set clock_min_delay 0
-set clock_max_delay [expr 0.05*1000]
-set_min_delay -from $pt_clk_in -to $pt_clk_out [expr ${clock_min_delay} + ${pt_i_delay} + ${o_delay}]
-set_max_delay -from $pt_clk_in -to $pt_clk_out [expr ${clock_max_delay} + ${pt_i_delay} + ${o_delay}]
+# -- Apply IO delay to passthrough signals
+set_input_delay  -clock ${clock_name} ${pt_i_delay} ${pt_clk_in}
+set_input_delay  -clock ${clock_name} ${pt_i_delay} ${pt_inputs}
+set_input_delay  -clock ${clock_name} ${pt_i_delay} ${pt_read_data_inputs}
+set_output_delay -clock ${clock_name} ${pt_o_delay} ${pt_clk_out}
+set_output_delay -clock ${clock_name} ${pt_o_delay} ${pt_outputs}
+set_output_delay -clock ${clock_name} ${pt_o_delay} ${pt_read_data_outputs}
 
-# Min and max delay a little more than our clock
-#set min_w_in [expr ${clock_max_delay} + ${i_delay}]
-set min_w_in [expr ${clock_max_delay} + ${pt_i_delay} + ${o_delay}]
-set_min_delay -to $pt_outputs ${min_w_in}
-set_min_delay -to $pt_read_data_outputs ${min_w_in}
+#=========================================================================
+# Max/Min Delay
+#=========================================================================
+# -- pt_clk
+set min_delay_pt_clk [expr ${pt_i_delay} + ${pt_o_delay} + 0]
+set max_delay_pt_clk [expr ${pt_i_delay} + ${pt_o_delay} + 50]
+set_min_delay -from ${pt_clk_in} -to ${pt_clk_out} ${min_delay_pt_clk}
+set_max_delay -from ${pt_clk_in} -to ${pt_clk_out} ${max_delay_pt_clk}
 
-# Pass through (not clock) timing margin
-set alt_passthru_margin [expr 0.03*1000]
-set alt_passthru_max [expr ${min_w_in} + ${alt_passthru_margin}]
-set_max_delay -to $pt_outputs ${alt_passthru_max}
-# This doesn't need to be as tight
-set rd_cfg_margin [expr 0.300*1000]
-set_max_delay -from $pt_read_data_inputs -to $pt_read_data_outputs [expr ${rd_cfg_margin} + ${pt_i_delay} + ${o_delay}]
+# -- pt_general
+set min_delay_pt_general [expr ${max_delay_pt_clk} + 0]
+set max_delay_pt_general [expr ${max_delay_pt_clk} + 30]
+set_min_delay -to ${pt_outputs} ${min_delay_pt_general}
+set_max_delay -to ${pt_outputs} ${max_delay_pt_general}
 
-# 5fF approx load
+# -- pt_read_config
+set min_delay_pt_rd_cfg [expr ${max_delay_pt_clk} + 0]
+set max_delay_pt_rd_cfg [expr ${pt_i_delay} + ${pt_o_delay} + 300]
+set_min_delay                              -to ${pt_read_data_outputs} ${min_delay_pt_rd_cfg}
+set_max_delay -from ${pt_read_data_inputs} -to ${pt_read_data_outputs} ${max_delay_pt_rd_cfg}
+
+#=========================================================================
+# Passthrough load capacitance
+#=========================================================================
 set mark_approx_cap 0.025
+set_load ${mark_approx_cap} ${pt_outputs}
+set_load ${mark_approx_cap} ${pt_read_data_outputs}
+set_load ${mark_approx_cap} ${pt_clk_out}
 
-set_load ${mark_approx_cap} $pt_outputs
-set_load ${mark_approx_cap} $pt_read_data_outputs
-set_load ${mark_approx_cap} $pt_clk_out
-
-# Set max transition on these outputs as well
-set max_trans_passthru [expr 0.020*1000]
-set_max_transition ${max_trans_passthru} $pt_outputs
-set_max_transition ${max_trans_passthru} $pt_read_data_outputs
-set_max_transition ${max_trans_passthru} $pt_clk_out
+#=========================================================================
+# Signal Transition
+#=========================================================================
+set max_trans_passthru 20
+set_max_transition ${max_trans_passthru} ${pt_outputs}
+set_max_transition ${max_trans_passthru} ${pt_read_data_outputs}
+set_max_transition ${max_trans_passthru} ${pt_clk_out}
 
 # Set input transition to match the max transition on outputs
-set_input_transition ${max_trans_passthru} $pt_clk_in
-set_input_transition ${max_trans_passthru} $pt_inputs
-set_input_transition ${max_trans_passthru} $pt_read_data_inputs
+set_input_transition ${max_trans_passthru} ${pt_clk_in}
+set_input_transition ${max_trans_passthru} ${pt_inputs}
+set_input_transition ${max_trans_passthru} ${pt_read_data_inputs}
 
+#=========================================================================
+# Multi-Cycle Path
+#=========================================================================
 # Relax config_addr -> read_config_data path
 set_multicycle_path 2 -from [get_ports config_config_addr*] -to [get_ports read_config_data] -setup
 set_multicycle_path 1 -from [get_ports config_config_addr*] -to [get_ports read_config_data] -hold
 set_multicycle_path 2 -to [get_ports read_config_data* -filter direction==out] -setup
 set_multicycle_path 1 -to [get_ports read_config_data* -filter direction==out] -hold
 
+#=========================================================================
+# Switch Box Delay
+#=========================================================================
 ## Constrain SB to ~200 ps
-set sb_delay [expr 0.210*1000]
+set sb_delay 200
 if { $WHICH_SOC == "amber" } {
-# Use this first command to constrain all feedthrough paths to just the desired SB delay
-set_max_delay -from SB*_IN_* -to SB*_OUT_* [expr ${sb_delay} + ${i_delay} + ${o_delay}]
-# Then override the rest of the paths to be full clock period
-set_max_delay -from SB*_IN_* -to SB*_OUT_* -through [get_pins [list CB*/* DECODE*/* PE_inst0*/* FEATURE*/*]] ${clock_period}
+    # Use this first command to constrain all feedthrough paths to just the desired SB delay
+    set_max_delay -from SB*_IN_* -to SB*_OUT_* [expr ${sb_delay} + ${i_delay} + ${o_delay}]
+    # Then override the rest of the paths to be full clock period
+    set_max_delay -from SB*_IN_* -to SB*_OUT_* -through [get_pins [list CB*/* DECODE*/* PE_inst0*/* FEATURE*/*]] ${clock_period}
 } else {
-# Use this first command to constrain all feedthrough paths to just the desired SB delay
-set_max_delay -from [get_ports SB* -filter direction==in] -to [get_ports SB* -filter direction==out] [expr ${sb_delay} + ${i_delay} + ${o_delay}]
-# Then override the rest of the paths to be full clock period
-set_max_delay -from [get_ports SB* -filter direction==in] -to [get_ports SB* -filter direction==out] -through [get_pins [list CB*/* DECODE*/* PE_inst0*/* FEATURE*/*]] ${clock_period}
+    # Use this first command to constrain all feedthrough paths to just the desired SB delay
+    set_max_delay -from [get_ports SB* -filter direction==in] -to [get_ports SB* -filter direction==out] [expr ${sb_delay} + ${i_delay} + ${o_delay}]
+    # Then override the rest of the paths to be full clock period
+    set_max_delay -from [get_ports SB* -filter direction==in] -to [get_ports SB* -filter direction==out] -through [get_pins [list CB*/* DECODE*/* PE_inst0*/* FEATURE*/*]] ${clock_period}
 }
 
 ########################################################################
-# END
+# Misc.
 ########################################################################
 
 if { $WHICH_SOC == "amber" } {
@@ -192,8 +186,9 @@ if $::env(PWR_AWARE) {
     set_dont_touch_network [get_ports *SB* -filter "direction==in"] -no_propagate
 } 
 
-# False paths
-
+#=========================================================================
+# False Path
+#=========================================================================
 set_false_path -to [get_ports hi]
 set_false_path -to [get_ports lo]
 set_false_path -from [get_ports tile_id]
@@ -202,10 +197,9 @@ set_false_path -from [get_ports tile_id]
 # register because we assume the register's value is constant during a read. 
 set_false_path -through [get_cells -hier *config_reg_*] -to [get_ports read_config_data]
 
-#set_tlu_plus_files -max_tluplus  $tluplus_max \
-#                   -min_tluplus  $tluplus_min \
-#                   -tech2itf_map $tluplus_map
-
+#=========================================================================
+# Dont Touch (for later constraints)
+#=========================================================================
 # Preserve the RMUXes so that we can easily constrain them later
 set rmux_cells [get_cells -hier -regexp .*RMUX_.*_sel_(inst0|value)]
 set_dont_touch $rmux_cells true
