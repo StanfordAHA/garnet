54508d59133d76d38401d155a68af1b53b89b37b
Po-Han Chen <pohan@stanford.edu>
2023-10-20 21:40:15 -0700

    oops, why are you here? you shouldn't be here!

--- mflowgen/glb_tile/custom-init/outputs/ln.sh
+++
@@ -1,6 +0,0 @@
-ln -s ../../../common/init-scripts/create-boundary-blockage.tcl     .
-ln -s ../../../common/init-scripts/innovus-pnr-config.tcl           .
-ln -s ../../../common/init-scripts/create-rows.tcl                  .
-ln -s ../../../common/init-scripts/add-tracks.tcl                   .
-ln -s ../../../common/init-scripts/insert-input-antenna-diodes.tcl  .
-ln -s ../../../common/init-scripts/create-special-grid.tcl          .
