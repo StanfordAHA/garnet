461cc603039331afa2b177fec29f4edbc2b269fe
Po-Han Chen <pohan@stanford.edu>
2023-10-27 17:59:54 -0700

    update wrong die ring width

--- mflowgen/common/init-fullchip/outputs/floorplan.tcl
+++ mflowgen/common/init-fullchip/outputs/floorplan.tcl
@@ -23,8 +23,14 @@ set hori_pitch [dbGet top.fPlan.coreSite.size_x]
 
 set fp_width          3924.72
 set fp_height         4084.92
-set fp_margin_width   [expr 71.28 + 8 * $hori_pitch]
-set fp_margin_height  [expr 71.82 + 2 * $vert_pitch]
+# set fp_margin_width   [expr 71.28 + 8 * $hori_pitch]
+# set fp_margin_height  [expr 71.82 + 2 * $vert_pitch]
+set die_ring_hori     16.2
+set die_ring_vert     17.64
+set prs_width         58.32
+set prs_height        57.96
+set fp_margin_width   [expr $die_ring_hori + $prs_width + 8 * $hori_pitch]
+set fp_margin_height  [expr $die_ring_vert + $prs_height + 2 * $vert_pitch]
 
 floorPlan \
     -coreMarginsBy die \
