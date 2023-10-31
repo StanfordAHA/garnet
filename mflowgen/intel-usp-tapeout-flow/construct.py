275da9de8dc9a6ea2c4ea72c276de57d0d0433f3
Po-Han Chen <pohan@stanford.edu>
2023-10-31 15:20:23 -0700

    add a parameter to controll the number of threads used in verification jobs

--- mflowgen/intel-usp-tapeout-flow/construct.py
+++ mflowgen/intel-usp-tapeout-flow/construct.py
@@ -28,7 +28,8 @@ def construct():
     'adk_view'            : 'multivt',
     'adk_stdcell'         : 'b15_7t_108pp',
     'design_name'         : 'GarnetSOC_pad_frame',
-    'intel_database_name' : '8su216a_tma2_0a'
+    'intel_database_name' : '8su216a_tma2_0a',
+    'nthreads'            : 16
   }
 
   #-----------------------------------------------------------------------
