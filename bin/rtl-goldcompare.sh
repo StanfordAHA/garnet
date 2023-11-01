1643af04bea2740a4160593352063f5478ba2a86
steveri <steveri@steveri.com>
2023-11-01 11:22:18 -0700

    still trying to keep up w onyx rtl changes

--- bin/rtl-goldcompare.sh
+++ bin/rtl-goldcompare.sh
@@ -19,10 +19,13 @@ EXAMPLES
 # Need 's/_O._value_O/...' because generator seems e.g. to randomly assign
 # the equivalent values 'PE_onyx_inst_onyxpeintf_O3_value_O' and '...O4_value_O' :(
 
+
+#     sed 's/clk_gate_unq/clk_gate/'    | # Treat clk_gate_unq same as clk_gate :(
+
 function vcompare {
     cat $1 |
     sed 's/_O._value_O/_Ox_value_O/g' | # Treat all zeroes as equivalent
-    sed 's/clk_gate_unq/clk_gate/'    | # Treat clk_gate_unq same as clk_gate :(
+    sed 's/clk_gate0 glb_clk_gate/clk_gate glb_clk_gate' | # Treat gate0 same as gate :(
     sed 's/,$//'           | # No trailing commas
     sed 's/unq[0-9*]/unq/' | # Canonicalize unq's
     sed '/^\s*$/d'         | # No blank lines
