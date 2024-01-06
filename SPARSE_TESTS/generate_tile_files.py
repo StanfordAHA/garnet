fed4647ffd7e3bc0a1ba2ea0c60235ac0cc2bec9
Akhilesh Balasingam <akhileshvb03@gmail.com>
2024-01-06 10:26:59 -0800

    tile files gen

--- SPARSE_TESTS/generate_tile_files.py
+++ SPARSE_TESTS/generate_tile_files.py
@@ -5,16 +5,17 @@ import os
 import sys
 
 app_name = sys.argv[1]
+dataset = sys.argv[2]
 
-tiles = glob.glob(f"./{app_name}_tile*")
+tiles = glob.glob(f"./{app_name}_{app_name}-{dataset}_tile*")
 
 print(tiles)
 num_tiles = len(tiles)
 print("there are ", num_tiles, "tiles")
 
 for i in range(num_tiles):
-    shutil.copy(f"{app_name}_tile{i}/GLB_DIR/{app_name}_combined_seed_tile{i}/output_gold_0.npy", f"{app_name}_tile{i}/GLB_DIR/{app_name}_combined_seed_tile{i}/bin")
-    shutil.copytree(f"{app_name}_tile{i}/GLB_DIR/{app_name}_combined_seed_tile{i}/bin", f"/aha/garnet/SPARSE_TESTS/tile{i}")
+    shutil.copy(f"{app_name}_{app_name}-{dataset}_tile{i}/GLB_DIR/{app_name}_combined_seed_{app_name}-{dataset}_tile{i}/output_gold_0.npy", f"{app_name}_{app_name}-{dataset}_tile{i}/GLB_DIR/{app_name}_combined_seed_{app_name}-{dataset}_tile{i}/bin")
+    shutil.copytree(f"{app_name}_{app_name}-{dataset}_tile{i}/GLB_DIR/{app_name}_combined_seed_{app_name}-{dataset}_tile{i}/bin", f"/aha/garnet/SPARSE_TESTS/tile{i}")
 
 
 # for tile in tiles:
@@ -25,7 +26,7 @@ for i in range(num_tiles):
 tile_directories = glob.glob("tile*")
 
 # Create the destination directory if it doesn't exist
-destination_dir = "../tiles_chip_test"
+destination_dir = "../codegen/tiles_chip_test"
 os.makedirs(destination_dir, exist_ok=True)
 
 # Move the tile directories to the destination directory
