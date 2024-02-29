8c2142f189b576a956d8661748dbf154d8896721
steveri <steveri@stanford.edu>
2024-02-29 13:59:26 -0800

    Better pruning of docker images for gen_rtl.sh (#1051)
    * better docker pruning in gen_rtl

--- mflowgen/common/rtl/gen_rtl.sh
+++ mflowgen/common/rtl/gen_rtl.sh
@@ -96,14 +96,6 @@ if [ "$use_container" == True ]; then
       mkdir -p aha; cd aha
       ########################################################################
 
-      # Prune docker images...
-      echo '--- gen_rtl docker prune BEGIN' `date +%H:%M`
-      docker image prune -f -a --filter "until=6h" --filter=label='description=garnet' || true
-
-      echo ""; echo "After pruning:"; echo ""
-      docker images; echo ""
-      docker ps    ; echo ""
-
       # Choose a docker image; can set via "rtl_docker_image" parameter
       default_image="stanfordaha/garnet:latest"
 
@@ -144,6 +136,16 @@ if [ "$use_container" == True ]; then
 
       # MAKE SURE the docker container gets killed when this script dies.
       trap "docker kill $container_name" EXIT
+
+      # Delete all dangling images created more than 6 hours ago.
+      # Notice that we prune images *after* starting container (pruner
+      # will refuse to kill image w/ active container)
+
+      echo '--- gen_rtl docker prune BEGIN' `date +%H:%M`
+      printf "\nBefore pruning:\n\n";  docker images; echo "";  docker ps; echo ""
+      docker image prune -f -a --filter "until=6h" --filter=label='description=garnet' || true
+      printf "\nAfter pruning:\n\n";   docker images; echo "";  docker ps; echo ""
+      echo '--- Continuing...'
 
       if [ "$use_local_garnet" == True ]; then
         docker exec $container_name /bin/bash -c "rm -rf /aha/garnet"
