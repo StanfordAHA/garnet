90db836f838f66aa6df3a11e81a573003fcdd90f
steveri <steveri@steveri.com>
2024-03-20 14:45:07 -0700

    still iterating; this one should fail again

--- bin/rtl-goldcompare.sh
+++ bin/rtl-goldcompare.sh
@@ -22,23 +22,26 @@ EXAMPLES
 
 #     sed 's/clk_gate_unq/clk_gate/'    | # Treat clk_gate_unq same as clk_gate :(
 
-set -e          # FAIL if any single command fails from here down
 set -o pipefail # FAIL if any command in any pipe fails from here down
 
-function vcompare { (
+function vcompare {
     cat $1 |
     sed 's/_O._value_O/_Ox_value_O/g'                     | # Treat all zeroes as equivalent
-    sed 's/clk_gate0 glb_clk_gate/clk_gate glb_clk_gate/' | # Treat gate0 same as gate :(
+    sed 's/clk_gate0 glb_clk_gate/clk_gate glb_clk_gate' | # Treat gate0 same as gate :(
     sed 's/,$//'           | # No trailing commas
     sed 's/unq[0-9*]/unq/' | # Canonicalize unq's
     sed '/^\s*$/d'         | # No blank lines
     sort                   | # Out-of-order is okay
     cat
-) }
+}
 
 # FAIL if something is wrong with vcompare
-vcompare $1 > /dev/null
-vcompare $2 > /dev/null
+errmsg='vcompare script failed in rtl-goldcompare.sh'
+if ! vcompare $1 > /dev/null; then echo $errmsg; exit 13; fi
+if ! vcompare $2 > /dev/null; then echo $errmsg; exit 13; fi
+
+# vcompare $1 > /dev/null || echo 'vcompare script failed in rtl-goldcompare.sh'
+# vcompare $2 > /dev/null || echo 'vcompare script failed in rtl-goldcompare.sh'
 
 # Do the final compare
 diff -Bb -I Date <(vcompare $1) <(vcompare $2)
