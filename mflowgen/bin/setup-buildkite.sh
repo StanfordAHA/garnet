cc41baa930b32c2658fcab7358713235d93beb5a
steveri <steveri@steveri.com>
2024-08-26 07:53:58 -0700

    calibre/mentor OA weirdness vs. rocket

--- mflowgen/bin/setup-buildkite.sh
+++ mflowgen/bin/setup-buildkite.sh
@@ -319,17 +319,18 @@ source $garnet/mflowgen/setup-garnet.sh
 echo Recheck python/pip versions
 check_pyversions
 
-##############################################################################
-# OA_HOME weirdness
-# OA_HOME *** WILL DRIVE ME MAD!!! ***
-echo "--- UNSET OA_HOME"
-echo ""
-echo "buildkite (but not arm7 (???)) errs if OA_HOME is set"
-echo "BEFORE: OA_HOME=$OA_HOME"
-echo "unset OA_HOME"
-unset OA_HOME
-echo "AFTER:  OA_HOME=$OA_HOME"
-echo ""
+# 08/2024 Moving this to setup-garnet.sh!!!
+# ##############################################################################
+# # OA_HOME weirdness
+# # OA_HOME *** WILL DRIVE ME MAD!!! ***
+# echo "--- UNSET OA_HOME"
+# echo ""
+# echo "buildkite (but not arm7 (???)) errs if OA_HOME is set"
+# echo "BEFORE: OA_HOME=$OA_HOME"
+# echo "unset OA_HOME"
+# unset OA_HOME
+# echo "AFTER:  OA_HOME=$OA_HOME"
+# echo ""
 
 # Okay let's check and see what we got.
 echo "--- REQUIREMENTS CHECK, sourcing $garnet/bin/requirements_check.sh"; echo ""
--- mflowgen/setup-garnet.sh
+++ mflowgen/setup-garnet.sh
@@ -38,6 +38,22 @@ else
     [ "$WHICH_SOC" == "amber" ] || module load calibre/2021.2_18
 fi
 
+module load prime/latest
+module load ext/latest
+
+##############################################################################
+# OA_HOME weirdness -- 08/2024 moved this here verbatim from setup-buildkite.sh
+# OA_HOME *** WILL DRIVE ME MAD!!! ***
+echo "--- UNSET OA_HOME"
+echo ""
+echo "buildkite (but not arm7 (???)) errs if OA_HOME is set"
+echo "BEFORE: OA_HOME=$OA_HOME"
+echo "unset OA_HOME"
+unset OA_HOME
+echo "AFTER:  OA_HOME=$OA_HOME"
+echo ""
+
+
 # 08/2024 Joe upgraded system to redhat-compatible "rocket" i.e.
 # 'cat /etc/redhat-release' yields the string "Rocky Linux release 8.10 (Green Obsidian)"
 # calibre binaries die because they invoke a script 'calibre_host_info' that expects
@@ -47,11 +63,16 @@ fi
 
 # We can prevent this (maybe?) by setting an environment variable
 # 'export USE_CALIBRE_VCO=aoi' that shortcuts the OS check.
-export USE_CALIBRE_VCO=aoi
 
-module load prime/latest
-module load ext/latest
+test -e /etc/os-release && source /etc/os-release  # Sets os-related vars including ID
+[ "$ID" == "rocky" ] && export USE_CALIBRE_VCO=aoi
+
+# 08/2024 Joe upgraded some of the machines from rhel 7 to rocket 8
+# and oh boy did that mess things up
 
 # Let's try a thing maybe
-unset OA_UNSUPPORTED_PLAT
-export OA_HOME=/cad/cadence/ICADVM20.10.330/oa_v22.60.090
+test -e /etc/os-release && source /etc/os-release  # Sets os-related vars including ID
+if [ "$ID" == "rocky" ]; then
+    unset OA_UNSUPPORTED_PLAT
+    export OA_HOME=/cad/cadence/ICADVM20.10.330/oa_v22.60.090
+fi
