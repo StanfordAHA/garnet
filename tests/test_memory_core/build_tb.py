d0044fe7439ef5e2ecbe611c473c799226cf28fe
Bo Wun Cheng <bwcheng@stanford.edu>
2024-09-09 11:21:12 -0700

    added software gold generation code for fp_matmul and fp_spmm

--- tests/test_memory_core/build_tb.py
+++ tests/test_memory_core/build_tb.py
@@ -65,6 +65,7 @@ from lake.top.fiber_access import FiberAccess
 from lake.modules.onyx_pe import OnyxPE
 from lassen.sim import PE_fc
 from lassen.utils import float2bfbin, bfbin2float
+from lassen.float import *
 from peak import family
 from lake.modules.scanner_pipe import ScannerPipe
 import tempfile
@@ -2174,7 +2175,7 @@ def software_gold(app_name, matrix_tmp_dir, give_tensor=False, print_inputs=None
         output_matrix = numpy.maximum(0,(numpy.matmul(b_mat, c_mat, dtype=numpy.int16, casting='unsafe')))
         output_format = "CSF"
         output_name = "x"
-    elif 'matmul_ijk.gv' in app_name:
+    elif 'matmul_ijk.gv' in app_name and 'fp' not in app_name:
         b_mat = get_tensor(input_name='B', shapes=[10, 12], give_tensor=give_tensor, tmp_dir=matrix_tmp_dir,
                             dump=matrix_tmp_dir, suffix=suffix, clean=clean, tensor_ordering=tensor_orderings['B'],
                             sparsity=0.7)
@@ -2190,6 +2191,67 @@ def software_gold(app_name, matrix_tmp_dir, give_tensor=False, print_inputs=None
         print("\nCMAT_TRANS is: \n", c_mat_trans)
 
         output_matrix = numpy.matmul(b_mat, c_mat_trans, dtype=numpy.uint16, casting='unsafe')
+        output_format = "CSF"
+        output_name = "X"
+    elif 'fp_matmul_ijk.gv' in app_name:
+        b_mat = get_tensor(input_name='B', shapes=[10, 12], give_tensor=give_tensor, tmp_dir=matrix_tmp_dir,
+                            dump=matrix_tmp_dir, suffix=suffix, clean=clean, tensor_ordering=tensor_orderings['B'],
+                            sparsity=0.7, use_fp=True)
+        # for seed: gets a C has ss01 then transposes it to make it a valid ss10
+        # for suitesparse: reads in C (ss10) transposed and then we transpose again to match the files we got in python to get gold
+        c_mat = get_tensor(input_name='C', shapes=[8, 12], give_tensor=give_tensor, tmp_dir=matrix_tmp_dir,
+                            dump=matrix_tmp_dir, suffix=suffix, clean=False, tensor_ordering=tensor_orderings['C'],
+                            sparsity=0.7, use_fp=True)
+        c_mat_trans = numpy.transpose(c_mat)
+
+        output_matrix = numpy.zeros((b_mat.shape[0], c_mat_trans.shape[1]), dtype=numpy.float32)
+        FPU = fpu.FPU_fc(PyFamily())
+        fpu_func = FPU()
+        for i in range(0, output_matrix.shape[0]):
+            for j in range(0, output_matrix.shape[1]):
+                partial_sum = float2bfbin(0.0)
+                partial_sum = Data(int(partial_sum, 2))
+                for k in range(0, b_mat.shape[1]):
+                    b_val = float2bfbin(b_mat[i][k])
+                    b_val = Data(int(b_val, 2))
+                    c_val = float2bfbin(c_mat_trans[k][j])
+                    c_val = Data(int(c_val, 2))
+                    partial_prod, _, _ = fpu_func(fpu.FPU_t.FP_mul, b_val, c_val)
+                    partial_sum, _, _ = fpu_func(fpu.FPU_t.FP_add, partial_sum, partial_prod)
+                output_matrix[i][j] = bfbin2float("{:016b}".format(int(partial_sum)))
+       
+        output_format = "CSF"
+        output_name = "X"
+    elif 'fp_spmm_ijk.gv' in app_name:
+        b_mat = get_tensor(input_name='B', shapes=[30, 30], give_tensor=give_tensor, tmp_dir=matrix_tmp_dir,
+                            dump=matrix_tmp_dir, suffix=suffix, clean=clean, tensor_ordering=tensor_orderings['B'],
+                            sparsity=0.7, use_fp=True)
+        # for seed: gets a C has ss01 then transposes it to make it a valid ss10
+        # for suitesparse: reads in C (ss10) transposed and then we transpose again to match the files we got in python to get gold
+        c_mat = get_tensor(input_name='C', shapes=[30, 30], give_tensor=give_tensor, tmp_dir=matrix_tmp_dir,
+                            dump=matrix_tmp_dir, suffix=suffix, clean=False, tensor_ordering=tensor_orderings['C'],
+                            sparsity=0.0, use_fp=True, format="UNC")
+        c_mat_trans = numpy.transpose(c_mat)
+
+        input_dims['B'] = tuple(b_mat.shape)
+        input_dims['C'] = tuple(c_mat.shape)
+
+        output_matrix = numpy.zeros((b_mat.shape[0], c_mat_trans.shape[1]), dtype=numpy.float32)
+        FPU = fpu.FPU_fc(PyFamily())
+        fpu_func = FPU()
+        for i in range(0, output_matrix.shape[0]):
+            for j in range(0, output_matrix.shape[1]):
+                partial_sum = float2bfbin(0.0)
+                partial_sum = Data(int(partial_sum, 2))
+                for k in range(0, b_mat.shape[1]):
+                    b_val = float2bfbin(b_mat[i][k])
+                    b_val = Data(int(b_val, 2))
+                    c_val = float2bfbin(c_mat_trans[k][j])
+                    c_val = Data(int(c_val, 2))
+                    partial_prod, _, _ = fpu_func(fpu.FPU_t.FP_mul, b_val, c_val)
+                    partial_sum, _, _ = fpu_func(fpu.FPU_t.FP_add, partial_sum, partial_prod)
+                output_matrix[i][j] = bfbin2float("{:016b}".format(int(partial_sum)))
+       
         output_format = "CSF"
         output_name = "X"
     # MO: new ikj test 
