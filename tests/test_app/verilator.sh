3d2b28a9fd7ce3094ab3798ceac6e1ab4f4784b1
steveri <steveri@steveri.com>
2024-10-11 11:09:11 -0700

    verilator.sh: added flags for wave tracing and debugging

--- tests/test_app/verilator.sh
+++ tests/test_app/verilator.sh
@@ -41,7 +41,10 @@ set -x
 
 # Note: libcgra.so must be relative to ./obj_dir :(
 # TODO: find a better way to do this :(
-verilator $warn --timing --cc --exe vfiles/CGRA.cpp \
+verilator $warn --trace --clk top.clk \
+  -CFLAGS -DVL_DEBUG=1 \
+  --timing --cc --exe \
+  vfiles/CGRA.cpp \
   --top-module top \
   vfiles/global_buffer_param.svh \
   $VFILES \
