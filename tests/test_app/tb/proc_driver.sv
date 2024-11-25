4631c7afd6649c70aa70401804d8120b2991379e
steveri <steveri@steveri.com>
2024-11-25 14:20:04 -0800

    proc_driver.sv ready

--- tests/test_app/tb/proc_driver.sv
+++ tests/test_app/tb/proc_driver.sv
@@ -1,3 +1,16 @@
+// Global signals to replace/enhance class automatics
+`define DBG_PROCDRIVER 0  // Set to '1' for debugging
+
+semaphore proc_lock; 
+initial proc_lock = new(1);
+
+bit [GLB_ADDR_WIDTH-1:0] start_addr;
+bit [GLB_ADDR_WIDTH-1:0] cur_addr;
+bitstream_t              bs_q;
+
+bit [GLB_ADDR_WIDTH-1:0]  ProcDriver_write_waddr;
+bit [BANK_DATA_WIDTH-1:0] ProcDriver_write_wdata;
+
 class ProcDriver;
     vProcIfcDriver vif;
     semaphore proc_lock;
@@ -18,6 +31,8 @@ task ProcDriver::write_bs(int start_addr, bitstream_t bs_q);
     bit [GLB_ADDR_WIDTH-1:0] cur_addr = start_addr;
     proc_lock.get(1);
     foreach (bs_q[i]) begin
+        ProcDriver_write_waddr = cur_addr;
+        ProcDriver_write_wdata = bs_q[i];
         write(cur_addr, bs_q[i]);
         cur_addr += 8;
     end
@@ -25,6 +40,10 @@ task ProcDriver::write_bs(int start_addr, bitstream_t bs_q);
     repeat (10) @(vif.cbd);
     proc_lock.put(1);
 endtask
+
+data_array_t data_q;
+bit [BANK_DATA_WIDTH-1:0] bdata;
+int size;
 
 // TODO: Change it to const ref
 task ProcDriver::write_data(int start_addr, data_array_t data_q);
@@ -44,6 +63,8 @@ task ProcDriver::write_data(int start_addr, data_array_t data_q);
         end else begin
             data = {data_q[i+3], data_q[i+2], data_q[i+1], data_q[i]};
         end
+        ProcDriver_write_waddr = cur_addr;
+        ProcDriver_write_wdata = data;
         write(cur_addr, data);
         cur_addr += 8;
     end
@@ -64,39 +85,54 @@ task ProcDriver::write(int addr, bit [BANK_DATA_WIDTH-1:0] data);
     vif.cbd.wr_data <= 0;
 endtask
 
+int num_words, num_trans;
 task ProcDriver::read_data(int start_addr, ref data_array_t data_q);
     int num_words = data_q.size();
     int num_trans = (num_words + 3) / 4;
     proc_lock.get(1);
     fork
+        // Process 1 initiates read by setting rd_en HIGH and feeding addresses one per cycle
+        // Process 2 unloads the data by waiting for data_valid signal and then reading data one per cycle
         begin
+            $display("Set     %0d consecutive addresses BEGIN", num_trans);
+            if (`DBG_PROCDRIVER) $display("Start reading at address %08x\n", start_addr);
             @(vif.cbd);
             for (int i = 0; i < num_trans; i++) begin
                 vif.cbd.rd_en   <= 1'b1;
                 // address increases by 8 every write
                 vif.cbd.rd_addr <= (start_addr + 8 * i);
+                if (`DBG_PROCDRIVER) $display("[%0t] Set addr = %08x", $time, p_ifc.rd_addr);
                 @(vif.cbd);
             end
             vif.cbd.rd_en   <= 0;
             vif.cbd.rd_addr <= 0;
+            $display("Set %d consecutive addresses END", num_trans);  // 4027ns
         end
         begin
+            $display("Offload %0d data chunks BEGIN", num_trans);  // 3002ns
             for (int i = 0; i < num_trans; i++) begin
                 wait (vif.cbd.rd_data_valid);
 
+                if (`DBG_PROCDRIVER) $display("\n[%0t] FOO got data word %0x", $time, p_ifc.rd_data);
                 data_q[i*4] = vif.cbd.rd_data & 'hFFFF;
+                if (`DBG_PROCDRIVER) $display("[%0t] -- 0 data_q[%04d] <= %0x", $time, i*4, data_q[i*4]);
                 if ((i * 4 + 1) < num_words) begin
                     data_q[i*4+1] = (vif.cbd.rd_data & (('hFFFF) << 16)) >> 16;
+                    if (`DBG_PROCDRIVER) $display("[%0t] -- 1 data_q[%04d] <= %0x", $time, i*4+1, data_q[i*4+1]);
                 end
                 if ((i * 4 + 2) < num_words) begin
                     data_q[i*4+2] = (vif.cbd.rd_data & (('hFFFF) << 32)) >> 32;
+                    if (`DBG_PROCDRIVER) $display("[%0t] -- 2 data_q[%04d] <= %0x", $time, i*4+2, data_q[i*4+2]);
                 end
                 if ((i * 4 + 3) < num_words) begin
                     data_q[i*4+3] = (vif.cbd.rd_data & (('hFFFF) << 48)) >> 48;
+                    if (`DBG_PROCDRIVER) $display("[%0t] -- 3 data_q[%04d] <= %0x", $time, i*4+3, data_q[i*4+3]);
                 end
 
                 @(vif.cbd);
             end
+            $display("Offload %d data chunks END", num_trans);  // 4027ns
+            $display("First output word is maybe...0x%04x",  data_q[0]);
         end
     join
 
