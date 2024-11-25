d9aaffed595bfd3983eb8d4d1e7c602ad37c72e5
steveri <steveri@steveri.com>
2024-11-25 08:18:49 -0800

    first round of changes to axil_driver

--- tests/test_app/tb/axil_driver.sv
+++ tests/test_app/tb/axil_driver.sv
@@ -1,3 +1,19 @@
+`define DBG_AXILDRIVER 0  // Set to '1' for debugging
+
+// If protocol is broken, can shorten SLAVE_WAIT timeout for debugging (10 is a good number)
+`define SLAVE_WAIT 100
+
+// (VERILATOR PREP; NOT USED YET) ======================================================
+// Can use same global addr and data for everybody because there are only three forks:
+// - one in wait_interrupt has a mem read, but
+//   - addr is same for all forks, and
+//   - data keeps getting overwritten, but we only care about the last one
+// - one in Env_run() is disabled and throws an error if anyone ever tries to use it;
+// - one in ProcDriver_read_data() does not use addr/data for reads
+
+bit [CGRA_AXI_ADDR_WIDTH-1:0] addr;
+bit [CGRA_AXI_DATA_WIDTH-1:0] data;
+
 class AxilDriver;
     vAxilIfcDriver vif;
     semaphore axil_lock;
@@ -21,33 +37,43 @@ task AxilDriver::config_write(Config cfg[]);
 endtask
 
 task AxilDriver::write(bit [CGRA_AXI_ADDR_WIDTH-1:0] addr, bit [CGRA_AXI_DATA_WIDTH-1:0] data);
-    // $display("AXI-Lite Write. Addr: %08h, Data: %08h", addr, data);
+    if (`DBG_AXILDRIVER) $display("[%0t] AXI-Lite Write. Addr: %08h, Data: %08h", $time, addr, data);
+
+    // Note: "slave not ready" timout will occur b/c awready stays false until data is read (below)
+    // awready advances controller state from WAIT(0) to WR_REQ_GLC(1)
+
     axil_lock.get(1);
     @(vif.cbd);
     vif.cbd.awaddr  <= addr;
     vif.cbd.awvalid <= 1'b1;
-    for (int i = 0; i < 100; i++) begin
-        if (vif.cbd.awready == 1) break;
-        @(vif.cbd);
-        if (i == 99) return;  // axi slave is not ready
+
+    // FIXME?? seems like this should happen BEFORE setting awvalid etc. above? First ready, then valid?
+    for (int i = 0; i < `SLAVE_WAIT; i++) begin
+        if (axil_ifc.awready == 1) break;
+        @(posedge axil_ifc.clk);
+        if (i == (`SLAVE_WAIT-1)) $display("axi slave is not ready 1");
     end
     @(vif.cbd);
     vif.cbd.awvalid <= 0;
     @(vif.cbd);
+    // Axi controller should now be in state 1 (WR_REQ_GLC==1, see glc_axi_ctrl.sv) // 132ns
     this.vif.cbd.wdata  <= data;
     this.vif.cbd.wvalid <= 1'b1;
-    for (int i = 0; i < 100; i++) begin
-        if (this.vif.cbd.wready == 1) break;
-        @(vif.cbd);
-        if (i == 99) return;  // axi slave is not ready
+    // Axi controller should now be in state 3 (WR_WAIT)  // 132ns
+    // Then, after one cycle, on to state 4 (WR_RESP)     // 133ns
+    // where it stays until wr_wait_cnt counts from clog2(14) down to zero
+    for (int i = 0; i < `SLAVE_WAIT; i++) begin
+        if (axil_ifc.wready == 1) break;
+        @(posedge axil_ifc.clk);
+        if (i == (`SLAVE_WAIT-1)) $display("axi slave is not ready 2");  // ~152ns
     end
     @(vif.cbd);
     this.vif.cbd.wvalid <= 0;
     this.vif.cbd.bready <= 1'b1;
-    for (int i = 0; i < 100; i++) begin
-        if (this.vif.cbd.bvalid == 1) break;
-        @(vif.cbd);
-        if (i == 99) return;  // axi slave is not ready
+    for (int i = 0; i < `SLAVE_WAIT; i++) begin
+        if (axil_ifc.bvalid == 1) break;
+        @(posedge axil_ifc.clk);
+        if (i == (`SLAVE_WAIT-1)) $display("axi slave is not ready 3");
     end
     @(vif.cbd);
     this.vif.cbd.bready <= 0;
@@ -62,24 +88,24 @@ task AxilDriver::read(bit [CGRA_AXI_ADDR_WIDTH-1:0] addr,
     this.vif.cbd.araddr  <= addr;
     this.vif.cbd.arvalid <= 1'b1;
     this.vif.cbd.rready  <= 1;
-    for (int i = 0; i < 100; i++) begin
-        if (this.vif.cbd.arready == 1) break;
-        @(vif.cbd);
-        if (i == 99) return;  // axi slave is not ready
+    for (int i = 0; i < `SLAVE_WAIT; i++) begin
+        if (axil_ifc.arready == 1) break;
+        @(posedge axil_ifc.clk);
+        if (i == (`SLAVE_WAIT-1)) $display("axi slave is not ready 1");
     end
     @(vif.cbd);
     this.vif.cbd.arvalid <= 0;
     @(vif.cbd);
-    for (int i = 0; i < 100; i++) begin
-        if (this.vif.cbd.rvalid == 1) break;
-        @(vif.cbd);
-        if (i == 99) return;  // axi slave is not ready
+    for (int i = 0; i < `SLAVE_WAIT; i++) begin
+        if (axil_ifc.rvalid == 1) break;
+        @(posedge axil_ifc.clk);
+        if (i == (`SLAVE_WAIT-1)) $display("axi slave is not ready 2");
     end
     data = this.vif.cbd.rdata;
     @(vif.cbd);
     this.vif.cbd.rready <= 0;
     @(vif.cbd);
-    // $display("AXI-Lite Read. Addr: %08h, Data: %08h", addr, data);
+    if (`DBG_AXILDRIVER) $display("[%0t] AXI-Lite Read. Addr: %08h, Data: %08h", $time, addr, data);
     axil_lock.put(1);
 endtask
 
