d2eebc403a34108174848332e0d34b95accd426d
root <mcoduoza@gmail.com>
2024-12-02 08:31:21 -0800

    More detailed manual placer checks

--- global_buffer/io_placement.py
+++ global_buffer/io_placement.py
@@ -151,7 +151,7 @@ def place_io_blk(id_to_name, app_dir, io_sides):
         placement[reset] = (1, 0)
 
     # manual placement of PE/MEM tiles if needed
-    if os.path.isfile(app_dir + "/manual.place"):
+    if "MANUAL_PLACER" in os.environ and os.environ.get("MANUAL_PLACER") == "1" and os.path.isfile(app_dir + "/manual.place"):
         with open(app_dir + "/manual.place", "r") as f:
             data = f.readlines()
             for dat in data:
