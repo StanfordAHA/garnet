eefd43992639a72fc4a0e42dc52053b407ccfb20
Michael Oduoza <mcoduoza@gmail.com>
2025-02-16 11:10:56 -0800

    zircon_mu_folder

---
+++ matrix_unit/header/matrix_unit_param.h
@@ -0,0 +1,3 @@
+#pragma once
+#define MU_DATAWIDTH 16
+#define OC_0 32
---
+++ matrix_unit/header/matrix_unit_param.svh
@@ -0,0 +1,7 @@
+`ifndef MATRIX_UNIT_PARAM
+`define MATRIX_UNIT_PARAM
+package matrix_unit_param;
+localparam int MU_DATAWIDTH = 16;
+localparam int OC_0 = 32;
+endpackage
+`endif
