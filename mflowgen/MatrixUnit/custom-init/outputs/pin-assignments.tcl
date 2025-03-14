2c3fc1a9dcf320a7d46b245297df66c9b86c4df3
Po-Han Chen <pohan@stanford.edu>
2025-03-13 23:35:06 -0700

    update floorplan and pin assignment

--- mflowgen/MatrixUnit/custom-init/outputs/pin-assignments.tcl
+++ mflowgen/MatrixUnit/custom-init/outputs/pin-assignments.tcl
@@ -1,87 +1,107 @@
 #=========================================================================
 # pin-assignments.tcl
 #=========================================================================
-# Author : 
+# Author : Po-Han Chen
 # Date   : 
+
+#-------------------------------------------------------------------------
+# Helper Functions
+#-------------------------------------------------------------------------
+proc print_collection_hierarchical_names {collection} {
+    foreach_in_collection c $collection {
+        puts [get_property $c hierarchical_name]
+    }
+}
+
+proc insert_into_middle_of_collection {base_collection new_collection} {
+    set output_collection {}
+    set middle_index [expr {[sizeof_collection $base_collection] / 2}]
+    set index 0
+    foreach_in_collection element_base $base_collection {
+        if {$index == $middle_index} {
+            foreach_in_collection element_new $new_collection {
+                append_to_collection output_collection $element_new
+            }
+        }
+        append_to_collection output_collection $element_base
+        incr index
+    }
+    return $output_collection
+}
+
+#-------------------------------------------------------------------------
+# Pin Collections
+#-------------------------------------------------------------------------
+# [Top Pins]
+#             clk
+#             rstn
+#             startSignal* (r/v)
+#             doneSignal* (r/v)
+#             serialMatrixParamsIn* (dat: 32b, with r/v)
+#             # Temporal Top Pins
+#             # New IWB pins will replace the following input/weight/bias pins
+#             IWB_Address_Request* (dat: 22b, with r/v)
+#             IWB_Data_Response* (dat: 256b, with r/v)
+#                 inputAddressRequest* ------------ 96
+#                 inputDataResponse* -------------- 512
+#                 inputScaleAddressRequest* ------- 96
+#                 inputScaleDataResponse* --------- 8
+#                 weightAddressRequest* ----------- 96
+#                 weightDataResponse* ------------- 256
+#                 weightScaleAddressRequest* ------ 96
+#                 weightScaleDataResponse* -------- 256
+#                 biasAddressRequest* ------------- 96
+#                 biasDataResponse* --------------- 256
+# [Bottom Pins]
+#             outputsFromSystolicArray* (dat: 512b, with r/v)
+
+# Organize ports into collection objects
+set ports_param   [sort_collection [get_ports {*Param*}]     hierarchical_name]
+set ports_input   [sort_collection [get_ports {*input*}]     hierarchical_name]
+set ports_weight  [sort_collection [get_ports {*weight*}]    hierarchical_name]
+set ports_bias    [sort_collection [get_ports {*bias*}]      hierarchical_name]
+set ports_clk_rst [sort_collection [get_ports {*clk* *rst*}] hierarchical_name]
+set ports_control [sort_collection [get_ports {*Signal*}]    hierarchical_name]
+set ports_output  [sort_collection [get_ports {*outputs*}]   hierarchical_name]
+
+# Distribute the ports to different sides
+set ports_top    [concat $ports_param $ports_input $ports_weight $ports_bias $ports_control]
+set ports_bottom [concat $ports_output]
+set ports_left   []
+set ports_right  []
+
+# Manipulate the orders -- make sure clk and reset are in the middle
+set ports_top [insert_into_middle_of_collection $ports_top $ports_clk_rst]
 
 #-------------------------------------------------------------------------
 # Pin Assignments
 #-------------------------------------------------------------------------
+set pin_layer_top_bottom M3
+set pin_layer_left_right M4
+set block_width  [dbGet top.fPlan.box_urx]
+set block_height [dbGet top.fPlan.box_ury]
+set pin_margin_top    25.0
+set pin_margin_bottom 25.0
 
-set num_cgra_tiles $savedvars(grid_num_cols)
-
-set all [get_ports]
-set all_tile_ports ""
-# per cgra tile ports
-for {set col 0} {$col < $num_cgra_tiles} {incr col} {
-    # Covert col to hex
-    set col_hex [format "%02X" $col]
-    # Data stream signals
-    set data_ports [get_ports [list \
-        glb2io_*_X${col_hex}_Y00* \
-        io2glb_*_X${col_hex}_Y00* \
-        ]]
-    set data_ports [sort_collection $data_ports hierarchical_name]
-    # Config and stall signals
-    set port_names [list \
-        stall\[${col}\] \
-        config_${col}_*]
-    # Insert a flush for every PR region (every 4 cols)
-    if {[expr (($col + 3) % 4)] == 0} {
-        set pr_region [expr (($col+3)/4) - 1]
-        lappend port_names flush\[${pr_region}\]
-    }
-    set config_ports [get_ports $port_names]
-    set config_ports [sort_collection $config_ports hierarchical_name]
-    set ports [concat $data_ports $config_ports]
-    set tile_ports($col) [get_object_name $ports]
-    set all_tile_ports [concat $all_tile_ports $tile_ports($col)] 
-}
-
-# Now put clk and reset in the center
-set center_ports [get_object_name [get_ports {clk reset}]]
-set all_tile_ports [concat $all_tile_ports $center_ports]
-set center_col   [expr $num_cgra_tiles / 2]
-if { $center_ports != 0 } {
-    set tile_ports($center_col) [concat $tile_ports($center_col) $center_ports]
-}
-
-# Collect any ports we missed
-foreach a [get_object_name $all] {
-    if {$a ni $all_tile_ports} {
-        echo "INFO: Assigning port $a to top left corner"
-        lappend topleft $a
-    }
-}
-
-set width [dbGet top.fPlan.box_urx]
-set height [dbGet top.fPlan.box_ury]
-
-set topleft_length [expr [llength $topleft] + 5]
-set edge_offset 10
+setPinAssignMode -pinEditInBatch true
 
 editPin \
-    -pin $topleft \
-    -start [list 0. [expr $height - $edge_offset - $topleft_length]] \
-    -end [list 0. [expr $height - $edge_offset]] \
-    -side LEFT \
-    -spreadType RANGE \
-    -spreadDirection clockwise \
-    -layer m6
+    -layer           $pin_layer_top_bottom \
+    -side            TOP \
+    -spreadType      START \
+    -unit            TRACK \
+    -spacing         7 \
+    -start           [list $pin_margin_top $block_height] \
+    -pin             [get_property $ports_top hierarchical_name]
 
-# This speeds up pin assignment
-setPinAssignMode -pinEditInBatch true
-for {set col 0} {$col < $num_cgra_tiles} {incr col} {
-    set start_x [expr $tiles(1,$col,x_loc) + 1] 
-    set end_x [expr $start_x + $tiles(1,$col,width) - 2] 
-    editPin \
-        -pin $tile_ports($col) \
-        -start [list $start_x $height ] \
-        -end [list $end_x $height ] \
-        -side TOP \
-        -spreadType RANGE \
-        -spreadDirection clockwise \
-        -layer m5
-}
+editPin \
+    -layer           $pin_layer_top_bottom \
+    -side            BOTTOM \
+    -spreadType      START \
+    -unit            TRACK \
+    -spacing         25 \
+    -start           [list $pin_margin_bottom 0.0] \
+    -spreadDirection counterClockwise \
+    -pin             [get_property $ports_bottom hierarchical_name]
+
 setPinAssignMode -pinEditInBatch false
-
