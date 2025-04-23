0547ec70bbb6caae4e3e7f461f6c3ba5b599288e
steveri <steveri@steveri.com>
2025-04-23 11:29:54 -0700

    freeze lassen at 77c32df from 2024

--- mflowgen/common/rtl/gen_rtl.sh
+++ mflowgen/common/rtl/gen_rtl.sh
@@ -9,6 +9,7 @@ function get_amber_updates {
       cd /aha/canal        && git checkout 6fec524;
       cd /aha/clockwork    && git checkout efdd95e;
       cd /aha/lake         && git checkout 837ffe1;
+      cd /aha/lassen       && git checkout 77c32df;
     '
 }
 function update_kratos {
