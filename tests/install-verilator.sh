ebf383afad27acd06cf911260c68bce8b0e21e44
steveri <steveri@steveri.com>
2025-07-07 12:40:46 -0700

    more experiments...how many CPUs are available?

--- tests/install-verilator.sh
+++ tests/install-verilator.sh
@@ -35,8 +35,9 @@ if insufficient $version; then
     cd /usr/share; test -d verilator || git clone https://github.com/verilator/verilator
     cd /usr/share/verilator; git checkout v5.028
     cd /usr/share/verilator; unset VERILATOR_ROOT; autoconf; ./configure
-
-    cd /usr/share/verilator; unset VERILATOR_ROOT; make -j 4 || echo ERROR
+    echo It looks like we have `nproc` processors available.
+    NPROC=$((`nproc`/2)) || NPROC=1  # Don't be greedy
+    cd /usr/share/verilator; unset VERILATOR_ROOT; make -j $NPROC || echo ERROR
     cd /usr/share/verilator; make clean || echo ERROR cannot clean for some reason i guess
     test -e /usr/local/bin/verilator && mv /usr/local/bin/verilator /usr/local/bin/verilator.orig || echo NOT YET
     cd /usr/local/bin; ln -s /usr/share/verilator/bin/verilator
