e4ce77cc309f2a4a666946c2e5f343bfcb1bdc2e
steveri <steveri@steveri.com>
2025-12-03 09:55:12 -0800

    fix mflowgen package list maybe

--- mflowgen/setup.py
+++ mflowgen/setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup
-
-
 setup(
+    packages=[],
     name='garnet-pd',
     author='Alex Carsello',
     url='https://github.com/StanfordAHA/garnet/tree/master/mflowgen',
-
     install_requires=[
         'mflowgen'
     ],
-
 )
