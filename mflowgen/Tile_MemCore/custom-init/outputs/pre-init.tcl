a3184b4f549b264fced6af543681759b5ded3357
steveri <steveri@steveri.com>
2026-01-16 11:59:11 -0800

    shift pre-init to global setup maybe

--- mflowgen/Tile_MemCore/custom-init/outputs/pre-init.tcl
+++
@@ -1,1 +0,0 @@
-../../../common/init-scripts/pre-init.tcl
--- mflowgen/common/global_setup.py
+++ mflowgen/common/global_setup.py
@@ -1,9 +1,25 @@
 import os
 
-def global_setup():
-    print("Hello woild I am global setup howdja do")
+def global_setup(caller__file__):
+    print("--- Hello woild I am global setup howdja do")
+
+    # E.g. /sim/buildkite-agent/builds/papers-2/tapeout-aha/mflowgen/common
     print("Hey I wonder where does this script live?")
     print("I think it is here maybe:")
+    print(os.path.dirname(os.path.realpath(__file__)))
 
-    # Maybe this fails if script is run interactively i.e. __file__ no exists then maybe
-    print(os.path.dirname(os.path.realpath(__file__)))    
+    # E.g. /sim/buildkite-agent/builds/papers-2/tapeout-aha/mflowgen/common
+    print("Okay okay but where is mflowgen repo tho")
+    print("Is it here:")
+    print(os.getcwd())
+
+    # Time to get fancy
+    # Replace mflowgen pre-init with fixed version
+    # Copy callee_dir/init_scripts/pre-init.tcl => repo_dir/nodes/cadence-innovus-init/scripts/
+    callee_dir = os.path.dirname(os.path.realpath(__file__))
+    repo_dir = os.getcwd()
+    import shutil
+    src = os.path.join(callee_dir, 'init_scripts/pre-init.tcl')
+    print(f"--- OMG gonna try and copy {src} to {repo_dir}")
+    shutil.copy(src, repo_dir)
+
