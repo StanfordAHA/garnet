15f1b792792327b70fce0d6fa54e1e91ad66e2f9
steveri <steveri@stanford.edu>
2026-01-30 08:08:53 -0800

    --include-sparse deprecated (#1167)
    * setup.py stopped working, had to add explicit package list
    * must now use 'typical' instead of 'default' maybe
    * add hacked pre-init.tcl to replace default(!!!)
    * fix cts reporting.tcl script error maybe
    * undo previous change :(
    * heavy handed fix for cts problems maybe
    * update construct.py to use the new cts node
    * hoping to fix mflowgen-update change to lvs.runset.template :(
    * heavy-handed fix for stdcells-lpe.spi problem
    * apply memcore fixes to tile-array maybe
    * apply memcore fixes to TilePE maybe
    * apply memcore fixes to TilePE maybe + tile-array fix
    * add pre-init.tcl for full-chip
    * add pre-init.tcl for *all* custom-init-amber dirs :(
    * propagate cts fix to all the other steps
    * propagate lvs fix to all the other steps
    * revert lvs.runset.template to match older version
    * global_setup v0.1
    * shift pre-init to global setup maybe
    * do all three CI tile tests, thru init step
    * disable an old check
    * better fix for cts reporting.tcl maybe
    * better fix for lvs runset problem maybe
    * make mflowgen private to individual build(s) omg why did we ever try to do different
    * mflowgen uses main not master now
    * turn off locking, dont need it no more
    * oops forgot to add lvs configure.yml patch
    * oops forgot to git-add lvs configure.yml patch
    * FINALLY address the original issue; removing include-sparse flag
    * restore accidentally-erased code
    * test up through lvs
    * huh patch file NOT same as mflowgen repo file :(
    * restore pmg to run through init stage only

--- mflowgen/full_chip/construct.py
+++ mflowgen/full_chip/construct.py
@@ -38,6 +38,9 @@ def construct():
 
     g = Graph()
 
+    from common.global_setup import global_setup
+    global_setup(__file__)
+
     # -----------------------------------------------------------------------
     # Parameters
     # -----------------------------------------------------------------------
